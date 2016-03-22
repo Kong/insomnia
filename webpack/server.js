633295f0b93ed20f68edc976d8c941475976ceb7
Gregory Schier <gschier1990@gmail.com>
2016-03-22 09:42:03 -0700

    Checkpoint

--- webpack/server.js
+++ webpack/server.js
@@ -1,6 +1,6 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack/dev.config.js');
+var config = require('./dev.config.js');
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
