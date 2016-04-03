72f2ef1964011b36d20ae7e9c589c5147d8d197b
Gregory Schier <gschier1990@gmail.com>
2016-04-03 15:58:35 -0700

    fix

--- app/actions/__tests__/requestgroups.test.js
+++ app/actions/__tests__/requestgroups.test.js
@@ -2,13 +2,10 @@ jest.unmock('../requestgroups');
 jest.unmock('../global');
 jest.unmock('../../constants/global');
 jest.unmock('../../constants/actionTypes');
-jest.unmock('../../validators/requestgroup');
+jest.unmock('../../validators/requestGroup');
 jest.unmock('jsonschema');
 jest.unmock('redux-thunk');
 jest.unmock('redux-mock-store');
-
-import thunk from 'redux-thunk';
-import configureMockStore from 'redux-mock-store';
 
 describe('RequestGroup Actions', () => {
   it('should add valid group', () => {
