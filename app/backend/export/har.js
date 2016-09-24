4e7d60ebdcc8c43c05eb12c7a913b0383b8b2f26
Gregory Schier <gschier1990@gmail.com>
2016-09-23 20:06:38 -0700

    Improvement

--- app/backend/export/har.js
+++ app/backend/export/har.js
@@ -5,39 +5,41 @@ const {getRenderedRequest} = require('../render');
 const {jarFromCookies} = require('../cookies');
 const util = require('backend/util');
 
+module.exports.exportHarWithRequest = (renderedRequest, addContentLength = false) => {
+  if (addContentLength) {
+    const hasContentLengthHeader = !!renderedRequest.headers.find(
+      h => h.name.toLowerCase() === 'content-length'
+    );
+
+    if (!hasContentLengthHeader) {
+      const name = 'content-length';
+      const value = Buffer.byteLength(renderedRequest.body).toString();
+      renderedRequest.headers.push({name, value})
+    }
+  }
+
+  return {
+    method: renderedRequest.method,
+    url: util.prepareUrlForSending(renderedRequest.url),
+    httpVersion: 'HTTP/1.1',
+    cookies: getCookies(renderedRequest),
+    headers: renderedRequest.headers,
+    queryString: renderedRequest.parameters,
+    postData: {
+      text: renderedRequest.body
+    },
+    headersSize: -1,
+    bodySize: -1
+  };
+};
+
 module.exports.exportHar = (requestId, addContentLength = false) => {
-  return new Promise((resolve, reject) => {
-    db.request.getById(requestId).then(request => {
-      return getRenderedRequest(request);
-    }).then(renderedRequest => {
-
-      if (addContentLength) {
-        const hasContentLengthHeader = !!renderedRequest.headers.find(
-          h => h.name.toLowerCase() === 'content-length'
-        );
-
-        if (!hasContentLengthHeader) {
-          const name = 'content-length';
-          const value = Buffer.byteLength(renderedRequest.body).toString();
-          renderedRequest.headers.push({name, value})
-        }
-      }
-
-      resolve({
-        method: renderedRequest.method,
-        url: util.prepareUrlForSending(renderedRequest.url),
-        httpVersion: 'HTTP/1.1',
-        cookies: getCookies(renderedRequest),
-        headers: renderedRequest.headers,
-        queryString: renderedRequest.parameters,
-        postData: {
-          text: renderedRequest.body
-        },
-        headersSize: -1,
-        bodySize: -1
-      });
-    })
-  });
+  return db.request.getById(requestId)
+    .then(getRenderedRequest)
+    .then(renderedRequest => module.exports.exportHarWithRequest(
+      renderedRequest,
+      addContentLength
+    ));
 };
 
 function getCookies (renderedRequest) {
