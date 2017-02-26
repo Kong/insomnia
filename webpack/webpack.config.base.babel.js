05f4d4dae14bbe261e3f2018777c3a7949d837c2
Gregory Schier <gschier1990@gmail.com>
2017-02-26 13:12:51 -0800

    Webpack 2 and Yarn (#92)
    * Version bump
    
    * Fix version numbers
    
    * Upgraded > WP2 and cleaned up a bunch
    
    * CI to yarn
    
    * Updated some deps
    
    * Updated some modules

--- webpack/webpack.config.base.babel.js
+++ webpack/webpack.config.base.babel.js
@@ -1,5 +1,5 @@
 import path from 'path';
-import pkg from '../app/package.json';
+import * as pkg from '../app/package.json';
 
 export default {
   devtool: 'source-map',
@@ -14,54 +14,32 @@ export default {
     libraryTarget: 'commonjs2'
   },
   module: {
-    loaders: [
+    rules: [
       {
-        id: 'babel',
         test: /\.js$/,
-        loaders: ['babel'],
-        exclude: [/node_modules/, /__fixtures__/, /__tests__/]
-      },
-      {
-        test: /\.json$/,
-        loader: 'json'
+        loader: 'babel-loader',
+        exclude: [/node_modules/, /__fixtures__/, /__tests__/],
       },
       {
         test: /\.(less|css)$/,
-        loader: 'style!css!less'
+        use: [
+          'style-loader',
+          {loader: 'css-loader', options: {importLoaders: 1}},
+          {loader: 'less-loader', options: {noIeCompat: true}},
+        ],
       },
       {
-        test: /\.html$/,
-        loader: "file?name=[name].[ext]"
-      },
-      {
-        test: /\.png$/,
-        loader: "file"
-      },
-      {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "file"
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url?limit=10000&mimetype=image/svg+xml"
-      },
-      {
-        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url?limit=10000&mimetype=application/font-woff"
-      },
-      {
-        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url?limit=10000&mimetype=application/font-woff"
-      },
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url?limit=10000&mimetype=application/octet-stream"
+        test: /\.(html|png|woff2)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]'
+        }
       }
     ]
   },
   resolve: {
-    extensions: ['', '.js', '.json', '.jsx'],
-    packageMains: ['webpack', 'browser', 'web', 'browserify', ['jam', 'main'], 'main']
+    extensions: ['.js', '.json'],
+    mainFields: ['webpack', 'browser', 'web', 'browserify', ['jam', 'main'], 'main']
   },
   externals: [
     // Omit all dependencies in app/package.json (we want them loaded at runtime via NodeJS)
