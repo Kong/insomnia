86b9c8ab56a428f07b302863da35850149060bf6
Gregory Schier <gschier1990@gmail.com>
2017-03-09 17:09:00 -0800

    Even better

--- app/ui/components/codemirror/extensions/environments-autocomplete.js
+++ app/ui/components/codemirror/extensions/environments-autocomplete.js
@@ -1,6 +1,9 @@
 import CodeMirror from 'codemirror';
 import 'codemirror/addon/mode/overlay';
 
+const MAX_HINT_LOOK_BACK = 100;
+const HINT_DELAY_MILLIS = 100;
+
 CodeMirror.defineOption('environmentAutocomplete', null, (cm, options) => {
   if (!options) {
     return;
@@ -10,64 +13,93 @@ CodeMirror.defineOption('environmentAutocomplete', null, (cm, options) => {
     if (!fn || fn()) {
       setTimeout(() => {
         if (!cm.state.completionActive) {
-          cm.showHint({
-            hint: showHint,
-            completeSingle: false,
-            getContext: options.getContext
-          });
+          const {getContext} = options;
+          cm.showHint({hint, completeSingle: false, getContext});
         }
-      }, 100);
+      }, HINT_DELAY_MILLIS);
     }
 
     return CodeMirror.Pass;
   }
 
-  function completeIfAfterSingleBracket (cm) {
+  function completeIfInsideDoubleBrackets (cm) {
     return completeAfter(cm, () => {
       const cur = cm.getCursor();
-      const pos = CodeMirror.Pos(cur.line, cur.ch - 2);
+      const pos = CodeMirror.Pos(cur.line, cur.ch - MAX_HINT_LOOK_BACK);
       const range = cm.getRange(pos, cur);
-      return range.match(/^[^{]{$/);
+      return range.match(/^[^{]+\{\{[^}]*/);
     });
   }
 
-  function completeIfAfterDoubleBrackets (cm) {
-    return completeAfter(cm, () => {
-      const cur = cm.getCursor();
-      const pos = CodeMirror.Pos(cur.line, cur.ch - 3);
-      const range = cm.getRange(pos, cur);
-      return range.match(/^[^{]{{$/);
-    });
-  }
+  // Add hot key triggers
+  cm.addKeyMap({'Ctrl-Space': 'autocomplete'});
+  cm.on('keydown', (cm, e) => {
+    // Only operate on one-letter keys. This will filter out
+    // any special keys (Backspace, Enter, etc)
+    if (e.key.length > 1) {
+      return;
+    }
 
-  cm.addKeyMap({
-    "'{'": completeIfAfterSingleBracket,
-    "' '": completeIfAfterDoubleBrackets,
-    'Ctrl-Space': 'autocomplete'
+    completeIfInsideDoubleBrackets(cm);
   });
 });
 
-async function showHint (cm, options) {
-  const ctx = await options.getContext();
-
+/**
+ * Function to retrieve the list items
+ * @param cm
+ * @param options
+ * @returns {Promise.<{list: Array, from, to}>}
+ */
+async function hint (cm, options) {
+  // Get the text from the cursor back
   const cur = cm.getCursor();
-  const pos = CodeMirror.Pos(cur.line, cur.ch - 100);
-  const range = cm.getRange(pos, cur);
-  const match = range.match(/{{\s*(\w*)$/);
-  const nameSection = match ? match[1] : '';
+  const pos = CodeMirror.Pos(cur.line, cur.ch - MAX_HINT_LOOK_BACK);
+  const previousText = cm.getRange(pos, cur);
 
-  const list = ctx.keys
-    .filter(k => k.indexOf(nameSection) >= 0)
+  // See if we're completing a variable name
+  const variableNameMatch = previousText.match(/{{\s*([\w.\][]*)$/);
+  const variableNameSegment = variableNameMatch ? variableNameMatch[1] : '__does_not_exist__';
+
+  // See if we're completing a filter name
+  const filterNameMatch = previousText.match(/{{\s*\w*\s*\|\s*(\w*)$/);
+  const filterNameSegment = filterNameMatch ? filterNameMatch[1] : '__does_not_exist__';
+
+  const stringsToMatch = variableNameMatch
+    ? (await options.getContext()).keys
+    : ['upper', 'lower', 'title'];
+
+  const matchedSegment = variableNameMatch
+    ? variableNameSegment
+    : filterNameSegment;
+
+  // Actually try to match the list of things
+  const list = stringsToMatch
+    .filter(k => k.indexOf(matchedSegment) >= 0)
     .map(k => ({
       text: k,
-      displayText: k
-      // hint: (cm, self, data) => {
-      //   console.log('HINT', cm, self, data);
-      //   cm.replaceRange(data.text, self.from, self.to);
-      // }
+      displayText: k,
+      hint: replaceHintMatch
     }));
 
-  const from = CodeMirror.Pos(cur.line, cur.ch - nameSection.length);
+  const from = CodeMirror.Pos(cur.line, cur.ch - matchedSegment.length);
   const to = CodeMirror.Pos(cur.line, cur.ch);
+
   return {list, from, to};
+}
+
+/**
+ * Replace the text in the editor when a hint is selected.
+ * This also makes sure there is whitespace surrounding it
+ * @param cm
+ * @param self
+ * @param data
+ */
+function replaceHintMatch (cm, self, data) {
+  const prevChar = cm.getRange(CodeMirror.Pos(self.from.line, self.from.ch - 1), self.from);
+  const nextChar = cm.getRange(self.to, CodeMirror.Pos(self.to.line, self.to.ch + 1));
+
+  let prefix = prevChar === ' ' ? '' : ' ';
+  let suffix = nextChar === ' ' ? '' : ' ';
+
+  cm.replaceRange(`${prefix}${data.text}${suffix}`, self.from, self.to);
 }
