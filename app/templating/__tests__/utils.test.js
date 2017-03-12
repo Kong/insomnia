deb3bc10153f6cfab63562fefaeaacac94df4b8e
Gregory Schier <gschier1990@gmail.com>
2017-03-11 16:24:04 -0800

    Fixed tests

--- app/templating/__tests__/utils.test.js
+++ app/templating/__tests__/utils.test.js
@@ -15,19 +15,13 @@ describe('getKeys()', () => {
 
     const keys = utils.getKeys(obj);
     expect(keys).toEqual({
-      'array': obj.array,
       'array[0]': obj.array[0],
-      'array[1]': obj.array[1],
       'array[1].hi': obj.array[1].hi,
       'array[2]': obj.array[2],
-      'array[3]': obj.array[3],
       'array[3][0]': obj.array[3][0],
       'array[3][1]': obj.array[3][1],
       'array[3][2]': obj.array[3][2],
-      'foo': obj.foo,
-      'nested': obj.nested,
-      'nested.a': obj.nested.a,
-      'nested.a.b': obj.nested.a.b
+      'foo': obj.foo
     });
   });
 
