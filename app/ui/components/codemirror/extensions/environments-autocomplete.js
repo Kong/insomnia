9c12b46b94c9dbdfc4f7f4c9a7368503d081a62c
Gregory Schier <gschier1990@gmail.com>
2017-03-12 16:53:49 -0700

    Autocomplete works better with segment types (#116)
    * Fixed duplication kve bug
    
    * Fixed autocompleteshow/long bug

--- app/ui/components/codemirror/extensions/environments-autocomplete.js
+++ app/ui/components/codemirror/extensions/environments-autocomplete.js
@@ -5,10 +5,10 @@ const NAME_MATCH_FLEXIBLE = /[\w.\][\-/]+$/;
 const NAME_MATCH = /[\w.\][]+$/;
 const AFTER_VARIABLE_MATCH = /{{\s*[\w.\][]*$/;
 const AFTER_TAG_MATCH = /{%\s*[\w.\][]*$/;
-const COMPLETE_AFTER_VARIABLE_NAME = /[\w.\][]+/;
+const COMPLETE_AFTER_WORD = /[\w.\][-]+/;
 const COMPLETE_AFTER_CURLIES = /[^{]*\{[{%]\s*/;
-const COMPLETION_CLOSE_KEYS = /[}|]/;
-const ESCAPE_FORE_REGEX_MATCH = /[-[\]/{}()*+?.\\^$|]/g;
+const COMPLETION_CLOSE_KEYS = /[}|-]/;
+const ESCAPE_REGEX_MATCH = /[-[\]/{}()*+?.\\^$|]/g;
 const MAX_HINT_LOOK_BACK = 100;
 const HINT_DELAY_MILLIS = 100;
 const TYPE_VARIABLE = 'variable';
@@ -50,8 +50,11 @@ CodeMirror.defineOption('environmentAutocomplete', null, (cm, options) => {
       return CodeMirror.Pass;
     }
 
+    console.log('ATTEMPT TO SHOW');
+
     // Bail early if completions are showing already
     if (cm.isHintDropdownActive()) {
+      console.log('IS OPEN PASS', cm.state.completionActive);
       return CodeMirror.Pass;
     }
 
@@ -66,6 +69,7 @@ CodeMirror.defineOption('environmentAutocomplete', null, (cm, options) => {
     }
 
     const constants = options.getConstants && options.getConstants();
+    console.log('SHOW HINT');
 
     // Actually show the hint
     cm.showHint({
@@ -74,8 +78,8 @@ CodeMirror.defineOption('environmentAutocomplete', null, (cm, options) => {
 
       // Codemirror native options
       hint,
-      getContext: options.getContext,
       showAllOnNoMatch,
+      getContext: options.getContext,
       container: hintsContainer,
       closeCharacters: COMPLETION_CLOSE_KEYS,
       completeSingle: false,
@@ -97,7 +101,7 @@ CodeMirror.defineOption('environmentAutocomplete', null, (cm, options) => {
       const cur = cm.getCursor();
       const pos = CodeMirror.Pos(cur.line, cur.ch - MAX_HINT_LOOK_BACK);
       const range = cm.getRange(pos, cur);
-      return range.match(COMPLETE_AFTER_VARIABLE_NAME);
+      return range.match(COMPLETE_AFTER_WORD);
     });
   }
 
@@ -141,6 +145,7 @@ CodeMirror.defineOption('environmentAutocomplete', null, (cm, options) => {
  * @returns {Promise.<{list: Array, from, to}>}
  */
 async function hint (cm, options) {
+  console.log('HINT');
   // Get the text from the cursor back
   const cur = cm.getCursor();
   const pos = CodeMirror.Pos(cur.line, cur.ch - MAX_HINT_LOOK_BACK);
@@ -165,38 +170,44 @@ async function hint (cm, options) {
 
   // Actually try to match the list of things
   const context = await options.getContext();
-  const allMatches = [];
+  const allShortMatches = [];
+  const allLongMatches = [];
 
+  // Match constants
   if (allowMatchingConstants) {
-    matchSegments(
-      options.extraConstants,
-      [nameSegmentLong, nameSegment],
-      TYPE_CONSTANT,
-      MAX_CONSTANTS
-    ).map(m => allMatches.push(m));
+    matchSegments(options.extraConstants, nameSegment, TYPE_CONSTANT, MAX_CONSTANTS)
+      .map(m => allShortMatches.push(m));
+    matchSegments(options.extraConstants, nameSegmentLong, TYPE_CONSTANT, MAX_CONSTANTS)
+      .map(m => allLongMatches.push(m));
   }
 
+  // Match variables
   if (allowMatchingVariables) {
-    matchSegments(
-      context.keys,
-      [nameSegmentLong, nameSegment],
-      TYPE_VARIABLE,
-      MAX_VARIABLES
-    ).map(m => allMatches.push(m));
+    matchSegments(context.keys, nameSegment, TYPE_VARIABLE, MAX_VARIABLES)
+      .map(m => allShortMatches.push(m));
+    matchSegments(context.keys, nameSegmentLong, TYPE_VARIABLE, MAX_VARIABLES)
+      .map(m => allLongMatches.push(m));
   }
 
+  // Match tags
   if (allowMatchingTags) {
-    matchSegments(
-      TAGS,
-      [nameSegmentLong, nameSegment],
-      TYPE_TAG,
-      MAX_TAGS
-    ).map(m => allMatches.push(m));
+    matchSegments(TAGS, nameSegment, TYPE_TAG, MAX_TAGS)
+      .map(m => allShortMatches.push(m));
+    matchSegments(TAGS, nameSegmentLong, TYPE_TAG, MAX_TAGS)
+      .map(m => allLongMatches.push(m));
   }
+
+  /*
+   * If anything matched the longer segment, only return those. Otherwise return only
+   * the short form. For example, if the long form is "application/json" and short is "json",
+   * prioritise matches form "application/json" if there were any.
+   */
+  const matches = allLongMatches.length ? allLongMatches : allShortMatches;
+  const segment = allLongMatches.length ? nameSegmentLong : nameSegment;
 
   return {
-    list: allMatches,
-    from: CodeMirror.Pos(cur.line, cur.ch - nameSegment.length),
+    list: matches,
+    from: CodeMirror.Pos(cur.line, cur.ch - segment.length),
     to: CodeMirror.Pos(cur.line, cur.ch)
   };
 }
@@ -252,50 +263,45 @@ function replaceHintMatch (cm, self, data) {
 /**
  * Match against a list of things
  * @param listOfThings - Can be list of strings or list of {name, value}
- * @param segments - List of segments to match against
+ * @param segment - segment to match against
  * @param type
  * @param limit
  * @returns {Array}
  */
-function matchSegments (listOfThings, segments, type, limit = -1) {
+function matchSegments (listOfThings, segment, type, limit = -1) {
   const matches = [];
 
   for (const t of listOfThings) {
     const name = typeof t === 'string' ? t : t.name;
     const value = typeof t === 'string' ? '' : t.value;
 
-    for (const segment of segments) {
-      const matchSegment = segment.toLowerCase();
-      const matchName = name.toLowerCase();
+    const matchSegment = segment.toLowerCase();
+    const matchName = name.toLowerCase();
 
-      // Throw away exact matches (why would we want to complete those?)
-      if (matchName === matchSegment) {
-        continue;
-      }
-
-      // Throw away things that don't match
-      if (!matchName.includes(matchSegment)) {
-        continue;
-      }
-
-      matches.push({
-        // Custom Insomnia keys
-        type,
-        segment,
-        comment: value,
-        displayValue: value ? JSON.stringify(value) : '',
-        score: name.length, // In case we want to sort by this
-
-        // CodeMirror
-        text: name,
-        displayText: name,
-        render: renderHintMatch,
-        hint: replaceHintMatch
-      });
-
-      // Only return the first match (2nd would be a duplicate)
-      break;
+    // Throw away exact matches (why would we want to complete those?)
+    if (matchName === matchSegment) {
+      continue;
     }
+
+    // Throw away things that don't match
+    if (!matchName.includes(matchSegment)) {
+      continue;
+    }
+
+    matches.push({
+      // Custom Insomnia keys
+      type,
+      segment,
+      comment: value,
+      displayValue: value ? JSON.stringify(value) : '',
+      score: name.length, // In case we want to sort by this
+
+      // CodeMirror
+      text: name,
+      displayText: name,
+      render: renderHintMatch,
+      hint: replaceHintMatch
+    });
   }
 
   if (limit >= 0) {
@@ -314,7 +320,7 @@ function matchSegments (listOfThings, segments, type, limit = -1) {
  * @returns string
  */
 function replaceWithSurround (text, find, prefix, suffix) {
-  const escapedString = find.replace(ESCAPE_FORE_REGEX_MATCH, '\\$&');
+  const escapedString = find.replace(ESCAPE_REGEX_MATCH, '\\$&');
   const re = new RegExp(escapedString, 'gi');
   return text.replace(re, matched => prefix + matched + suffix);
 }
