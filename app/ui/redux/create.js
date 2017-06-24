96837a6dd0213bbc3507a549dbd7d62a9baf3d6c
Louis St-Amour <LouisStAmour@gmail.com>
2017-06-24 01:50:00 -0400

    Corrected lint error

--- app/ui/redux/create.js
+++ app/ui/redux/create.js
@@ -5,8 +5,8 @@ import {reducer} from './modules';
 export default function () {
   const composeEnhancers =
     typeof window === 'object' &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
         // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
       }) : compose;
   const middleware = [thunkMiddleware];
