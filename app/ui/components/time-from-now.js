210a9e44ba856dc5973ef5722d89dffe9a08fd22
Gregory Schier <gschier1990@gmail.com>
2017-08-10 14:30:47 -0700

    GraphQL updates and fixes

---
+++ app/ui/components/time-from-now.js
@@ -0,0 +1,47 @@
+// @flow
+import React from 'react';
+import autobind from 'autobind-decorator';
+import moment from 'moment';
+
+@autobind
+class TimeFromNow extends React.PureComponent {
+  props: {
+    timestamp: number
+  };
+
+  state: {
+    text: string
+  };
+
+  _interval: any;
+
+  constructor (props: any) {
+    super(props);
+    this._interval = null;
+    this.state = {
+      text: ''
+    };
+  }
+
+  _update () {
+    const {timestamp} = this.props;
+    this.setState({text: moment(timestamp).fromNow()});
+  }
+
+  componentDidMount () {
+    this._interval = setInterval(this._update, 5000);
+    this._update();
+  }
+
+  componentWillUnmount () {
+    clearInterval(this._interval);
+  }
+
+  render () {
+    return <span title={moment(this.props.timestamp).toString()}>
+      {this.state.text}
+      </span>;
+  }
+}
+
+export default TimeFromNow;
