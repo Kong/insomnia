ad237c6d77f9b71336f1a3e8cbbda33a200055b6
Gregory Schier <gschier1990@gmail.com>
2017-11-01 14:14:53 +0100

    Drag and drop import files

--- app/ui/containers/app.js
+++ app/ui/containers/app.js
@@ -36,8 +36,8 @@ import {getKeys} from '../../templating/utils';
 import {showAlert, showPrompt} from '../components/modals/index';
 import {exportHarRequest} from '../../common/har';
 import * as hotkeys from '../../common/hotkeys';
-import KeydownBinder from '../components/keydown-binder';
 import {executeHotKey} from '../../common/hotkeys';
+import KeydownBinder from '../components/keydown-binder';
 import ErrorBoundary from '../components/error-boundary';
 
 @autobind
@@ -698,6 +698,36 @@ class App extends PureComponent {
 
       this.props.handleCommand(command, args);
     });
+
+    // NOTE: This is required for "drop" event to trigger.
+    document.addEventListener('dragover', e => {
+      e.preventDefault();
+    }, false);
+
+    document.addEventListener('drop', async e => {
+      e.preventDefault();
+      const {activeWorkspace, handleImportUriToWorkspace} = this.props;
+      if (!activeWorkspace) {
+        return;
+      }
+
+      if (e.dataTransfer.files.length === 0) {
+        console.log('Ignored drop event because no files present');
+        return;
+      }
+
+      const file = e.dataTransfer.files[0];
+      const {path} = file;
+      const uri = `file://${path}`;
+
+      await showAlert({
+        title: 'Confirm Data Import',
+        message: <span>Import <code>{path}</code>?</span>,
+        addCancel: true
+      });
+
+      handleImportUriToWorkspace(activeWorkspace._id, uri);
+    }, false);
 
     ipcRenderer.on('toggle-changelog', () => {
       showModal(ChangelogModal);
