53965ed1fdc85625d2a87c5280c1488604ef381b
Gregory Schier <gschier1990@gmail.com>
2017-11-03 20:31:08 +0100

    Revert url resolver

--- app/network/o-auth-2/misc.js
+++ app/network/o-auth-2/misc.js
@@ -1,6 +1,5 @@
 import electron from 'electron';
 import uuid from 'uuid';
-import {resolve as urlResolve} from 'url';
 import querystring from 'querystring';
 
 const AUTH_WINDOW_SESSION_ID = uuid.v4();
@@ -53,7 +52,7 @@ export function authorizeUserInWindow (url, urlRegex = /.*/) {
     // Catch the redirect after login
     child.webContents.on('did-navigate', () => {
       // Be sure to resolve URL so that we can handle redirects with no host like /foo/bar
-      const currentUrl = urlResolve(url, child.webContents.getURL());
+      const currentUrl = child.webContents.getURL();
       if (currentUrl.match(urlRegex)) {
         console.log(`[oauth2] Matched redirect to "${currentUrl}" with ${urlRegex.toString()}`);
         finalUrl = currentUrl;
