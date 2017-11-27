9a4eafcbe4658ace3471023249e255a56ad35bbe
Gregory Schier <gschier1990@gmail.com>
2017-11-27 00:29:27 +0000

    Remove website

--- website/src/analytics.js
+++
@@ -1,7 +0,0 @@
-export function trackEvent (...args) {
-  window.ga && window.ga('send', 'event', ...args);
-}
-
-export function setUserId (accountId) {
-  window.ga && window.ga('set', 'userId', accountId);
-}
