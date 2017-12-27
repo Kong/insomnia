241dbb8b49d91a8f735380106251229163c43f35
slawus <slawus.dev@gmail.com>
2017-12-27 22:13:11 +0100

    Added support for async importers

--- packages/insomnia-importers/src/__tests__/import-errors.test.js
+++ packages/insomnia-importers/src/__tests__/import-errors.test.js
@@ -3,8 +3,10 @@
 const importers = require('../../index');
 
 describe('Import errors', () => {
-  it('fail to find importer', () => {
-    const fn = () => importers.convert('foo');
-    expect(fn).toThrowError('No importers found for file')
+  it('fail to find importer', async () => {
+    const fn = importers.convert('foo');
+    await expect(fn).rejects.toHaveProperty('message', 'No importers found for file');
+    //Bug in jest, that doesn't allow to check the error message using #toThrow
+    //Check: https://github.com/facebook/jest/pull/4884
   })
 });
