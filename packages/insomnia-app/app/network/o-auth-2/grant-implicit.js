50329424962eebf867ab1d50476c364728199829
Emanuel Fonseca <emdfonseca@gmail.com>
2017-12-30 15:57:26 +0000

    Set OAuth 2.0 default responseType value to token

--- packages/insomnia-app/app/network/o-auth-2/grant-implicit.js
+++ packages/insomnia-app/app/network/o-auth-2/grant-implicit.js
@@ -7,20 +7,21 @@ export default async function (
   requestId: string,
   authorizationUrl: string,
   clientId: string,
+  responseType: string = c.RESPONSE_TYPE_TOKEN,
   redirectUri: string = '',
   scope: string = '',
-  state: string = '',
-  responseType: string = ''
+  state: string = ''
 ): Promise<Object> {
   const params = [
     {name: c.P_CLIENT_ID, value: clientId}
   ];
+  const RESPONSE_NONCE: string = ((Math.floor(Math.random() * 9999999999999) + 1): any);
 
   // Add optional params
   redirectUri && params.push({name: c.P_REDIRECT_URI, value: redirectUri});
   scope && params.push({name: c.P_SCOPE, value: scope});
   state && params.push({name: c.P_STATE, value: state});
-  responseType === c.RESPONSE_TYPE_BOTH && params.push({name: c.P_NONCE, value: 'nonce' + Math.floor(Math.random() * 999999) + 1});
+  responseType === c.RESPONSE_TYPE_BOTH && params.push({name: c.P_NONCE, value: RESPONSE_NONCE});
   responseType && params.push({name: c.P_RESPONSE_TYPE, value: responseType});
 
   // Add query params to URL
