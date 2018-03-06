da818d7be8b81069b1547ae7a4142a9fd6bac5d6
Gregory Schier <gschier1990@gmail.com>
2018-03-06 18:26:37 +1300

    Add JSONPath template tag plugin

---
+++ plugins/insomnia-plugin-jsonpath/index.js
@@ -0,0 +1,35 @@
+const jq = require('jsonpath');
+
+module.exports.templateTags = [{
+  displayName: 'JSONPath',
+  name: 'jsonpath',
+  description: 'Pull data from JSON strings with JSONPath',
+  args: [{
+    displayName: 'JSON string',
+    type: 'string'
+  }, {
+    displayName: 'JSONPath Filter',
+    type: 'string'
+  }],
+  run (context, jsonString, filter) {
+    let body;
+    try {
+      body = JSON.parse(jsonString);
+    } catch (err) {
+      throw new Error(`Invalid JSON: ${err.message}`);
+    }
+
+    let results;
+    try {
+      results = jq.query(body, filter);
+    } catch (err) {
+      throw new Error(`Invalid JSONPath query: ${filter}`);
+    }
+
+    if (results.length === 0) {
+      throw new Error(`JSONPath query returned no results: ${filter}`);
+    }
+
+    return results[0];
+  }
+}];
