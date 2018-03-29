2cc5c541f2fea620bd1b2b73c7c6f7f3608e2315
Gregory Schier <gschier1990@gmail.com>
2018-03-29 13:23:49 -0400

    Use Yarn for all plugin install stages (Closes #825)

--- packages/insomnia-app/app/plugins/install.js
+++ packages/insomnia-app/app/plugins/install.js
@@ -1,94 +1,57 @@
 // @flow
 import * as electron from 'electron';
+import fs from 'fs';
+import fsx from 'fs-extra';
 import childProcess from 'child_process';
-import {isDevelopment, PLUGIN_PATH} from '../common/constants';
+import {getTempDir, isDevelopment, PLUGIN_PATH} from '../common/constants';
 import mkdirp from 'mkdirp';
 import path from 'path';
-import * as tar from 'tar';
-import * as crypto from 'crypto';
 
 export default async function (moduleName: string): Promise<void> {
   return new Promise(async (resolve, reject) => {
     let info: Object = {};
     try {
       info = await _isInsomniaPlugin(moduleName);
+
+      const pluginDir = path.join(PLUGIN_PATH, moduleName);
+
+      // Make plugin directory
+      mkdirp.sync(pluginDir);
+
+      // Download the module
+      const request = electron.remote.net.request(info.dist.tarball);
+      request.on('error', err => {
+        reject(new Error(`Failed to make plugin request ${info.dist.tarball}: ${err.message}`));
+      });
+
+      const {tmpDir} = await _installPluginToTmpDir(moduleName);
+      console.log(`[plugins] Moving plugin from ${tmpDir} to ${pluginDir}`);
+
+      // Move entire module to plugins folder
+      fsx.moveSync(
+        path.join(tmpDir, moduleName),
+        pluginDir,
+        {overwrite: true}
+      );
+
+      // Move each dependency into node_modules folder
+      const pluginModulesDir = path.join(pluginDir, 'node_modules');
+      mkdirp.sync(pluginModulesDir);
+      for (const name of fs.readdirSync(tmpDir)) {
+        const src = path.join(tmpDir, name);
+        if (name === moduleName || !fs.statSync(src).isDirectory()) {
+          continue;
+        }
+
+        const dest = path.join(pluginModulesDir, name);
+        fsx.moveSync(src, dest, {overwrite: true});
+      }
     } catch (err) {
       reject(err);
       return;
     }
 
-    const pluginDir = path.join(PLUGIN_PATH, moduleName);
-
-    // Make plugin directory
-    mkdirp.sync(pluginDir);
-
-    // Download the module
-    const request = electron.remote.net.request(info.dist.tarball);
-    request.on('error', err => {
-      reject(new Error(`Failed to make plugin request ${info.dist.tarball}: ${err.message}`));
-    });
-
-    request.on('response', response => {
-      const bodyBuffers = [];
-
-      console.log(`[plugins] Downloading plugin tarball from ${info.dist.tarball}`);
-      response.on('error', err => {
-        reject(new Error(`Failed to download plugin ${info.dist.tarball}: ${err.message}`));
-      });
-
-      response.on('end', () => {
-        console.log(`[plugins] Extracting plugin to ${pluginDir}`);
-        const w = tar.extract({
-          cwd: pluginDir, // Extract to plugin's directory
-          strict: true, // Fail on anything
-          strip: 1 // Skip the "package/*" parent folder
-        });
-
-        w.on('error', err => {
-          reject(new Error(`Failed to extract ${info.dist.tarball}: ${err.message}`));
-        });
-
-        console.log(`[plugins] Running Yarn install in "${pluginDir}"`);
-        w.on('end', () => {
-          childProcess.execFile(
-            process.execPath,
-            [_getYarnPath(), 'install'],
-            {
-              timeout: 5 * 60 * 1000,
-              maxBuffer: 1024 * 1024,
-              cwd: pluginDir,
-              env: {
-                'NODE_ENV': 'production',
-                'ELECTRON_RUN_AS_NODE': 'true'
-              }
-            },
-            (err, stdout, stderr) => {
-              if (err) {
-                reject(new Error(stderr));
-              } else {
-                resolve();
-              }
-            }
-          );
-        });
-
-        const body = Buffer.concat(bodyBuffers);
-
-        const shasum = crypto.createHash('sha1').update(body).digest('hex');
-        if (shasum !== info.dist.shasum) {
-          reject(new Error('Plugin shasum doesn\'t match npm'));
-          return;
-        }
-
-        w.end(body);
-      });
-
-      response.on('data', chunk => {
-        bodyBuffers.push(chunk);
-      });
-    });
-
-    request.end();
+    resolve();
   });
 }
 
@@ -107,7 +70,7 @@ async function _isInsomniaPlugin (moduleName: string): Promise<Object> {
         }
       }, (err, stdout, stderr) => {
         if (err) {
-          reject(new Error(`${moduleName} install error: ${err.message}`));
+          reject(new Error(`${moduleName} npm error: ${err.message}`));
           return;
         }
 
@@ -141,6 +104,46 @@ async function _isInsomniaPlugin (moduleName: string): Promise<Object> {
             tarball: data.dist.tarball
           }
         });
+      }
+    );
+  });
+}
+
+async function _installPluginToTmpDir (moduleName: string): Promise<{tmpDir: string}> {
+  return new Promise((resolve, reject) => {
+    const tmpDir = path.join(getTempDir(), `${moduleName}-${Date.now()}`);
+    mkdirp.sync(tmpDir);
+    console.log(`[plugins] Installing plugin to ${tmpDir}`);
+    childProcess.execFile(
+      process.execPath,
+      [
+        _getYarnPath(), 'add', moduleName,
+        '--modules-folder', tmpDir,
+        '--cwd', tmpDir,
+        '--no-lockfile',
+        '--production',
+        '--no-progress'
+      ],
+      {
+        timeout: 5 * 60 * 1000,
+        maxBuffer: 1024 * 1024,
+        cwd: tmpDir,
+        env: {
+          'NODE_ENV': 'production',
+          'ELECTRON_RUN_AS_NODE': 'true'
+        }
+      }, (err, stdout, stderr) => {
+        if (err) {
+          reject(new Error(`${moduleName} install error: ${err.message}`));
+          return;
+        }
+
+        if (stderr) {
+          reject(new Error(`Yarn error ${stderr.toString('utf8')}`));
+          return;
+        }
+
+        resolve({tmpDir});
       }
     );
   });
---
+++ packages/insomnia-app/flow-typed/fs-extra.js
@@ -0,0 +1,5 @@
+declare module 'fs-extra' {
+  declare module.exports: {
+    moveSync: (src: string, dest: string, options?: {overwrite?: boolean}) => void
+  }
+}
