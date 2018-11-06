a7e8374cfb80fd9d145f24d65c1dac8775e4b3f1
Gregory Schier <gschier1990@gmail.com>
2018-11-06 13:08:16 -0500

    Silent tar

--- docker/install-dependencies.sh
+++ docker/install-dependencies.sh
@@ -21,7 +21,7 @@ apt-get install -y nodejs graphicsmagick icnsutils
 # Build zlib from source (for Curl)
 wget -q https://github.com/madler/zlib/archive/v1.2.11.tar.gz -O ./zlib.tar.gz
 mkdir -p /src/zlib /build/zlib
-tar -xvf zlib.tar.gz -C /src/zlib --strip 1
+tar -xf zlib.tar.gz -C /src/zlib --strip 1
 cd /src/zlib
 ./configure --prefix=/build/zlib
 make
@@ -31,7 +31,7 @@ ldconfig
 # Build OpenSSL from source (for Curl)
 wget -q https://github.com/openssl/openssl/archive/OpenSSL_1_1_0h.tar.gz -O ./openssl.tar.gz
 mkdir -p /src/openssl /build/openssl
-tar -xvf openssl.tar.gz -C /src/openssl --strip 1
+tar -xf openssl.tar.gz -C /src/openssl --strip 1
 cd /src/openssl
 ./config no-shared --static --prefix=/build/openssl --openssldir=/build/openssl
 make
@@ -41,7 +41,7 @@ ldconfig
 # Build nghttp2 from source (for Curl)
 wget -q https://github.com/nghttp2/nghttp2/releases/download/v1.31.1/nghttp2-1.31.1.tar.gz -O ./nghttp2.tar.gz
 mkdir -p /src/nghttp2 /build/nghttp2
-tar -xvf nghttp2.tar.gz -C /src/nghttp2 --strip 1
+tar -xf nghttp2.tar.gz -C /src/nghttp2 --strip 1
 cd /src/nghttp2
 CFLAGS="-fPIC" ./configure --enable-lib-only --disable-shared --prefix=/build/nghttp2
 make
@@ -51,7 +51,7 @@ ldconfig
 # Build Curl from source
 wget -q https://github.com/curl/curl/releases/download/curl-7_61_1/curl-7.61.1.tar.gz -O ./curl.tar.gz
 mkdir -p /src/curl
-tar -xvf curl.tar.gz -C /src/curl --strip 1
+tar -xf curl.tar.gz -C /src/curl --strip 1
 cd /src/curl
 ./buildconf
 LIBS="-ldl" CPPFLAGS="-I/build/openssl/include" LDFLAGS="-L/build/openssl/lib" \
