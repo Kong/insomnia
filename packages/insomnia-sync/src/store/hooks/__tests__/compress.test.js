b32227e1db6cf7133383648e217e8a36eca09046
Gregory Schier <gschier1990@gmail.com>
2019-04-04 16:06:07 -0400

    Fix tests

--- packages/insomnia-sync/src/store/hooks/__tests__/compress.test.js
+++ packages/insomnia-sync/src/store/hooks/__tests__/compress.test.js
@@ -1,20 +1,15 @@
 import hook from '../compress';
 
-describe('read()', () => {
-  it('writes compressed data', async () => {
+describe('compress hook', () => {
+  it('compresses non-extension keys', async () => {
     const compressed = await hook.write('', 'hello');
-    expect(compressed.toString('base64')).toBe('H4sIAAAAAAAAE8tIzcnJBwCGphA2BQAAAA==');
+    const uncompressed = await hook.read('', compressed);
+    expect(uncompressed.toString()).toBe('hello');
   });
 
   it('writes raw data for extensions', async () => {
     const compressed = await hook.write('.json', 'hello');
     expect(compressed.toString('base64')).toBe('hello');
-  });
-
-  it('reads compressed data', async () => {
-    const data = Buffer.from('H4sIAAAAAAAAE8tIzcnJBwCGphA2BQAAAA==', 'base64');
-    const compressed = await hook.read('', data);
-    expect(compressed.toString('utf8')).toBe('hello');
   });
 
   it('reads compressed data', async () => {
