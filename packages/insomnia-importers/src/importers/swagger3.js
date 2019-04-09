95c4b29090359caae92899dcfe4650dd0570c6c3
Joshua May <notjosh@gmail.com>
2019-04-09 12:23:08 +0200

    Empty test, but it's green now

---
+++ packages/insomnia-importers/src/importers/swagger3.js
@@ -0,0 +1,6 @@
+module.exports.id = 'swagger3';
+module.exports.name = 'Swagger 3.0';
+module.exports.description = 'Importer for Swagger 3.0 specification (json/yaml)';
+
+module.exports.convert = async function(rawData) {
+}
