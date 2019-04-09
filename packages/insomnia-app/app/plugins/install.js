033c08684ecc9c6b374a23e07d214e1c531b9238
Patras Vlad <senoctar@gmail.com>
2019-04-09 21:51:59 +0300

    Add comments for electron exit code workaround.

--- packages/insomnia-app/app/plugins/install.js
+++ packages/insomnia-app/app/plugins/install.js
@@ -87,6 +87,9 @@ async function _isInsomniaPlugin(lookupName: string): Promise<Object> {
         try {
           yarnOutput = JSON.parse(stdout.toString('utf8'));
         } catch (ex) {
+          // Output is not JSON. Check if yarn/electron terminated with non-zero exit code.
+          // In certain environments electron can exit with error even if output is OK.
+          // Parsing is attemted before checking exit code as workaround for false errors.
           if (err) {
             reject(new Error(`${lookupName} npm error: ${err.message}`));
           } else {
@@ -148,6 +151,9 @@ async function _installPluginToTmpDir(lookupName: string): Promise<{ tmpDir: str
         },
       },
       (err, stdout, stderr) => {
+        // Check yarn/electron process exit code.
+        // In certain environments electron can exit with error even if the command was perfomed sucesfully.
+        // Checking for sucess message in output is a workaround for false errors.
         if (err && !stdout.toString('utf8').includes('success')) {
           reject(new Error(`${lookupName} install error: ${err.message}`));
           return;
