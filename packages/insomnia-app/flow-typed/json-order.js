a5da7a72dd33d52af6a052c73919b16705f4e408
Opender Singh <opender94@gmail.com>
2019-05-27 23:34:25 +1200

    Updated flowtypes

--- packages/insomnia-app/flow-typed/json-order.js
+++ packages/insomnia-app/flow-typed/json-order.js
@@ -2,19 +2,21 @@
 
 declare module 'json-order' {
   declare type OrderedParseResult = {
-    object: object,
+    object: Object,
     map: { [key: string]: Array<string> },
   };
 
+  declare type orderedJSON = {
+    parse: (jsonString: string, prefix?: string, separator?: string) => OrderedParseResult,
+    stringify: (
+      sourceObject: Object,
+      map: { [key: string]: Array<string> } | null,
+      separator?: string,
+      space?: number | void,
+    ) => string,
+  };
+
   declare module.exports: {
-    orderedJSON: {
-      parse: (jsonString: string, prefix?: string, separator?: string) => OrderedParseResult,
-      stringify: (
-        sourceObject: object,
-        map: { [key: string]: Array<string> } | null,
-        separator?: string,
-        space?: number | void,
-      ) => string,
-    },
+    orderedJSON: typeof orderedJSON,
   };
 }
