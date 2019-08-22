773eefc05950cdd6bd839df5d59b8f0e22f6c254
Gregory Schier <gschier1990@gmail.com>
2019-08-21 23:59:06 -0700

    Initial try at creating GitHub actions (#1646)
    * Initial try at creating GitHub actions
    
    * Fix Git checkout
    
    * Just Mac for now
    
    * Add Windows back
    
    * Try change run for WIndows
    
    * Configure msvs
    
    * Try again
    
    * Try again
    
    * Try downgrading windows
    
    * Change msvs version
    
    * Separate Windows steps
    
    * Try adding Ubuntu
    
    * Linux packages
    
    * SUdo?
    
    * Try something else
    
    * Make test more robust

--- plugins/insomnia-plugin-file/__tests__/index.test.js
+++ plugins/insomnia-plugin-file/__tests__/index.test.js
@@ -26,7 +26,7 @@ describe('FileExtension', () => {
   it(
     'fails on missing file',
     assertTemplateFails(
-      [process.platform === 'win32' ? 'C:\\foo' : '/foo'],
+      [path.resolve('/foo')],
       `ENOENT: no such file or directory, open '${path.resolve('/foo')}'`,
     ),
   );
