dd0b9bf5263362c335844a1e35e37f0edf0b7d19
Gregory Schier <gschier1990@gmail.com>
2019-08-26 11:10:45 -0700

    POC fs module for isomorphic-git with tests

---
+++ packages/insomnia-app/app/sync/git/__tests__/git-vcs.test.js
@@ -0,0 +1,377 @@
+import GitVCS, { MemPlugin } from '../git-vcs';
+
+const AUTHOR = {
+  name: 'Karen Brown',
+  email: 'karen@example.com',
+};
+
+describe('Git-VCS', () => {
+  beforeEach(() => {
+    let ts = 1000000000000;
+
+    class fakeDate extends Date {
+      constructor(arg) {
+        if (!arg) {
+          return new Date(ts++);
+        } else {
+          super(arg);
+        }
+      }
+
+      static now() {
+        return new Date().getTime();
+      }
+    }
+
+    global.Date = fakeDate;
+  });
+
+  describe('status', () => {
+    it('does the thing', async () => {
+      const vcs = new GitVCS();
+      await vcs.init('/', MemPlugin.createPlugin());
+
+      const files = await vcs.status();
+      expect(files).toEqual([]);
+    });
+  });
+
+  describe('playing around', () => {
+    it('does the thing', async () => {
+      const fs = MemPlugin.createPlugin();
+
+      const vcs = new GitVCS();
+      await vcs.init('/', fs);
+
+      // No files exist yet
+      const files1 = await vcs.listFiles();
+      expect(files1).toEqual([]);
+
+      await fs.promises.writeFile('/foo.txt', 'bar');
+      const files2 = await vcs.listFiles();
+      expect(files2).toEqual([]);
+    });
+
+    it('stage and unstage file', async () => {
+      const fs = MemPlugin.createPlugin();
+      await fs.promises.writeFile('/foo.txt', 'foo');
+      await fs.promises.writeFile('/bar.txt', 'bar');
+
+      const vcs = new GitVCS();
+      await vcs.init('/', fs);
+      expect(await vcs.status()).toEqual([['bar.txt', 0, 2, 0], ['foo.txt', 0, 2, 0]]);
+
+      await vcs.add('foo.txt');
+
+      expect(await vcs.status()).toEqual([['bar.txt', 0, 2, 0], ['foo.txt', 0, 2, 2]]);
+
+      await vcs.remove('foo.txt');
+
+      expect(await vcs.status()).toEqual([['bar.txt', 0, 2, 0], ['foo.txt', 0, 2, 0]]);
+    });
+
+    it('commit file', async () => {
+      const fs = MemPlugin.createPlugin();
+      await fs.promises.writeFile('/foo.txt', 'foo');
+      await fs.promises.writeFile('/bar.txt', 'bar');
+
+      const vcs = new GitVCS();
+      await vcs.init('/', fs);
+      await vcs.add('foo.txt');
+      await vcs.commit('First commit!', AUTHOR);
+
+      expect(await vcs.status()).toEqual([['bar.txt', 0, 2, 0], ['foo.txt', 1, 1, 1]]);
+
+      expect(await vcs.log()).toEqual([
+        {
+          author: {
+            email: 'karen@example.com',
+            name: 'Karen Brown',
+            timestamp: 1000000000,
+            timezoneOffset: 420,
+          },
+          committer: {
+            email: 'karen@example.com',
+            name: 'Karen Brown',
+            timestamp: 1000000000,
+            timezoneOffset: 420,
+          },
+          message: 'First commit!\n',
+          oid: '1cbd843cc8bcd60d35f6f94f21c011c0731fe9d2',
+          parent: [],
+          tree: '95d3aa5f7462c052c821f8fe976c506c94946d68',
+        },
+      ]);
+    });
+
+    it('create branch', async () => {
+      const fs = MemPlugin.createPlugin();
+      await fs.promises.writeFile('/foo.txt', 'foo');
+      await fs.promises.writeFile('/bar.txt', 'bar');
+
+      const vcs = new GitVCS();
+      await vcs.init('/', fs);
+      await vcs.add('foo.txt');
+      await vcs.commit('First commit!', AUTHOR);
+
+      expect((await vcs.log()).length).toBe(1);
+
+      await vcs.checkout('new-branch');
+      expect((await vcs.log()).length).toBe(1);
+      await vcs.add('bar.txt');
+      await vcs.commit('Second commit!', AUTHOR);
+      expect((await vcs.log()).length).toBe(2);
+
+      await vcs.checkout('master');
+      expect((await vcs.log()).length).toBe(1);
+    });
+  });
+});
+
+describe('MemPlugin', () => {
+  beforeEach(() => {
+    let ts = 1000000000000;
+    Date.now = jest.fn(() => ts++);
+  });
+
+  describe('readfile()', () => {
+    it('fails to read', async () => {
+      const p = new MemPlugin();
+      await assertAsyncError(p.readFile('/foo.txt'), 'ENOENT');
+    });
+
+    it('reads a file', async () => {
+      const p = new MemPlugin();
+      await p.writeFile('/foo.txt', 'Hello World!');
+      expect((await p.readFile('/foo.txt')).toString()).toBe('Hello World!');
+    });
+  });
+
+  describe('writeFile()', () => {
+    it('fails to write over directory', async () => {
+      const p = new MemPlugin();
+
+      await p.mkdir('/foo');
+      await assertAsyncError(p.writeFile('/foo', 'Hello World 2!'), 'EISDIR');
+    });
+
+    it('overwrites file', async () => {
+      const p = new MemPlugin();
+
+      await p.writeFile('/foo.txt', 'Hello World!');
+      await p.writeFile('/foo.txt', 'Hello World 2!');
+      expect((await p.readFile('/foo.txt')).toString()).toBe('Hello World 2!');
+    });
+
+    it('flag "a" file', async () => {
+      const p = new MemPlugin();
+
+      await p.writeFile('/foo.txt', 'Hello World!', { flag: 'a' });
+      await p.writeFile('/foo.txt', 'xxx', { flag: 'a' });
+      expect((await p.readFile('/foo.txt')).toString()).toBe('Hello World!xxx');
+    });
+
+    it('flags "ax" and "wx" fail if path exists', async () => {
+      const p = new MemPlugin();
+
+      await p.writeFile('/foo.txt', 'Hello World!');
+      await assertAsyncError(p.writeFile('/foo.txt', 'aaa', { flag: 'ax' }), 'EEXIST');
+      await assertAsyncError(p.writeFile('/foo.txt', 'aaa', { flag: 'wx' }), 'EEXIST');
+    });
+
+    it('fails if flag "r"', async () => {
+      const p = new MemPlugin();
+      await assertAsyncError(p.writeFile('/foo.txt', 'aaa', { flag: 'r' }), 'EBADF');
+    });
+
+    it('fails if dir missing', async () => {
+      const p = new MemPlugin();
+
+      await assertAsyncError(p.writeFile('/foo.txt', 'aaa', { flag: 'r' }), 'EBADF');
+    });
+
+    it('works with flags', async () => {
+      const p = new MemPlugin();
+
+      await p.writeFile('/foo.txt', 'Hello World!', { flag: 'a' });
+      await p.writeFile('/foo.txt', 'xxx', { flag: 'a' });
+      expect((await p.readFile('/foo.txt')).toString()).toBe('Hello World!xxx');
+    });
+  });
+
+  describe('unlink()', () => {
+    it('unlinks file', async () => {
+      const p = new MemPlugin();
+
+      await p.writeFile('/foo.txt', 'xxx');
+      await p.unlink('/foo.txt');
+      await assertAsyncError(p.readFile('/foo.txt'), 'ENOENT');
+    });
+
+    it('fails to unlinks missing file', async () => {
+      const p = new MemPlugin();
+
+      await assertAsyncError(p.unlink('/not/exist.txt'), 'ENOENT');
+    });
+  });
+
+  describe('readdir()', () => {
+    it('lists dir', async () => {
+      const p = new MemPlugin();
+
+      // Root dir should always exist
+      expect(await p.readdir('/')).toEqual([]);
+
+      // Write a file and list it again
+      await p.writeFile('/foo.txt', 'Hello World!');
+      await p.writeFile('/bar.txt', 'Bar!');
+      expect(await p.readdir('/')).toEqual(['bar.txt', 'foo.txt']);
+    });
+
+    it('errors on file', async () => {
+      const p = new MemPlugin();
+      await p.writeFile('/foo.txt', 'Bar!');
+
+      let message = false;
+      try {
+        await p.readdir('/foo.txt');
+      } catch (e) {
+        message = e.message;
+      }
+
+      expect(message).toBe(`ENOTDIR: not a directory, scandir '/foo.txt'`);
+    });
+
+    it('errors on missing directory', async () => {
+      const p = new MemPlugin();
+
+      let message = false;
+      try {
+        await p.readdir('/invalid');
+      } catch (e) {
+        message = e.message;
+      }
+
+      expect(message).toBe(`ENOENT: no such file or directory, scandir '/invalid'`);
+    });
+  });
+
+  describe('mkdir()', () => {
+    it('creates directory', async () => {
+      const p = new MemPlugin();
+
+      await p.mkdir('/foo');
+      await p.mkdir('/foo/bar');
+
+      expect(await p.readdir('/')).toEqual(['foo']);
+      expect(await p.readdir('/foo')).toEqual(['bar']);
+    });
+
+    it('creates directory recursively', async () => {
+      const p = new MemPlugin();
+
+      await p.mkdir('/foo/bar/baz', { recursive: true });
+      expect(await p.readdir('/foo/bar/baz')).toEqual([]);
+    });
+
+    it('fails to create if no parent', async () => {
+      const p = new MemPlugin();
+
+      await assertAsyncError(p.mkdir('/foo/bar/baz'), 'ENOENT');
+    });
+  });
+
+  describe('rmdir()', () => {
+    it('removes a dir', async () => {
+      const p = new MemPlugin();
+
+      await p.mkdir('/a/b/c', { recursive: true });
+      expect(await p.readdir('/a/b')).toEqual(['c']);
+      await p.rmdir('/a/b/c');
+      expect(await p.readdir('/a/b')).toEqual([]);
+    });
+
+    it('fails on non-empty dir', async () => {
+      const p = new MemPlugin();
+
+      await p.mkdir('/a/b/c', { recursive: true });
+      await p.writeFile('/a/b/c/foo.txt', 'xxx');
+
+      await assertAsyncError(p.rmdir('/a/b'), 'ENOTEMPTY');
+      await assertAsyncError(p.rmdir('/a/b/c'), 'ENOTEMPTY');
+    });
+
+    it('fails on file', async () => {
+      const p = new MemPlugin();
+
+      await p.writeFile('/foo.txt', 'xxx');
+      await assertAsyncError(p.rmdir('/foo.txt'), 'ENOTDIR');
+    });
+  });
+
+  describe('stat()', () => {
+    it('stats root dir', async () => {
+      const p = new MemPlugin();
+
+      const stat = await p.stat('/');
+
+      expect(stat).toEqual({
+        ctimeMs: 1000000000000,
+        mtimeMs: 1000000000000,
+        dev: 1,
+        gid: 1,
+        ino: 0,
+        mode: 0o777,
+        size: 0,
+        type: 'dir',
+        uid: 1,
+      });
+
+      expect(stat.isDirectory()).toBe(true);
+      expect(stat.isFile()).toBe(false);
+      expect(stat.isSymbolicLink()).toBe(false);
+    });
+
+    it('stats file', async () => {
+      const p = new MemPlugin();
+
+      await p.writeFile('/foo.txt', 'xxx');
+      const stat = await p.stat('/foo.txt');
+
+      expect(stat).toEqual({
+        ctimeMs: 1000000000001,
+        mtimeMs: 1000000000001,
+        dev: 1,
+        gid: 1,
+        ino: 0,
+        mode: 0o777,
+        size: 4,
+        type: 'file',
+        uid: 1,
+      });
+
+      expect(stat.isDirectory()).toBe(false);
+      expect(stat.isFile()).toBe(true);
+      expect(stat.isSymbolicLink()).toBe(false);
+    });
+
+    it('fails to stat missing', async () => {
+      const p = new MemPlugin();
+      await assertAsyncError(p.stat('/bar.txt'), 'ENOENT');
+    });
+  });
+});
+
+async function assertAsyncError(promise, code) {
+  try {
+    await promise;
+  } catch (err) {
+    expect(err.message).toMatch(new RegExp(`^${code}.+`));
+    expect(err.code).toBe(code);
+    expect(typeof err.syscall).toBe('string');
+    expect(typeof err.path).toBe('string');
+    return;
+  }
+
+  throw new Error(`Promise did not throw`);
+}
---
+++ packages/insomnia-app/app/sync/git/git-vcs.js
@@ -0,0 +1,553 @@
+// @flow
+import * as git from 'isomorphic-git';
+import fs from 'fs';
+import path from 'path';
+
+export default class GitVCS {
+  _dir: string;
+  _git: Object;
+
+  async init(directory: string, fsPlugin: Object) {
+    this._dir = directory;
+    git.plugins.set('fs', fsPlugin);
+    await git.init({ dir: directory });
+    this._git = git;
+  }
+
+  async listFiles(): Promise<Array<string>> {
+    console.log('[git] List files');
+    return git.listFiles({ dir: this._dir });
+  }
+
+  async status(): Promise<Array<[string, number, number]>> {
+    console.log('[git] Status');
+    return git.statusMatrix({ dir: this._dir });
+  }
+
+  async add(relPath: string): Promise<void> {
+    console.log('[git] Add', relPath);
+    return git.add({ dir: this._dir, filepath: relPath });
+  }
+
+  async remove(relPath: string): Promise<void> {
+    console.log('[git] Remove', relPath);
+    return git.remove({ dir: this._dir, filepath: relPath });
+  }
+
+  async commit(message: string, author: { name: string, email: string }): Promise<string> {
+    console.log('[git] Commit', message);
+    return git.commit({ dir: this._dir, message, author });
+  }
+
+  async log(depth: number = 5): Promise<string> {
+    console.log('[git] Log', depth);
+    return git.log({ dir: this._dir, depth: depth });
+  }
+
+  async checkout(branch: string): Promise<void> {
+    console.log('[git] Checkout', branch);
+    try {
+      return await git.checkout({ dir: this._dir, ref: branch });
+    } catch (err) {
+      // Create if doesn't exist
+      return git.branch({ dir: this._dir, ref: branch, checkout: true });
+    }
+  }
+}
+
+export class FSPlugin {
+  _basePath: string;
+
+  constructor(basePath: string) {
+    this._basePath = basePath;
+    console.log(`[FSPlugin] Created in ${basePath}`);
+  }
+
+  static createPlugin(basePath: string) {
+    return {
+      promises: new FSPlugin(basePath),
+    };
+  }
+
+  readFile(filePath: string, ...x: Array<any>): Promise<Buffer | string> {
+    filePath = this._joinPath(filePath);
+    return fs.promises.readFile(filePath, ...x);
+  }
+
+  writeFile(filePath: string, data: Buffer | string, ...x: Array<any>) {
+    filePath = this._joinPath(filePath);
+    return fs.promises.writeFile(filePath, data, ...x);
+  }
+
+  unlink(filePath: string, ...x: Array<any>) {
+    filePath = this._joinPath(filePath);
+    return fs.promises.unlink(filePath, ...x);
+  }
+
+  readdir(filePath: string, ...x: Array<any>) {
+    filePath = this._joinPath(filePath);
+    return fs.promises.readdir(filePath, ...x);
+  }
+
+  mkdir(filePath: string, ...x: Array<any>) {
+    filePath = this._joinPath(filePath);
+    return fs.promises.mkdir(filePath, ...x);
+  }
+
+  rmdir(filePath: string, ...x: Array<any>) {
+    filePath = this._joinPath(filePath);
+    return fs.promises.rmdir(filePath, ...x);
+  }
+
+  stat(filePath: string, ...x: Array<any>) {
+    filePath = this._joinPath(filePath);
+    return (fs.promises: any).stat(filePath, ...x);
+  }
+
+  lstat(filePath: string, ...x: Array<any>) {
+    filePath = this._joinPath(filePath);
+    return (fs.promises: any).lstat(filePath, ...x);
+  }
+
+  readlink(filePath: string, ...x: Array<any>) {
+    filePath = this._joinPath(filePath);
+    return (fs.promises: any).readlink(filePath, ...x);
+  }
+
+  symlink(targetPath: string, filePath: string, ...x: Array<any>) {
+    filePath = this._joinPath(filePath);
+    return (fs.promises: any).symlink(targetPath, filePath, ...x);
+  }
+
+  _joinPath(filePath: string) {
+    return path.join(this._basePath, filePath);
+  }
+}
+
+type FSFile = {|
+  +type: 'file',
+  +ino: number,
+  +mtimeMs: number,
+  +name: string,
+  +path: string,
+  contents: string,
+|};
+
+type FSLink = {|
+  +type: 'symlink',
+  +ino: number,
+  +mtimeMs: number,
+  +name: string,
+  +path: string,
+  +linkTo: string,
+|};
+
+type FSDir = {|
+  +type: 'dir',
+  +ino: number,
+  +mtimeMs: number,
+  +name: string,
+  +path: string,
+  +children: Array<FSFile | FSDir | FSLink>,
+|};
+
+type FSEntry = FSDir | FSFile | FSLink;
+
+export class MemPlugin {
+  __fs: FSEntry;
+  __ino: 0;
+
+  static createPlugin() {
+    return {
+      promises: new MemPlugin(),
+    };
+  }
+
+  constructor() {
+    this.__ino = 0;
+    this.__fs = {
+      type: 'dir',
+      path: '/',
+      name: '',
+      children: [],
+      ino: this.__ino,
+      mtimeMs: Date.now(),
+    };
+  }
+
+  async tree(baseDir: string = '/') {
+    const next = async (dir: string, toPrint: string): Promise<string> => {
+      const entry = this._find(dir);
+
+      if (!entry) {
+        return toPrint;
+      }
+
+      let indent = new Array((dir.match(/\//g) || []).length).join('|   ');
+      if (entry.type === 'dir') {
+        if (entry.path !== baseDir) {
+          toPrint += `${indent}${entry.name}/\n`;
+        }
+
+        for (const name of await this.readdir(dir)) {
+          toPrint = await next(path.join(dir, name), toPrint);
+        }
+      } else {
+        toPrint += `${indent}${entry.name}\n`;
+      }
+
+      return toPrint;
+    };
+
+    console.log(await next(baseDir, ''));
+  }
+
+  async readFile(
+    filePath: string,
+    options: buffer$Encoding | { encoding?: buffer$Encoding },
+  ): Promise<Buffer | string> {
+    if (typeof options === 'string') {
+      options = { encoding: options };
+    }
+
+    const encoding = options ? options.encoding : null;
+
+    const entry = this._assertFile(filePath);
+    const raw = Buffer.from(entry.contents, 'base64');
+    if (encoding) {
+      return raw.toString(encoding);
+    } else {
+      return raw;
+    }
+  }
+
+  async writeFile(
+    filePath: string,
+    data: Buffer | string,
+    options: buffer$Encoding | { encoding?: buffer$Encoding, flag?: string },
+  ): Promise<void> {
+    if (typeof options === 'string') {
+      options = { encoding: options };
+    }
+
+    const flag = options && options.flag ? options.flag : 'w';
+    const encoding = options && options.encoding ? options.encoding : 'utf8';
+
+    // Make sure file doesn't exist for "x" flags
+    if (flag[1] === 'x') {
+      await this._assertDoesNotExist(filePath);
+    }
+
+    const dirEntry: FSDir = this._assertDir(path.dirname(filePath));
+
+    let file: FSEntry | null = this._find(filePath);
+
+    if (file) {
+      file = this._assertFileEntry(file);
+    } else {
+      const name = path.basename(filePath);
+      file = {
+        name,
+        type: 'file',
+        ino: this.__ino++,
+        mtimeMs: Date.now(),
+        contents: '',
+        path: filePath,
+      };
+      dirEntry.children.push(file);
+    }
+
+    const dataBuff: Buffer = data instanceof Buffer ? data : Buffer.from(data, encoding);
+    let newContents = Buffer.alloc(0);
+    if (flag[0] === 'w') {
+      newContents = dataBuff;
+    } else if (flag[0] === 'a') {
+      const contentsBuff: Buffer = Buffer.from(file.contents, 'base64');
+      newContents = Buffer.concat([contentsBuff, dataBuff]);
+    } else {
+      const e: ErrnoError = new Error('EBADF: bad file descriptor, write');
+      e.errno = -9;
+      e.code = 'EBADF';
+      e.syscall = 'write';
+      e.path = filePath;
+      throw e;
+    }
+
+    file.contents = newContents.toString('base64');
+
+    return Promise.resolve();
+  }
+
+  async unlink(filePath: string, ...x: Array<any>): Promise<void> {
+    this._remove(this._assertFile(filePath));
+  }
+
+  async readdir(basePath: string, ...x: Array<any>): Promise<Array<string>> {
+    const entry = this._assertDir(basePath);
+
+    const names = entry.children.map(c => c.name);
+    names.sort();
+    return names;
+  }
+
+  async mkdir(dirPath: string, options?: { recursive?: boolean }): Promise<void> {
+    const doRecursive = (options || {}).recursive || false;
+
+    // If not recursive, ensure parent exists
+    if (!doRecursive) {
+      this._assertDir(path.dirname(dirPath));
+    }
+
+    const pathSegments = dirPath.split('/').filter(s => s !== '');
+
+    // Recurse over all subpaths, ensure they are all directories,
+    // create them if they don't exist
+    let currentPath = '';
+    for (const pathSegment of pathSegments) {
+      const dirEntry = this._assertDir(currentPath);
+      const nextPath = currentPath + '/' + pathSegment;
+
+      // Create dir if it doesn't exist yet
+      if (!dirEntry.children.find(e => e.name === pathSegment)) {
+        dirEntry.children.push({
+          type: 'dir',
+          ino: this.__ino++,
+          mtimeMs: Date.now(),
+          name: pathSegment,
+          path: nextPath,
+          children: [],
+        });
+      }
+
+      currentPath = nextPath;
+    }
+  }
+
+  async rmdir(dirPath: string, ...x: Array<any>) {
+    const dirEntry = this._assertDir(dirPath);
+
+    if (dirEntry.children.length > 0) {
+      const e: ErrnoError = new Error(`ENOTEMPTY: directory not empty, rmdir '${dirPath}'`);
+      e.errno = -66;
+      e.syscall = 'rmdir';
+      e.code = 'ENOTEMPTY';
+      e.path = dirPath;
+      throw e;
+    }
+
+    this._remove(dirEntry);
+  }
+
+  async stat(filePath: string, ...x: Array<any>): Promise<Stat> {
+    return this._statEntry(this._assertExists(filePath));
+  }
+
+  async lstat(filePath: string, ...x: Array<any>) {
+    const linkEntry = this._assertExists(filePath);
+    return this._statEntry(this._resolveLinks(linkEntry));
+  }
+
+  async readlink(filePath: string, ...x: Array<any>) {
+    const linkEntry = this._assertSymlink(filePath);
+    return linkEntry.linkTo;
+  }
+
+  async symlink(target: string, filePath: string, ...x: Array<any>) {
+    // Make sure we don't already have one there
+    // TODO: Check what to do in this case (might be wrong)
+    this._assertDoesNotExist(filePath);
+
+    this._assertExists(target);
+    const parentEntry = this._assertDir(path.dirname(filePath));
+
+    parentEntry.children.push({
+      type: 'symlink',
+      ino: this.__ino++,
+      mtimeMs: Date.now(),
+      name: path.basename(filePath),
+      path: filePath,
+      linkTo: target,
+    });
+  }
+
+  _statEntry(entry: FSEntry): Stat {
+    return new Stat({
+      type: entry.type,
+      mode: 0o777,
+      size: entry.contents ? entry.contents.length : 0,
+      ino: entry.ino,
+      mtimeMs: entry.mtimeMs,
+    });
+  }
+
+  _find(filePath: string): FSEntry | null {
+    let current = this.__fs;
+    const pathSegments = filePath.split('/').filter(s => s !== '');
+    for (const expectedName of pathSegments) {
+      const e = (current.children || []).find(c => c.name === expectedName);
+
+      if (!e) {
+        return null;
+      }
+
+      current = e;
+    }
+
+    // It's the root
+    return current;
+  }
+
+  _assertDoesNotExist(filePath: string): void {
+    const entry = this._find(filePath);
+
+    if (entry) {
+      const e: ErrnoError = new Error(`EEXIST: file already exists, open '${filePath}'`);
+      e.errno = -17;
+      e.code = 'EEXIST';
+      e.syscall = 'open';
+      e.path = filePath;
+      throw e;
+    }
+  }
+
+  _assertExists(filePath: string): FSEntry {
+    const entry = this._find(filePath);
+
+    if (!entry) {
+      const e: ErrnoError = new Error(`ENOENT: no such file or directory, scandir '${filePath}'`);
+      e.errno = -2;
+      e.code = 'ENOENT';
+      e.syscall = 'scandir';
+      e.path = filePath;
+      throw e;
+    }
+
+    return entry;
+  }
+
+  _assertDirEntry(entry: FSEntry): FSDir {
+    if (entry.type !== 'dir') {
+      const e: ErrnoError = new Error(`ENOTDIR: not a directory, scandir '${entry.path}'`);
+      e.errno = -20;
+      e.code = 'ENOTDIR';
+      e.syscall = 'scandir';
+      e.path = entry.path;
+      throw e;
+    }
+
+    return entry;
+  }
+
+  _assertDir(filePath: string): FSDir {
+    const entry = this._assertExists(filePath);
+    return this._assertDirEntry(entry);
+  }
+
+  _assertSymlinkEntry(entry: FSEntry): FSLink {
+    if (entry.type !== 'symlink') {
+      const e: ErrnoError = new Error(`ENOTDIR: not a simlink, scandir '${entry.path}'`);
+      e.errno = -20;
+      e.code = 'ENOTDIR';
+      e.syscall = 'scandir';
+      e.path = entry.path;
+      throw e;
+    }
+
+    return entry;
+  }
+
+  _assertSymlink(filePath: string): FSLink {
+    const entry = this._assertExists(filePath);
+    return this._assertSymlinkEntry(entry);
+  }
+
+  _resolveLinks(entry: FSEntry): FSFile | FSDir {
+    if (entry.type === 'symlink') {
+      const other = this._find(entry.linkTo);
+      if (!other) {
+        // Should never happen
+        throw new Error('Failed to resolve link');
+      }
+
+      return this._resolveLinks(other);
+    }
+
+    return entry;
+  }
+
+  _assertFileEntry(entry: FSEntry): FSFile {
+    entry = this._resolveLinks(entry);
+
+    if (entry.type === 'dir') {
+      const e: ErrnoError = new Error(`EISDIR: illegal operation on a directory '${entry.path}'`);
+      e.errno = -21;
+      e.code = 'EISDIR';
+      e.syscall = 'open';
+      e.path = entry.path;
+      throw e;
+    }
+
+    return entry;
+  }
+
+  _assertFile(filePath: string): FSFile {
+    const entry = this._assertExists(filePath);
+    return this._assertFileEntry(entry);
+  }
+
+  _remove(entry: FSEntry) {
+    const parentEntry = this._assertDir(path.dirname(entry.path));
+    const index = parentEntry.children.findIndex(c => c === entry);
+    if (index < 0) {
+      // Should never happen so w/e
+      return;
+    }
+
+    parentEntry.children.splice(index, 1);
+  }
+}
+
+type StatObj = {
+  type: 'file' | 'dir' | 'symlink',
+  mode: number,
+  size: number,
+  ino: number,
+  mtimeMs: number,
+  ctimeMs?: number,
+};
+
+class Stat {
+  type: 'file' | 'dir' | 'symlink';
+  mode: number;
+  size: number;
+  ino: number;
+  mtimeMs: number;
+  ctimeMs: number;
+  uid: 1;
+  gid: 1;
+  dev: 1;
+
+  constructor(stats: StatObj) {
+    this.type = stats.type;
+    this.mode = stats.mode;
+    this.size = stats.size;
+    this.ino = stats.ino;
+    this.mtimeMs = stats.mtimeMs;
+    this.ctimeMs = stats.ctimeMs || stats.mtimeMs;
+    this.uid = 1;
+    this.gid = 1;
+    this.dev = 1;
+  }
+
+  isFile() {
+    return this.type === 'file';
+  }
+
+  isDirectory() {
+    return this.type === 'dir';
+  }
+
+  isSymbolicLink() {
+    return this.type === 'symlink';
+  }
+}
