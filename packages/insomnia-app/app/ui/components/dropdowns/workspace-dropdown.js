bdb09f9eeab5660dce79501af1c82a3f33dac6e1
Gregory Schier <gschier1990@gmail.com>
2019-09-30 11:44:04 -0700

    Send app property with notification request

--- packages/insomnia-app/app/ui/components/dropdowns/workspace-dropdown.js
+++ packages/insomnia-app/app/ui/components/dropdowns/workspace-dropdown.js
@@ -9,7 +9,7 @@ import DropdownItem from '../base/dropdown/dropdown-item';
 import DropdownHint from '../base/dropdown/dropdown-hint';
 import SettingsModal, { TAB_INDEX_EXPORT } from '../modals/settings-modal';
 import * as models from '../../../models';
-import { getAppVersion } from '../../../common/constants';
+import { getAppName, getAppVersion } from '../../../common/constants';
 import { showAlert, showModal, showPrompt } from '../modals';
 import Link from '../base/link';
 import WorkspaceSettingsModal from '../modals/workspace-settings-modal';
@@ -307,7 +307,9 @@ class WorkspaceDropdown extends React.PureComponent<Props, State> {
             </DropdownItem>
           ))}
 
-          <DropdownDivider>Insomnia Version {getAppVersion()}</DropdownDivider>
+          <DropdownDivider>
+            {getAppName()} v{getAppVersion()}
+          </DropdownDivider>
 
           <DropdownItem onClick={WorkspaceDropdown._handleShowSettings}>
             <i className="fa fa-cog" /> Preferences
