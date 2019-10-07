9909f8e907dd590c058fd91a25d57f9914d516f3
v1ktor <kalinin.viktor@gmail.com>
2019-10-07 21:37:42 +0300

    Escape yarn paths (#1699)

--- packages/insomnia-app/app/plugins/install.js
+++ packages/insomnia-app/app/plugins/install.js
@@ -63,7 +63,7 @@ async function _isInsomniaPlugin(lookupName: string): Promise<Object> {
       escape(process.execPath),
       [
         '--no-deprecation', // Because Yarn still uses `new Buffer()`
-        _getYarnPath(),
+        escape(_getYarnPath()),
         'info',
         lookupName,
         '--json',
@@ -129,7 +129,7 @@ async function _installPluginToTmpDir(lookupName: string): Promise<{ tmpDir: str
       escape(process.execPath),
       [
         '--no-deprecation', // Because Yarn still uses `new Buffer()`
-        _getYarnPath(),
+        escape(_getYarnPath()),
         'add',
         lookupName,
         '--modules-folder',
