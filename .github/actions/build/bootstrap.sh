5a8933152dacc6eed8c5d2aae903b9faf5db4c61
Gregory Schier <gschier1990@gmail.com>
2019-10-09 12:12:14 -0700

    Use GITHUB_WORKSPACE in docker script

--- .github/actions/build/bootstrap.sh
+++ .github/actions/build/bootstrap.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+if [[ -z "$GITHUB_WORKSPACE" ]]; then
+  echo "Set the GITHUB_WORKSPACE env variable."
+  exit 1
+fi
+
 # Fail on any errors
 set -e
 
 # Install root project dependencies
+cd "$GITHUB_WORKSPACE"
 npm run bootstrap
 npm install --no-save 7zip-bin-linux app-builder-bin-linux
 
