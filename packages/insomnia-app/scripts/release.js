76d11266a710ed2a86246acca374035ac771e079
Gregory Schier <gschier1990@gmail.com>
2019-10-30 10:56:10 -0400

    Put ref check in correct script

--- packages/insomnia-app/scripts/release.js
+++ packages/insomnia-app/scripts/release.js
@@ -16,12 +16,15 @@ const GITHUB_REPO = 'insomnia';
 
 // Start package if ran from CLI
 if (require.main === module) {
-  if (!process.env.GITHUB_REF.match(/v\d+\.\d+\.\d+(-(beta|alpha)\.\d+)?$/)) {
-    console.log(`[release] Not running release for ref ${process.env.GITHUB_REF}`);
-    process.exit(0);
-  }
-
   process.nextTick(async () => {
+    // First check if we need to publish (uses Git tags)
+    const gitRefStr = process.env.GITHUB_REF || process.env.TRAVIS_TAG;
+    const skipPublish = !gitRefStr || !gitRefStr.match(/v\d+\.\d+\.\d+(-(beta|alpha)\.\d+)?$/);
+    if (skipPublish) {
+      console.log(`[package] Not packaging for ref=${gitRefStr}`);
+      process.exit(0);
+    }
+
     try {
       await buildTask.start();
       await packageTask.start();
