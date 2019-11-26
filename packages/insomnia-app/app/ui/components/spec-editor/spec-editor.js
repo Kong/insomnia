10af3b7f901b2c11f8bbb3bd6c03de26655b0740
MikeEllanKong <52717970+MikeEllanKong@users.noreply.github.com>
2019-11-26 14:34:04 -0500

    Ts 37 swagger preview (#40)
    * rolling in swagger ui & starting to style it.
    
    * toggle swagger/preview mode

--- packages/insomnia-app/app/ui/components/spec-editor/spec-editor.js
+++ packages/insomnia-app/app/ui/components/spec-editor/spec-editor.js
@@ -5,6 +5,9 @@ import CodeEditor from '../codemirror/code-editor';
 import type { Workspace } from '../../../models/workspace';
 import type { ApiSpec } from '../../../models/api-spec';
 import HelpLink from '../help-link';
+import YAML from 'yaml';
+import SwaggerUI from 'swagger-ui-react';
+import 'swagger-ui-react/swagger-ui.css';
 
 type Props = {|
   apiSpec: ApiSpec,
@@ -21,8 +24,16 @@ type Props = {|
 class SpecEditor extends React.PureComponent<Props> {
   editor: ?CodeEditor;
 
+  state = {
+    previewActive: true,
+  };
+
   _setEditorRef(n: ?CodeEditor) {
     this.editor = n;
+  }
+
+  _togglePreview() {
+    this.setState({ previewActive: !this.state.previewActive });
   }
 
   async _handleOnChange(v: string) {
@@ -54,17 +65,27 @@ class SpecEditor extends React.PureComponent<Props> {
       handleTest,
     } = this.props;
 
+    let swaggerSpec = YAML.parse(apiSpec.contents);
+
     return (
-      <div className="spec-editor theme--pane">
+      <div
+        className={'spec-editor theme--pane ' + (this.state.previewActive ? '' : 'previewHidden')}>
         <div className="spec-editor__header theme--pane__header">
           <h1>
             Edit API Specification <HelpLink slug="editing-specs" />
           </h1>
           <nav className="spec-editor__header__buttons">
+            <button className="btn" onClick={this._togglePreview}>
+              <i className={'fa ' + (this.state.previewActive ? 'fa-eye' : 'fa-eye-slash')} />{' '}
+              Toggle Preview
+            </button>
             <button className="btn" onClick={handleTest}>
-              Debug
+              <i className="fa fa-cogs" /> Generate Requests
             </button>
           </nav>
+        </div>
+        <div id="swagger-ui-wrapper">
+          <SwaggerUI spec={swaggerSpec} />
         </div>
         <div className="spec-editor__body theme--pane__body">
           <CodeEditor
