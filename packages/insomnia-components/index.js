1a6bdad51766de37692c99761359b607aa863fb4
Gregory Schier <gschier1990@gmail.com>
2020-01-06 14:02:54 -0500

    insomnia-ui -> insomnia-components

--- packages/insomnia-components/index.js
+++ packages/insomnia-components/index.js
@@ -2,8 +2,4 @@ import Button from './components/button';
 import NoticeTable from './components/notice-table';
 import Table from './components/table';
 
-export {
-  Button,
-  NoticeTable,
-  Table,
-};
+export { Button, NoticeTable, Table };
