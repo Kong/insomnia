621f3509e5acb091cc88bf77f32fbe753d97a9a8
MikeEllanKong <mike.ellan@konghq.com>
2020-04-28 10:21:29 -0400

    Various stylistic updates
    - card title padding fix
    - card tag removal if “unknown” type
    - header padding, logo size, border radius

--- packages/insomnia-app/app/ui/components/wrapper-home.js
+++ packages/insomnia-app/app/ui/components/wrapper-home.js
@@ -349,7 +349,7 @@ class WrapperHome extends React.PureComponent<Props, State> {
     );
 
     if (spec || w.scope === 'spec') {
-      let label: string = 'Unknown';
+      let label: string = '';
       if (specFormat === 'openapi') {
         label = `OpenAPI ${specFormatVersion}`;
       } else if (specFormat === 'swagger') {
@@ -429,7 +429,7 @@ class WrapperHome extends React.PureComponent<Props, State> {
             className="app-header"
             gridLeft={
               <React.Fragment>
-                <img src={designerLogo} alt="Insomnia" width="24" height="24" />
+                <img src={designerLogo} alt="Insomnia" width="32" height="32" />
                 <Breadcrumb className="breadcrumb" crumbs={['Documents']} />
               </React.Fragment>
             }
@@ -441,7 +441,7 @@ class WrapperHome extends React.PureComponent<Props, State> {
                     type="text"
                     placeholder="Filter..."
                     onChange={this._handleFilterChange}
-                    className="no-margin workspace-filter"
+                    className="no-margin"
                   />
                   <span className="fa fa-search filter-icon"></span>
                 </KeydownBinder>
