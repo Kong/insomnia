1bc15745b74ea2e802c239a5119170cade6f3635
Gregory Schier <gschier1990@gmail.com>
2020-04-29 12:30:48 -0700

    Remove empty test folder

--- packages/insomnia-app/app/node-libcurl/__tests__/index.test.js
+++
@@ -1,9 +0,0 @@
-// TODO: Figure out good way to get tests working AND run in Electron
-// const {Curl} = require('../curl');
-//
-// describe('Curl', () => {
-//   it('hello', () => {
-//     const curl = new Curl();
-//     expect(curl).toBeDefined();
-//   });
-// });
