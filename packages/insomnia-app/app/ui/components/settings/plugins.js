6b5647f5844f04f1fe7e78749755b760909327c1
Opender Singh <opender.singh@konghq.com>
2020-05-03 11:31:52 +1200

    Add hyperlinks to plugin listing

--- packages/insomnia-app/app/ui/components/settings/plugins.js
+++ packages/insomnia-app/app/ui/components/settings/plugins.js
@@ -1,4 +1,5 @@
 // @flow
+import * as path from 'path';
 import type { Plugin } from '../../../plugins/index';
 import { getPlugins } from '../../../plugins/index';
 import * as React from 'react';
@@ -10,7 +11,7 @@ import installPlugin from '../../../plugins/install';
 import HelpTooltip from '../help-tooltip';
 import Link from '../base/link';
 import { delay } from '../../../common/misc';
-import { PLUGIN_PATH } from '../../../common/constants';
+import { NPM_PACKAGE_BASE, PLUGIN_HUB_BASE, PLUGIN_PATH } from '../../../common/constants';
 import type { PluginConfig, Settings } from '../../../models/settings';
 import ToggleSwitch from './toggle-switch';
 import { Button } from 'insomnia-components';
@@ -115,9 +116,8 @@ class Plugins extends React.PureComponent<Props, State> {
       submitName: 'Generate',
       label: 'Plugin Name',
       selectText: true,
-      validate: (name) => name.match(/^[a-z][a-z-]*[a-z]$/)
-        ? ''
-        : 'Plugin name must be of format my-plugin-name',
+      validate: name =>
+        name.match(/^[a-z][a-z-]*[a-z]$/) ? '' : 'Plugin name must be of format my-plugin-name',
       onComplete: async name => {
         // Remove insomnia-plugin- prefix if they accidentally typed it
         name = name.replace(/^insomnia-plugin-/, '');
@@ -188,6 +188,22 @@ class Plugins extends React.PureComponent<Props, State> {
     );
   }
 
+  renderLink(plugin: Plugin) {
+    const { name } = plugin;
+
+    let base = NPM_PACKAGE_BASE;
+    if (/^insomnia-plugin-/.test(name)) {
+      base = PLUGIN_HUB_BASE;
+    }
+    const link = path.join(base, name);
+
+    return (
+      <a className="space-left" href={link} title={link}>
+        <i className="fa fa-external-link-square" />
+      </a>
+    );
+  }
+
   render() {
     const { plugins, error, isInstallingFromNpm, isRefreshingPlugins } = this.state;
 
@@ -203,45 +219,48 @@ class Plugins extends React.PureComponent<Props, State> {
         ) : (
           <table className="table--fancy table--striped table--valign-middle margin-top margin-bottom">
             <thead>
-            <tr>
-              <th>Enable?</th>
-              <th>Name</th>
-              <th>Version</th>
-              <th>Folder</th>
-            </tr>
+              <tr>
+                <th>Enable?</th>
+                <th>Name</th>
+                <th>Version</th>
+                <th>Folder</th>
+              </tr>
             </thead>
             <tbody>
-            {plugins.map(plugin =>
-              !plugin.directory ? null : (
-                <tr key={plugin.name}>
-                  <td style={{ width: '4rem' }}>{this.renderToggleSwitch(plugin)}</td>
-                  <td>
-                    {plugin.name}
-                    {plugin.description && (
-                      <HelpTooltip info className="space-left">
-                        {plugin.description}
-                      </HelpTooltip>
-                    )}
-                  </td>
-                  <td>{plugin.version}</td>
-                  <td className="no-wrap" style={{ width: '10rem' }}>
-                    <CopyButton
-                      size="small"
-                      variant="contained"
-                      title={plugin.directory}
-                      content={plugin.directory}>
-                      Copy Path
-                    </CopyButton>{' '}
-                    <Button
-                      size="small"
-                      variant="contained"
-                      onClick={Plugins._handleOpenDirectory.bind(this, plugin.directory)}>
-                      Reveal Folder
-                    </Button>
-                  </td>
-                </tr>
-              ),
-            )}
+              {plugins.map(plugin =>
+                !plugin.directory ? null : (
+                  <tr key={plugin.name}>
+                    <td style={{ width: '4rem' }}>{this.renderToggleSwitch(plugin)}</td>
+                    <td>
+                      {plugin.name}
+                      {plugin.description && (
+                        <HelpTooltip info className="space-left">
+                          {plugin.description}
+                        </HelpTooltip>
+                      )}
+                    </td>
+                    <td>
+                      {plugin.version}
+                      {this.renderLink(plugin)}
+                    </td>
+                    <td className="no-wrap" style={{ width: '10rem' }}>
+                      <CopyButton
+                        size="small"
+                        variant="contained"
+                        title={plugin.directory}
+                        content={plugin.directory}>
+                        Copy Path
+                      </CopyButton>{' '}
+                      <Button
+                        size="small"
+                        variant="contained"
+                        onClick={Plugins._handleOpenDirectory.bind(this, plugin.directory)}>
+                        Reveal Folder
+                      </Button>
+                    </td>
+                  </tr>
+                ),
+              )}
             </tbody>
           </table>
         )}
@@ -277,12 +296,8 @@ class Plugins extends React.PureComponent<Props, State> {
         <hr />
 
         <div className="text-right">
-          <Button onClick={this._handleCreatePlugin}>
-            Generate New Plugin
-          </Button>
-          <Button
-            className="space-left"
-            onClick={Plugins._handleClickShowPluginsFolder}>
+          <Button onClick={this._handleCreatePlugin}>Generate New Plugin</Button>
+          <Button className="space-left" onClick={Plugins._handleClickShowPluginsFolder}>
             Reveal Plugins Folder
           </Button>
           <Button
