02baa7633056cc12463446a09b50d8ea991cc42f
Opender Singh <opender.singh@konghq.com>
2020-05-06 10:28:48 +1200

    Update switch back to regular component

--- packages/insomnia-components/components/toggle-switch.js
+++ packages/insomnia-components/components/toggle-switch.js
@@ -16,36 +16,55 @@ const ThemedSwitch: React.ComponentType<{ checked: boolean }> = styled.div`
   }
 `;
 
-const ToggleSwitch = ({ className, checked: checkedProp, onChange, disabled }: Props) => {
-  const [checked, setChecked] = React.useState<boolean>(!!checkedProp);
-
-  // If prop changes and differs from state, update state
-  React.useEffect(() => {
-    setChecked(!!checkedProp);
-  }, [checkedProp]);
-
-  const callback = React.useCallback(
-    c => {
-      setChecked(c);
-      onChange(c);
-    },
-    [onChange],
-  );
-
-  return (
-    <ThemedSwitch checked={checked}>
-      <Switch
-        className={className}
-        checked={checked}
-        onColor="" // Linked to insomnia themes via ThemedSwitch
-        offColor="" // Linked to insomnia themes via ThemedSwitch
-        disabled={disabled}
-        onChange={callback}
-        height={20}
-        width={40}
-      />
-    </ThemedSwitch>
-  );
+type State = {
+  checked: boolean,
 };
+
+class ToggleSwitch extends React.PureComponent<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = { checked: props.checked || false };
+  }
+
+  setChecked(checked: boolean) {
+    this.setState({ checked });
+  }
+
+  componentWillReceiveProps(newProps: Props) {
+    if (newProps.checked === this.props.checked) {
+      return;
+    }
+
+    const newChecked = newProps.checked || false;
+
+    if (this.state.checked !== newChecked) {
+      this.setChecked(newChecked);
+    }
+  }
+
+  render() {
+    const { checked } = this.state;
+    const { className, disabled, onChange } = this.props;
+
+    return (
+      <ThemedSwitch>
+        <Switch
+          className={className}
+          checked={checked}
+          onColor="" // Linked to insomnia themes via ThemedSwitch
+          offColor="" // Linked to insomnia themes via ThemedSwitch
+          disabled={disabled}
+          onChange={c => {
+            this.setChecked(c);
+            onChange(c);
+          }}
+          height={20}
+          width={40}
+        />
+      </ThemedSwitch>
+    );
+  }
+}
 
 export default ToggleSwitch;
