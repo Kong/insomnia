eaf669df3ef78eb9d9388b0d9cdac495ae45f044
MikeEllanKong <mike.ellan@konghq.com>
2020-05-19 13:03:45 -0400

    component migration to new SB instance
    Migrating help tooltip to new SB instance at /insomnia-components/ also styling component directly via styled components

---
+++ packages/insomnia-components/components/help-tooltip.js
@@ -0,0 +1,29 @@
+// @flow
+import * as React from 'react';
+import autobind from 'autobind-decorator';
+import Tooltip from './tooltip';
+import SvgIcon from './svg-icon';
+
+type Props = {
+  children: React.Node,
+
+  // Optional
+  position?: string,
+  className?: string,
+  style?: Object,
+  info?: boolean,
+};
+
+@autobind
+class HelpTooltip extends React.PureComponent<Props> {
+  render() {
+    const { children, className, style, info } = this.props;
+    return (
+      <Tooltip position="top" className={className} message={children} style={style}>
+        {info ? <SvgIcon icon="info" /> : <SvgIcon icon="question" />}
+      </Tooltip>
+    );
+  }
+}
+
+export default HelpTooltip;
---
+++ packages/insomnia-components/components/help-tooltip.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HelpTooltip from './help-tooltip';
+
+export default { title: 'Helpers | Help Tooltip' };
+
+export const _default = () => (
+  <p>
+    Hover over the question mark to view <HelpTooltip info>This is the help message</HelpTooltip>
+  </p>
+);
