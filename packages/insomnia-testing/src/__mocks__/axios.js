1c859131b5ef3705f05bec16f89a90edb8553c37
Opender Singh <opender.singh@konghq.com>
2020-06-18 11:17:31 +1200

    Linting

--- packages/insomnia-testing/src/__mocks__/axios.js
+++ packages/insomnia-testing/src/__mocks__/axios.js
@@ -14,7 +14,11 @@ axios.request = async function({ method, url }) {
   const k = key(method, url);
   const resp = mockResponses[k];
   if (!resp) {
-    throw new Error(`Could not find mock axios response for ${k}. Options are [${Object.keys(mockResponses).join(', ')}]`);
+    throw new Error(
+      `Could not find mock axios response for ${k}. Options are [${Object.keys(mockResponses).join(
+        ', ',
+      )}]`,
+    );
   }
 
   return Promise.resolve(resp);
