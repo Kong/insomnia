410ca7f32ca029644bc5f0bfa40d4cd6529dc966
Opender Singh <opender.singh@konghq.com>
2020-06-24 17:17:28 +1200

    More tests

--- packages/insomnia-cli/src/__tests__/util.test.js
+++ packages/insomnia-cli/src/__tests__/util.test.js
@@ -1,6 +1,6 @@
 // @flow
 import commander from 'commander';
-import { getAllOptions } from '../util';
+import { getAllOptions, exit, logErrorExit1 } from '../util';
 
 describe('getAllOptions()', () => {
   it('should combine options from all commands into one object', () => {
@@ -19,5 +19,47 @@ describe('getAllOptions()', () => {
     const parent = new commander.Command().option('-g, --global').addCommand(command);
 
     parent.parse('node test command subCommand --global --subCmd'.split(' '));
+  });
+});
+
+describe('exit()', () => {
+  it('should exit 0 if successful result', async () => {
+    const exitSpy = jest.spyOn(process, 'exit').mockImplementation(() => {});
+
+    await exit(new Promise(resolve => resolve(true)));
+
+    expect(exitSpy).toHaveBeenCalledWith(0);
+  });
+
+  it('should exit 1 if unsuccessful result', async () => {
+    const exitSpy = jest.spyOn(process, 'exit').mockImplementation(() => {});
+
+    await exit(new Promise(resolve => resolve(false)));
+
+    expect(exitSpy).toHaveBeenCalledWith(1);
+  });
+
+  it('should exit 1 and print to console and if rejected', async () => {
+    const exitSpy = jest.spyOn(process, 'exit').mockImplementation(() => {});
+    const errorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+
+    const error = new Error('message');
+    await exit(new Promise((resolve, reject) => reject(error)));
+
+    expect(errorSpy).toHaveBeenCalledWith(error);
+    expect(exitSpy).toHaveBeenCalledWith(1);
+  });
+});
+
+describe('logErrorExit1()', () => {
+  it('should exit 1 and print error to console', async () => {
+    const exitSpy = jest.spyOn(process, 'exit').mockImplementation(() => {});
+    const errorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+
+    const error = new Error('message');
+    await logErrorExit1(error);
+
+    expect(errorSpy).toHaveBeenCalledWith(error);
+    expect(exitSpy).toHaveBeenCalledWith(1);
   });
 });
