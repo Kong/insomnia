ad8ba59e56c0ffa3d97d7c3eb3a1d35e4aa5eef2
Gregory Schier <gschier1990@gmail.com>
2020-06-25 11:19:48 -0700

    Remove tests tab from response pane

--- packages/insomnia-app/app/ui/components/response-pane.js
+++ packages/insomnia-app/app/ui/components/response-pane.js
@@ -31,7 +31,6 @@ import type { RequestVersion } from '../../models/request-version';
 import { showError } from '../components/modals/index';
 import { json as jsonPrettify } from 'insomnia-prettify';
 import type { Environment } from '../../models/environment';
-import TestResultsViewer from './unit-testing/TestResultsViewer';
 import type { UnitTestResult } from '../../models/unit-test-result';
 
 type Props = {
@@ -340,9 +339,6 @@ class ResponsePane extends React.PureComponent<Props> {
             <Tab tabIndex="-1">
               <Button>Timeline</Button>
             </Tab>
-            <Tab tabIndex="-1">
-              <Button>Tests</Button>
-            </Tab>
           </TabList>
           <TabPanel className="react-tabs__tab-panel">
             <ResponseViewer
@@ -390,17 +386,6 @@ class ResponsePane extends React.PureComponent<Props> {
             <ErrorBoundary key={response._id} errorClassName="font-error pad text-center">
               <ResponseTimelineViewer
                 response={response}
-                editorLineWrapping={editorLineWrapping}
-                editorFontSize={editorFontSize}
-                editorIndentSize={editorIndentSize}
-              />
-            </ErrorBoundary>
-          </TabPanel>
-          <TabPanel className="react-tabs__tab-panel">
-            <ErrorBoundary key={response._id} errorClassName="font-error pad text-center">
-              <TestResultsViewer
-                test={response.test}
-                testResults={response.testResults}
                 editorLineWrapping={editorLineWrapping}
                 editorFontSize={editorFontSize}
                 editorIndentSize={editorIndentSize}
