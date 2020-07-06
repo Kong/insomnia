25cefdc3e9751495dc2a548515f369247b4e2399
Opender Singh <opender.singh@konghq.com>
2020-07-07 10:15:12 +1200

    fix

--- packages/insomnia-inso/webpack/webpack.config.development.js
+++ packages/insomnia-inso/webpack/webpack.config.development.js
@@ -1,5 +1,5 @@
 const webpack = require('webpack');
-const merge = require('webpack-merge');
+const {merge} = require('webpack-merge');
 const base = require('./webpack.config.base');
 
 module.exports = merge(base, {
