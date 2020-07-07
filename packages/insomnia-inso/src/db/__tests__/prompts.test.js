ecbce682f44aa534085cf19ec1fe9d89a28f14ea
Opender Singh <opender.singh@konghq.com>
2020-07-07 20:05:02 +1200

    Update

--- packages/insomnia-inso/src/db/__tests__/prompts.test.js
+++ packages/insomnia-inso/src/db/__tests__/prompts.test.js
@@ -17,23 +17,29 @@ import enquirer from 'enquirer';
 
 jest.mock('enquirer');
 
-describe('prompts', () => {
+describe('Api Spec', () => {
   let db: Database = emptyDb();
+
+  const spec: $Shape<ApiSpec> = {
+    _id: 'spc_1234567890',
+    fileName: 'fileName',
+  };
+
   beforeEach(() => {
     db = emptyDb();
+
+    const dummySpec: $Shape<ApiSpec> = {
+      _id: 'spc_dummy',
+      fileName: 'dummy spec',
+    };
+
+    db.ApiSpec.push(spec);
+    db.ApiSpec.push(dummySpec);
+
     jest.clearAllMocks();
   });
 
   describe('promptApiSpec()', () => {
-    const spec: $Shape<ApiSpec> = {
-      _id: 'spc_1234567890',
-      fileName: 'fileName',
-    };
-
-    beforeEach(() => {
-      db.ApiSpec.push(spec);
-    });
-
     it('should return null if ci', () => {
       expect(promptApiSpec(db, true)).resolves.toBeNull();
     });
@@ -64,37 +70,71 @@ describe('prompts', () => {
     });
   });
 
-  describe('promptTestSuites()', () => {
-    const workspace: $Shape<Workspace> = {
-      _id: 'wrk_1234567890',
-      name: 'workspace name',
-    };
-
-    const spec: $Shape<ApiSpec> = {
-      _id: 'spc_1234567890',
-      fileName: 'spec name',
-      parentId: workspace._id,
-    };
-
-    const suite1: $Shape<UnitTestSuite> = {
-      _id: 'uts_1234567890',
-      name: 'suite one',
-      parentId: workspace._id,
-    };
-
-    const suite2: $Shape<UnitTestSuite> = {
-      _id: 'uts_987654321',
-      name: 'suite two',
-      parentId: workspace._id,
-    };
-
-    beforeEach(() => {
-      db.Workspace.push(workspace);
-      db.ApiSpec.push(spec);
-      db.UnitTestSuite.push(suite1);
-      db.UnitTestSuite.push(suite2);
+  describe('loadApiSpec()', () => {
+    it('should return null if spec not found', () => {
+      expect(loadApiSpec(db, 'not-found')).toBeNull();
     });
 
+    it.each([generateIdIsh(spec), spec._id, spec.fileName])(
+      'should return spec with identifier: %o',
+      identifier => {
+        expect(loadApiSpec(db, identifier)).toBe(spec);
+      },
+    );
+
+    it('should throw error if multiple specs matched', () => {
+      db.ApiSpec.push({ ...spec });
+
+      expect(() => loadApiSpec(db, spec._id)).toThrowError();
+    });
+  });
+});
+
+describe('Unit Test Suite', () => {
+  let db: Database = emptyDb();
+
+  const workspace: $Shape<Workspace> = {
+    _id: 'wrk_1234567890',
+    name: 'workspace name',
+  };
+
+  const spec: $Shape<ApiSpec> = {
+    _id: 'spc_1234567890',
+    fileName: 'spec name',
+    parentId: workspace._id,
+  };
+
+  const suite1: $Shape<UnitTestSuite> = {
+    _id: 'uts_1234567890',
+    name: 'suite one',
+    parentId: workspace._id,
+  };
+
+  const suite2: $Shape<UnitTestSuite> = {
+    _id: 'uts_987654321',
+    name: 'suite two',
+    parentId: workspace._id,
+  };
+
+  beforeEach(() => {
+    db = emptyDb();
+
+    const dummySuite: $Shape<UnitTestSuite> = {
+      _id: 'uts_dummy',
+      name: 'dummy suite',
+      parentId: 'dummy parent',
+    };
+
+    db.Workspace.push(workspace);
+    db.ApiSpec.push(spec);
+    db.UnitTestSuite.push(suite1);
+    db.UnitTestSuite.push(suite2);
+    db.UnitTestSuite.push(dummySuite);
+
+    jest.clearAllMocks();
+  });
+
+  describe('promptTestSuites()', () => {
     it('should return empty array if ci', () => {
       expect(promptTestSuites(db, true)).resolves.toHaveLength(0);
     });
@@ -126,156 +166,7 @@ describe('prompts', () => {
     });
   });
 
-  describe('promptEnvironment()', () => {
-    const workspace: $Shape<Workspace> = {
-      _id: 'wrk_1234567890',
-      name: 'workspace name',
-    };
-
-    const environment: $Shape<Environment> = {
-      _id: 'env_1234567890',
-      name: 'base env',
-      parentId: workspace._id,
-    };
-
-    const subEnvironment: $Shape<Environment> = {
-      _id: 'env_env_1234567890',
-      name: 'sub env',
-      parentId: environment._id,
-    };
-
-    beforeEach(() => {
-      db.Workspace.push(workspace);
-      db.Environment.push(environment);
-      db.Environment.push(subEnvironment);
-    });
-
-    it('should return null if ci', () => {
-      expect(promptEnvironment(db, true, workspace._id)).resolves.toBeNull();
-    });
-
-    it('should return null if no environments exist', () => {
-      db.Environment = [];
-
-      expect(promptEnvironment(db, false, workspace._id)).resolves.toBeNull();
-    });
-
-    it('should throw error if base env for workspace not found', () => {
-      expect(promptEnvironment(db, false, 'workspace-not-found')).rejects.toThrowError();
-    });
-
-    it('should throw error if multiple base env for workspace found', () => {
-      db.Environment.push({ ...environment });
-      expect(promptEnvironment(db, false, workspace._id)).rejects.toThrowError();
-    });
-
-    it('should load sub environment after prompt result', async () => {
-      enquirer.__mockPromptRun('environment - env_env_123456');
-
-      expect(promptEnvironment(db, false, workspace._id)).resolves.toBe(subEnvironment);
-    });
-
-    it('should match snapshot of autocomplete config', async () => {
-      await promptEnvironment(db, false, workspace._id);
-
-      expect(enquirer.__constructorMock.mock.calls[0][0]).toMatchSnapshot();
-    });
-  });
-});
-
-describe('loaders', () => {
-  let db: Database = emptyDb();
-  beforeEach(() => {
-    db = emptyDb();
-  });
-
-  describe('loadApiSpec()', () => {
-    const spec: $Shape<ApiSpec> = {
-      _id: 'spc_1234567890',
-      fileName: 'fileName',
-    };
-
-    beforeEach(() => {
-      db.ApiSpec.push(spec);
-    });
-
-    it('should return null if spec not found', () => {
-      expect(loadApiSpec(db, 'not-found')).toBeNull();
-    });
-
-    it.each([generateIdIsh(spec), spec._id, spec.fileName])(
-      'should return spec with identifier: %o',
-      identifier => {
-        expect(loadApiSpec(db, identifier)).toBe(spec);
-      },
-    );
-
-    it('should throw error if multiple specs matched', () => {
-      db.ApiSpec.push({ ...spec });
-
-      expect(() => loadApiSpec(db, spec._id)).toThrowError();
-    });
-  });
-
-  describe('loadWorkspace()', () => {
-    const workspace: $Shape<Workspace> = {
-      _id: 'wrk_1234567890',
-      name: 'workspace name',
-    };
-
-    beforeEach(() => {
-      db.Workspace.push(workspace);
-    });
-
-    it('should return null if workspace not found', () => {
-      expect(loadWorkspace(db, 'not-found')).toBeNull();
-    });
-
-    it.each([generateIdIsh(workspace), workspace._id, workspace.name])(
-      'should return workspace with identifier: %o',
-      identifier => {
-        expect(loadWorkspace(db, identifier)).toBe(workspace);
-      },
-    );
-
-    it('should throw error if multiple workspace matched', () => {
-      db.Workspace.push({ ...workspace });
-
-      expect(() => loadWorkspace(db, workspace._id)).toThrowError();
-    });
-  });
-
   describe('loadTestSuites()', () => {
-    const workspace: $Shape<Workspace> = {
-      _id: 'wrk_1234567890',
-      name: 'workspace name',
-    };
-
-    const spec: $Shape<ApiSpec> = {
-      _id: 'spc_1234567890',
-      fileName: 'spec name',
-      parentId: workspace._id,
-    };
-
-    const suite1: $Shape<UnitTestSuite> = {
-      _id: 'uts_1234567890',
-      name: 'suite one',
-      parentId: workspace._id,
-    };
-
-    const suite2: $Shape<UnitTestSuite> = {
-      _id: 'uts_987654321',
-      name: 'suite two',
-      parentId: workspace._id,
-    };
-
-    beforeEach(() => {
-      db.Workspace.push(workspace);
-      db.ApiSpec.push(spec);
-      db.UnitTestSuite.push(suite1);
-      db.UnitTestSuite.push(suite2);
-    });
-
     it('should return empty array if no suites matched', () => {
       expect(loadTestSuites(db, 'not-found')).toHaveLength(0);
     });
@@ -309,31 +200,79 @@ describe('loaders', () => {
       },
     );
   });
+});
 
-  describe('loadEnvironment()', () => {
-    const workspace: $Shape<Workspace> = {
-      _id: 'wrk_1234567890',
-      name: 'workspace name',
-    };
+describe('Environment', () => {
+  let db: Database = emptyDb();
 
-    const environment: $Shape<Environment> = {
-      _id: 'env_1234567890',
-      name: 'base env',
-      parentId: workspace._id,
-    };
+  const workspace: $Shape<Workspace> = {
+    _id: 'wrk_1234567890',
+    name: 'workspace name',
+  };
 
-    const subEnvironment: $Shape<Environment> = {
-      _id: 'env_env_1234567890',
-      name: 'sub env',
+  const environment: $Shape<Environment> = {
+    _id: 'env_1234567890',
+    name: 'base env',
+    parentId: workspace._id,
+  };
+
+  const subEnvironment: $Shape<Environment> = {
+    _id: 'env_env_1234567890',
+    name: 'sub env',
+    parentId: environment._id,
+  };
+
+  beforeEach(() => {
+    db = emptyDb();
+
+    const dummySubEnv: $Shape<Environment> = {
+      _id: 'env_env_dummy',
+      name: 'dummy sub env',
       parentId: environment._id,
     };
 
-    beforeEach(() => {
-      db.Workspace.push(workspace);
-      db.Environment.push(environment);
-      db.Environment.push(subEnvironment);
+    db.Workspace.push(workspace);
+    db.Environment.push(environment);
+    db.Environment.push(subEnvironment);
+    db.Environment.push(dummySubEnv);
+
+    jest.clearAllMocks();
+  });
+
+  describe('promptEnvironment()', () => {
+    it('should return null if ci', () => {
+      expect(promptEnvironment(db, true, workspace._id)).resolves.toBeNull();
     });
 
+    it('should return null if no environments exist', () => {
+      db.Environment = [];
+
+      expect(promptEnvironment(db, false, workspace._id)).resolves.toBeNull();
+    });
+
+    it('should throw error if base env for workspace not found', () => {
+      expect(promptEnvironment(db, false, 'workspace-not-found')).rejects.toThrowError();
+    });
+
+    it('should throw error if multiple base env for workspace found', () => {
+      db.Environment.push({ ...environment });
+      expect(promptEnvironment(db, false, workspace._id)).rejects.toThrowError();
+    });
+
+    it('should load sub environment after prompt result', async () => {
+      enquirer.__mockPromptRun('environment - env_env_123456');
+
+      expect(promptEnvironment(db, false, workspace._id)).resolves.toBe(subEnvironment);
+    });
+
+    it('should match snapshot of autocomplete config', async () => {
+      await promptEnvironment(db, false, workspace._id);
+
+      expect(enquirer.__constructorMock.mock.calls[0][0]).toMatchSnapshot();
+    });
+  });
+
+  describe('loadEnvironment()', () => {
     it('should return null if no environments exist', () => {
       db.Environment = [];
 
@@ -363,6 +302,48 @@ describe('loaders', () => {
     it('should return the base environment if no sub envs exist', () => {
       db.Environment = [environment];
       expect(loadEnvironment(db, workspace._id, subEnvironment._id)).toBe(environment);
+    });
+  });
+});
+
+describe('Workspace', () => {
+  let db: Database = emptyDb();
+
+  const workspace: $Shape<Workspace> = {
+    _id: 'wrk_1234567890',
+    name: 'workspace name',
+  };
+
+  beforeEach(() => {
+    db = emptyDb();
+
+    const dummyWorkspace: $Shape<Workspace> = {
+      _id: 'wrk_dummy',
+      name: 'dummy workspace',
+    };
+
+    db.Workspace.push(workspace);
+    db.Workspace.push(dummyWorkspace);
+
+    jest.clearAllMocks();
+  });
+
+  describe('loadWorkspace()', () => {
+    it('should return null if workspace not found', () => {
+      expect(loadWorkspace(db, 'not-found')).toBeNull();
+    });
+
+    it.each([generateIdIsh(workspace), workspace._id, workspace.name])(
+      'should return workspace with identifier: %o',
+      identifier => {
+        expect(loadWorkspace(db, identifier)).toBe(workspace);
+      },
+    );
+
+    it('should throw error if multiple workspace matched', () => {
+      db.Workspace.push({ ...workspace });
+
+      expect(() => loadWorkspace(db, workspace._id)).toThrowError();
     });
   });
 });
