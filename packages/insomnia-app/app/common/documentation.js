653c546883135e922bee18493eacdd85230231d5
Opender Singh <opender94@gmail.com>
2020-08-18 08:57:21 +1200

    Add bitbucket cloud docs, add documentation tooltip to git settings modal header (#2518)

--- packages/insomnia-app/app/common/documentation.js
+++ packages/insomnia-app/app/common/documentation.js
@@ -5,3 +5,11 @@ function insomniaDocs(slug: string): string {
 
 export const docsBase = insomniaDocs('/');
 export const docsGitSync = insomniaDocs('/article/96-git-sync');
+export const docsGitAccessToken = {
+  github:
+    'https://docs.github.com/github/authenticating-to-github/creating-a-personal-access-token',
+  gitlab: 'https://docs.gitlab.com/ee/user/profile/personal_access_tokens.html',
+  bitbucket: 'https://support.atlassian.com/bitbucket-cloud/docs/app-passwords/',
+  bitbucketServer:
+    'https://confluence.atlassian.com/bitbucketserver/personal-access-tokens-939515499.html',
+};
