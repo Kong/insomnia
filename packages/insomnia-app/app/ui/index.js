ff42d8e55919fd1e9a40d2836a8f0e5d40793f7b
Opender Singh <opender.singh@konghq.com>
2020-08-20 11:55:21 +1200

    add comments

--- packages/insomnia-app/app/ui/index.js
+++ packages/insomnia-app/app/ui/index.js
@@ -77,6 +77,10 @@ if (isDevelopment()) {
   window.db = db;
 }
 
+// Styled components is added to the window object here, for plugins to use.
+// UI plugins built with webpack (such as insomnia-plugin-kong-portal) define styled-components as an external resolved
+// from the window object. This is to ensure there is only one instance of styled-components on the page.
+// Because styled-components are loaded at runtime, they don't have direct access to modules in the electron bundle
 window['styled-components'] = styledComponents;
 
 // Catch uncaught errors and report them
