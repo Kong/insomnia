1c6d674eaed881ea7567a3b8aeb921fa61f917be
Opender Singh <opender94@gmail.com>
2020-08-26 16:58:27 +1200

    Update check-version.js

--- scripts/check-version.js
+++ scripts/check-version.js
@@ -1,12 +1,12 @@
 // const fs = require('fs');
 
 const foundNodeVersion = process.versions.node;
-// const expectedNodeVersion = fs
+const expectedNodeVersion = '12.8.0' //fs
 //   .readFileSync('.nvmrc')
 //   .toString()
 //   .trim();
 
-if (foundNodeVersion !== '12.8.0') {
+if (foundNodeVersion !== expectedNodeVersion) {
   console.log(`Current node version: ${foundNodeVersion}`);
   console.log(`Expected node version: ${expectedNodeVersion}`);
   console.log(
