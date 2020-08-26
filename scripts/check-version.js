27184c4f2d1483f7547c7d1a822d677fe935e3f7
Yash Shah <yashah1234@gmail.com>
2020-08-26 20:31:29 +0530

    fixes #2552:addded a (slightly) more verbose output in check-version.js (#2559)
    * fixes #2552:
    * updated version script to show more verbose errors in case of a mismatch node version
    
    * added suggested changes
    * No output is printed if the node version is aaccurate
    * changed colours for the messages to make them more prominent

--- scripts/check-version.js
+++ scripts/check-version.js
@@ -1,5 +1,12 @@
 const fs = require('fs');
 
+const GREEN_FG = '\x1b[32m';
+const RED_FG = '\x1b[31m';
+const YELLOW_FG = '\x1b[33m';
+const UNDERSCORE = '\x1b[4m';
+const BRIGHT = '\x1b[1m';
+const RESET = '\x1b[0m';
+
 const foundNodeVersion = process.versions.node;
 const expectedNodeVersion = fs
   .readFileSync('.nvmrc')
@@ -7,10 +14,16 @@ const expectedNodeVersion = fs
   .trim();
 
 if (foundNodeVersion !== expectedNodeVersion) {
-  console.log(`Current node version: ${foundNodeVersion}`);
-  console.log(`Expected node version: ${expectedNodeVersion}`);
+  console.log(`${BRIGHT}${RED_FG}Incorrect node version installed ...${RESET}\n`);
+  console.log(`Current node version -> ${RED_FG}${foundNodeVersion}${RESET}`);
+  console.log(`Expected node version -> ${GREEN_FG}${expectedNodeVersion}${RESET}`);
+  console.log();
   console.log(
-    `Remember to 'npm run clean && npm run bootstrap' after installing the expected version.`,
+    `One solution to manage multiple versions of node is nvm, visit ${UNDERSCORE}https://github.com/nvm-sh/nvm${RESET}`,
+  );
+  console.log();
+  console.log(
+    `${BRIGHT}${YELLOW_FG}Remember to 'npm run clean && npm run bootstrap' after installing the expected version.${RESET}`,
   );
   console.log();
   process.exit(1);
