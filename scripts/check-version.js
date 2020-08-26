2de45477166fc1ab126259125bab0a27a34ec31e
Yash Shah <yashah1234@gmail.com>
2020-08-26 12:10:38 +0530

    fixes #2552: * updated version script to show more verbose errors in case of a mismatch node version

--- scripts/check-version.js
+++ scripts/check-version.js
@@ -1,16 +1,28 @@
 const fs = require('fs');
 
+const GREEN_FG = '\x1b[32m';
+const RED_FG = '\x1b[31m';
+const UNDERSCORE = '\x1b[4m';
+const BRIGHT = '\x1b[1m';
+const RESET = '\x1b[0m';
+
 const foundNodeVersion = process.versions.node;
 const expectedNodeVersion = fs
   .readFileSync('.nvmrc')
   .toString()
   .trim();
 
+console.log('Checking the current node version ...\n');
 if (foundNodeVersion !== expectedNodeVersion) {
-  console.log(`Current node version: ${foundNodeVersion}`);
-  console.log(`Expected node version: ${expectedNodeVersion}`);
+  console.log(`Current node version -> ${RED_FG}${foundNodeVersion}${RESET}`);
+  console.log(`Expected node version -> ${GREEN_FG}${expectedNodeVersion}${RESET}`);
+  console.log();
   console.log(
-    `Remember to 'npm run clean && npm run bootstrap' after installing the expected version.`,
+    `One solution to manage multiple versions of node is nvm, visit ${UNDERSCORE}https://github.com/nvm-sh/nvm${RESET}`,
+  );
+  console.log();
+  console.log(
+    `${BRIGHT}Remember to 'npm run clean && npm run bootstrap' after installing the expected version.${RESET}`,
   );
   console.log();
   process.exit(1);
