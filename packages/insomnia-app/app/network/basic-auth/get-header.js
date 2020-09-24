9d4390de2f3940fb643fc5409855917b5af90535
Julien Giovaresco <dev@giovaresco.fr>
2020-09-24 22:58:28 +0200

    Introduce a checkbox in Basic Auth form to choose ISO-8859-1 encoding for credentials. (#2642)
    Co-authored-by: Opender Singh <opender94@gmail.com>

--- packages/insomnia-app/app/network/basic-auth/get-header.js
+++ packages/insomnia-app/app/network/basic-auth/get-header.js
@@ -2,10 +2,14 @@
 
 import type { RequestHeader } from '../../models/request';
 
-export function getBasicAuthHeader(username: ?string, password: ?string): RequestHeader {
+export function getBasicAuthHeader(
+  username: ?string,
+  password: ?string,
+  encoding: ?string = 'utf8',
+): RequestHeader {
   const name = 'Authorization';
   const header = `${username || ''}:${password || ''}`;
-  const authString = Buffer.from(header, 'utf8').toString('base64');
+  const authString = Buffer.from(header, encoding).toString('base64');
   const value = `Basic ${authString}`;
   return { name, value };
 }
