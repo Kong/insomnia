bc12ddc2dc4adb33ffaf8f2013cd664b358b917c
johnhorsema <jcema@connect.ust.hk>
2020-10-16 10:48:33 +0800

    Fix tests again.

--- packages/insomnia-app/app/ui/components/editors/__tests__/environment-editor.test.js
+++ packages/insomnia-app/app/ui/components/editors/__tests__/environment-editor.test.js
@@ -1,6 +1,7 @@
 // @flow
 import { ensureKeyIsValid } from '../environment-editor';
 import { NUNJUCKS_TEMPLATE_GLOBAL_PROPERTY_NAME } from '../../../../templating';
+const name = NUNJUCKS_TEMPLATE_GLOBAL_PROPERTY_NAME;
 
 describe('ensureNestedKeyIsValid', () => {
   it.each(['$', '$a', '$ab'])('%s should be invalid when key begins with $', key => {
@@ -13,12 +14,6 @@ describe('ensureNestedKeyIsValid', () => {
 
   it.each(['$a.b', '$.'])('%s should be invalid when key starts with $ and contains .', key => {
     expect(ensureKeyIsValid(key)).toBe(`"${key}" cannot begin with '$' or contain a '.'`);
-  });
-
-  const name = NUNJUCKS_TEMPLATE_GLOBAL_PROPERTY_NAME;
-
-  it(`${name} as a key should be invalid`, () => {
-    expect(ensureKeyIsValid(name)).toBe(`"${name}" is a reserved key`);
   });
 
   it.each(['a', 'ab', 'a$', 'a$b', 'a-b', `a${name}b`, `${name}ab`])('%s should be valid', key => {
