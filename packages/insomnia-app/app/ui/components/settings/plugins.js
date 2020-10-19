5abeb559cbb79b6975218159d47d9ed37bdb379d
Martin Pastore <martinpablopastore@gmail.com>
2020-10-19 08:41:32 -0300

    fix: disable link when plugin has not modules

--- packages/insomnia-app/app/ui/components/settings/plugins.js
+++ packages/insomnia-app/app/ui/components/settings/plugins.js
@@ -197,9 +197,10 @@ class Plugins extends React.PureComponent<Props, State> {
 
     const base = /^insomnia-plugin-/.test(name) ? PLUGIN_HUB_BASE : NPM_PACKAGE_BASE;
     const link = path.join(base, name);
+    const disabled = Object.keys(plugin.module).length === 0;
 
     return (
-      <a className="space-left" href={link} title={link}>
+      <a className={`space-left ${disabled ? 'disabled' : ''}`} href={link} title={link}>
         <i className="fa fa-external-link-square" />
       </a>
     );
