8870e079784c44597a6d44d28da28da9ae2e2faf
Joe Mirizio <joe@joemirizio.com>
2020-10-23 23:58:19 -0400

    Add specification linting for files

--- packages/insomnia-inso/src/commands/__tests__/lint-specification.test.js
+++ packages/insomnia-inso/src/commands/__tests__/lint-specification.test.js
@@ -1,4 +1,5 @@
 // @flow
+import path from 'path';
 import { lintSpecification } from '../lint-specification';
 import { globalBeforeAll, globalBeforeEach } from '../../../__jest__/before';
 import logger from '../../logger';
@@ -23,6 +24,38 @@ describe('lint specification', () => {
     expect(result).toBe(true);
   });
 
+  it('should lint specification from file with relative path', async () => {
+    const result = await lintSpecification(
+      '.insomnia/ApiSpec/spc_46c5a4a40e83445a9bd9d9758b86c16c.yml',
+      {
+        workingDir: 'src/db/__fixtures__/git-repo',
+      },
+    );
+
+    expect(result).toBe(true);
+  });
+
+  it('should lint specification from file with relative path and no working directory', async () => {
+    const result = await lintSpecification(
+      'src/db/__fixtures__/git-repo/.insomnia/ApiSpec/spc_46c5a4a40e83445a9bd9d9758b86c16c.yml',
+      {},
+    );
+
+    expect(result).toBe(true);
+  });
+
+  it('should lint specification from file with absolute path', async () => {
+    const directory = path.join(process.cwd(), '/src/db/__fixtures__/git-repo/');
+    const result = await lintSpecification(
+      path.join(directory, '.insomnia/ApiSpec/spc_46c5a4a40e83445a9bd9d9758b86c16c.yml'),
+      {
+        workingDir: 'src/db/__fixtures__/git-repo',
+      },
+    );
+
+    expect(result).toBe(true);
+  });
+
   it('should return false for linting failed', async () => {
     const result = await lintSpecification('spc_46c5a4a40e83445a9bd9d9758b86c16c', {
       workingDir: 'src/db/__fixtures__/git-repo-malformed-spec',
@@ -39,5 +72,15 @@ describe('lint specification', () => {
     expect(result).toBe(false);
     const logs = logger.__getLogs();
     expect(logs.fatal).toContain('Specification not found.');
+  });
+
+  it('should return false if spec was not specified', async () => {
+    const result = await lintSpecification('', {
+      workingDir: 'src/db/__fixtures__/git-repo',
+    });
+
+    expect(result).toBe(false);
+    const logs = logger.__getLogs();
+    expect(logs.fatal).toContain('Invalid specification.');
   });
 });
