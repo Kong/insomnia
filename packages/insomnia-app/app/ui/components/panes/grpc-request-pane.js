2ee8dccd9923eb69582f5f421395b1ff945482ad
Opender Singh <opender.singh@konghq.com>
2020-11-02 15:45:16 +1300

    refactor: update grpc-request-pane and delete the old one

--- packages/insomnia-app/app/ui/components/panes/grpc-request-pane.js
+++ packages/insomnia-app/app/ui/components/panes/grpc-request-pane.js
@@ -1,7 +1,24 @@
 // @flow
 import React from 'react';
-import BlankPane from './blank-pane';
+import { Pane, PaneBody, PaneHeader } from './pane';
+import { Button } from 'insomnia-components';
+import * as grpc from '../../../network/grpc';
+import type { Request } from '../../../models/request';
+import type { GrpcRequest } from '../../../models/grpc-request';
 
-const GrpcRequestPane = () => <BlankPane type="request" />;
+type Props = {
+  activeRequest: Request | GrpcRequest,
+};
+
+const GrpcRequestPane = ({ activeRequest }: Props) => {
+  return (
+    <Pane type="request">
+      <PaneHeader>
+        <Button onClick={() => grpc.sendUnary(activeRequest._id)}>send unary</Button>
+      </PaneHeader>
+      <PaneBody />
+    </Pane>
+  );
+};
 
 export default GrpcRequestPane;
