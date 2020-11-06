3a17138a86230f610ca1f9072074eeedbe755d9e
Opender Singh <opender.singh@konghq.com>
2020-11-06 17:13:56 +1300

    feat: protoServiceName field is no longer needed

--- packages/insomnia-app/app/network/grpc/proto-loader.js
+++ packages/insomnia-app/app/network/grpc/proto-loader.js
@@ -17,11 +17,12 @@ const isTypeOrEnumDefinition = (obj: Object) => 'format' in obj; // same check e
 const isServiceDefinition = (obj: Object) => !isTypeOrEnumDefinition(obj);
 
 // TODO: instead of writing to a temp file and loading the protoFile every time methods are required,
-//  add an in-memory caching strategy, indexed by the protoFile._id.
-//  The file path for protoLoader.load can also be a URL, so we can avoid
+//  add an in-memory caching strategy, indexed by the protoFile._id - INS-272
+
+// TODO: The file path for protoLoader.load can also be a URL, so we can avoid
 //  writing to a file in those cases, but it becomes more important to cache
 //  We also need to think about how to store a reference to a proto file and it's
-//  implications on import/export/sync.
+//  implications on import/export/sync - INS-271
 export const loadMethods = async (protoFile: ProtoFile): Promise<Array<GrpcMethodDefinition>> => {
   const tempProtoFile = await writeProtoFile(protoFile.protoText);
   const definition = await protoLoader.load(tempProtoFile, GRPC_LOADER_OPTIONS);
