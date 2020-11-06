190411fa487b9e4b176d2bdc0cc8d2fae4434e80
Opender Singh <opender.singh@konghq.com>
2020-11-06 18:08:15 +1300

    test: fix ipc-grpc tests

--- packages/insomnia-app/app/network/grpc/__mocks__/index.js
+++ packages/insomnia-app/app/network/grpc/__mocks__/index.js
@@ -1,6 +1,8 @@
 module.exports = {
   sendUnary: jest.fn(),
   startClientStreaming: jest.fn(),
+  startServerStreaming: jest.fn(),
+  startBidiStreaming: jest.fn(),
   sendMessage: jest.fn(),
   commit: jest.fn(),
   cancel: jest.fn(),
