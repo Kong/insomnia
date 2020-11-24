6d259a4e73973f7355df7fc24f5f383ed0c6c226
Julien Giovaresco <dev@giovaresco.fr>
2020-11-24 02:33:16 +0100

    Fix Copy as CURL with Multipart (#2740)
    The issue was located in the `exportHar` function. It was not handled `file` param correctly.
    For this type of file, the `value` attribute is empty, therefore we should take instead the `fileName`
    
    Closes #2282

--- packages/insomnia-app/app/common/__tests__/har.test.js
+++ packages/insomnia-app/app/common/__tests__/har.test.js
@@ -372,4 +372,58 @@ describe('exportHarWithRequest()', () => {
       settingEncodeUrl: true,
     });
   });
+
+  it('export multipart request with file', async () => {
+    const workspace = await models.workspace.create();
+    const request = Object.assign(models.request.init(), {
+      _id: 'req_123',
+      parentId: workspace._id,
+      headers: [{ name: 'Content-Type', value: 'multipart/form-data' }],
+      parameters: [],
+      method: 'POST',
+      body: {
+        mimeType: 'multipart/form-data',
+        params: [
+          { name: 'a_file', value: '', fileName: '/tmp/my_file', type: 'file' },
+          { name: 'a_simple_field', value: 'a_simple_value' },
+          { name: 'a_second_file', value: '', fileName: '/tmp/my_file_2', type: 'file' },
+        ],
+      },
+      url: 'http://example.com/post',
+      authentication: {},
+    });
+
+    const renderedRequest = await render.getRenderedRequest(request);
+    const har = await harUtils.exportHarWithRequest(renderedRequest);
+
+    expect(har).toEqual({
+      bodySize: -1,
+      cookies: [],
+      headers: [{ name: 'Content-Type', value: 'multipart/form-data' }],
+      headersSize: -1,
+      httpVersion: 'HTTP/1.1',
+      method: 'POST',
+      postData: {
+        mimeType: 'multipart/form-data',
+        params: [
+          {
+            name: 'a_file',
+            fileName: '/tmp/my_file',
+          },
+          {
+            name: 'a_simple_field',
+            value: 'a_simple_value',
+          },
+          {
+            name: 'a_second_file',
+            fileName: '/tmp/my_file_2',
+          },
+        ],
+        text: '',
+      },
+      queryString: [],
+      url: 'http://example.com/post',
+      settingEncodeUrl: true,
+    });
+  });
 });
