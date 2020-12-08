00b63194eddd9de8fcd20819dd7db0e2eb8c8bbc
Opender Singh <opender.singh@konghq.com>
2020-12-09 11:09:42 +1300

    Revert "feat: update the grpc method tag to be a pill"
    This reverts commit d27f5b7e2f396965c312e752bba53d56fb12c388.

--- packages/insomnia-app/app/ui/components/tags/grpc-method-tag.js
+++ packages/insomnia-app/app/ui/components/tags/grpc-method-tag.js
@@ -12,17 +12,14 @@ type Props = {
 
 const StyledTag = styled.div`
   width: 1.5em;
-  text-align: center;
-  background: #2fa3a3;
-  color: white;
-  border-radius: 3px;
-  border: 1px solid #267d7d !important;
-  padding: 0 var(--padding-xs);
+  text-align: right;
 `;
 
 const GrpcMethodTag = ({ methodType }: Props) => (
   <Tooltip message={GrpcMethodTypeName[methodType]} position="left" delay={500}>
-    <StyledTag>{GrpcMethodTypeAcronym[methodType]}</StyledTag>
+    <StyledTag>
+      <em>{GrpcMethodTypeAcronym[methodType]}</em>
+    </StyledTag>
   </Tooltip>
 );
 
