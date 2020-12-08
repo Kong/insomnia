d27f5b7e2f396965c312e752bba53d56fb12c388
Opender Singh <opender.singh@konghq.com>
2020-12-08 14:08:12 +1300

    feat: update the grpc method tag to be a pill

--- packages/insomnia-app/app/ui/components/tags/grpc-method-tag.js
+++ packages/insomnia-app/app/ui/components/tags/grpc-method-tag.js
@@ -12,14 +12,17 @@ type Props = {
 
 const StyledTag = styled.div`
   width: 1.5em;
-  text-align: right;
+  text-align: center;
+  background: #2fa3a3;
+  color: white;
+  border-radius: 3px;
+  border: 1px solid #267d7d !important;
+  padding: 0 var(--padding-xs);
 `;
 
 const GrpcMethodTag = ({ methodType }: Props) => (
   <Tooltip message={GrpcMethodTypeName[methodType]} position="left" delay={500}>
-    <StyledTag>
-      <em>{GrpcMethodTypeAcronym[methodType]}</em>
-    </StyledTag>
+    <StyledTag>{GrpcMethodTypeAcronym[methodType]}</StyledTag>
   </Tooltip>
 );
 
