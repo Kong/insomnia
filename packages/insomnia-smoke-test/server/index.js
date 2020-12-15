cddd887ec8be7d15dc34114ca3bcc42e83ca5fb1
Opender Singh <opender.singh@konghq.com>
2020-12-16 12:18:09 +1300

    feat: serve csv and pdf files locally

--- packages/insomnia-smoke-test/server/index.js
+++ packages/insomnia-smoke-test/server/index.js
@@ -6,7 +6,7 @@ const app = express();
 const basicAuthRouter = express.Router();
 const port = 4010;
 
-// Artificallly slow each request down
+// Artificially slow each request down
 app.use((req, res, next) => {
   setTimeout(next, 500);
 });
@@ -15,11 +15,10 @@ app.get('/pets/:id', (req, res) => {
   res.status(200).send({ id: req.params.id });
 });
 
-app.get('/csv', (_, res) => {
-  res
-    .status(200)
-    .header('content-type', 'text/csv')
-    .send(`a,b,c\n1,2,3`);
+const options = { root: 'fixtures' };
+
+app.get('/file/:fileName', (req, res) => {
+  res.sendFile(req.params.fileName, options);
 });
 
 const { utf8, latin1 } = basicAuthCreds;
@@ -29,11 +28,7 @@ const users = {
   [latin1.encoded.user]: latin1.encoded.pass,
 };
 
-basicAuthRouter.use(
-  basicAuth({
-    users,
-  }),
-);
+basicAuthRouter.use(basicAuth({ users }));
 
 basicAuthRouter.get('/', (_, res) => {
   res
