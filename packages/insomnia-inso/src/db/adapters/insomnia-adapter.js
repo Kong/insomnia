393eaf4c536afd4d8ace6811660ca9c9d0b49ccf
Giuseppe Baittiner <baittiner.giuseppe.dev@gmail.com>
2021-02-09 08:10:35 +0100

    Fix object representation for send-request package

--- packages/insomnia-inso/src/db/adapters/insomnia-adapter.js
+++ packages/insomnia-inso/src/db/adapters/insomnia-adapter.js
@@ -2,7 +2,18 @@
 import fs from 'fs';
 import { emptyDb } from '../index';
 import type { Database, DbAdapter } from '../index';
-import type { ApiSpec } from '../models/types';
+import type { BaseModel } from '../models/types';
+import YAML from 'yaml';
+
+const keyToType = {
+  api_spec: 'ApiSpec',
+  environment: 'Environment',
+  request: 'Request',
+  request_group: 'RequestGroup',
+  workspace: 'Workspace',
+  unit_test_suite: 'UnitTestSuite',
+  unit_test: 'UnitTest',
+};
 
 const insomniaAdapter: DbAdapter = async path => {
   // Sanity check - do db files exist?
@@ -11,27 +22,51 @@ const insomniaAdapter: DbAdapter = async path => {
   const db: Database = emptyDb();
 
   // Now, reading and parsing
-  const contents = await fs.promises.readFile(path);
-  const object = JSON.parse(contents);
+  const content = await fs.promises.readFile(path, { encoding: 'utf-8' });
+  const object = YAML.parse(content);
   // We are supporting only v4 files
   if (object.__export_format !== 4) return null;
   const raw = object?.resources;
 
   if (raw?.length !== 0) {
-    db.ApiSpec.push(...extract(raw, 'api_spec'));
-    db.Environment.push(...extract(raw, 'environment'));
-    db.Request.push(...extract(raw, 'request'));
-    db.RequestGroup.push(...extract(raw, 'request_group'));
-    db.Workspace.push(...extract(raw, 'workspace'));
-    db.UnitTestSuite.push(...extract(raw, 'unit_test_suite'));
-    db.UnitTest.push(...extract(raw, 'unit_test'));
+    db.ApiSpec.push(...read(raw, 'api_spec'));
+    db.Environment.push(...read(raw, 'environment'));
+    db.Request.push(...read(raw, 'request'));
+    db.RequestGroup.push(...read(raw, 'request_group'));
+    db.Workspace.push(...read(raw, 'workspace'));
+    db.UnitTestSuite.push(...read(raw, 'unit_test_suite'));
+    db.UnitTest.push(...read(raw, 'unit_test'));
   }
 
   return db;
 };
 
-const extract: Array<ApiSpec> = (raw, _type) => {
-  return raw.filter(e => e?._type === _type);
+const read: Array<BaseModel> = (raw, _type) => {
+  // Extract fields of the same types
+  const extracted = extract(raw, _type);
+  // Renaming from ._type to .type
+  const renamed = rename(extracted, _type);
+  // Now replacing with the correct type
+  return replace(renamed, _type);
+};
+
+const replace: Array<BaseModel> = (raw, _type) => {
+  return raw.map(item => {
+    item.type = keyToType[_type];
+    return item;
+  });
+};
+
+const rename: BaseModel = arr => {
+  return arr.map(obj => {
+    obj.type = obj._type;
+    delete obj._type;
+    return obj;
+  });
+};
+
+const extract: Array<BaseModel> = (raw, _type) => {
+  return raw.filter(item => item?._type === _type);
 };
 
 export default insomniaAdapter;
