b8926f8f5268fbbbc90e98e7780d5e7cbcdecef5
Opender Singh <opender.singh@konghq.com>
2021-02-11 10:14:59 +1300

    chore: adjusting some dimensions after design review

--- packages/insomnia-app/app/ui/components/wrapper-home.js
+++ packages/insomnia-app/app/ui/components/wrapper-home.js
@@ -463,7 +463,7 @@ class WrapperHome extends React.PureComponent<Props, State> {
             className="app-header"
             gridLeft={
               <React.Fragment>
-                <img src={coreLogo} alt="Insomnia" width="32" height="32" />
+                <img src={coreLogo} alt="Insomnia" width="24" height="24" />
                 <Breadcrumb className="breadcrumb" crumbs={[getAppName()]} />
               </React.Fragment>
             }
