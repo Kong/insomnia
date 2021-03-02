e84382e48676cfce7a1346abbb3ed4dc45b8440d
sonicyeti <mike.ellan@konghq.com>
2021-03-02 18:43:42 -0500

    Adding prop types for header & sub-header verbiage.

--- packages/insomnia-app/app/ui/components/wrapper-onboarding.js
+++ packages/insomnia-app/app/ui/components/wrapper-onboarding.js
@@ -18,6 +18,8 @@ type Props = {|
   wrapperProps: WrapperProps,
   handleImportFile: HandleImportFileCallback,
   handleImportUri: HandleImportUriCallback,
+  header: string,
+  subHeader: string,
 |};
 
 type State = {|
