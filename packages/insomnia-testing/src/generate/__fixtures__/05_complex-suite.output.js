ad9b3ea48f32f20b4dfaaac71378e0f98c0e61cb
James switzer <48921597+jswitzer-lulus@users.noreply.github.com>
2021-03-22 08:22:23 -0700

    Update 05_complex-suite.output.js

--- packages/insomnia-testing/src/generate/__fixtures__/05_complex-suite.output.js
+++ packages/insomnia-testing/src/generate/__fixtures__/05_complex-suite.output.js
@@ -6,22 +6,22 @@ beforeEach(() => insomnia.clearActiveRequest());
 describe('Parent Suite', () => {
   describe('Nested Suite', () => {
     describe('Nested Again Suite', () => {
-      it('should return -1 when the value is not present', async () => {
+      it('should return -1 when the value is not present', async function() {
         expect([1, 2, 3].indexOf(4)).to.equal(-1);
         expect(true).to.equal(true);
       });
 
-      it('should be true', async () => {
+      it('should be true', async function() {
         expect(true).to.equal(true);
       });
     });
   });
 
-  it('should return -1 when the value is not present', async () => {
+  it('should return -1 when the value is not present', async function() {
     expect([1, 2, 3].indexOf(4)).to.equal(-1);
     expect(true).to.equal(true);
   });
 
-  it('is an empty test', async () => {
+  it('is an empty test', async function() {
   });
 });
