b7a891d4d44b975205b50055fae943e31d2b5d15
James switzer <48921597+jswitzer-lulus@users.noreply.github.com>
2021-03-22 08:21:24 -0700

    Update 04_nested-suite.output.js

--- packages/insomnia-testing/src/generate/__fixtures__/04_nested-suite.output.js
+++ packages/insomnia-testing/src/generate/__fixtures__/04_nested-suite.output.js
@@ -5,12 +5,12 @@ beforeEach(() => insomnia.clearActiveRequest());
 
 describe('Parent Suite', () => {
   describe('Nested Suite', () => {
-    it('should return -1 when the value is not present', async () => {
+    it('should return -1 when the value is not present', async function() {
       expect([1, 2, 3].indexOf(4)).to.equal(-1);
       expect(true).to.equal(true);
     });
 
-    it('is an empty test', async () => {
+    it('is an empty test', async function() {
     });
   });
 });
