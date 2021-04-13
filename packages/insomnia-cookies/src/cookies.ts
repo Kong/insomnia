f5562c65bdde5ed35e86240a6d8ff0e790d11527
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2021-04-13 11:55:01 -0400

    insomnia-cookies

--- packages/insomnia-cookies/src/cookies.ts
+++ packages/insomnia-cookies/src/cookies.ts
@@ -1,19 +1,17 @@
-const { CookieJar, Cookie } = require('tough-cookie');
+import { CookieJar, Cookie, CookieJSON } from 'tough-cookie';
 
 /**
  * Get a list of cookie objects from a request.jar()
- *
- * @param jar
  */
-module.exports.cookiesFromJar = function(jar) {
+export const cookiesFromJar = (cookieJar: CookieJar): Promise<CookieJSON[]> => {
   return new Promise(resolve => {
-    jar.store.getAllCookies((err, cookies) => {
+    cookieJar.store.getAllCookies((err, cookies) => {
       if (err) {
         console.warn('Failed to get cookies form jar', err);
         resolve([]);
       } else {
         // NOTE: Perform toJSON so we have a plain JS object instead of Cookie instance
-        resolve(cookies.map(c => c.toJSON()));
+        resolve(cookies.map(cookie => cookie.toJSON()));
       }
     });
   });
@@ -21,20 +19,18 @@ module.exports.cookiesFromJar = function(jar) {
 
 /**
  * Get a request.jar() from a list of cookie objects
- *
- * @param cookies
  */
-module.exports.jarFromCookies = function(cookies) {
-  let jar;
+export const jarFromCookies = (cookies: Cookie[]) => {
+  let jar: CookieJar;
 
   try {
-    // For some reason, fromJSON modifies `cookies`. Create a copy first
-    // just to be sure
+    // For some reason, fromJSON modifies `cookies`.
+    // Create a copy first just to be sure.
     const copy = JSON.stringify({ cookies });
     jar = CookieJar.fromJSON(copy);
   } catch (e) {
     console.log('[cookies] Failed to initialize cookie jar', e);
-    jar = new CookieJar();
+    jar = new CookieJar() as CookieJar;
   }
 
   jar.rejectPublicSuffixes = false;
@@ -43,18 +39,21 @@ module.exports.jarFromCookies = function(cookies) {
   return jar;
 };
 
-module.exports.cookieToString = function(cookie) {
+export const cookieToString = (cookie: Parameters<typeof Cookie.fromJSON>[0] | Cookie) => {
   // Cookie can either be a plain JS object or Cookie instance
   if (!(cookie instanceof Cookie)) {
-    cookie = Cookie.fromJSON(cookie);
-  }
+    cookie = Cookie.fromJSON(cookie) as Cookie;
 
+    if (cookie === null) {
+      throw new Error(`Unable to read cookie: ${cookie}`);
+    }
+  }
   let str = cookie.toString();
 
   // tough-cookie toString() doesn't put domain on all the time.
   // This hack adds when tough-cookie won't
-  if (cookie.domain && cookie.hostOnly) {
-    str += '; Domain=' + cookie.domain;
+  if ((cookie as Cookie).domain && (cookie as Cookie).hostOnly) {
+    str += `; Domain=${(cookie as Cookie).domain}`;
   }
 
   return str;
