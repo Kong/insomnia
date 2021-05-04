5419831775729d67c03c71458d859436870985de
Opender Singh <opender.singh@konghq.com>
2021-05-04 12:00:09 +1200

    [app] temporarily disable no explicit any rule

--- packages/insomnia-app/.eslintrc.js
+++ packages/insomnia-app/.eslintrc.js
@@ -2,6 +2,7 @@ module.exports = /** @type { import('eslint').Linter.Config } */ {
   extends: '../../.eslintrc.js',
   rules: {
     '@typescript-eslint/no-use-before-define': 'off', // TSCONVERSION
+    '@typescript-eslint/no-explicit-any': 'off', // TSCONVERSION
     // 'padding-line-between-statements': ['error', { blankLine: "always", prev: ["*"], next: "export"}],
   },
 };
