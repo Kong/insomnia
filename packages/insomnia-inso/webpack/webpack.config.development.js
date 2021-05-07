f3e6d3b11c7dab4e0d5260701fb3397b7d908417
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2021-05-07 09:14:18 -0400

    updates webpack-merge and adds types

--- packages/insomnia-inso/webpack/webpack.config.development.js
+++ packages/insomnia-inso/webpack/webpack.config.development.js
@@ -2,15 +2,13 @@ const webpack = require('webpack');
 const { merge } = require('webpack-merge');
 const base = require('./webpack.config.base');
 
-module.exports = merge(
-  base,
-  /** @type { import('webpack').Configuration } */ ({
-    mode: 'development',
-    plugins: [
-      new webpack.DefinePlugin({
-        __DEV__: true,
-        'process.env.DEFAULT_APP_NAME': JSON.stringify('insomnia-app'),
-      }),
-    ],
-  }),
-);
+/** @type { import('webpack').Configuration } */
+module.exports = merge(base, {
+  mode: 'development',
+  plugins: [
+    new webpack.DefinePlugin({
+      __DEV__: true,
+      'process.env.DEFAULT_APP_NAME': JSON.stringify('insomnia-app'),
+    }),
+  ],
+});
--- packages/insomnia-inso/webpack/webpack.config.production.js
+++ packages/insomnia-inso/webpack/webpack.config.production.js
@@ -2,17 +2,15 @@ const webpack = require('webpack');
 const { merge } = require('webpack-merge');
 const base = require('./webpack.config.base');
 
-module.exports = merge(
-  base,
-  /** @type { import('webpack').Configuration } */ ({
-    mode: 'production',
-    optimization: {
-      minimize: true,
-    },
-    plugins: [
-      new webpack.DefinePlugin({
-        'process.env.DEFAULT_APP_NAME': JSON.stringify('Insomnia'),
-      }),
-    ],
-  }),
-);
+/** @type { import('webpack').Configuration } */
+module.exports = merge(base, {
+  mode: 'production',
+  optimization: {
+    minimize: true,
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.DEFAULT_APP_NAME': JSON.stringify('Insomnia'),
+    }),
+  ],
+});
