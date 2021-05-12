4ef93d4d55620927220016e7b1dacb459a7174ef
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2021-05-12 15:47:16 -0400

    updates to use URL constructor per review feedback

--- packages/insomnia-app/app/common/electron-helpers.ts
+++ packages/insomnia-app/app/common/electron-helpers.ts
@@ -4,7 +4,8 @@ import appConfig from '../../config/config.json';
 import mkdirp from 'mkdirp';
 
 export function clickLink(href: string) {
-  if (href.startsWith('http')) {
+  const { protocol } = new URL(href);
+  if (protocol === 'http:' || protocol === 'https:') {
     // eslint-disable-next-line no-restricted-properties -- this is, other than tests, what _should be_ the one and only place in this project where this is called.
     electron.shell.openExternal(href);
   }
