5694bec98d48b435a9733335c12147132f76714d
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2021-05-13 08:29:29 -0400

    fixes types for mock helpers
    These `mock` wrappers have no utility to us anymore.  They were there before as wrappers to help Flow, but we can just use jest.MockedFunction now as elsewhere.

--- packages/insomnia-inso/src/commands/generate-config.test.ts
+++ packages/insomnia-inso/src/commands/generate-config.test.ts
@@ -1,15 +1,18 @@
 import { conversionTypeMap, generateConfig, GenerateConfigOptions } from './generate-config';
 import o2k from 'openapi-2-kong';
 import path from 'path';
-import { writeFileWithCliOptions } from '../write-file';
+import { writeFileWithCliOptions as _writeFileWithCliOptions } from '../write-file';
 import { globalBeforeAll, globalBeforeEach } from '../jest/before';
 import { logger } from '../logger';
 import { InsoError } from '../errors';
 import os from 'os';
-import { UNKNOWN } from '../types';
 
 jest.mock('openapi-2-kong');
 jest.mock('../write-file');
+
+const generate = o2k.generate as jest.MockedFunction<typeof o2k.generate>;
+const generateFromString = o2k.generateFromString as jest.MockedFunction<typeof o2k.generateFromString>;
+const writeFileWithCliOptions = _writeFileWithCliOptions as jest.MockedFunction<typeof _writeFileWithCliOptions>;
 
 describe('generateConfig()', () => {
   beforeAll(() => {
@@ -19,8 +22,6 @@ describe('generateConfig()', () => {
   beforeEach(() => {
     globalBeforeEach();
   });
-
-  const mock = (mockFn: UNKNOWN): jest.Mock => mockFn;
 
   afterEach(() => {
     jest.restoreAllMocks();
@@ -33,32 +34,30 @@ describe('generateConfig()', () => {
       // @ts-expect-error intentionally invalid input
       type: 'invalid',
     });
-    expect(o2k.generate).not.toHaveBeenCalled();
+    expect(generate).not.toHaveBeenCalled();
     expect(logger.__getLogs().fatal).toEqual([
       'Config type "invalid" not unrecognized. Options are [kubernetes, declarative].',
     ]);
   });
 
   it('should print conversion documents to console', async () => {
-    mock(o2k.generate).mockResolvedValue({ documents: ['a', 'b'] });
+    generate.mockResolvedValue({ documents: ['a', 'b'] });
 
     await generateConfig(filePath, { type: 'kubernetes', tags: 'tag' });
 
-    expect(o2k.generate).toHaveBeenCalledWith(filePath, conversionTypeMap.kubernetes, ['tag']);
+    expect(generate).toHaveBeenCalledWith(filePath, conversionTypeMap.kubernetes, ['tag']);
     expect(logger.__getLogs().log).toEqual(['a\n---\nb\n']);
   });
 
   it('should load identifier from database', async () => {
-    mock(o2k.generateFromString).mockResolvedValue({
-      documents: ['a', 'b'],
-    });
+    generateFromString.mockResolvedValue({ documents: ['a', 'b'] });
     await generateConfig('spc_46c5a4a40e83445a9bd9d9758b86c16c', {
       type: 'kubernetes',
       workingDir: 'src/db/fixtures/git-repo',
       tags: 'first,second',
     });
 
-    expect(o2k.generateFromString).toHaveBeenCalledWith(
+    expect(generateFromString).toHaveBeenCalledWith(
       expect.stringMatching(/.+/),
       conversionTypeMap.kubernetes,
       ['first', 'second'],
@@ -67,11 +66,9 @@ describe('generateConfig()', () => {
   });
 
   it('should output generated document to a file', async () => {
-    mock(o2k.generate).mockResolvedValue({
-      documents: ['a', 'b'],
-    });
+    generate.mockResolvedValue({ documents: ['a', 'b'] });
     const outputPath = 'this-is-the-output-path';
-    mock(writeFileWithCliOptions).mockResolvedValue(outputPath);
+    writeFileWithCliOptions.mockResolvedValue(outputPath);
     const options: Partial<GenerateConfigOptions> = {
       type: 'kubernetes',
       output: 'output.yaml',
@@ -88,11 +85,9 @@ describe('generateConfig()', () => {
   });
 
   it('should return false if failed to write file', async () => {
-    mock(o2k.generate).mockResolvedValue({
-      documents: ['a', 'b'],
-    });
+    generate.mockResolvedValue({ documents: ['a', 'b'] });
     const error = new Error('error message');
-    mock(writeFileWithCliOptions).mockRejectedValue(error);
+    writeFileWithCliOptions.mockRejectedValue(error);
     const options: Partial<GenerateConfigOptions> = {
       type: 'kubernetes',
       output: 'output.yaml',
@@ -103,11 +98,9 @@ describe('generateConfig()', () => {
   });
 
   it('should generate documents using workingDir', async () => {
-    mock(o2k.generate).mockResolvedValue({
-      documents: ['a', 'b'],
-    });
+    generate.mockResolvedValue({ documents: ['a', 'b'] });
     const outputPath = 'this-is-the-output-path';
-    mock(writeFileWithCliOptions).mockResolvedValue(outputPath);
+    writeFileWithCliOptions.mockResolvedValue(outputPath);
     const result = await generateConfig('file.yaml', {
       type: 'kubernetes',
       workingDir: 'test/dir',
@@ -115,7 +108,7 @@ describe('generateConfig()', () => {
     });
     expect(result).toBe(true); // Read from workingDir
 
-    expect(o2k.generate).toHaveBeenCalledWith(
+    expect(generate).toHaveBeenCalledWith(
       path.normalize('test/dir/file.yaml'),
       conversionTypeMap.kubernetes,
       undefined,
@@ -124,11 +117,9 @@ describe('generateConfig()', () => {
   });
 
   it('should generate documents using absolute path', async () => {
-    mock(o2k.generate).mockResolvedValue({
-      documents: ['a', 'b'],
-    });
+    generate.mockResolvedValue({ documents: ['a', 'b'] });
     const outputPath = 'this-is-the-output-path';
-    mock(writeFileWithCliOptions).mockResolvedValue(outputPath);
+    writeFileWithCliOptions.mockResolvedValue(outputPath);
     const absolutePath = path.join(os.tmpdir(), 'dev', 'file.yaml');
     const result = await generateConfig(absolutePath, {
       type: 'kubernetes',
@@ -138,26 +129,23 @@ describe('generateConfig()', () => {
     expect(result).toBe(true);
 
     // Read from workingDir
-    expect(o2k.generate).toHaveBeenCalledWith(absolutePath, conversionTypeMap.kubernetes, undefined);
+    expect(generate).toHaveBeenCalledWith(absolutePath, conversionTypeMap.kubernetes, undefined);
     expect(logger.__getLogs().log).toEqual([`Configuration generated to "${outputPath}".`]);
   });
 
   it('should throw InsoError if there is an error thrown by openapi-2-kong', async () => {
     const error = new Error('err');
-    mock(o2k.generate).mockRejectedValue(error);
-    const promise = generateConfig('file.yaml', {
-      type: 'kubernetes',
-    });
+    generate.mockRejectedValue(error);
+    const promise = generateConfig('file.yaml', { type: 'kubernetes' });
     await expect(promise).rejects.toThrowError(
       new InsoError('There was an error while generating configuration', error),
     );
   });
 
   it('should warn if no valid spec can be found', async () => {
-    mock(o2k.generate).mockResolvedValue({});
-    const result = await generateConfig('file.yaml', {
-      type: 'kubernetes',
-    });
+    // @ts-expect-error intentionally passing in a bad value
+    generate.mockResolvedValue({});
+    const result = await generateConfig('file.yaml', { type: 'kubernetes' });
     expect(result).toBe(false);
     expect(logger.__getLogs().log).toEqual([
       'Could not find a valid specification to generate configuration.',
