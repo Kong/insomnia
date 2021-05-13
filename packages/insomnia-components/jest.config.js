5fe277ee02a77b1ce3a3d58f19e526a3b2f884e7
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2021-05-13 07:29:02 -0400

    remove duplication of all jest configs (#3375)
    * use jest-preset for all but app
    
    * separate properties that are the same as the preset
    
    for some reason, it doesn't work.  you get:
    ```
    Preset ../../jest-preset.js not found.
    ```
    
    This needs to be investigated.
    
    * removes unused/unneeded npm script in `app`, `test:jest`

--- packages/insomnia-components/jest.config.js
+++ packages/insomnia-components/jest.config.js
@@ -1,21 +1,6 @@
 /** @type { import('@jest/types').Config.InitialOptions } */
 module.exports = {
-  globals: {
-    'ts-jest': {
-      isolatedModules: true,
-    },
-  },
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
+  preset: '../../jest-preset.js',
   setupFilesAfterEnv: ['./src/jest/setup-after-env.ts'],
-  verbose: false,
-  resetMocks: true,
-  resetModules: true,
-  testRegex: ['.+\\.test\\.tsx?$'],
-  collectCoverage: false,
-  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  coverageReporters: ['text-summary', 'lcov'],
+  testEnvironment: 'jsdom',
 };
--- packages/insomnia-testing/jest.config.js
+++ packages/insomnia-testing/jest.config.js
@@ -1,20 +1,4 @@
 /** @type { import('@jest/types').Config.InitialOptions } */
 module.exports = {
-  globals: {
-    'ts-jest': {
-      isolatedModules: true,
-    },
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  testMatch: [
-    '**/*.test.ts',
-  ],
-  verbose: false,
-  resetMocks: true,
-  resetModules: true,
-  collectCoverage: false,
+  preset: '../../jest-preset.js',
 };
