2ed3a6742be2dad4774f81dbe793f958599dbf90
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2021-05-18 13:41:56 -0400

    removes shim types and exports actual types from openapi-2-kong
    we had everything in place for the rest of the build pipeline to consume these types, but were missing the main exports at the root index.ts file

--- packages/openapi-2-kong/src/index.ts
+++ packages/openapi-2-kong/src/index.ts
@@ -4,3 +4,9 @@ export {
   generateFromSpec,
   parseSpec,
 } from './generate';
+
+export * from './types/declarative-config';
+export * from './types/k8splugins';
+export * from './types/kubernetes-config';
+export * from './types/openapi3';
+export * from './types/outputs';
