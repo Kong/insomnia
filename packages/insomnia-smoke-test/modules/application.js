7e0a26d7292995008df05a418139401d7689b2b3
Opender Singh <opender.singh@konghq.com>
2021-05-19 21:20:23 +1200

    Update application.js

--- packages/insomnia-smoke-test/modules/application.js
+++ packages/insomnia-smoke-test/modules/application.js
@@ -63,7 +63,7 @@ const launch = async config => {
     chromeDriverArgs: ['remote-debugging-port=9222'],
 
     env: config.env,
-    waitTimeout: 10000,
+    waitTimeout: 20000,
   });
 
   await app.start().then(async () => {
