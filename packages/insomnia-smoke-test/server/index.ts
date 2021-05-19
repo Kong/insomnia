917825f1c53c0d4850d36d5ad0c8bcdc6994f461
Opender Singh <opender.singh@konghq.com>
2021-05-19 17:44:34 +1200

    typesync

--- packages/insomnia-smoke-test/server/index.ts
+++ packages/insomnia-smoke-test/server/index.ts
@@ -40,7 +40,7 @@ app.listen(port, () => {
 });
 
 app.get('/delay/seconds/:duration', (req, res) => {
-  const delaySec = Number.parseInt(req.params.duration || 2);
+  const delaySec = Number.parseInt(req.params.duration || '2');
   setTimeout(function() {
       res.send(`Delayed by ${delaySec} seconds`);
   }, delaySec * 1000);
