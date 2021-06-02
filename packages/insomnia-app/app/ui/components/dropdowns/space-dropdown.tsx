a18a5606d4b7b0fc077bc322abf5e4cbcc01efe7
Opender Singh <opender.singh@konghq.com>
2021-06-02 13:44:11 +1200

    fetch and store remote spaces

--- packages/insomnia-app/app/ui/components/dropdowns/space-dropdown.tsx
+++ packages/insomnia-app/app/ui/components/dropdowns/space-dropdown.tsx
@@ -1,37 +1,77 @@
 import { Dropdown, DropdownDivider, DropdownItem } from 'insomnia-components';
-import React, { FC, useCallback, useMemo } from 'react';
+import React, { FC, useCallback, useMemo, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { isLoggedIn } from '../../../account/session';
 import { getAppName } from '../../../common/constants';
 import { strings } from '../../../common/strings';
 import { BASE_SPACE_ID, Space } from '../../../models/space';
+import VCS from '../../../sync/vcs';
 import { setActiveSpace } from '../../redux/modules/global';
 import { createSpace } from '../../redux/modules/space';
 import { selectActiveSpace, selectSpaces } from '../../redux/selectors';
 import { showModal } from '../modals';
 import SpaceSettingsModal from '../modals/space-settings-modal';
+import * as models from '../../../models';
+import { database } from '../../../common/database';
 
-const mapSpace = ({ _id, name }: Space) => ({ id: _id, name });
-const defaultSpace = { id: BASE_SPACE_ID, name: getAppName() };
+type SpaceSubset = Pick<Space, '_id'> & Pick<Space, 'name'> & Pick<Space, 'remoteId'>;
+
+const defaultSpace: SpaceSubset = { _id: BASE_SPACE_ID, name: getAppName() };
 
 const check = <i className="fa fa-check" />;
 const cog = <i className="fa fa-cog" />;
 const plus = <i className="fa fa-plus" />;
+const spinner = <i className="fa fa-spin fa-refresh" />;
+const home = <i className="fa fa-home" />;
 
-export const SpaceDropdown: FC = () => {
+interface Props {
+  vcs?: VCS;
+}
+
+const useRemoteSpaces = (vcs?: VCS) => {
+  const [loading, setLoading] = useState(false);
+
+  const refresh = useCallback(async () => {
+    if (vcs && isLoggedIn()) {
+      setLoading(true);
+      const teams = await vcs.teams();
+      const spaces = await Promise.all(teams.map(team => models.initModel<Space>(models.space.type, { _id: team.id, name: team.name })));
+      await database.batchModifyDocs({ upsert: spaces });
+      setLoading(false);
+    }
+  }, [vcs]);
+
+  return { loading, refresh };
+};
+
+export const SpaceDropdown: FC<Props> = ({ vcs }) => {
+  const { loading, refresh } = useRemoteSpaces(vcs);
+
   // get list of spaces
-  const loadedSpaces = useSelector(selectSpaces);
-  const spaces = [defaultSpace, ...(loadedSpaces.map(mapSpace))];
+  const spaces = useSelector(selectSpaces);
 
   // figure out which space is selected
   const activeSpace = useSelector(selectActiveSpace);
-  const selectedSpace = spaces.find(space => space.id === activeSpace?._id) || defaultSpace;
+  const selectedSpace = activeSpace || defaultSpace;
+  const spaceHasSettings = selectedSpace !== defaultSpace;
 
   // select a new space
   const dispatch = useDispatch();
   const setActive = useCallback((id) => dispatch(setActiveSpace(id)), [dispatch]);
   const createNew = useCallback(() => dispatch(createSpace()), [dispatch]);
   const showSettings = useCallback(() => showModal(SpaceSettingsModal), []);
-  const spaceHasSettings = selectedSpace !== defaultSpace;
+
+  const renderDropdownItem = useCallback(({ _id, name }: SpaceSubset) => (
+    <DropdownItem
+      key={_id}
+      icon={_id === defaultSpace._id && home}
+      right={_id === selectedSpace._id && check}
+      value={_id}
+      onClick={setActive}
+    >
+      {name}
+    </DropdownItem>),
+  [selectedSpace, setActive]);
 
   // dropdown button
   const button = useMemo(() => (
@@ -42,17 +82,10 @@ export const SpaceDropdown: FC = () => {
   [selectedSpace]);
 
   return (
-    <Dropdown renderButton={button}>
-      {spaces.map(({ id, name }) => (
-        <DropdownItem
-          key={id}
-          right={id === selectedSpace.id && check}
-          value={id}
-          onClick={setActive}
-        >
-          {name}
-        </DropdownItem>
-      ))}
+    <Dropdown renderButton={button} onOpen={refresh}>
+      {renderDropdownItem(defaultSpace)}
+      <DropdownDivider>All spaces{' '}{loading && spinner}</DropdownDivider>
+      {spaces.map(renderDropdownItem)}
       {spaceHasSettings && <>
         <DropdownDivider />
         <DropdownItem icon={cog} onClick={showSettings}>
