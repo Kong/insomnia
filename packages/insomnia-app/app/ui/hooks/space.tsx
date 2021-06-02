48fa80d5f8bc57870ef84927c76ddd9eabf45791
Opender Singh <opender.singh@konghq.com>
2021-06-02 18:06:46 +1200

    wip

---
+++ packages/insomnia-app/app/ui/hooks/space.tsx
@@ -0,0 +1,30 @@
+
+import * as models from '../../models';
+import { database } from '../../common/database';
+import VCS from '../../sync/vcs';
+import { useCallback, useEffect, useState } from 'react';
+import { isLoggedIn } from '../../account/session';
+import { Space } from '../../models/space';
+
+export const useRemoteSpaces = (vcs?: VCS) => {
+  const [loading, setLoading] = useState(false);
+
+  const refresh = useCallback(async () => {
+    if (vcs && isLoggedIn()) {
+      setLoading(true);
+      const teams = await vcs.teams();
+      const spaces = await Promise.all(teams.map(team => models.initModel<Space>(models.space.type, { _id: team.id, name: team.name })));
+      await database.batchModifyDocs({ upsert: spaces });
+      setLoading(false);
+    }
+  }, [vcs]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      await refresh();
+    };
+    fetch();
+  }, [refresh]);
+
+  return { loading, refresh };
+};
