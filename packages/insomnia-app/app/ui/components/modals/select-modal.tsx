f3368ed52afed977fceffe7e3b5c8dbc5e45df46
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2021-06-02 14:39:09 -0400

    updates SelectModal
    it's too big of a change to make this a hook right here and now, but we can do the following to make this easier to work with:
    
    1. use createRef instead of custom code
    1. actually make private thing private
    1. remove unnecessary autobind decorator
    1. alphabetize props (to make it easier to make sure none were missed)
    1. move `onDone` to state.  from what I can tell, there is no reason to do this, but please verify.  from looking at the two usages of this component, they're always the same anyway but if they change I would imagine we would want to not call the previous onDone, but rather the new one.

--- packages/insomnia-app/app/ui/components/modals/select-modal.tsx
+++ packages/insomnia-app/app/ui/components/modals/select-modal.tsx
@@ -1,94 +1,77 @@
-import React, { PureComponent } from 'react';
-import { autoBindMethodsForReact } from 'class-autobind-decorator';
-import { AUTOBIND_CFG } from '../../../common/constants';
+import React, { createRef, PureComponent } from 'react';
 import Modal from '../base/modal';
 import ModalBody from '../base/modal-body';
 import ModalHeader from '../base/modal-header';
 import ModalFooter from '../base/modal-footer';
 
 export interface SelectModalShowOptions {
-  title: string | null;
   message: string | null;
+  onCancel?: () => void;
+  onDone?: (selectedValue: string | null) => Promise<void>;
   options: {
     name: string;
     value: string;
   }[];
+  title: string | null;
   value: string | null;
-  onDone?: (selectedValue: string | null) => Promise<void>;
-  onCancel?: () => void;
 }
 
-type State = Omit<SelectModalShowOptions, 'onDone'>;
-
-const initialState: State = {
-  title: null,
-  options: [],
+const initialState: SelectModalShowOptions = {
   message: null,
+  options: [],
+  title: null,
   value: null,
 };
 
-@autoBindMethodsForReact(AUTOBIND_CFG)
-export class SelectModal extends PureComponent<{}, State> {
-  modal: Modal | null = null;
-  doneButton: HTMLButtonElement | null = null;
-  onDone: SelectModalShowOptions['onDone'] | null = null;
+export class SelectModal extends PureComponent<{}, SelectModalShowOptions> {
+  modal = createRef<Modal>();
+  doneButton = createRef<HTMLButtonElement>();
+  state: SelectModalShowOptions = initialState;
 
-  state: State = initialState;
-
-  _setModalRef(m: Modal) {
-    this.modal = m;
-  }
-
-  _setDoneButtonRef(n: HTMLButtonElement) {
-    this.doneButton = n;
-  }
-
-  _handleDone = async () => {
+  #onDone = async () => {
     this.hide();
-    await this.onDone?.(this.state.value);
+    await this.state.onDone?.(this.state.value);
   }
 
-  _handleSelectChange(e: React.SyntheticEvent<HTMLSelectElement>) {
-    this.setState({
-      value: e.currentTarget.value,
-    });
+  #handleSelectChange = (event: React.SyntheticEvent<HTMLSelectElement>) => {
+    this.setState({ value: event.currentTarget.value });
   }
 
-  hide() {
-    this.modal?.hide();
+  hide = () => {
+    this.modal.current?.hide();
   }
 
-  show({
-    title,
+  show = ({
     message,
-    options,
-    value,
-    onDone,
     onCancel,
-  }: SelectModalShowOptions = initialState) {
-    this.onDone = onDone;
+    onDone,
+    options,
+    title,
+    value,
+  }: SelectModalShowOptions = initialState) => {
     this.setState({
-      title,
       message,
-      options,
-      value,
       onCancel,
+      onDone,
+      options,
+      title,
+      value,
     });
-    this.modal?.show();
+    this.modal.current?.show();
     setTimeout(() => {
-      this.doneButton?.focus();
+      this.doneButton.current?.focus();
     }, 100);
   }
 
   render() {
     const { message, title, options, value, onCancel } = this.state;
     return (
-      <Modal ref={this._setModalRef} onCancel={onCancel}>
+      <Modal ref={this.modal} onCancel={onCancel}>
         <ModalHeader>{title || 'Confirm?'}</ModalHeader>
         <ModalBody className="wide pad">
           <p>{message}</p>
           <div className="form-control form-control--outlined">
-            <select onChange={this._handleSelectChange} value={value ?? undefined}>
+            <select onChange={this.#handleSelectChange} value={value ?? undefined}>
               {options.map(({ name, value }) => (
                 <option key={value} value={value}>
                   {name}
@@ -98,7 +81,7 @@ export class SelectModal extends PureComponent<{}, State> {
           </div>
         </ModalBody>
         <ModalFooter>
-          <button ref={this._setDoneButtonRef} className="btn" onClick={this._handleDone}>
+          <button ref={this.doneButton} className="btn" onClick={this.#onDone}>
             Done
           </button>
         </ModalFooter>
