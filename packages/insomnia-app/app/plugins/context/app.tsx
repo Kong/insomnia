e5bc96137c7097fe0452146f20de4d669debb187
Opender Singh <opender.singh@konghq.com>
2021-06-16 14:42:53 +1200

    cleanup

--- packages/insomnia-app/app/plugins/context/app.tsx
+++ packages/insomnia-app/app/plugins/context/app.tsx
@@ -77,7 +77,7 @@ export function init(renderPurpose: RenderPurpose = RENDER_PURPOSE_GENERAL): {
               reject(new Error(`Prompt ${title} cancelled`));
             },
 
-            async onComplete(value: string) {
+            onComplete(value: string) {
               resolve(value);
             },
           });
