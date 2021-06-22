3b9d7fad8b6d46f95a225d1e1c3ea22153121824
Opender Singh <opender.singh@konghq.com>
2021-06-22 12:02:43 +1200

    extend send request

--- packages/insomnia-app/app/common/send-request.ts
+++ packages/insomnia-app/app/common/send-request.ts
@@ -1,10 +1,16 @@
 import { BaseModel, stats, types as modelTypes } from '../models';
+import * as models from '../models';
 import { getBodyBuffer } from '../models/response';
+import { Settings } from '../models/settings';
 import { send } from '../network/network';
 import * as plugins from '../plugins';
 import { database as db } from './database';
 
-export async function getSendRequestCallbackMemDb(environmentId, memDB) {
+// The network layer uses settings from the settings model
+// We want to give inso the ability to override certain settings
+type SettingsOverride = Pick<Settings, 'validateSSL'>;
+
+export async function getSendRequestCallbackMemDb(environmentId, memDB, settingsOverrides?: SettingsOverride) {
   // Initialize the DB in-memory and fill it with data if we're given one
   await db.init(
     modelTypes(),
@@ -16,6 +22,9 @@ export async function getSendRequestCallbackMemDb(environmentId, memDB) {
   );
   const docs: BaseModel[] = [];
 
+  const settings = await models.settings.getOrCreate();
+  docs.push({ ...settings, ...settingsOverrides });
+
   for (const type of Object.keys(memDB)) {
     for (const doc of memDB[type]) {
       docs.push(doc);
@@ -26,6 +35,7 @@ export async function getSendRequestCallbackMemDb(environmentId, memDB) {
     upsert: docs,
     remove: [],
   });
+
   // Return callback helper to send requests
   return async function sendRequest(requestId) {
     return sendAndTransform(requestId, environmentId);
