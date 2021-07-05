c04cc05311590eb32bacae61751b3c7543557f21
Opender Singh <opender.singh@konghq.com>
2021-07-06 10:34:56 +1200

    fix selector test types

--- packages/insomnia-app/app/models/__schemas__/model-schemas.ts
+++ packages/insomnia-app/app/models/__schemas__/model-schemas.ts
@@ -1,7 +1,23 @@
 import { Schema } from '@develohpanda/fluent-builder';
-import { BaseModel, gitRepository, workspace } from '..';
+import { BaseModel, gitRepository, grpcRequest, request, requestGroup, workspace } from '..';
 import { GitRepository } from '../git-repository';
 import { Workspace } from '../workspace';
+import { Request } from '../request';
+import clone from 'clone';
+import { GrpcRequest } from '../grpc-request';
+import { RequestGroup } from '../request-group';
+
+// move into fluent-builder
+const toSchema = <T>(obj: T): Schema<T> => {
+  const cloned = clone(obj);
+  const output: Partial<Schema<T>> = {};
+
+  Object.keys(cloned).forEach(key => {
+    output[key] = () => cloned[key];
+  });
+
+  return output as Schema<T>;
+};
 
 export const baseModelSchema: Schema<BaseModel> = {
   _id: () => 'id',
@@ -19,6 +35,24 @@ export const workspaceModelSchema: Schema<Workspace> = {
   description: () => '',
   certificates: () => undefined,
   scope: () => 'collection',
+};
+
+export const requestModelSchema: Schema<Request> = {
+  ...baseModelSchema,
+  ...toSchema(request.init()),
+  type: () => request.type,
+};
+
+export const grpcRequestModelSchema: Schema<GrpcRequest> = {
+  ...baseModelSchema,
+  ...toSchema(grpcRequest.init()),
+  type: () => grpcRequest.type,
+};
+
+export const requestGroupModelSchema: Schema<RequestGroup> = {
+  ...baseModelSchema,
+  ...toSchema(requestGroup.init()),
+  type: () => requestGroup.type,
 };
 
 export const gitRepositorySchema: Schema<GitRepository> = {
