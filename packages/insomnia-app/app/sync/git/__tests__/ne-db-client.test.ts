8a955125e6ec6f061601c6fa59d0ba2387d73bb8
Opender Singh <opender.singh@konghq.com>
2021-07-22 18:27:47 +1200

    fix tests

--- packages/insomnia-app/app/sync/git/__tests__/ne-db-client.test.ts
+++ packages/insomnia-app/app/sync/git/__tests__/ne-db-client.test.ts
@@ -19,6 +19,7 @@ describe('NeDBClient', () => {
     workspaceBuilder.reset();
     setupDateMocks();
     // Create some sample models
+    await models.space.all();
     await models.workspace.create({
       _id: 'wrk_1',
     });
@@ -40,7 +41,7 @@ describe('NeDBClient', () => {
 
   describe('readdir()', () => {
     it('reads model IDs from model type folders', async () => {
-      const neDbClient = new NeDBClient('wrk_1');
+      const neDbClient = new NeDBClient('wrk_1', BASE_SPACE_ID);
       const reqDir = path.join(GIT_INSOMNIA_DIR, models.request.type);
       const wrkDir = path.join(GIT_INSOMNIA_DIR, models.workspace.type);
       expect(await neDbClient.readdir(GIT_CLONE_DIR)).toEqual([GIT_INSOMNIA_DIR_NAME]);
@@ -66,11 +67,11 @@ describe('NeDBClient', () => {
       const wrk1Yml = path.join(GIT_INSOMNIA_DIR, models.workspace.type, 'wrk_1.yml');
       const req1Yml = path.join(GIT_INSOMNIA_DIR, models.request.type, 'req_1.yml');
       const reqXYml = path.join(GIT_INSOMNIA_DIR, models.request.type, 'req_x.yml');
-      const pNeDB = new NeDBClient('wrk_1');
+      const pNeDB = new NeDBClient('wrk_1', BASE_SPACE_ID);
       expect(YAML.parse((await pNeDB.readFile(wrk1Yml, 'utf8')).toString())).toEqual(
         expect.objectContaining({
           _id: 'wrk_1',
-          parentId: BASE_SPACE_ID,
+          parentId: null, // should be reset to default value instead of the active space id
         }),
       );
       expect(YAML.parse((await pNeDB.readFile(req1Yml, 'utf8')).toString())).toEqual(
@@ -95,7 +96,7 @@ describe('NeDBClient', () => {
         type: 'file',
       });
       // Act
-      const neDbClient = new NeDBClient('wrk_1');
+      const neDbClient = new NeDBClient('wrk_1', BASE_SPACE_ID);
       // Assert
       expect(await neDbClient.stat(GIT_CLONE_DIR)).toEqual(dirType);
       expect(await neDbClient.stat(GIT_INSOMNIA_DIR)).toEqual(dirType);
@@ -110,7 +111,7 @@ describe('NeDBClient', () => {
       // Arrange
       const upsertSpy = jest.spyOn(db, 'upsert');
       const workspaceId = 'wrk_1';
-      const neDbClient = new NeDBClient(workspaceId);
+      const neDbClient = new NeDBClient(workspaceId, BASE_SPACE_ID);
       const env = {
         _id: 'env_1',
         type: models.environment.type,
@@ -128,7 +129,7 @@ describe('NeDBClient', () => {
     it('should write files in GIT_INSOMNIA_DIR directory to db', async () => {
       // Arrange
       const workspaceId = 'wrk_1';
-      const neDbClient = new NeDBClient(workspaceId);
+      const neDbClient = new NeDBClient(workspaceId, BASE_SPACE_ID);
       const upsertSpy = jest.spyOn(db, 'upsert');
       const env = {
         _id: 'env_1',
@@ -202,7 +203,7 @@ describe('NeDBClient', () => {
     it('should throw error if id does not match', async () => {
       // Arrange
       const workspaceId = 'wrk_1';
-      const neDbClient = new NeDBClient(workspaceId);
+      const neDbClient = new NeDBClient(workspaceId, BASE_SPACE_ID);
       const env = {
         _id: 'env_1',
         type: models.environment.type,
@@ -220,7 +221,7 @@ describe('NeDBClient', () => {
     it('should throw error if type does not match', async () => {
       // Arrange
       const workspaceId = 'wrk_1';
-      const neDbClient = new NeDBClient(workspaceId);
+      const neDbClient = new NeDBClient(workspaceId, BASE_SPACE_ID);
       const env = {
         _id: 'env_1',
         type: models.environment.type,
@@ -239,7 +240,7 @@ describe('NeDBClient', () => {
   describe('mkdir()', () => {
     it('should throw error', async () => {
       const workspaceId = 'wrk_1';
-      const neDbClient = new NeDBClient(workspaceId);
+      const neDbClient = new NeDBClient(workspaceId, BASE_SPACE_ID);
       const promiseResult = neDbClient.mkdir();
       await expect(promiseResult).rejects.toThrowError('NeDBClient is not writable');
     });
