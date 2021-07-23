31d38e7debc2fdd281635b37f975dd86ce92f503
Opender Singh <opender.singh@konghq.com>
2021-07-23 17:14:42 +1200

    use strings

--- packages/insomnia-app/app/ui/components/dropdowns/space-dropdown.tsx
+++ packages/insomnia-app/app/ui/components/dropdowns/space-dropdown.tsx
@@ -58,9 +58,9 @@ const TooltipIcon = ({ message, icon }: { message: string, icon: SvgIconProps['i
 );
 
 const spinner = <i className="fa fa-spin fa-refresh" />;
-const home = <TooltipIcon message="Base Space (Always Local)" icon="home" />;
-const remoteSpace = <TooltipIcon message="Remote Space" icon="globe" />;
-const localSpace = <TooltipIcon message="Local Space" icon="laptop" />;
+const home = <TooltipIcon message={`Base ${strings.space.singular} (Always Local)`} icon="home" />;
+const remoteSpace = <TooltipIcon message={`Remote ${strings.space.singular}`} icon="globe" />;
+const localSpace = <TooltipIcon message={`Local ${strings.space.singular}`} icon="laptop" />;
 
 interface Props {
   vcs?: VCS;
