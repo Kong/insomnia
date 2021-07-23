1ed598289a71ffd7170846aa6ca1a579ea5b3284
Opender Singh <opender.singh@konghq.com>
2021-07-23 18:39:13 +1200

    validation

--- packages/insomnia-app/app/ui/components/modals/workspace-duplicate-modal.tsx
+++ packages/insomnia-app/app/ui/components/modals/workspace-duplicate-modal.tsx
@@ -1,14 +1,16 @@
 import { autoBindMethodsForReact } from 'class-autobind-decorator';
-import React, { createRef, forwardRef, ForwardRefRenderFunction, PureComponent, useCallback } from 'react';
+import React, { createRef, FC, forwardRef, ForwardRefRenderFunction, PureComponent } from 'react';
 import { useForm } from 'react-hook-form';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { AUTOBIND_CFG } from '../../../common/constants';
 import { getWorkspaceLabel } from '../../../common/get-workspace-label';
+import { strings } from '../../../common/strings';
 import { ApiSpec } from '../../../models/api-spec';
 import { baseSpace, SpaceSubset } from '../../../models/helpers/base-space';
 import getWorkspaceName from '../../../models/helpers/get-workspace-name';
 import * as workspaceOperations from '../../../models/helpers/workspace-operations';
+import { isBaseSpace, isLocalSpace } from '../../../models/space';
 import { Workspace } from '../../../models/workspace';
 import { setActiveSpace, setActiveWorkspace } from '../../redux/modules/global';
 import { selectActiveSpace, selectSpaces } from '../../redux/selectors';
@@ -26,27 +28,40 @@ interface Options {
 
 interface FormFields {
   newName: string;
-   spaceId: string;
+  spaceId: string;
 }
 
 interface Props extends Options {
   hide: () => void,
 }
 
+const SpaceOption: FC<SpaceSubset> = space => (
+  <option key={space._id} value={space._id}>
+    {space.name} ({isBaseSpace(space) ? 'Base' : isLocalSpace(space) ? 'Local' : 'Remote'})
+  </option>
+);
+
 const WorkspaceDuplicateModalInternalWithRef: ForwardRefRenderFunction<Modal, Props> = ({ workspace, apiSpec, onDone, hide }, ref) => {
   const dispatch = useDispatch();
 
   const spaces = useSelector(selectSpaces);
   const activeSpace = useSelector(selectActiveSpace) || baseSpace;
-  const renderSpaceOption = useCallback(({ _id, name }: SpaceSubset) =>
-    <option key={_id} value={_id}>
-      {name}
-    </option>, []);
 
   const title = `Duplicate ${getWorkspaceLabel(workspace).singular}`;
   const defaultWorkspaceName = getWorkspaceName(workspace, apiSpec);
 
-  const { register, handleSubmit } = useForm<FormFields>({ defaultValues: { newName: defaultWorkspaceName, spaceId: activeSpace._id } });
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: {
+      errors,
+    } } = useForm<FormFields>({
+      defaultValues: {
+        newName: defaultWorkspaceName,
+        spaceId: activeSpace?._id,
+      },
+    });
 
   const onSubmit = async ({ spaceId, newName }: FormFields) => {
     // TODO: just use the default base space ID instead of using null (once we have a base space by default)
@@ -59,22 +74,23 @@ const WorkspaceDuplicateModalInternalWithRef: ForwardRefRenderFunction<Modal, Pr
     onDone?.();
   };
 
-  return <Modal ref={ref}>
+  return <Modal ref={ref} onShow={reset}>
     <ModalHeader>{title}</ModalHeader>
     <ModalBody className="wide">
       <form className="wide pad" onSubmit={handleSubmit(onSubmit)}>
         <div className="form-control form-control--wide form-control--outlined">
           <label>
             New Name
-            <input {...register('newName')} />
+            <input {...register('newName', { validate: v => Boolean(v.trim()) || 'Should not be blank' })} />
+            {errors.newName && <div className="font-error space-top">{errors.newName.message}</div>}
           </label>
         </div>
         <div className="form-control form-control--outlined">
           <label>
-            Space to duplicate into
+            {strings.space.plural} to duplicate into
             <select {...register('spaceId')}>
-              {renderSpaceOption(baseSpace)}
-              {spaces.map(renderSpaceOption)}
+              {SpaceOption(baseSpace)}
+              {spaces.map(SpaceOption)}
             </select>
           </label>
         </div>
