cbb6bd48e8beb8241f090946451a284f88c5bad2
Jonathan Thompson <jonathan.thompson@nimbusnine.co>
2021-08-12 14:55:22 -0600

    Fixed minor typos. Added unit test to verify SSL cURL opts are set correctly when disabling cert verification.

--- packages/insomnia-app/app/network/o-auth-2/__tests__/helpers.ts
+++ packages/insomnia-app/app/network/o-auth-2/__tests__/helpers.ts
@@ -1,8 +1,8 @@
 import electron from 'electron';
 import EventEmitter from 'events';
-type certVerifyProceFn = ((request: Request, callback: (verificationResult: number) => void) => void) | (null);
+type certVerifyProcFn = ((request: Request, callback: (verificationResult: number) => void) => void) | (null);
 
-export function createBWRedirectMock(redirectTo, certifiateVerifyMock:certVerifyProceFn = () => {
+export function createBWRedirectMock(redirectTo, certificateVerifyMock:certVerifyProcFn = () => {
 }) {
   electron.remote.BrowserWindow = jest.fn(function() {
     this._emitter = new EventEmitter();
@@ -20,7 +20,7 @@ export function createBWRedirectMock(redirectTo, certifiateVerifyMock:certVerify
     this.webContents.getURL = () => redirectTo;
 
     this.webContents.session = {
-      setCertificateVerifyProc: certifiateVerifyMock,
+      setCertificateVerifyProc: certificateVerifyMock,
     };
 
     this._emitter.emit('ready-to-show');
