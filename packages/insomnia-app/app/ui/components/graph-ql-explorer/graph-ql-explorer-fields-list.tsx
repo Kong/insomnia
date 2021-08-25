d003400cd48bf9da03afea1a34a66ba841e4f531
Opender Singh <opender.singh@konghq.com>
2021-08-25 16:28:25 +1200

    extract an arg links component

--- packages/insomnia-app/app/ui/components/graph-ql-explorer/graph-ql-explorer-fields-list.tsx
+++ packages/insomnia-app/app/ui/components/graph-ql-explorer/graph-ql-explorer-fields-list.tsx
@@ -1,11 +1,12 @@
 import type { GraphQLType } from 'graphql';
 import { SvgIcon } from 'insomnia-components';
-import React, { Fragment } from 'react';
+import React, { FC } from 'react';
 
 import Tooltip from '../../components/tooltip';
 import MarkdownPreview from '../markdown-preview';
 import GraphQLDefaultValue from './graph-ql-default-value';
 import { GraphQLFieldWithParentName } from './graph-ql-explorer';
+import { GraphQLExplorerArgLinks } from './graph-ql-explorer-arg-links';
 import GraphQLExplorerFieldLink from './graph-ql-explorer-field-link';
 import GraphQLExplorerTypeLink from './graph-ql-explorer-type-link';
 
@@ -15,54 +16,40 @@ interface Props {
   onNavigateField: (field: GraphQLFieldWithParentName) => void;
 }
 
-export const GraphQLExplorerFieldsList = (props: Props) => {
-  const { fields, onNavigateType, onNavigateField } = props;
+export const GraphQLExplorerFieldsList: FC<Props> = ({ fields, onNavigateType, onNavigateField }) => {
+  const fieldsList = fields.map(field => {
+    const argLinks = <GraphQLExplorerArgLinks onNavigate={onNavigateType} args={field.args} />;
+    const fieldLink = <GraphQLExplorerFieldLink onNavigate={onNavigateField} field={field} />;
+    const typeLink = <GraphQLExplorerTypeLink onNavigate={onNavigateType} type={field.type} />;
+
+    const isDeprecated = field.isDeprecated;
+    const description = field.description;
+
+    return (
+      <li key={field.name + field.parentName}>
+        {fieldLink}
+        {argLinks}: {typeLink} <GraphQLDefaultValue field={field} />
+        {isDeprecated && (
+          <Tooltip
+            message={`The field "${field.name}" is deprecated. ${field.deprecationReason}`}
+            position="bottom"
+            delay={1000}
+          >
+            <SvgIcon icon="warning" />
+          </Tooltip>
+        )}
+        {description && (
+          <div className="graphql-explorer__defs__description">
+            <MarkdownPreview markdown={description} />
+          </div>
+        )}
+      </li>
+    );
+  });
+
   return (
     <ul className="graphql-explorer__defs">
-      {fields.map(field => {
-        let argLinks: JSX.Element | null = null;
-        const { args } = field;
-
-        if (args && args.length) {
-          argLinks = (
-            <Fragment>
-              (
-              {args.map(a => (
-                <div key={a.name} className="graphql-explorer__defs__arg">
-                  <span className="info">{a.name}</span>:{' '}
-                  <GraphQLExplorerTypeLink onNavigate={onNavigateType} type={a.type} />
-                </div>
-              ))}
-              )
-            </Fragment>
-          );
-        }
-
-        const fieldLink = <GraphQLExplorerFieldLink onNavigate={onNavigateField} field={field} />;
-        const typeLink = <GraphQLExplorerTypeLink onNavigate={onNavigateType} type={field.type} />;
-        const isDeprecated = field.isDeprecated;
-        const description = field.description;
-        return (
-          <li key={field.name + field.parentName}>
-            {fieldLink}
-            {argLinks}: {typeLink} <GraphQLDefaultValue field={field} />
-            {isDeprecated && (
-              <Tooltip
-                message={`The field "${field.name}" is deprecated. ${field.deprecationReason}`}
-                position="bottom"
-                delay={1000}
-              >
-                <SvgIcon icon="warning" />
-              </Tooltip>
-            )}
-            {description && (
-              <div className="graphql-explorer__defs__description">
-                <MarkdownPreview markdown={description} />
-              </div>
-            )}
-          </li>
-        );
-      })}
+      {fieldsList}
     </ul>
   );
 };
