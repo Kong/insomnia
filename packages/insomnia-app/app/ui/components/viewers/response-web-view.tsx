25520154a5fd52dbec5d51bf8666e1a8553bba56
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2021-09-01 16:48:05 -0400

    Minor Electon-related cleanup (#3957)
    * adds (now available) electron types
    
    * MenuItemConstructorOptions.selector module augmentation
    
    * updates note on allowRendererProcessReuse with more up-to-date info
    
    * separates and formats comments that were intended to be separate
    
    if you check out the commit where these were created, for some reason the comment for the `loadURL` line was put under the line of interest, and over time the space between the comment blocks was removed - making it look like the last line of the comment is related to the same thing (it, apparently, is not).
    
    * use createRef for ResponseWebView

--- packages/insomnia-app/app/ui/components/viewers/response-web-view.tsx
+++ packages/insomnia-app/app/ui/components/viewers/response-web-view.tsx
@@ -1,7 +1,7 @@
 import { autoBindMethodsForReact } from 'class-autobind-decorator';
 import contextMenu from 'electron-context-menu';
 import { EventEmitter } from 'events';
-import React, { PureComponent } from 'react';
+import React, { createRef, PureComponent } from 'react';
 
 import { AUTOBIND_CFG } from '../../../common/constants';
 
@@ -13,73 +13,73 @@ interface Props {
 }
 
 @autoBindMethodsForReact(AUTOBIND_CFG)
-class ResponseWebView extends PureComponent<Props> {
-  _webview: HTMLElement | null = null;
+export class ResponseWebView extends PureComponent<Props> {
+  webview = createRef<HTMLWebViewElement>();
 
-  _handleSetWebViewRef(n: HTMLElement) {
-    this._webview = n;
-
-    if (this._webview) {
-      this._webview.addEventListener('dom-ready', this._handleDOMReady);
-    }
-  }
-
-  _handleDOMReady() {
-    if (!this._webview) {
+  componentDidMount() {
+    if (!this.webview.current) {
+      // This is not supposed to be possible in react since the webview is not conditionally rendered (i.e. it's always rendered), but putting an error here just in case.
+      console.error('ResponseWebView was mounted without a ref to the underlying webview');
       return;
     }
 
-    this._webview.removeEventListener('dom-ready', this._handleDOMReady);
-
-    contextMenu({
-      // @ts-expect-error -- TSCONVERSION type mismatch
-      window: this._webview,
-    });
-
-    this._setBody();
-  }
-
-  _setBody() {
-    const webview = this._webview;
-
-    if (!webview) {
-      return;
-    }
-
-    const { body, contentType, url } = this.props;
-    const bodyWithBase = body.replace('<head>', `<head><base href="${url}">`);
-    // @ts-expect-error -- TSCONVERSION type mismatch
-    webview.loadURL(`data:${contentType},${encodeURIComponent(bodyWithBase)}`);
-    // NOTE: We *should* be setting the base URL by specifying the baseURLForDataURL
-    // option, but there is a bug in baseURLForDataURL since Electron 6 (still exists
-    // in 9) that makes it impossible.
-    //
-    // For now we inject the <base> tag to achieve a similar effect. This was actually the
-    // way we did it before discovering the baseURLForDataURL setting.
-    //
-    //    https://github.com/electron/electron/issues/20700
-    //
-    // webview.loadURL(`data:${contentType},${encodeURIComponent(body)}`, {
-    //   baseURLForDataURL: url,
-    // });
-    // This is kind of hacky but electron-context-menu fails to save images if
-    // this isn't here.
-    // @ts-expect-error -- TSCONVERSION type mismatch
-    webview.webContents = webview;
-    // @ts-expect-error -- TSCONVERSION type mismatch
-    webview.webContents.session = new EventEmitter();
+    this.webview.current.addEventListener('dom-ready', this._handleDOMReady);
   }
 
   componentDidUpdate() {
     this._setBody();
   }
 
+  _handleDOMReady() {
+    if (!this.webview.current) {
+      return;
+    }
+
+    this.webview.current.removeEventListener('dom-ready', this._handleDOMReady);
+
+    contextMenu({
+      // @ts-expect-error -- TSCONVERSION type mismatch
+      window: this.webview.current,
+    });
+
+    this._setBody();
+  }
+
+  _setBody() {
+    if (!this.webview.current) {
+      return;
+    }
+
+    const { body, contentType, url } = this.props;
+    const bodyWithBase = body.replace('<head>', `<head><base href="${url}">`);
+
+    // NOTE: We *should* be setting the base URL by specifying the baseURLForDataURL option, but there is a bug in baseURLForDataURL since Electron 6 (that still exists in 9) that makes it impossible.
+    //
+    // For now we inject the <base> tag to achieve a similar effect. This was actually the way we did it before discovering the baseURLForDataURL setting.
+    //
+    //    https://github.com/electron/electron/issues/20700
+    //
+    // webview.loadURL(`data:${contentType},${encodeURIComponent(body)}`, {
+    //   baseURLForDataURL: url,
+    // });
+    // @ts-expect-error -- TSCONVERSION type mismatch
+    this.webview.current.loadURL(`data:${contentType},${encodeURIComponent(bodyWithBase)}`);
+
+    // This is kind of hacky but electron-context-menu fails to save images if this isn't here.
+    // @ts-expect-error -- TSCONVERSION type mismatch
+    this.webview.current.webContents = this.webview.current;
+    // @ts-expect-error -- TSCONVERSION type mismatch
+    this.webview.current.webContents.session = new EventEmitter();
+  }
+
   render() {
     const { webpreferences } = this.props;
     return (
-      <webview ref={this._handleSetWebViewRef} src="about:blank" webpreferences={webpreferences} />
+      <webview
+        ref={this.webview}
+        src="about:blank"
+        webpreferences={webpreferences}
+      />
     );
   }
 }
-
-export default ResponseWebView;
