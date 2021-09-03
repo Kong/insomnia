cfc1825826ad107af45084d9f0c6b3eaef4115bf
Ramón Márquez <ramn.marquez@gmail.com>
2021-09-03 18:42:12 -0400

    added test for new method

--- packages/insomnia-app/app/plugins/context/__tests__/app.test.ts
+++ packages/insomnia-app/app/plugins/context/__tests__/app.test.ts
@@ -1,10 +1,11 @@
-import electron from 'electron';
+import electron, { app } from 'electron';
 import { mocked } from 'ts-jest/utils';
 
 import { globalBeforeEach } from '../../../__jest__/before-each';
 import { RENDER_PURPOSE_SEND } from '../../../common/render';
 import * as modals from '../../../ui/components/modals';
 import * as plugin from '../app';
+import appConfig from '../../../../config/config.json';
 
 describe('init()', () => {
   beforeEach(globalBeforeEach);
@@ -17,6 +18,7 @@ describe('init()', () => {
       'clipboard',
       'dialog',
       'getPath',
+      'getInfo',
       'prompt',
       'showGenericModalDialog',
       'showSaveDialog',
@@ -103,6 +105,19 @@ describe('app.prompt()', () => {
       ],
     ]);
   });
+});
+
+describe('app.getInfo()', () => {
+  beforeEach(globalBeforeEach);
+
+  it('provides app info', async () => {
+    const result = plugin.init();
+    expect(result.app.getInfo()).toEqual({
+      "version": appConfig.version,
+      "platform": process.platform
+    })
+  });
+
 });
 
 describe('app.clipboard', () => {
