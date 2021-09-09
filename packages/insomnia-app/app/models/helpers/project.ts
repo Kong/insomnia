7dd6e7b562cbee1b0d2f663ec0395c3031b360ae
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2021-09-09 17:22:47 -0400

    makes sortProjects generic to handle full and partial project sorting

--- packages/insomnia-app/app/models/helpers/project.ts
+++ packages/insomnia-app/app/models/helpers/project.ts
@@ -2,9 +2,9 @@ import { ascend, descend, prop, sortWith } from 'ramda';
 
 import { isDefaultProject, isLocalProject, isRemoteProject, Project } from '../project';
 
-export const sortProjects = sortWith<Pick<Project, 'name' | 'remoteId' | '_id'>>([
+export const sortProjects = <T extends Pick<Project, 'name' | 'remoteId' | '_id'>>(projects: T[]) => sortWith<T>([
   descend(isDefaultProject),
   descend(isLocalProject),
   descend(isRemoteProject),
   ascend(prop('name')),
-]);
+], projects);
