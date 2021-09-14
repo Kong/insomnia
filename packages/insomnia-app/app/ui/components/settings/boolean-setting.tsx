d77f3909fdd76ddc9372f07c15dd6be8baf5e48e
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2021-09-14 19:39:34 -0400

    displayment for config-controlled boolean settings

--- packages/insomnia-app/app/ui/components/settings/boolean-setting.tsx
+++ packages/insomnia-app/app/ui/components/settings/boolean-setting.tsx
@@ -1,15 +1,17 @@
+import { Settings } from 'insomnia-common';
 import React, { ChangeEvent, FC, useCallback } from 'react';
 import { useSelector } from 'react-redux';
 
 import * as models from '../../../models/index';
-import { BaseSettings } from '../../../models/settings';
+import { isConfigControlledSetting } from '../../../models/settings';
 import { selectSettings } from '../../redux/selectors';
 import { HelpTooltip } from '../help-tooltip';
 import { Tooltip } from '../tooltip';
+import { ControlledByConfig } from './controlled-by-config';
 
 export const BooleanSetting: FC<{
   label: string;
-  setting: keyof BaseSettings;
+  setting: keyof Settings;
   help?: string;
   forceRestart?: boolean;
 }> = ({
@@ -24,6 +26,8 @@ export const BooleanSetting: FC<{
     throw new Error(`Invalid boolean setting name ${setting}`);
   }
 
+  const isControlled = isConfigControlledSetting(setting);
+
   const onChange = useCallback(async (event: ChangeEvent<HTMLInputElement>) => {
     const { checked } = event.currentTarget;
     await models.settings.patch({
@@ -32,22 +36,25 @@ export const BooleanSetting: FC<{
   }, [setting]);
 
   return (
-    <div className="form-control form-control--thin">
-      <label className="inline-block">
-        {label}
-        {help && <HelpTooltip className="space-left">{help}</HelpTooltip>}
-        {forceRestart && (
-          <Tooltip message="Will restart the app" className="space-left">
-            <i className="fa fa-refresh super-duper-faint" />
-          </Tooltip>
-        )}
-        <input
-          checked={Boolean(settings[setting])}
-          name={setting}
-          onChange={onChange}
-          type="checkbox"
-        />
-      </label>
-    </div>
+    <ControlledByConfig setting={setting}>
+      <div className="form-control form-control--thin">
+        <label className="inline-block">
+          {label}
+          {help && <HelpTooltip className="space-left">{help}</HelpTooltip>}
+          {forceRestart && (
+            <Tooltip message="Will restart the app" className="space-left">
+              <i className="fa fa-refresh super-duper-faint" />
+            </Tooltip>
+          )}
+          <input
+            checked={Boolean(settings[setting])}
+            name={setting}
+            onChange={onChange}
+            type="checkbox"
+            disabled={isControlled}
+          />
+        </label>
+      </div>
+    </ControlledByConfig>
   );
 };
---
+++ packages/insomnia-app/app/ui/components/settings/controlled-by-config.tsx
@@ -0,0 +1,62 @@
+import { Settings } from 'insomnia-common';
+import React, { FC } from 'react';
+import styled from 'styled-components';
+
+import { isConfigControlledSetting } from '../../../models/settings';
+import HelpTooltip from '../help-tooltip';
+
+const Wrapper = styled.div({
+  position: 'relative',
+  marginBottom: 4,
+  marginTop: 14,
+});
+
+const ControlledWrapper = styled.div({
+  border: '1px solid var(--color-surprise)',
+  borderRadius: 5,
+  padding: '6px 10px',
+  position: 'relative',
+  '&:hover': {
+    cursor: 'not-allowed',
+  },
+});
+
+const ControlledOverlay = styled.div({
+  position: 'absolute',
+  top: 0,
+  right: 0,
+  bottom: 0,
+  left: 0,
+  zIndex: 2,
+});
+
+const ControlledHelper = styled.div({
+  position: 'absolute',
+  top: -8,
+  right: 8,
+  background: 'var(--color-bg)',
+  zIndex: 3,
+  padding: '0 5px',
+});
+
+export const ControlledByConfig: FC<{ setting: keyof Settings }> = ({ children, setting }) => {
+  const controlled = isConfigControlledSetting(setting);
+
+  const helpText = `this value is controlled by \`settings.${setting}\` in your Insomnia Config`;
+  if (!controlled) {
+    return <div>{children}</div>;
+  }
+
+  return (
+    <Wrapper>
+      <ControlledHelper>
+        <span>controlled by insomnia config</span>{' '}
+        <HelpTooltip>{helpText}</HelpTooltip>
+      </ControlledHelper>
+      <ControlledWrapper>
+        <ControlledOverlay title={helpText} />
+        {children}
+      </ControlledWrapper>
+    </Wrapper>
+  );
+};
