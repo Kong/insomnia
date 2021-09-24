102e69107500628dbfdc3800ac215e1d56380dde
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2021-09-24 16:24:59 -0400

    adds verify-pkg script and cross-compile targets

--- packages/insomnia-smoke-test/cli/app.test.ts
+++ packages/insomnia-smoke-test/cli/app.test.ts
@@ -1,10 +1,43 @@
+import { isNonGlibcLinux } from 'detect-libc';
 import execa from 'execa';
 import { getBinPathSync } from 'get-bin-path';
+import { platform } from 'os';
 import { resolve } from 'path';
+
+const isAlpineLinux = () => {
+  const nodePlatform = platform();
+  if (nodePlatform !== 'linux') {
+    return false;
+  }
+
+  return isNonGlibcLinux;
+};
+
+/** the `pkg` npm has its own platforms that it appends on the end of binaries which are similar but different to NodeJS */
+const getPkgPlatform = () => {
+  const nodePlatform = platform();
+
+  switch (nodePlatform) {
+    case 'darwin':
+      return 'macos';
+
+    case 'win32':
+      return 'win';
+
+    case 'linux':
+      if (isAlpineLinux()) {
+        return 'alpine';
+      }
+      return 'linux';
+
+    default:
+      throw new Error(`you are running smoke tests on an unsupported platform: ${nodePlatform}`);
+  }
+};
 
 describe('run test', () => {
   const npmPackage = getBinPathSync({ cwd: '../insomnia-inso' });
-  const singleExecutable = resolve('../insomnia-inso/binaries/insomnia-inso');
+  const singleExecutable = resolve(`../insomnia-inso/binaries/insomnia-inso-${getPkgPlatform()}`);
 
   it.each([
     npmPackage,
