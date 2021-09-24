a6107c3a9703ebb40f07f12f745fefe69e2381c7
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2021-09-24 14:03:20 -0400

    adds failing test for simultaneously smoke testing the single executable
    also fixes some other things, e.g. using the (now deprecated) `--appDataDir` option.
    
    also adds a (hopefully) helpful message for if this breaks in the future because of the files moving

--- packages/insomnia-smoke-test/cli/app.test.ts
+++ packages/insomnia-smoke-test/cli/app.test.ts
@@ -1,12 +1,26 @@
 import execa from 'execa';
 import { getBinPathSync } from 'get-bin-path';
+import { resolve } from 'path';
 
 describe('run test', () => {
-  it('can run unit test', () => {
-    const command = 'run test -a fixtures/inso-nedb TestSuite -e Dev';
+  const npmPackage = getBinPathSync({ cwd: '../insomnia-inso' });
+  const singleExecutable = resolve('../insomnia-inso/binaries/insomnia-inso');
 
-    // @ts-expect-error TSCONVERSION
-    const { failed } = execa.sync(getBinPathSync({ cwd: '../insomnia-inso' }), command.split(' '));
+  it.each([
+    npmPackage,
+    singleExecutable,
+  ])('can run unit test with %s', binPath => {
+    if (!binPath) {
+      fail('The inso executable was not found.  Check if it has moved.');
+    }
+
+    const { failed } = execa.sync(binPath, [
+      'run',
+      '--src', 'fixtures/inso-nedb',
+      'test',
+      '--env', 'Dev',
+      'TestSuite',
+    ]);
 
     expect(failed).toBe(false);
   });
