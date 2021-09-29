6adda5c1f8f3b344e70b0dc297ad1a9bb60d3570
Opender Singh <opender.singh@konghq.com>
2021-09-30 00:17:01 +1300

    move jsonpath plugin to ts

---
+++ plugins/insomnia-plugin-jsonpath/src/index.ts
@@ -0,0 +1,51 @@
+import { JSONPath } from 'jsonpath-plus';
+
+// The context type should come from a standalone package which publishes plugin type definitions
+type Context = unknown;
+
+export const templateTags = [
+  {
+    displayName: 'JSONPath',
+    name: 'jsonpath',
+    description: 'pull data from JSON strings with JSONPath',
+    args: [
+      {
+        displayName: 'JSON string',
+        type: 'string',
+      },
+      {
+        displayName: 'JSONPath Filter',
+        encoding: 'base64', // So it doesn't cause syntax errors
+        type: 'string',
+      },
+    ],
+    run(_: Context, jsonString: string, filter: string) {
+      let body;
+      try {
+        body = JSON.parse(jsonString);
+      } catch (err) {
+        if (err instanceof Error) {
+          throw new Error(`Invalid JSON: ${err.message}`);
+        } else {
+          throw err;
+        }
+      }
+
+      let results;
+      try {
+        results = JSONPath({
+          json: body,
+          path: filter,
+        });
+      } catch (err) {
+        throw new Error(`Invalid JSONPath query: ${filter}`);
+      }
+
+      if (results.length === 0) {
+        throw new Error(`JSONPath query returned no results: ${filter}`);
+      }
+
+      return results[0];
+    },
+  },
+];
