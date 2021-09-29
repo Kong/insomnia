fc32856e55d4f757f02f2839623c2696ed84f70f
James Gatz <jamesgatzos@gmail.com>
2021-09-29 09:44:28 +0200

    Use forwardRef for the Pane components (#4053)
    * Use forwardRef for the Pane components
    
    * Update packages/insomnia-app/app/ui/containers/app.tsx
    
    Co-authored-by: Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>

--- packages/insomnia-app/app/ui/components/sidebar/sidebar.tsx
+++ packages/insomnia-app/app/ui/components/sidebar/sidebar.tsx
@@ -1,9 +1,7 @@
-import { autoBindMethodsForReact } from 'class-autobind-decorator';
 import classnames from 'classnames';
-import React, { PureComponent, ReactNode } from 'react';
+import React, { forwardRef, memo, ReactNode } from 'react';
 
 import {
-  AUTOBIND_CFG,
   COLLAPSE_SIDEBAR_REMS,
   SIDEBAR_SKINNY_REMS,
 } from '../../../common/constants';
@@ -25,18 +23,19 @@ interface Props {
   workspaces: Workspace[];
 }
 
-@autoBindMethodsForReact(AUTOBIND_CFG)
-export class Sidebar extends PureComponent<Props> {
-  render() {
+export const Sidebar = memo(
+  forwardRef<HTMLElement, Props>((props, ref) => {
     const {
       activeEnvironment,
       children,
       environmentHighlightColorStyle,
       hidden,
       width,
-    } = this.props;
+    } = props;
+
     return (
       <aside
+        ref={ref}
         className={classnames('sidebar', 'theme--sidebar', {
           'sidebar--hidden': hidden,
           'sidebar--skinny': width < SIDEBAR_SKINNY_REMS,
@@ -54,5 +53,7 @@ export class Sidebar extends PureComponent<Props> {
         {children}
       </aside>
     );
-  }
-}
+  })
+);
+
+Sidebar.displayName = 'Sidebar';
