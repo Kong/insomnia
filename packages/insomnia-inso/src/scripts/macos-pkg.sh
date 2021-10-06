46d87f53c9ac07035f37e11cb35f95b627a6c52f
Opender Singh <opender.singh@konghq.com>
2021-10-07 12:46:57 +1300

    update scopes

--- packages/insomnia-inso/src/scripts/macos-pkg.sh
+++ packages/insomnia-inso/src/scripts/macos-pkg.sh
@@ -11,18 +11,15 @@ security default-keychain -s $KEYCHAIN
 security set-keychain-settings $KEYCHAIN
 security unlock-keychain -p $KEYCHAIN_PASSWORD $KEYCHAIN
 
-# Unlock the keychain
-security unlock-keychain -p $KEYCHAIN_PASSWORD $KEYCHAIN
-
 # Import certificate
 echo $MACOS_CERTIFICATE_LINK | base64 --decode > certificate.p12
-security import certificate.p12 -k $KEYCHAIN -P $MACOS_CERTIFICATE_PWD -T /usr/bin/codesign -T /usr/bin/productbuild
+security import certificate.p12 -k $KEYCHAIN -P $MACOS_CERTIFICATE_PWD -T /usr/bin/codesign -T /usr/bin/pkgbuild
 
 # Detect the identity
 IDENTITY=$(security find-identity -v -p codesigning $KEYCHAIN | head -1 | grep '"' | sed -e 's/[^"]*"//' -e 's/".*//')
 
 # New requirement for MacOS 10.12+
-security set-key-partition-list -S apple-tool:,apple:,codesign:,productbuild: -s -k $KEYCHAIN_PASSWORD $KEYCHAIN
+security set-key-partition-list -S apple-tool:,apple:,codesign:,pkgbuild: -s -k $KEYCHAIN_PASSWORD $KEYCHAIN
 
 # Create a staging area for the installer package.
 mkdir -p usr/local/bin
