d098a6b68a1f3a5374acf5166ef2eac517e8ce86
Opender Singh <opender.singh@konghq.com>
2021-10-07 00:25:09 +1300

    sign app

--- packages/insomnia-inso/src/scripts/macos-pkg.sh
+++ packages/insomnia-inso/src/scripts/macos-pkg.sh
@@ -15,13 +15,16 @@ security unlock-keychain -p $KEYCHAIN_PASSWORD $KEYCHAIN
 
 # Import certificate
 echo $MACOS_CERTIFICATE_LINK | base64 --decode > certificate.p12
-security import certificate.p12 -k $KEYCHAIN -P $MACOS_CERTIFICATE_PWD -T /usr/bin/codesign
+security import certificate.p12 -k $KEYCHAIN -P $MACOS_CERTIFICATE_PWD -T /usr/bin/codesign -T /usr/bin/productbuild
 
 # Detect the identity
 IDENTITY=$(security find-identity -v -p codesigning $KEYCHAIN | head -1 | grep '"' | sed -e 's/[^"]*"//' -e 's/".*//')
 
 # New requirement for MacOS 10.12+
-security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $KEYCHAIN_PASSWORD $KEYCHAIN
+security set-key-partition-list -S apple-tool:,apple:,codesign:,productbuild: -s -k $KEYCHAIN_PASSWORD $KEYCHAIN
 
-# Build and sign
+# Sign the app
+/usr/bin/codesign --force -v -s $IDENTITY packages/insomnia-inso/binaries/inso
+
+# Create and sign the package
 productbuild --sign $IDENTITY --component packages/insomnia-inso/binaries/inso /Applications packages/insomnia-inso/compressed/inso-macos.pkg
