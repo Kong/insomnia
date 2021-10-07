29f8d25f9eee4d52bb6c2fd0bb5b8754beb0d16e
Opender Singh <opender.singh@konghq.com>
2021-10-07 17:30:14 +1300

    add notarization step

--- packages/insomnia-inso/src/scripts/macos-pkg.sh
+++ packages/insomnia-inso/src/scripts/macos-pkg.sh
@@ -16,15 +16,8 @@ echo $MACOS_CERTIFICATE_LINK | base64 --decode > certificate.p12
 security import certificate.p12 -k $KEYCHAIN -P $MACOS_CERTIFICATE_PWD -T /usr/bin/codesign -T /usr/bin/pkgbuild
 
 # Detect the identity
-ALL_IDENTITIES=$(security find-identity -v $KEYCHAIN | sed -e 's/[^"]*"//' -e 's/".*//')
-APP_IDENTITY=$(security find-identity -v -p codesigning $KEYCHAIN | grep 'Application' | sed -e 's/[^"]*"//' -e 's/".*//')
+APP_IDENTITY=$(security find-identity -v $KEYCHAIN | grep 'Application' | sed -e 's/[^"]*"//' -e 's/".*//')
 INSTALL_IDENTITY=$(security find-identity -v $KEYCHAIN | grep 'Installer' | sed -e 's/[^"]*"//' -e 's/".*//')
-
-echo "::group::Debugging identities"
-echo "all - $ALL_IDENTITIES"
-echo "app - $APP_IDENTITY"
-echo "install - $INSTALL_IDENTITY"
-echo "::endgroup::"
 
 # New requirement for MacOS 10.12+
 security set-key-partition-list -S apple-tool:,apple:,codesign:,pkgbuild: -s -k $KEYCHAIN_PASSWORD $KEYCHAIN
@@ -43,14 +36,13 @@ cp binaries/inso macos-installer/bin
 # Based on https://developer.apple.com/forums/thread/128166
 # Build the package
 # TODO: add version here
-# TODO: use installer signing identity (instead of application signing identity)
 mkdir compressed
-pkgbuild --identifier $BUNDLE_ID --sign "$INSTALL_IDENTITY" --keychain $KEYCHAIN --timestamp --root macos-installer/bin --install-location /usr/local/ compressed/$PKG_NAME
+pkgbuild --identifier $BUNDLE_ID --sign "$INSTALL_IDENTITY" --keychain $KEYCHAIN --timestamp --root macos-installer/bin --install-location /usr/local/bin compressed/$PKG_NAME
 
 # # Based on https://developer.apple.com/documentation/security/notarizing_macos_software_before_distribution/customizing_the_notarization_workflow
 # # Notarise
-# xcrun notarytool submit compressed/$PKG_NAME --apple-id $APPLE_ID --password $APPLE_ID_PASSWORD --team-id TODO --wait
+xcrun notarytool submit compressed/$PKG_NAME --apple-id $APPLE_ID --password $APPLE_ID_PASSWORD --wait
 
 # # Based on https://developer.apple.com/forums/thread/128166
 # # Staple
-# xcrun stapler staple compressed/$PKG_NAME
+xcrun stapler staple compressed/$PKG_NAME
