e539061afd845a127fe0f0e00fe3e327fb18e2d7
Opender Singh <opender.singh@konghq.com>
2021-10-07 15:11:13 +1300

    wip

--- packages/insomnia-inso/src/scripts/macos-pkg.sh
+++ packages/insomnia-inso/src/scripts/macos-pkg.sh
@@ -20,9 +20,11 @@ ALL_IDENTITIES=$(security find-identity -v -p codesigning $KEYCHAIN | sed -e 's/
 APP_IDENTITY=$(security find-identity -v -p codesigning $KEYCHAIN | grep 'Application' | sed -e 's/[^"]*"//' -e 's/".*//')
 INSTALL_IDENTITY=$(security find-identity -v -p codesigning $KEYCHAIN | grep 'Installer' | sed -e 's/[^"]*"//' -e 's/".*//')
 
-echo "::debug::all - $ALL_IDENTITIES"
-echo "::debug::app - $APP_IDENTITY"
-echo "::debug::install - $INSTALL_IDENTITY"
+echo "::group::Debugging identities"
+echo "all - $ALL_IDENTITIES"
+echo "app - $APP_IDENTITY"
+echo "install - $INSTALL_IDENTITY"
+echo "::endgroup::"
 
 # New requirement for MacOS 10.12+
 security set-key-partition-list -S apple-tool:,apple:,codesign:,pkgbuild: -s -k $KEYCHAIN_PASSWORD $KEYCHAIN
