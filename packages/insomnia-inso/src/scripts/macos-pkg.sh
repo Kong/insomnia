d23cc2f118f80d800427dafbe0a49edcd445dda0
Opender Singh <opender.singh@konghq.com>
2021-10-07 14:05:25 +1300

    wip

--- packages/insomnia-inso/src/scripts/macos-pkg.sh
+++ packages/insomnia-inso/src/scripts/macos-pkg.sh
@@ -17,7 +17,6 @@ security import certificate.p12 -k $KEYCHAIN -P $MACOS_CERTIFICATE_PWD -T /usr/b
 
 # Detect the identity
 APP_IDENTITY=$(security find-identity -v -p codesigning $KEYCHAIN | head -1 | grep 'Application' | sed -e 's/[^"]*"//' -e 's/".*//')
-INSTALL_IDENTITY=$(security find-identity -v -p codesigning $KEYCHAIN | head -1 | grep 'Installer' | sed -e 's/[^"]*"//' -e 's/".*//')
 
 # New requirement for MacOS 10.12+
 security set-key-partition-list -S apple-tool:,apple:,codesign:,pkgbuild: -s -k $KEYCHAIN_PASSWORD $KEYCHAIN
@@ -38,7 +37,7 @@ cp binaries/inso usr/local/bin
 # TODO: add version here
 # TODO: use installer signing identity (instead of application signing identity)
 mkdir compressed
-pkgbuild --identifier $BUNDLE_ID --sign "$INSTALL_IDENTITY" --keychain $KEYCHAIN --timestamp --root /usr/local/bin --install-location /usr/local/ compressed/$PKG_NAME
+pkgbuild --identifier $BUNDLE_ID --keychain $KEYCHAIN --timestamp --root /usr/local/bin --install-location /usr/local/ compressed/$PKG_NAME
 
 # # Based on https://developer.apple.com/documentation/security/notarizing_macos_software_before_distribution/customizing_the_notarization_workflow
 # # Notarise
