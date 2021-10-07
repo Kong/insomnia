1d3f28c4b9c0aed8c7ae78ad78bed49acb6e664b
Opender Singh <opender.singh@konghq.com>
2021-10-08 09:52:46 +1300

    make security less verbose and hard-code identity

--- packages/insomnia-inso/src/scripts/macos-pkg.sh
+++ packages/insomnia-inso/src/scripts/macos-pkg.sh
@@ -16,11 +16,12 @@ echo $MACOS_CERTIFICATE_LINK | base64 --decode > certificate.p12
 security import certificate.p12 -k $KEYCHAIN -P $MACOS_CERTIFICATE_PWD -T /usr/bin/codesign -T /usr/bin/pkgbuild
 
 # Detect the identity
-APP_IDENTITY=$(security find-identity -v $KEYCHAIN | grep 'Application' | sed -e 's/[^"]*"//' -e 's/".*//')
-INSTALL_IDENTITY=$(security find-identity -v $KEYCHAIN | grep 'Installer' | sed -e 's/[^"]*"//' -e 's/".*//')
+# APP_IDENTITY=$(security find-identity -v $KEYCHAIN | grep 'Application' | sed -e 's/[^"]*"//' -e 's/".*//')
+APP_IDENTITY="Developer ID Application: Kong Inc. (FX44YY62GV)"
+INSTALL_IDENTITY="Developer ID Installer: Kong Inc. (FX44YY62GV)"
 
 # New requirement for MacOS 10.12+
-security set-key-partition-list -S apple-tool:,apple:,codesign:,pkgbuild: -s -k $KEYCHAIN_PASSWORD $KEYCHAIN
+security set-key-partition-list -q -S apple-tool:,apple:,codesign:,pkgbuild: -s -k $KEYCHAIN_PASSWORD $KEYCHAIN
 
 # Create a staging area for the installer package.
 mkdir -p macos-installer/bin
