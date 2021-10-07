17fe37881d785b649d8a489e387abd5b235817b5
Opender Singh <opender.singh@konghq.com>
2021-10-07 14:31:14 +1300

    wip

--- packages/insomnia-inso/src/scripts/macos-pkg.sh
+++ packages/insomnia-inso/src/scripts/macos-pkg.sh
@@ -16,28 +16,29 @@ echo $MACOS_CERTIFICATE_LINK | base64 --decode > certificate.p12
 security import certificate.p12 -k $KEYCHAIN -P $MACOS_CERTIFICATE_PWD -T /usr/bin/codesign -T /usr/bin/pkgbuild
 
 # Detect the identity
-APP_IDENTITY=$(security find-identity -v -p codesigning $KEYCHAIN | head -1 | grep 'Application' | sed -e 's/[^"]*"//' -e 's/".*//')
+APP_IDENTITY=$(security find-identity -v -p codesigning $KEYCHAIN | grep 'Application' | sed -e 's/[^"]*"//' -e 's/".*//')
+INSTALL_IDENTITY=$(security find-identity -v -p codesigning $KEYCHAIN | grep 'Installer' | sed -e 's/[^"]*"//' -e 's/".*//')
 
 # New requirement for MacOS 10.12+
 security set-key-partition-list -S apple-tool:,apple:,codesign:,pkgbuild: -s -k $KEYCHAIN_PASSWORD $KEYCHAIN
 
 # Create a staging area for the installer package.
-mkdir -p usr/local/bin
+mkdir -p macos-installer/bin
 
 # Copy the binary into the staging area.
-cp binaries/inso usr/local/bin
+cp binaries/inso macos-installer/bin
 
 # Based on https://developer.apple.com/forums/thread/128166
 # Based on https://developer.apple.com/forums/thread/669188
 # Sign the binary
-/usr/bin/codesign --force -s "$APP_IDENTITY" usr/local/bin/inso
+/usr/bin/codesign --force -s "$APP_IDENTITY" macos-installer/bin/inso
 
 # Based on https://developer.apple.com/forums/thread/128166
 # Build the package
 # TODO: add version here
 # TODO: use installer signing identity (instead of application signing identity)
 mkdir compressed
-pkgbuild --identifier $BUNDLE_ID --keychain $KEYCHAIN --timestamp --root /usr/local/bin --install-location /usr/local/ compressed/$PKG_NAME
+pkgbuild --identifier $BUNDLE_ID --sign "$INSTALL_IDENTITY" --keychain $KEYCHAIN --timestamp --root macos-installer/bin --install-location /usr/local/ compressed/$PKG_NAME
 
 # # Based on https://developer.apple.com/documentation/security/notarizing_macos_software_before_distribution/customizing_the_notarization_workflow
 # # Notarise
