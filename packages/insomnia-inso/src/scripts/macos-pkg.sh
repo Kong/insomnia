bfcb9c6832344e653ec33112df4d6a1dbd89cec5
Opender Singh <opender.singh@konghq.com>
2021-10-08 14:18:41 +1300

    update entitlements again

--- packages/insomnia-inso/src/scripts/macos-pkg.sh
+++ packages/insomnia-inso/src/scripts/macos-pkg.sh
@@ -42,7 +42,7 @@ echo $MACOS_CERTIFICATE | base64 --decode > certificate.p12
 security import certificate.p12 -k "$KEYCHAIN" -P "$MACOS_CERTIFICATE_PWD" -T /usr/bin/codesign -T /usr/bin/pkgbuild
 
 # New requirement for MacOS 10.12+
-security -q set-key-partition-list -S apple-tool:,apple:,codesign:,pkgbuild: -s -k "$KEYCHAIN_PASSWORD" "$KEYCHAIN"
+security set-key-partition-list -S apple-tool:,apple:,codesign:,pkgbuild: -s -k "$KEYCHAIN_PASSWORD" "$KEYCHAIN"
 
 # Create a staging area for the installer package.
 mkdir -p "$STAGING_AREA"
