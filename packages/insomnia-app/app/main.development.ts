89c4cad587706f8749fe6c8dc2a60766889288d0
gatzjames <jamesgatzos@gmail.com>
2021-10-11 14:20:53 +0200

    Move all app.on('ready') calls in one place

--- packages/insomnia-app/app/main.development.ts
+++ packages/insomnia-app/app/main.development.ts
@@ -39,6 +39,12 @@ if (!isDevelopment()) {
 global.window = global.window || undefined;
 // When the app is first launched
 app.on('ready', async () => {
+  // There's no option that prevents Electron from fetching spellcheck dictionaries from Chromium's CDN and passing a non-resolving URL is the only known way to prevent it from fetching.
+  // see: https://github.com/electron/electron/issues/22995
+  // On macOS the OS spellchecker is used and therefore we do not download any dictionary files.
+  // This API is a no-op on macOS.
+  session.defaultSession.setSpellCheckerDictionaryDownloadURL('https://00.00/');
+
   if (isDevelopment()) {
     try {
       const extensions = [REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS];
