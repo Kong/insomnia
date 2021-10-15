acc18f89f52818d4b7038b2855767a197845eff0
Jarrett Confrey <jarrett.confrey@sap.com>
2021-10-15 16:23:42 -0700

    feat: add system ca support

--- packages/insomnia-app/app/network/ca-certs.js
+++ packages/insomnia-app/app/network/ca-certs.js
@@ -1,9 +1,12 @@
 // NOTE: this file is used by the `val-loader` webpack loader to export the NodeJS trust store during compile time.
 // Do not convert it to TypeScript.  It's a native node module by design.
-const tls = require('tls');
+require('linux-ca');
+require('mac-ca');
+require('win-ca');
+const https = require('https');
 
 module.exports = () => {
   return {
-    code: 'module.exports = `' + tls.rootCertificates.join('\n') + '`',
+    code: 'module.exports = `' + https.globalAgent.options.ca.join('\n') + '`',
   };
 };
