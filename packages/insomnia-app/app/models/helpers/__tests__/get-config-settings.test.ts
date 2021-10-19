656d8eab66678f8bf49bde2762baa9da26683efb
Opender Singh <opender.singh@konghq.com>
2021-10-20 12:11:06 +1300

    add test for get config settings error

--- packages/insomnia-app/app/models/helpers/__tests__/get-config-settings.test.ts
+++ packages/insomnia-app/app/models/helpers/__tests__/get-config-settings.test.ts
@@ -43,4 +43,30 @@ describe('getConfigSettings', () => {
     // Cleanup
     readFileSyncSpy.mockRestore();
   });
+
+  it('returns an error if validation result has errors', () => {
+    // Arrange
+    const invalidConfig = {
+      insomniaConfig: '1.0.0',
+      settings: {
+        random: 'abcd',
+      },
+    };
+
+    const readFileSyncSpy = jest.spyOn(fs, 'readFileSync').mockReturnValue(JSON.stringify(invalidConfig));
+    const consoleErrorMock = jest.spyOn(console, 'error').mockImplementation();
+
+    // Act
+    const result = getConfigSettings();
+
+    // Assert
+    expect(result).toHaveProperty('error');
+    if ('error' in result) {
+      expect(consoleErrorMock).toHaveBeenCalledWith('invalid insomnia config', result.error);
+    }
+
+    // Cleanup
+    readFileSyncSpy.mockRestore();
+    consoleErrorMock.mockRestore();
+  });
 });
