2a1a18c76976b0a94fd55d2d17940211debc2a2e
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2021-10-20 15:34:28 -0400

    fails validation if the config has a parse error (#4139)

--- packages/insomnia-app/app/common/validate-insomnia-config.ts
+++ packages/insomnia-app/app/common/validate-insomnia-config.ts
@@ -1,27 +1,48 @@
 import electron from 'electron';
 
-import { getConfigSettings } from '../models/helpers/settings';
+import { getConfigSettings, isConfigError, isParseError } from '../models/helpers/settings';
 import { exitApp } from './electron-helpers';
 
 export const validateInsomniaConfig = () => {
   const configSettings = getConfigSettings();
-  if ('error' in configSettings) {
-    const errors = configSettings.error.humanReadableErrors?.map(({ message, path, suggestion }, index) => ([
+
+  if (!('error' in configSettings)) {
+    return;
+  }
+
+  if (isParseError(configSettings)) {
+    const { syntaxError, configPath } = configSettings.error;
+    electron.dialog.showErrorBox('Invalid Insomnia Config', [
+      'Failed to parse JSON file for Insomnia Config.',
+      '',
+      '[Path]',
+      configPath,
+      '',
+      '[Syntax Error]',
+      syntaxError.message,
+    ].join('\n'));
+  } else if (isConfigError(configSettings)) {
+    const { humanReadableErrors, configPath } = configSettings.error;
+    const errors = humanReadableErrors.map(({ message, path, suggestion }, index) => ([
       `[Error ${index + 1}]`,
       `Path: ${path}`,
       `${message}.${suggestion ? `  ${suggestion}` : ''}`,
     ]).join('\n')).join('\n\n');
 
-    electron.dialog.showErrorBox('Invalid Insomnia Config',
-      [
-        `Your Insomnia Config was found to be invalid.  Please check the path below for the following error${configSettings.error.humanReadableErrors?.length > 1 ? 's' : ''}:`,
-        '',
-        '[Path]',
-        configSettings.error.configPath,
-        '',
-        errors,
-      ].join('\n'),
+    electron.dialog.showErrorBox('Invalid Insomnia Config', [
+      `Your Insomnia Config was found to be invalid.  Please check the path below for the following error${configSettings.error.humanReadableErrors?.length > 1 ? 's' : ''}:`,
+      '',
+      '[Path]',
+      configPath,
+      '',
+      errors,
+    ].join('\n'));
+  } else {
+    electron.dialog.showErrorBox(
+      'An unexpected error occured while parsing Insomnia Config',
+      JSON.stringify(configSettings),
     );
-    exitApp();
   }
+
+  exitApp();
 };
