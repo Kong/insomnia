9e35b75b668c599bac1d8da18f705cfeea202e7e
gatzjames <jamesgatzos@gmail.com>
2021-10-21 00:36:19 +0200

    fix lint error

--- packages/insomnia-smoke-test/modules/fixtures.ts
+++ packages/insomnia-smoke-test/modules/fixtures.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 export const getFixtureContent = async (fixturePath: string) => {
-  const buffer = await fs.promises.readFile(path.join(__dirname, '..', 'fixtures', fixturePath))
+  const buffer = await fs.promises.readFile(path.join(__dirname, '..', 'fixtures', fixturePath));
 
   const fixtureContent = buffer.toString('utf-8');
 
