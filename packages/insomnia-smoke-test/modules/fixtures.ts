6e835230d1ba53b018e4320287a80569cdc439a6
gatzjames <jamesgatzos@gmail.com>
2021-10-22 20:19:33 +0200

    Extend fixtures and improve import test cases

--- packages/insomnia-smoke-test/modules/fixtures.ts
+++ packages/insomnia-smoke-test/modules/fixtures.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-export const getFixtureContent = async (fixturePath: string) => {
+export const loadFixture = async (fixturePath: string) => {
   const buffer = await fs.promises.readFile(path.join(__dirname, '..', 'fixtures', fixturePath));
 
   const fixtureContent = buffer.toString('utf-8');
