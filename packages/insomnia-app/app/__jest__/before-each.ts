1ddf319722caf35cdaf037486c9b8fff8535a110
jackkav <jackkav@gmail.com>
2021-11-08 16:06:01 +0100

    fix tests

--- packages/insomnia-app/app/__jest__/before-each.ts
+++ packages/insomnia-app/app/__jest__/before-each.ts
@@ -1,3 +1,5 @@
+import electron from 'electron';
+
 import * as fetch from '../account/fetch';
 import { database as db } from '../common/database';
 import * as models from '../models';
@@ -5,6 +7,7 @@ import * as models from '../models';
 export async function globalBeforeEach() {
   // Setup the local database in case it's used
   fetch.setup('insomnia-tests', 'http://localhost:8000');
+  window.app = electron.app;
   await db.init(
     models.types(),
     {
