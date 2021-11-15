c23bbb7759cce0371714edb1f1598230394dbc90
jackkav <jackkav@gmail.com>
2021-11-15 10:57:33 +0100

    check only OpenAPI spec version

--- plugins/insomnia-plugin-kong-declarative-config/src/generate.js
+++ plugins/insomnia-plugin-kong-declarative-config/src/generate.js
@@ -1,38 +1,31 @@
-const YAML = require('yaml');
 const o2k = require('openapi-2-kong');
 
 module.exports = {
   label: 'Declarative Config',
-  docsLink: 'https://docs.insomnia.rest/insomnia/kong-for-kubernetes',
-  generate: async ({ contents, format, formatVersion }) => {
-    const isSupported = format === 'openapi' && formatVersion.match(/^3./);
-    const capitalisedInitial = format?.replace(/^\w/, c => c.toUpperCase());
-    const capitalisedFormat = format === 'openapi' ? 'OpenAPI' : capitalisedInitial;
-    // Return to signify that it's not supported
+  docsLink: 'https://docs.insomnia.rest/insomnia/declarative-config',
+  generate: async ({ contents, formatVersion }) => {
+    const isSupported = formatVersion && formatVersion.match(/^3./);
+
     if (!isSupported) {
       return {
         document: null,
-        error: `Unsupported spec format ${capitalisedFormat} ${formatVersion}`,
+        error: `Unsupported OpenAPI spec format ${formatVersion}`,
       };
     }
 
-    let result;
-
     try {
-      result = await o2k.generateFromString(contents, 'kong-declarative-config');
+      const result = await o2k.generateFromString(contents, 'kong-declarative-config');
+      // We know for certain the result.documents has only one entry for declarative config: packages/openapi-2-kong/src/declarative-config/generate.ts#L20
+      const declarativeConfig = result.documents?.[0]
+      return {
+        document: JSON.stringify(declarativeConfig, null, '\t'),
+        error: null,
+      };
     } catch (err) {
       return {
         document: null,
         error: err.message,
       };
     }
-    
-    // We know for certain the result.documents has only one entry for declarative config: packages/openapi-2-kong/src/declarative-config/generate.ts#L20
-    const document = JSON.stringify(result.documents?.[0], null, '\t');
-
-    return {
-      document,
-      error: null,
-    };
   },
 };
--- plugins/insomnia-plugin-kong-kubernetes-config/src/generate.js
+++ plugins/insomnia-plugin-kong-kubernetes-config/src/generate.js
@@ -3,39 +3,31 @@ const o2k = require('openapi-2-kong');
 
 module.exports = {
   label: 'Kong for Kubernetes',
-  docsLink: 'https://docs.insomnia.rest/insomnia/declarative-config',
-  generate: async ({ contents, format, formatVersion }) => {
-    const isSupported = format === 'openapi' && formatVersion.match(/^3./);
-    const capitalisedInitial = format?.replace(/^\w/, c => c.toUpperCase());
-    const capitalisedFormat = format === 'openapi' ? 'OpenAPI' : capitalisedInitial;
+  docsLink: 'https://docs.insomnia.rest/insomnia/kong-for-kubernetes',
+  generate: async ({ contents, formatVersion }) => {
+    const isSupported = formatVersion && formatVersion.match(/^3./);
 
-    // Return to signify that it's not supported
     if (!isSupported) {
       return {
         document: null,
-        error: `Unsupported spec format ${capitalisedFormat} ${formatVersion}`,
+        error: `Unsupported OpenAPI spec format ${formatVersion}`,
       };
     }
 
-    let result;
-
     try {
-      result = await o2k.generateFromString(contents, 'kong-for-kubernetes');
+      const result = await o2k.generateFromString(contents, 'kong-for-kubernetes');
+      const yamlDocs = result.documents.map(d => YAML.stringify(d));
+
+      return {
+        // Join the YAML docs with "---" and strip any extra newlines surrounding them
+        document: yamlDocs.join('\n---\n').replace(/\n+---\n+/g, '\n---\n'),
+        error: null,
+      };
     } catch (err) {
       return {
         document: null,
         error: err.message,
       };
     }
-
-    const yamlDocs = result.documents.map(d => YAML.stringify(d));
-
-    // Join the YAML docs with "---" and strip any extra newlines surrounding them
-    const document = yamlDocs.join('\n---\n').replace(/\n+---\n+/g, '\n---\n');
-
-    return {
-      document,
-      error: null,
-    };
   },
 };
