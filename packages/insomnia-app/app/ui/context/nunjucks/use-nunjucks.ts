db4b0bec5165bb8401eb8b523de736dc3446bec1
Opender Singh <opender.singh@konghq.com>
2021-11-30 14:14:30 +1300

    rename

--- packages/insomnia-app/app/ui/context/nunjucks/use-nunjucks.ts
+++ packages/insomnia-app/app/ui/context/nunjucks/use-nunjucks.ts
@@ -4,27 +4,14 @@ import { useNunjucksRenderFuncs } from './nunjucks-render-function-context';
 /**
  * Access functions useful for Nunjucks rendering
  */
-export const useNunjucks = () => {
-  return useNunjucksRenderFuncs();
-};
-
-const shouldEnableNunjucks = ({ enabledByReactContext, enabledByProp }: { enabledByReactContext: boolean; enabledByProp: boolean }) => {
-  // context: disabled, prop: disabled -> should disable
-  // context: disabled, prop: enabled  -> should disable
-  // context: enabled,  prop: disabled -> should disable
-  // context: enabled,  prop: enabled  -> should enable
-
-  if (enabledByReactContext && enabledByProp) {
-    return true;
-  }
-
-  return false;
-};
+export const useNunjucks = () => useNunjucksRenderFuncs();
 
 /**
  * Gated access to functions useful for Nunjucks rendering. Access is only granted if:
  *  1. Nunjucks is not diabled via the hook props
  *  2. Nunjucks is not disabled by the last NunjucksEnabledProvider in the React tree
+ *
+ * For ungated access, use `useNunjucks` instead
  */
 export const useGatedNunjucks = (props: { disabled?: boolean } = {}): Partial<ReturnType<typeof useNunjucks>> => {
   const { enabled } = useNunjucksEnabled();
@@ -32,7 +19,7 @@ export const useGatedNunjucks = (props: { disabled?: boolean } = {}): Partial<Re
 
   const shouldEnable = shouldEnableNunjucks({
     enabledByProp: !Boolean(props.disabled),
-    enabledByReactContext: enabled,
+    enabledByProvider: enabled,
   });
 
   if (shouldEnable) {
@@ -40,4 +27,17 @@ export const useGatedNunjucks = (props: { disabled?: boolean } = {}): Partial<Re
   }
 
   return {};
+};
+
+const shouldEnableNunjucks = ({ enabledByProvider, enabledByProp }: { enabledByProvider: boolean; enabledByProp: boolean }) => {
+  // context: disabled, prop: disabled -> should disable
+  // context: disabled, prop: enabled  -> should disable
+  // context: enabled,  prop: disabled -> should disable
+  // context: enabled,  prop: enabled  -> should enable
+
+  if (enabledByProvider && enabledByProp) {
+    return true;
+  }
+
+  return false;
 };
