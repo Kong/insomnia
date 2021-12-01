04a663e17ef4842c5baeb81532aeca9a1638a103
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2021-12-01 14:47:02 -0500

    revert usage of useDebounce since it stutters app boot

--- packages/insomnia-app/app/ui/hooks/use-font-mutations.ts
+++ packages/insomnia-app/app/ui/hooks/use-font-mutations.ts
@@ -1,26 +1,25 @@
 import { useLayoutEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { useDebounce } from 'react-use';
 import { selectSettings } from "../redux/selectors"
 
-const updateFontThing = (key: string, value: string | null) => document?.querySelector('html')?.style.setProperty(key, value);
+const updateFontStyle = (key: string, value: string | null) => document?.querySelector('html')?.style.setProperty(key, value);
 
 export const useFontMutations = () => {
   const settings = useSelector(selectSettings);
 
   useLayoutEffect(() => {
-    updateFontThing('--font-default', settings.fontInterface);
+    updateFontStyle('--font-default', settings.fontInterface);
   }, [settings.fontInterface]);
 
   useLayoutEffect(() => {
-    updateFontThing('--font-monospace', settings.fontMonospace);
+    updateFontStyle('--font-monospace', settings.fontMonospace);
   }, [settings.fontMonospace]);
 
   useLayoutEffect(() => {
-    updateFontThing('--font-ligatures', settings.fontVariantLigatures ? 'normal' : 'none');
+    updateFontStyle('--font-ligatures', settings.fontVariantLigatures ? 'normal' : 'none');
   }, [settings.fontVariantLigatures]);
 
-  useDebounce(() => {
-    updateFontThing('font-size', `${settings.fontSize}px`);
-  }, 100, [settings.fontSize]);
+  useLayoutEffect(() => {
+    updateFontStyle('font-size', `${settings.fontSize}px`);
+  }, [settings.fontSize]);
 };
