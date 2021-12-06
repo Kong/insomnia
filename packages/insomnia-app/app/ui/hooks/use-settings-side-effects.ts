0ef79c37a363c4b8c6e0696f5e079ba59a6dc34c
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2021-12-06 12:35:51 -0500

    makes settings side effects consistent

--- packages/insomnia-app/app/ui/hooks/use-settings-side-effects.ts
+++ packages/insomnia-app/app/ui/hooks/use-settings-side-effects.ts
@@ -1,6 +1,7 @@
-import { useLayoutEffect } from 'react';
+import { useEffect, useLayoutEffect } from 'react';
 import { useSelector } from 'react-redux';
 
+import { setMenuBarVisibility } from '../../common/electron-helpers';
 import { selectSettings } from '../redux/selectors';
 
 const updateFontStyle = (key: string, value: string | null) => document?.querySelector('html')?.style.setProperty(key, value);
@@ -23,4 +24,8 @@ export const useSettingsSideEffects = () => {
   useLayoutEffect(() => {
     updateFontStyle('font-size', `${settings.fontSize}px`);
   }, [settings.fontSize]);
+
+  useEffect(() => {
+    setMenuBarVisibility(!settings.autoHideMenuBar);
+  }, [settings.autoHideMenuBar]);
 };
