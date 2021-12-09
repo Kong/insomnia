5d936485322195ac95e5a409f8f974a0548dfe22
Opender Singh <opender.singh@konghq.com>
2021-12-09 14:12:28 +1300

    lint fix

--- packages/insomnia-app/app/ui/components/panes/grpc-request-pane/__tests__/use-action-handlers.test.ts
+++ packages/insomnia-app/app/ui/components/panes/grpc-request-pane/__tests__/use-action-handlers.test.ts
@@ -2,12 +2,12 @@ import { createBuilder } from '@develohpanda/fluent-builder';
 import { renderHook } from '@testing-library/react-hooks';
 import { ipcRenderer } from 'electron';
 import { mocked } from 'ts-jest/utils';
+
 import { GrpcRequestEventEnum } from '../../../../../common/ipc-events';
-import { GrpcMethodTypeEnum } from '../../../../../network/grpc/method';
-import { prepareGrpcMessage as _prepareGrpcMessage, prepareGrpcRequest as _prepareGrpcRequest } from '../../../../../network/grpc/prepare';
 import { grpcIpcMessageParamsSchema } from '../../../../../network/grpc/__schemas__/grpc-ipc-message-params-schema';
 import { grpcIpcRequestParamsSchema } from '../../../../../network/grpc/__schemas__/grpc-ipc-request-params-schema';
-
+import { GrpcMethodTypeEnum } from '../../../../../network/grpc/method';
+import { prepareGrpcMessage as _prepareGrpcMessage, prepareGrpcRequest as _prepareGrpcRequest } from '../../../../../network/grpc/prepare';
 import { GrpcDispatch } from '../../../../context/grpc';
 import { GrpcActionTypeEnum } from '../../../../context/grpc/grpc-actions';
 import useActionHandlers from '../use-action-handlers';
@@ -20,7 +20,7 @@ jest.mock('../../../../../network/grpc/prepare', () => {
   };
 
   return module;
-})
+});
 
 const mockGrpcDispatch = mocked<GrpcDispatch>(jest.fn());
 const prepareGrpcMessage = mocked(_prepareGrpcMessage);
@@ -32,33 +32,33 @@ const requestParamsBuilder = createBuilder(grpcIpcRequestParamsSchema);
 describe('useActionHandlers', () => {
   it('should send start', async () => {
     const requestParams = requestParamsBuilder.build();
-    
-    prepareGrpcRequest.mockResolvedValue(requestParams)
-    
-    const {result} = renderHook(() => useActionHandlers(requestParams.request._id, 'env', GrpcMethodTypeEnum.unary, mockGrpcDispatch));
+
+    prepareGrpcRequest.mockResolvedValue(requestParams);
+
+    const { result } = renderHook(() => useActionHandlers(requestParams.request._id, 'env', GrpcMethodTypeEnum.unary, mockGrpcDispatch));
 
     await result.current.start();
 
     expect(ipcRenderer.send).toHaveBeenCalledWith(GrpcRequestEventEnum.start, requestParams);
-    expect(mockGrpcDispatch).toHaveBeenCalledWith(expect.objectContaining({type: GrpcActionTypeEnum.clear}));
+    expect(mockGrpcDispatch).toHaveBeenCalledWith(expect.objectContaining({ type: GrpcActionTypeEnum.clear }));
   });
 
   it('should send stream', async () => {
     const messageParams = messageParamsBuilder.build();
-    
-    prepareGrpcMessage.mockResolvedValue(messageParams)
-    
-    const {result} = renderHook(() => useActionHandlers(messageParams.requestId, 'env', GrpcMethodTypeEnum.client, mockGrpcDispatch));
+
+    prepareGrpcMessage.mockResolvedValue(messageParams);
+
+    const { result } = renderHook(() => useActionHandlers(messageParams.requestId, 'env', GrpcMethodTypeEnum.client, mockGrpcDispatch));
 
     await result.current.stream();
 
     expect(ipcRenderer.send).toHaveBeenCalledWith(GrpcRequestEventEnum.sendMessage, messageParams);
-    expect(mockGrpcDispatch).toHaveBeenCalledWith(expect.objectContaining({type: GrpcActionTypeEnum.requestMessage}));
+    expect(mockGrpcDispatch).toHaveBeenCalledWith(expect.objectContaining({ type: GrpcActionTypeEnum.requestMessage }));
   });
 
   it('should send cancel', () => {
     const requestId = 'req';
-    const {result} = renderHook(() => useActionHandlers(requestId, 'env', GrpcMethodTypeEnum.client, mockGrpcDispatch));
+    const { result } = renderHook(() => useActionHandlers(requestId, 'env', GrpcMethodTypeEnum.client, mockGrpcDispatch));
 
     result.current.cancel();
 
@@ -68,7 +68,7 @@ describe('useActionHandlers', () => {
 
   it('should send commit', () => {
     const requestId = 'req';
-    const {result} = renderHook(() => useActionHandlers(requestId, 'env', GrpcMethodTypeEnum.client, mockGrpcDispatch));
+    const { result } = renderHook(() => useActionHandlers(requestId, 'env', GrpcMethodTypeEnum.client, mockGrpcDispatch));
 
     result.current.commit();
 
