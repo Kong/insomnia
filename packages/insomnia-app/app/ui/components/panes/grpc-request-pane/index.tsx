833cdfb005e0a389978e78dfcb9e0e2a61f61db1
Vincenzo De Petris <37916223+vincendep@users.noreply.github.com>
2021-12-10 18:34:48 +0100

    Handle request send hotkey for gRPC (#4126)
    Co-authored-by: Vincenzo De Petris <vincenzodepetris@gmail.it>
    Co-authored-by: Opender Singh <opender.singh@konghq.com>
    Co-authored-by: Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>

--- packages/insomnia-app/app/ui/components/panes/grpc-request-pane/index.tsx
+++ packages/insomnia-app/app/ui/components/panes/grpc-request-pane/index.tsx
@@ -1,8 +1,10 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useCallback } from 'react';
 import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 import styled from 'styled-components';
 
 import { getCommonHeaderNames, getCommonHeaderValues } from '../../../../common/common-headers';
+import { hotKeyRefs } from '../../../../common/hotkeys';
+import { executeHotKey } from '../../../../common/hotkeys-listener';
 import type { GrpcRequest } from '../../../../models/grpc-request';
 import type { Settings } from '../../../../models/settings';
 import { useGrpc } from '../../../context/grpc';
@@ -11,6 +13,7 @@ import { OneLineEditor } from '../../codemirror/one-line-editor';
 import { GrpcMethodDropdown } from '../../dropdowns/grpc-method-dropdown/grpc-method-dropdown';
 import { ErrorBoundary } from '../../error-boundary';
 import { KeyValueEditor } from '../../key-value-editor/key-value-editor';
+import { KeydownBinder } from '../../keydown-binder';
 import { GrpcTabbedMessages } from '../../viewers/grpc-tabbed-messages';
 import { Pane, PaneBody, PaneHeader } from '../pane';
 import useActionHandlers from './use-action-handlers';
@@ -66,103 +69,113 @@ export const GrpcRequestPane: FunctionComponent<Props> = ({
   // Used to refresh input fields to their default value when switching between requests.
   // This is a common pattern in this codebase.
   const uniquenessKey = `${forceRefreshKey}::${activeRequest._id}`;
-  return (
-    <Pane type="request">
-      <PaneHeader>
-        <StyledUrlBar>
-          <div className="method-grpc pad-right pad-left vertically-center">gRPC</div>
-          <StyledUrlEditor title={activeRequest.url}>
-            <OneLineEditor
-              key={uniquenessKey}
-              type="text"
-              forceEditor
-              defaultValue={activeRequest.url}
-              placeholder="grpcb.in:9000"
-              onChange={handleChange.url}
-              isVariableUncovered={isVariableUncovered}
-              getAutocompleteConstants={getExistingGrpcUrls}
-            />
-          </StyledUrlEditor>
-          <StyledDropdown>
-            <GrpcMethodDropdown
-              disabled={running}
-              methods={methods}
-              selectedMethod={method}
-              handleChange={handleChange.method}
-              handleChangeProtoFile={handleChange.protoFile}
-            />
-          </StyledDropdown>
 
-          <GrpcSendButton
-            running={running}
-            methodType={methodType}
-            handleCancel={handleAction.cancel}
-            handleStart={handleAction.start}
-          />
-        </StyledUrlBar>
-      </PaneHeader>
-      <PaneBody>
-        {methodType && (
-          <Tabs className="react-tabs" forceRenderTabPanel>
-            <TabList>
-              <Tab>
-                <button>{methodTypeLabel}</button>
-              </Tab>
-              <Tab>
-                <button>Headers</button>
-              </Tab>
-            </TabList>
-            <TabPanel className="react-tabs__tab-panel">
-              <GrpcTabbedMessages
-                uniquenessKey={uniquenessKey}
-                tabNamePrefix="Stream"
-                messages={requestMessages}
-                bodyText={activeRequest.body.text}
-                handleBodyChange={handleChange.body}
-                showActions={running && enableClientStream}
-                handleStream={handleAction.stream}
-                handleCommit={handleAction.commit}
+  const { start } = handleAction;
+  const _handleKeyDown = useCallback((event: KeyboardEvent) => {
+    if (method && !running) {
+      executeHotKey(event, hotKeyRefs.REQUEST_SEND, start);
+    }
+  }, [method, running, start]);
+
+  return (
+    <KeydownBinder onKeydown={_handleKeyDown}>
+      <Pane type="request">
+        <PaneHeader>
+          <StyledUrlBar>
+            <div className="method-grpc pad-right pad-left vertically-center">gRPC</div>
+            <StyledUrlEditor title={activeRequest.url}>
+              <OneLineEditor
+                key={uniquenessKey}
+                type="text"
+                forceEditor
+                defaultValue={activeRequest.url}
+                placeholder="grpcb.in:9000"
+                onChange={handleChange.url}
                 isVariableUncovered={isVariableUncovered}
+                getAutocompleteConstants={getExistingGrpcUrls}
               />
-            </TabPanel>
-            <TabPanel className="react-tabs__tab-panel">
-              <div className="tall wide scrollable-container">
-                <div className="scrollable">
-                  <ErrorBoundary key={uniquenessKey} errorClassName="font-error pad text-center">
-                    <KeyValueEditor
-                      sortable
-                      namePlaceholder="header"
-                      valuePlaceholder="value"
-                      descriptionPlaceholder="description"
-                      pairs={activeRequest.metadata}
-                      isVariableUncovered={isVariableUncovered}
-                      handleGetAutocompleteNameConstants={getCommonHeaderNames}
-                      handleGetAutocompleteValueConstants={getCommonHeaderValues}
-                      onChange={handleChange.metadata}
-                    />
-                  </ErrorBoundary>
+            </StyledUrlEditor>
+            <StyledDropdown>
+              <GrpcMethodDropdown
+                disabled={running}
+                methods={methods}
+                selectedMethod={method}
+                handleChange={handleChange.method}
+                handleChangeProtoFile={handleChange.protoFile}
+              />
+            </StyledDropdown>
+
+            <GrpcSendButton
+              running={running}
+              methodType={methodType}
+              handleCancel={handleAction.cancel}
+              handleStart={handleAction.start}
+            />
+          </StyledUrlBar>
+        </PaneHeader>
+        <PaneBody>
+          {methodType && (
+            <Tabs className="react-tabs" forceRenderTabPanel>
+              <TabList>
+                <Tab>
+                  <button>{methodTypeLabel}</button>
+                </Tab>
+                <Tab>
+                  <button>Headers</button>
+                </Tab>
+              </TabList>
+              <TabPanel className="react-tabs__tab-panel">
+                <GrpcTabbedMessages
+                  uniquenessKey={uniquenessKey}
+                  tabNamePrefix="Stream"
+                  messages={requestMessages}
+                  bodyText={activeRequest.body.text}
+                  handleBodyChange={handleChange.body}
+                  showActions={running && enableClientStream}
+                  handleStream={handleAction.stream}
+                  handleCommit={handleAction.commit}
+                  isVariableUncovered={isVariableUncovered}
+                />
+              </TabPanel>
+              <TabPanel className="react-tabs__tab-panel">
+                <div className="tall wide scrollable-container">
+                  <div className="scrollable">
+                    <ErrorBoundary key={uniquenessKey} errorClassName="font-error pad text-center">
+                      <KeyValueEditor
+                        sortable
+                        namePlaceholder="header"
+                        valuePlaceholder="value"
+                        descriptionPlaceholder="description"
+                        pairs={activeRequest.metadata}
+                        isVariableUncovered={isVariableUncovered}
+                        handleGetAutocompleteNameConstants={getCommonHeaderNames}
+                        handleGetAutocompleteValueConstants={getCommonHeaderValues}
+                        onChange={handleChange.metadata}
+                      />
+                    </ErrorBoundary>
+                  </div>
                 </div>
-              </div>
-            </TabPanel>
-          </Tabs>
-        )}
-        {!methodType && (
-          <div className="overflow-hidden editor vertically-center text-center">
-            <p className="pad super-faint text-sm text-center">
-              <i
-                className="fa fa-hand-peace-o"
-                style={{
-                  fontSize: '8rem',
-                  opacity: 0.3,
-                }}
-              />
-              <br />
-              <br />
-              Select a gRPC method from above
-            </p>
-          </div>
-        )}
-      </PaneBody>
-    </Pane>
+              </TabPanel>
+            </Tabs>
+          )}
+          {!methodType && (
+            <div className="overflow-hidden editor vertically-center text-center">
+              <p className="pad super-faint text-sm text-center">
+                <i
+                  className="fa fa-hand-peace-o"
+                  style={{
+                    fontSize: '8rem',
+                    opacity: 0.3,
+                  }}
+                />
+                <br />
+                <br />
+                Select a gRPC method from above
+              </p>
+            </div>
+          )}
+        </PaneBody>
+      </Pane>
+    </KeydownBinder>
   );
 };
