3c27c572e2a20c737ae2ee9db38a67373043949f
jackkav <jackkav@gmail.com>
2021-12-13 20:53:30 +0100

    simplify tests

--- packages/insomnia-smoke-test/specs/with_fixture.spec.ts
+++ packages/insomnia-smoke-test/specs/with_fixture.spec.ts
@@ -4,16 +4,32 @@ import path from 'path';
 import * as uuid from 'uuid';
 
 import { loadFixture } from '../modules/fixtures';
-import { insomniaTestFixture, runElectronApp } from '../playwright/test';
+const mainPath = path.resolve(__dirname, '..', '..', 'insomnia-app', 'build', 'main.min.js');
+const executablePath = path.resolve(__dirname, '..', '..', 'insomnia-app', 'node_modules/.bin', process.platform === 'win32' ? 'electron.cmd' : 'electron');
 
-const temporaryDataPath = path.join(os.tmpdir(), 'insomnia-smoke-test', `${uuid.v4()}`);
-test('can migrate designer', async ({ playwright }) => {
-  const designerDataPath = path.join(__dirname, '..', 'fixtures', 'basic-designer');
-  const options = { DESIGNER_DATA_PATH: designerDataPath, INSOMNIA_DATA_PATH: temporaryDataPath };
-  const insomniaApp = await runElectronApp(playwright._electron, options);
-  await insomniaApp.waitForEvent('window');
-  const page = await insomniaApp.firstWindow();
+const newPage = async ({ playwright, options }) => {
+  const electronApp = await playwright._electron.launch({
+    executablePath,
+    args: [mainPath],
+    env: {
+      ...process.env,
+      ...options,
+      ELECTRON_ENABLE_LOGGING: 'true',
+      PLAYWRIGHT: 'true',
+    },
+  });
+  await electronApp.waitForEvent('window');
+  const page = await electronApp.firstWindow();
   page.on('console', console.log);
+  if (process.platform === 'win32') await page.reload();
+  return { electronApp, page };
+};
+const sharedPathForMigrationTests = path.join(os.tmpdir(), 'insomnia-smoke-test', `${uuid.v4()}`);
+const designerPath = path.join(__dirname, '..', 'fixtures', 'basic-designer');
+
+test('can migrate designer', async ({ playwright }) => {
+  const options = { DESIGNER_DATA_PATH: designerPath, INSOMNIA_DATA_PATH: sharedPathForMigrationTests };
+  const { page } = await newPage({ playwright, options });
   await page.click('text=Copy Workspaces');
   await page.click('text=Copy Plugins');
   await page.click('text=Copy Designer Application Settings');
@@ -22,25 +38,23 @@ test('can migrate designer', async ({ playwright }) => {
   await page.close();
 });
 test('can see migrated stuff', async ({ playwright }) => {
-  const designerDataPath = path.join(__dirname, '..', 'fixtures', 'basic-designer');
-  const options = { DESIGNER_DATA_PATH: designerDataPath, INSOMNIA_DATA_PATH: temporaryDataPath };
-  const insomniaApp = await runElectronApp(playwright._electron, options);
-  await insomniaApp.waitForEvent('window');
-  const page = await insomniaApp.firstWindow();
+  const designerPath = path.join(__dirname, '..', 'fixtures', 'basic-designer');
+  const options = { DESIGNER_DATA_PATH: designerPath, INSOMNIA_DATA_PATH: sharedPathForMigrationTests };
+  const { page } = await newPage({ playwright, options });
   await page.click('text=Don\'t share usage analytics');
   await page.click('text=Skip');
   await page.click('text=BASIC-DESIGNER-FIXTURE');
 });
 
-insomniaTestFixture('can send requests', async ({ insomniaApp }) => {
-  await insomniaApp.waitForEvent('window');
-  const page = await insomniaApp.firstWindow();
+test('can send requests', async ({ playwright }) => {
+  const options = { INSOMNIA_DATA_PATH: path.join(os.tmpdir(), 'insomnia-smoke-test', `${uuid.v4()}`) };
+  const { page, electronApp } = await newPage({ playwright, options });
   await page.click('text=Don\'t share usage analytics');
   await page.click('text=Skip');
   await page.click('text=Create');
 
   const requestCollectionFixture = await loadFixture('insomnia-request-collection.yaml');
-  await insomniaApp.evaluate(async ({ clipboard }, requestCollectionFixture) => {
+  await electronApp.evaluate(async ({ clipboard }, requestCollectionFixture) => {
     // This runs in the main Electron process, parameter here is always
     // the result of the require('electron') in the main app script.
     return clipboard.writeText(requestCollectionFixture);
