819c9a32d4c32c9484d5bba44ce289ade48bca0a
Opender Singh <opender.singh@konghq.com>
2021-12-14 20:27:00 +1300

    fix test

--- packages/insomnia-app/app/__tests__/package.test.ts
+++ packages/insomnia-app/app/__tests__/package.test.ts
@@ -11,13 +11,13 @@ describe('package.json', () => {
     }
   });
 
-  it('packages must be included in webpack build or else errors happen', () => {
+  it('should be an external in webpack otherwise errors will happen', () => {
     // If this is built by Webpack it fails on multipart/form-data
-    expect(globalPackage.externalDependencies.includes('httpsnippet')).toBe(false);
+    expect(globalPackage.externalDependencies).toContain('httpsnippet');
   });
 
-  it('packages must NOT be included in webpack build or else errors happen', () => {
+  it('should NOT be an external in webpack otherwise errors happen', () => {
     // PDFJS breaks if not part of Webpack build
-    expect(globalPackage.externalDependencies.includes('pdfjs-dist')).toBe(true);
+    expect(globalPackage.externalDependencies).not.toContain('pdfjs-dist');
   });
 });
