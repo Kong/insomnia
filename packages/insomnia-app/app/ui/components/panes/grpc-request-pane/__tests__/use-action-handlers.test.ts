18565c52187ac48e44e0986f371b16e579ce3d31
Opender Singh <opender.singh@konghq.com>
2021-12-16 14:31:41 +1300

    add some comments

--- packages/insomnia-app/app/ui/components/panes/grpc-request-pane/__tests__/use-action-handlers.test.ts
+++ packages/insomnia-app/app/ui/components/panes/grpc-request-pane/__tests__/use-action-handlers.test.ts
@@ -18,12 +18,13 @@ jest.mock('../../../../../network/grpc/prepare', () => ({
   prepareGrpcRequest: jest.fn(),
 }));
 
+// Typed mocks using the mocked utility
 const mockGrpcDispatch = mocked<GrpcDispatch>(jest.fn());
 const prepareGrpcMessage = mocked(_prepareGrpcMessage);
 const prepareGrpcRequest = mocked(_prepareGrpcRequest);
-
 const ipcRenderer = mocked(_ipcRenderer);
 
+// Object builders
 const messageParamsBuilder = createBuilder(grpcIpcMessageParamsSchema);
 const requestParamsBuilder = createBuilder(grpcIpcRequestParamsSchema);
 
@@ -34,60 +35,71 @@ describe('useActionHandlers', () => {
     GrpcMethodTypeEnum.server,
     GrpcMethodTypeEnum.bidi,
   ])('should send start: %s', async method => {
+    // Arrange
     const requestParams = requestParamsBuilder.build();
-
     prepareGrpcRequest.mockResolvedValue(requestParams);
-
     const { result } = renderHook(() => useActionHandlers(requestParams.request._id, 'env', method, mockGrpcDispatch));
 
+    // Act
     await result.current.start();
 
+    // Assert
     expect(prepareGrpcRequest).toHaveBeenCalledWith(requestParams.request._id, 'env', method);
     expect(ipcRenderer.send).toHaveBeenCalledWith(GrpcRequestEventEnum.start, requestParams);
     expect(mockGrpcDispatch).toHaveBeenCalledWith(expect.objectContaining({ type: GrpcActionTypeEnum.clear }));
   });
 
   it('should send stream', async () => {
+    // Ararnge
     const messageParams = messageParamsBuilder.build();
-
     prepareGrpcMessage.mockResolvedValue(messageParams);
-
     const { result } = renderHook(() => useActionHandlers(messageParams.requestId, 'env', GrpcMethodTypeEnum.client, mockGrpcDispatch));
 
+    // Act
     await result.current.stream();
 
+    // Assert
     expect(ipcRenderer.send).toHaveBeenCalledWith(GrpcRequestEventEnum.sendMessage, messageParams);
     expect(mockGrpcDispatch).toHaveBeenCalledWith(expect.objectContaining({ type: GrpcActionTypeEnum.requestMessage }));
   });
 
   it('should send cancel', () => {
+    // Arrange
     const requestId = 'req';
     const { result } = renderHook(() => useActionHandlers(requestId, 'env', GrpcMethodTypeEnum.client, mockGrpcDispatch));
 
+    // Act
     result.current.cancel();
 
+    // Assert
     expect(ipcRenderer.send).toHaveBeenCalledWith(GrpcRequestEventEnum.cancel, requestId);
     expect(mockGrpcDispatch).not.toHaveBeenCalled();
   });
 
   it('should send commit', () => {
+    // Arrange
     const requestId = 'req';
     const { result } = renderHook(() => useActionHandlers(requestId, 'env', GrpcMethodTypeEnum.client, mockGrpcDispatch));
 
+    // Act
     result.current.commit();
 
+    // Assert
     expect(ipcRenderer.send).toHaveBeenCalledWith(GrpcRequestEventEnum.commit, requestId);
     expect(mockGrpcDispatch).not.toHaveBeenCalled();
   });
 
   it('should exit if request id is falsey', () => {
+    // Arrange
     const { result } = renderHook(() => useActionHandlers('', 'env', GrpcMethodTypeEnum.client, mockGrpcDispatch));
 
+    // Act
     result.current.start();
     result.current.stream();
     result.current.commit();
     result.current.cancel();
 
+    // Assert
     expect(ipcRenderer.send).not.toHaveBeenCalled();
     expect(mockGrpcDispatch).not.toHaveBeenCalled();
   });
