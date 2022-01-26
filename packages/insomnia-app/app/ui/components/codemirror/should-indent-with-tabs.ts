5a2cb2e0c230416cba7cff23ccd7441d09c53749
Opender Singh <opender94@gmail.com>
2022-01-26 13:42:18 +1300

    Update packages/insomnia-app/app/ui/components/codemirror/should-indent-with-tabs.ts
    Co-authored-by: James Gatz <jamesgatzos@gmail.com>

--- packages/insomnia-app/app/ui/components/codemirror/should-indent-with-tabs.ts
+++ packages/insomnia-app/app/ui/components/codemirror/should-indent-with-tabs.ts
@@ -5,7 +5,7 @@ export const shouldIndentWithTabs = ({ mode, indentWithTabs }: Pick<CodeEditorPr
   const isYaml = mode?.includes('yaml') || false;
 
   // OpenAPI is not valid when indented with Tabs
-  // TODO: OpenAPI in yaml is not valid with tabs, but in JSON is. Currently we do not differentiate and disable tabs regardless.
+  // TODO: OpenAPI in yaml is not valid with tabs, but in JSON is. Currently we do not differentiate and disable tabs regardless. INS-1390
   const isOpenAPI = mode === 'openapi';
 
   const actuallyIndentWithTabs = indentWithTabs && !isYaml && !isOpenAPI;
