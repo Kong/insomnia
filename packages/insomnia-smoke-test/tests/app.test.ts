aa8f9b919ad0a3ce0b0d414fd426bd9e7688c7d6
Filipe Freire <filipe.freire@konghq.com>
2022-02-15 12:20:47 +0000

    Change 200 OK assertion
    + Add assertions to PDF canvas
    
    + Add assertion to Raw Data preview.

--- packages/insomnia-smoke-test/tests/app.test.ts
+++ packages/insomnia-smoke-test/tests/app.test.ts
@@ -11,6 +11,9 @@ test.describe.parallel('suite', () => {
   });
 
   test('can send requests', async ({ app, page }) => {
+    test.setTimeout(120000);
+    const successTag = page.locator('header >> div >> .tag >> text=200 OK');
+
     await page.click('[data-testid="project"]');
     await page.click('text=Create');
 
@@ -22,25 +25,39 @@ test.describe.parallel('suite', () => {
 
     await page.click('button:has-text("GETsend JSON request")');
     await page.click('text=http://127.0.0.1:4010/pets/1Send >> button');
-    await page.click('text=200 OK');
+    await expect(successTag).toBeVisible();
+    // Check Raw data option
+    await page.click('button:has-text("Preview")');
+    await page.click('button:has-text("Raw Data")');
+    await page.click('text=/.*\\{"id":"1"\\}.*/');
+
     await page.click('button:has-text("GETsends dummy.csv request and shows rich response")');
     await page.click('text=http://127.0.0.1:4010/file/dummy.csvSend >> button');
-    await page.click('text=200 OK');
+    await expect(successTag).toBeVisible();
+
     await page.click('button:has-text("GETsends dummy.xml request and shows raw response")');
     await page.click('text=http://127.0.0.1:4010/file/dummy.xmlSend >> button');
-    await page.click('text=200 OK');
+    await expect(successTag).toBeVisible();
+
     await page.click('button:has-text("GETsends dummy.pdf request and shows rich response")');
     await page.click('text=http://127.0.0.1:4010/file/dummy.pdfSend >> button');
+    await expect(successTag).toBeVisible();
+    // Check if we load a canvas for the pdf file
+    const pdfElement = page.locator('canvas');
+    await expect(pdfElement).toBeVisible();
+
     await page.click('button:has-text("GETsends request with basic authentication")');
     await page.click('text=http://127.0.0.1:4010/auth/basicSend >> button');
-    await page.click('text=200 OK');
+    await expect(successTag).toBeVisible();
 
     // Send request, check if no cookie was sent (server will reply with received cookies + a new cookie as response)
     await page.click('button:has-text("GETsends request with cookie and get cookie in response")');
     await page.click('text=http://127.0.0.1:4010/cookiesSend >> button');
+    await expect(successTag).toBeVisible();
     await page.click('text=undefined');
     // Send request, check if new cookie sent by server in previous request is now also sent
     await page.click('text=http://127.0.0.1:4010/cookiesSend >> button');
+    await expect(successTag).toBeVisible();
     await page.click('text=insomnia-test-cookie=value123');
   });
 
