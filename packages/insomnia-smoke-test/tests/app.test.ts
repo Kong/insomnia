d21405c0644b31acdb9527bdee4c464f5612fc94
jackkav <jackkav@gmail.com>
2022-02-25 10:12:35 +0100

    simple oauth2 test

--- packages/insomnia-smoke-test/tests/app.test.ts
+++ packages/insomnia-smoke-test/tests/app.test.ts
@@ -3,6 +3,31 @@ import { expect } from '@playwright/test';
 import { loadFixture } from '../playwright/paths';
 import { test } from '../playwright/test';
 
+test('can make oauth2 requests', async ({ app, page }) => {
+  await page.click('[data-testid="project"]');
+  await page.click('text=Create');
+
+  const text = await loadFixture('oauth.yaml');
+  await app.evaluate(async ({ clipboard }, text) => clipboard.writeText(text), text);
+
+  await page.click('button:has-text("Clipboard")');
+  await page.click('text=CollectionOauth Testingjust now');
+
+  await page.click('button:has-text("Authorization Code")');
+  await page.click('button:has-text("GETNo PKCE")');
+
+  const [signinPage] = await Promise.all([
+    app.context().waitForEvent('page'),
+    page.click('text=Send'),
+  ]);
+
+  await signinPage.type('[name="login"]', 'admin');
+  await signinPage.type('[name="password"]', 'admin');
+  await signinPage.click('button:has-text("Sign-in")');
+  await signinPage.click('text=Continue');
+  await page.click('text=/.*"sub": "admin".*/');
+});
+
 test('can send requests', async ({ app, page }) => {
   test.slow(process.platform === 'darwin' || process.platform === 'win32', 'Slow app start on these platforms');
   const statusTag = page.locator('[data-testid="response-status-tag"]:visible');
