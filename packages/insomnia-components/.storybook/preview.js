5b7f4f07adcbaf022f1ae769a512ab6c56f3ee1c
David Marby <david@dmarby.se>
2022-03-01 12:37:00 +0100

    Remove storybook

--- packages/insomnia-components/.storybook/preview.js
+++
@@ -1,7 +0,0 @@
-import { configure, addDecorator } from '@storybook/react';
-import { withContexts } from '@storybook/addon-contexts/react';
-import { contexts } from './configs/contexts';
-
-addDecorator(withContexts(contexts));
-
-configure(require.context('../src', true, /\.stories\.tsx?$/), module);
