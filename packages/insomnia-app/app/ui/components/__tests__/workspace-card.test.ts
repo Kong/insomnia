9c1961b30a595098f58c64f557b31aec3d761c01
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2022-03-08 15:18:43 -0500

    hotfix: invalid OpenAPI spec versions causing the app to crash

--- packages/insomnia-app/app/ui/components/__tests__/workspace-card.test.ts
+++ packages/insomnia-app/app/ui/components/__tests__/workspace-card.test.ts
@@ -1,10 +1,16 @@
 import { getVersionDisplayment } from '../workspace-card';
 
 describe('getVersionDisplayment', () => {
-  it('returns falsey values as-is', () => {
-    expect(getVersionDisplayment(undefined)).toEqual(undefined);
+  it('returns null, undefined, and empty string as-is', () => {
     expect(getVersionDisplayment(null)).toEqual(null);
+    expect(getVersionDisplayment(undefined)).toEqual(undefined);
     expect(getVersionDisplayment('')).toEqual('');
+  });
+
+  it('returns numbers as strings', () => {
+    expect(getVersionDisplayment(0)).toEqual('v0'); // important to make sure we handle, since `0` is falsey
+    expect(getVersionDisplayment(1)).toEqual('v1');
+    expect(getVersionDisplayment(1.1)).toEqual('v1.1');
   });
 
   it('does not add a `v` if the string starts with one', () => {
