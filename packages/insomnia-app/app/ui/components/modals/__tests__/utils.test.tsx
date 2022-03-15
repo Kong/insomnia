d385ffa5d74f3251a32d697051bc0309e98c12af
Akiva Gordon <akivag29@gmail.com>
2022-03-15 11:18:59 -0400

    Fix request switcher selection cycling (#4519)
    Co-authored-by: Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
    Co-authored-by: Filipe Freire <livrofubia@gmail.com>

---
+++ packages/insomnia-app/app/ui/components/modals/__tests__/utils.test.tsx
@@ -0,0 +1,20 @@
+import { wrapToIndex } from '../utils';
+
+describe('wrapToIndex', () => {
+  it.each([
+    { index: 0, maxCount: 4, result: 0 },
+    { index: 1, maxCount: 4, result: 1 },
+    { index: 3, maxCount: 3, result: 0 },
+    { index: -1, maxCount: 3, result: 2 },
+    { index: -3, maxCount: 3, result: 0 },
+  ])('%p', ({ index, maxCount, result }) => {
+    expect(wrapToIndex(index, maxCount)).toBe(result);
+  });
+
+  it('throws when max is negative', () => {
+    const index = 1;
+    const maxCount = -1;
+    const execute = () => wrapToIndex(index, maxCount);
+    expect(execute).toThrow();
+  });
+});
