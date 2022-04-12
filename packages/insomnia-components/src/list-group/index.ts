af948f9b3d0ee42855de54eb3b33e2a3a7c607e2
gatzjames <jamesgatzos@gmail.com>
2022-04-12 10:24:50 +0200

    use vite lib mode for insomnia-components

--- packages/insomnia-components/src/list-group/index.ts
+++ packages/insomnia-components/src/list-group/index.ts
@@ -1,7 +1,7 @@
-export { ListGroupItem, ListGroupItemProps } from './list-group-item';
-export { ListGroup, ListGroupProps } from './list-group';
-export { UnitTestItem, UnitTestItemProps, TestItem } from './unit-test-item';
-export { UnitTestRequestSelector, UnitTestRequestSelectorProps } from './unit-test-request-selector';
-export { UnitTestResultBadge, UnitTestResultBadgeProps } from './unit-test-result-badge';
-export { UnitTestResultItem, UnitTestResultItemProps } from './unit-test-result-item';
-export { UnitTestResultTimestamp, UnitTestResultTimestampProps } from './unit-test-result-timestamp';
+export { ListGroupItem, type ListGroupItemProps } from './list-group-item';
+export { ListGroup, type ListGroupProps } from './list-group';
+export { UnitTestItem, type UnitTestItemProps, type TestItem } from './unit-test-item';
+export { UnitTestRequestSelector, type UnitTestRequestSelectorProps } from './unit-test-request-selector';
+export { UnitTestResultBadge, type UnitTestResultBadgeProps } from './unit-test-result-badge';
+export { UnitTestResultItem, type UnitTestResultItemProps } from './unit-test-result-item';
+export { UnitTestResultTimestamp, type UnitTestResultTimestampProps } from './unit-test-result-timestamp';
