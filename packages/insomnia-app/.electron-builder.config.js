385afea3cc893be9d6825043723f89d6d24749f7
jackkav <jackkav@gmail.com>
2022-04-18 19:02:46 +0200

    hard links workaround

--- packages/insomnia-app/.electron-builder.config.js
+++ packages/insomnia-app/.electron-builder.config.js
@@ -2,102 +2,91 @@
  * @type {import('electron-builder').Configuration}
  * @see https://www.electron.build/configuration/configuration
  */
-
+// NOTE: USE_HARD_LINKS
+// https://github.com/electron-userland/electron-builder/issues/4594#issuecomment-574653870
 const config = {
-    "appId": "com.insomnia.app",
-    electronVersion: '17.3.0',
-    "protocols": [
+    appId: "com.insomnia.app",
+    electronVersion: "17.3.0",
+    protocols: [
         {
-            "name": "Insomnia",
-            "role": "Viewer",
-            "schemes": [
-                "insomnia"
-            ]
-        }
+            name: "Insomnia",
+            role: "Viewer",
+            schemes: ["insomnia"],
+        },
     ],
     files: [
         {
-            "from": "./build",
-            "to": "./app",
-            "filter": ["**/*"]
+            from: "./build",
+            to: "./app",
+            filter: ["**/*"],
         },
-        './package.json',
+        "./package.json",
     ],
-    "publish": null,
-    "afterSign": "./scripts/afterSignHook.js",
-    "extraResources": [
+    publish: null,
+    afterSign: "./scripts/afterSignHook.js",
+    extraResources: [
         {
-            "from": "./bin",
-            "to": "./bin",
-            "filter": "yarn-standalone.js"
+            from: "./bin",
+            to: "./bin",
+            filter: "yarn-standalone.js",
         },
         {
-            "from": "./build",
-            "to": ".",
-            "filter": "opensource-licenses.txt"
-        }
+            from: "./build",
+            to: ".",
+            filter: "opensource-licenses.txt",
+        },
     ],
-    "fileAssociations": [],
-    "directories": {
-        "output": "dist"
+    fileAssociations: [],
+    directories: {
+        app: ".",
+        output: "dist",
     },
-    "mac": {
-        "hardenedRuntime": true,
-        "category": "public.app-category.developer-tools",
-        "entitlements": "./build/static/entitlements.mac.inherit.plist",
-        "artifactName": "Insomnia.Core-${version}.${ext}",
-        "target": [
-            "dmg",
-            "zip"
-        ],
-        "extendInfo": {
-            "NSRequiresAquaSystemAppearance": false
-        }
-    },
-    "dmg": {
-        "window": {
-            "width": 540,
-            "height": 380
+    mac: {
+        hardenedRuntime: true,
+        category: "public.app-category.developer-tools",
+        entitlements: "./build/static/entitlements.mac.inherit.plist",
+        artifactName: "Insomnia.Core-${version}.${ext}",
+        target: ["dmg", "zip"],
+        extendInfo: {
+            NSRequiresAquaSystemAppearance: false,
         },
-        "contents": [
+    },
+    dmg: {
+        window: {
+            width: 540,
+            height: 380,
+        },
+        contents: [
             {
-                "x": 130,
-                "y": 186
+                x: 130,
+                y: 186,
             },
             {
-                "x": 409,
-                "y": 186,
-                "type": "link",
-                "path": "/Applications"
-            }
-        ]
+                x: 409,
+                y: 186,
+                type: "link",
+                path: "/Applications",
+            },
+        ],
     },
-    "win": {
-        "target": [
-            "squirrel",
-            "portable"
-        ]
+    win: {
+        target: ["squirrel", "portable"],
     },
-    "squirrelWindows": {
-        "artifactName": "Insomnia.Core-${version}.${ext}",
-        "iconUrl": "https://github.com/kong/insomnia/blob/develop/packages/insomnia-app/app/icons/icon.ico?raw=true"
+    squirrelWindows: {
+        artifactName: "Insomnia.Core-${version}.${ext}",
+        iconUrl:
+            "https://github.com/kong/insomnia/blob/develop/packages/insomnia-app/app/icons/icon.ico?raw=true",
     },
-    "portable": {
-        "artifactName": "Insomnia.Core-${version}-portable.${ext}"
+    portable: {
+        artifactName: "Insomnia.Core-${version}-portable.${ext}",
     },
-    "linux": {
-        "artifactName": "Insomnia.Core-${version}.${ext}",
-        "executableName": "insomnia",
-        "synopsis": "The Collaborative API Client and Design Tool",
-        "category": "Development",
-        "target": [
-            "AppImage",
-            "deb",
-            "tar.gz",
-            "rpm",
-            "snap"
-        ]
-    }
+    linux: {
+        artifactName: "Insomnia.Core-${version}.${ext}",
+        executableName: "insomnia",
+        synopsis: "The Collaborative API Client and Design Tool",
+        category: "Development",
+        target: ["AppImage", "deb", "tar.gz", "rpm", "snap"],
+    },
 };
 
 module.exports = config;
