0f2dbfd181879fdbbd7220941bf041251a5a3b5e
Matthew Jones <matthewbtjones2000@gmail.com>
2022-04-19 16:09:49 +0100

    Bumped electron to 17.3.1

--- packages/insomnia-app/.babelrc.js
+++ packages/insomnia-app/.babelrc.js
@@ -1,7 +1,7 @@
 /** @type { import('@babel/core').TransformOptions } */
 module.exports = {
   presets: [
-    ['@babel/preset-env',{targets: {electron: '17.3.0'}}],
+    ['@babel/preset-env',{targets: {electron: '17.3.1'}}],
     '@babel/preset-typescript',
     '@babel/preset-react',
   ],
