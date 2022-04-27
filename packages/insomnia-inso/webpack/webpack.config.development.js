19c41500892bcff223aab0c15b4cf365795639a7
Jack Kavanagh <jackkav@gmail.com>
2022-04-27 16:03:26 +0100

    good old undocumented quirks of inso

--- packages/insomnia-inso/webpack/webpack.config.development.js
+++ packages/insomnia-inso/webpack/webpack.config.development.js
@@ -9,7 +9,7 @@ module.exports = merge(base, {
   plugins: [
     new webpack.DefinePlugin({
       __DEV__: true,
-      'process.env.DEFAULT_APP_NAME': JSON.stringify('insomnia'),
+      'process.env.DEFAULT_APP_NAME': JSON.stringify('insomnia-app'),
       'process.env.VERSION': JSON.stringify('dev'),
     }),
   ],
