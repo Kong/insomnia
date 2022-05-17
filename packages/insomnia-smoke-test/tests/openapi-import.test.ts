7cf6d01df8d42f7dc84eba3294fe587571c880ca
Filipe Freire <livrofubia@gmail.com>
2022-05-17 12:44:59 +0100

    Add other import tests

--- packages/insomnia-smoke-test/tests/openapi-import.test.ts
+++ packages/insomnia-smoke-test/tests/openapi-import.test.ts
@@ -2,76 +2,96 @@ import { expect } from '@playwright/test';
 
 import { test } from '../playwright/test';
 
-test.skip('can render Petstore internal example', async ({ page }) => {
+test('can render Petstore internal example and can render errors', async ({ page }) => {
   test.slow(process.platform === 'darwin' || process.platform === 'win32', 'Slow app start on these platforms');
   const specPreview = page.locator('.information-container');
+  const codeEditor = page.locator('[data-testid="CodeEditor"]');
   const tagsOnTextEditor = page.locator('[data-testid="CodeEditor"] >> text=tags').first();
   const noticeTable = page.locator('_react=NoticeTable');
-  await page.click('[data-testid="project"]');
-  await page.click('text=Create');
-  await page.click('button:has-text("Design Document")');
-  await page.click('.modal__footer >> text=Create');
+  await page.click('text=Design');
   await page.click('text=start from an example');
-
+  await expect(codeEditor).toContainText('3.0.0');
   await expect(specPreview).toContainText('This is a sample server Petstore server');
 
-  // can render errors
+  // Can render errors
   await tagsOnTextEditor.click();
   await tagsOnTextEditor.press('Tab');
   await expect(noticeTable).toContainText('oas3-schema Property `ta gs` is not expected to be here.');
 });
 
-// can import yaml from copy paste inside empty document
-// test.skip('can render spec and requests when pasting OA3 yaml spec directly inside empty document', async ({ app, page }) => {
-//   test.slow(process.platform === 'darwin' || process.platform === 'win32', 'Slow app start on these platforms');
-//   const specPreview = page.locator('.information-container');
-//   // const tagsOnTextEditor = page.locator('[data-testid="CodeEditor"] >> text=tags').first();
-//   // const noticeTable = page.locator('_react=NoticeTable');
-//   await page.click('[data-testid="project"]');
-//   await page.click('text=Create');
-//   await page.click('button:has-text("Design Document")');
-//   await page.click('.modal__footer >> text=Create');
-//   await page.click('pre[role="presentation"]:has-text("")');
+const specs = [
+  {
+    url: 'https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/examples/v3.0/petstore.json',
+    // url: 'http://localhost:4010/file/dummy-openapi.json', // TODO: fix import spec CORS issue
+    name: 'json',
+  },
+  {
+    url: 'https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/examples/v3.0/petstore.yaml',
+    // url: 'http://localhost:4010/file/dummy-openapi.yaml', // TODO: fix import spec CORS issue
+    name: 'yaml',
+  },
+];
 
-//   const text = await loadFixture('openapi3.yaml');
-//   await app.evaluate(async ({ clipboard }, text) => clipboard.writeText(text), text);
-//   await expect(specPreview).toContainText('Smoke Test API server');
-//   await expect(specPreview).toContainText('Make basic auth request');
-//   await expect(specPreview).toContainText('Get dummy PDF file');
-// });
+for (const spec of specs) {
+  test(`can import ${spec.name} from url from dashboard`, async ({ app, page }) => {
+    await app.firstWindow(); // TODO: Convenience method that waits for app to be ready
+    test.slow(process.platform === 'darwin' || process.platform === 'win32', 'Slow app start on these platforms');
+    const specPreview = page.locator('.information-container');
+    const codeEditor = page.locator('[data-testid="CodeEditor"]');
+    await page.click('[data-testid="project"]');
+    await page.click('text=Create');
+    await page.click('button:has-text("URL")');
 
-// can import yaml from copy paste via import/export for empty document
-// test('can render spec and requests when pasting OA3 yaml spec from import/export menu inside empty document', async ({ app, page }) => {
-//   test.slow(process.platform === 'darwin' || process.platform === 'win32', 'Slow app start on these platforms');
-//   // const specPreview = page.locator('.information-container');
-//   // const tagsOnTextEditor = page.locator('[data-testid="CodeEditor"] >> text=tags').first();
-//   // const noticeTable = page.locator('_react=NoticeTable');
-//   await page.click('[data-testid="project"]');
-//   await page.click('text=Create');
-//   await page.click('button:has-text("Design Document")');
-//   await page.click('.modal__footer >> text=Create');
+    // TODO: fix modal flakiness
+    await page.click('input[id="prompt-input"]', { force: true });
+    await page.fill('input[id="prompt-input"]', spec.url, { force: true });
+    await page.click('text=Fetch and Import');
 
-//   console.log('debug');
+    await page.click('button:has-text("New")');
+    await page.click('text=Design Document');
+    await page.click('text=Ok');
+    await page.click('text=Swagger Petstore 1.0.0');
+    await expect(codeEditor).toContainText('3.0.0');
+    await expect(specPreview).toContainText('Swagger Petstore');
+  });
 
-// });
+  test(`can import ${spec.name} from within an empty doc`, async ({ app, page }) => {
+    await app.firstWindow(); // TODO: Convenience method that waits for app to be ready
+    test.slow(process.platform === 'darwin' || process.platform === 'win32', 'Slow app start on these platforms');
+    const specPreview = page.locator('.information-container');
+    const codeEditor = page.locator('[data-testid="CodeEditor"]');
+    await page.click('text=Design');
+    await page.click('text=Import OpenAPI');
+    await page.click('button:has-text("URL")');
 
-// can import json from copy paste inside empty document
+    // TODO: fix modal flakiness
+    await page.click('input[id="prompt-input"]', { force: true });
+    await page.fill('input[id="prompt-input"]', spec.url, { force: true });
+    await page.click('text=Fetch and Import');
 
-// can import json from copy paste via import/export for empty document
+    await expect(codeEditor).toContainText('3.0.0');
+    await expect(specPreview).toContainText('Swagger Petstore');
+  });
 
-// can import yaml from url inside empty document
+  test(`can import ${spec.name} from import/export menu`, async ({ app, page }) => {
+    test.fail(); // FAILING DUE TO https://linear.app/insomnia/issue/INS-1393/no-text-rendered-when-folks-import-a-file-into-an-empty-design-doc
+    await app.firstWindow(); // TODO: Convenience method that waits for app to be ready
+    test.slow(process.platform === 'darwin' || process.platform === 'win32', 'Slow app start on these platforms');
+    const codeEditor = page.locator('[data-testid="CodeEditor"]');
+    const specPreview = page.locator('.information-container');
+    await page.click('text=Design');
+    await page.click('button:has-text("New Document")');
+    await page.click('button:has-text("Import/Export")');
+    await page.click('text=Import Data');
+    await page.click('#dropdowns-container button:has-text("From URL")');
 
-// can import yaml from url via import/export for empty document
+    // TODO: fix modal flakiness
+    await page.click('input[id="prompt-input"]', { force: true });
+    await page.fill('input[id="prompt-input"]', spec.url, { force: true });
+    await page.click('text=Fetch and Import');
 
-// can import json from url inside empty document
-
-// can import json from url via import/export for empty document
-
-// FOR ANOTHER FILE!!!!
-// can import insomnia json collection
-
-// can import insomnia yaml collection
-
-// can import insomnia yaml design doc
-
-// can import insomnia yaml design doc
+    await page.locator('text=Ok').first().click();
+    await expect(codeEditor).toContainText('3.0.0');
+    await expect(specPreview).toContainText('Swagger Petstore');
+  });
+}
