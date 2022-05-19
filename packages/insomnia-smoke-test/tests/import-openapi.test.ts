1f0327e34441cc0d0e71d4af43379b76b0e0304e
Filipe Freire <livrofubia@gmail.com>
2022-05-19 11:19:53 +0100

    Replace react selector to simpler one to fix CI execution

--- packages/insomnia-smoke-test/tests/import-openapi.test.ts
+++ packages/insomnia-smoke-test/tests/import-openapi.test.ts
@@ -2,10 +2,10 @@ import { expect } from '@playwright/test';
 
 import { test } from '../playwright/test';
 
-test('can render Petstore internal example and can render errors', async ({ app, page }) => {
+test('can render Petstore internal example and can render errors', async ({ page }) => {
   const specPreview = page.locator('.information-container');
   const codeEditor = page.locator('[data-testid="CodeEditor"]');
-  const noticeTable = page.locator('_react=NoticeTable');
+  const noticeTable = page.locator('text=oas3-schema Property `ta gs` is not expected to be here.');
   await page.click('text=Design');
   await page.click('text=start from an example');
   await expect(codeEditor).toContainText('3.0.0');
@@ -14,5 +14,5 @@ test('can render Petstore internal example and can render errors', async ({ app,
   // Can render errors
   await page.locator('[data-testid="CodeEditor"] >> text=tags').first().click();
   await page.locator('textarea').nth(1).press('Tab');
-  await expect(noticeTable).toContainText('oas3-schema Property `ta gs` is not expected to be here.');
+  await expect(noticeTable).toBeVisible();
 });
