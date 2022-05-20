80ce71b0e42ed8787fcecf1b76245288f7bb7702
Filipe Freire <livrofubia@gmail.com>
2022-05-20 18:01:06 +0100

    chore: Upgrade spectral dependency

--- packages/insomnia/src/ui/components/wrapper-design.tsx
+++ packages/insomnia/src/ui/components/wrapper-design.tsx
@@ -1,4 +1,4 @@
-import { IRuleResult } from '@stoplight/spectral';
+import { IRuleResult } from '@stoplight/spectral-core';
 import { Button, Notice, NoticeTable } from 'insomnia-components';
 import React, { createRef, FC, Fragment, ReactNode, RefObject, useCallback, useEffect, useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
