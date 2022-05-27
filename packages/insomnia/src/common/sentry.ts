a3ff22ef212f23d6e548ca6dd7439d1f2edb1d03
John Chadwick <86682572+johnwchadwick@users.noreply.github.com>
2022-05-27 16:02:53 -0400

    Move sentry DSN into config.json.

--- packages/insomnia/src/common/sentry.ts
+++ packages/insomnia/src/common/sentry.ts
@@ -1,9 +1,9 @@
 import type { ElectronOptions } from '@sentry/electron';
 
-import { getAppEnvironment, getAppVersion } from './constants';
+import { getAppEnvironment, getAppVersion, getSentryDsn } from './constants';
 
 export const SENTRY_OPTIONS: Partial<ElectronOptions> = {
-  dsn: 'https://aaec2e800e644070a8daba5b7ad02c16@o1147619.ingest.sentry.io/6311804',
+  dsn: getSentryDsn(),
   environment: getAppEnvironment(),
   release: getAppVersion(),
 };
