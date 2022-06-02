42355b2b3971826b05a04976bfdd18eb82720150
jackkav <jackkav@gmail.com>
2022-06-02 10:01:03 +0200

    level 1 only render methods

--- packages/insomnia/src/ui/components/help-tooltip.tsx
+++ packages/insomnia/src/ui/components/help-tooltip.tsx
@@ -1,8 +1,6 @@
-import { autoBindMethodsForReact } from 'class-autobind-decorator';
-import React, { PureComponent, ReactNode } from 'react';
+import React, { FC, ReactNode } from 'react';
 import { CSSProperties } from 'styled-components';
 
-import { AUTOBIND_CFG } from '../../common/constants';
 import { Tooltip } from './tooltip';
 
 interface Props {
@@ -13,20 +11,20 @@ interface Props {
   info?: boolean;
 }
 
-@autoBindMethodsForReact(AUTOBIND_CFG)
-export class HelpTooltip extends PureComponent<Props> {
-  render() {
-    const { children, className, style, info } = this.props;
-    return (
-      <Tooltip
-        position="top"
-        className={className}
-        message={children}
-        // @ts-expect-error -- TSCONVERSION appears to be a genuine error because style is not accepted or used or spread by Tooltip
-        style={style}
-      >
-        <i className={'fa ' + (info ? 'fa-info-circle' : 'fa-question-circle')} />
-      </Tooltip>
-    );
-  }
-}
+export const HelpTooltip: FC<Props> = props => {
+  const {
+    children,
+    className,
+    style,
+    info,
+  } = props;
+  return <Tooltip
+    position="top"
+    className={className}
+    message={children}
+    // @ts-expect-error -- TSCONVERSION appears to be a genuine error because style is not accepted or used or spread by Tooltip
+    style={style}
+  >
+    <i className={'fa ' + (info ? 'fa-info-circle' : 'fa-question-circle')} />
+  </Tooltip>;
+};
