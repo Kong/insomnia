c88dd8155de872e4b1ba71a0e66f5aea2aa7831d
Filipe Freire <livrofubia@gmail.com>
2022-06-13 10:34:36 +0100

    Fix #4856 UI spacing on deploy to dev portal modal

--- plugins/insomnia-plugin-kong-portal/src/deploy-to-portal.tsx
+++ plugins/insomnia-plugin-kong-portal/src/deploy-to-portal.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-trailing-spaces */
 import { AxiosError, AxiosRequestConfig } from 'axios';
 import type ReactLib from 'react';
 import { type SyntheticEvent } from 'react';
@@ -431,7 +432,7 @@ export function getDeployToPortalComponent(options: {
         return (
           <div className="pad">
             <p className="no-pad no-margin-top">
-              The Document is now available on
+              The Document is now available on 
               {kongPortalLegacyMode ? (
                 'Dev Portal'
               ) : (
