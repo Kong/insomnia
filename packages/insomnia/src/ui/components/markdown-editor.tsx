d4036836248c3136f63f5b6fb54ba5a3f30b8ad9
Mark Kim <mark.kim@konghq.com>
2022-06-25 10:33:36 -0400

    convert markdown editor to functional

--- packages/insomnia/src/ui/components/markdown-editor.tsx
+++ packages/insomnia/src/ui/components/markdown-editor.tsx
@@ -1,9 +1,7 @@
-import { autoBindMethodsForReact } from 'class-autobind-decorator';
 import classnames from 'classnames';
-import React, { PureComponent } from 'react';
+import React, { forwardRef, ForwardRefRenderFunction, ReactElement, useState } from 'react';
 import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 
-import { AUTOBIND_CFG } from '../../common/constants';
 import { Button } from './base/button';
 import { CodeEditor,  UnconnectedCodeEditor } from './codemirror/code-editor';
 import { MarkdownPreview } from './markdown-preview';
@@ -18,84 +16,59 @@ interface Props {
   tall?: boolean;
 }
 
-interface State {
-  markdown: string;
-}
+const MarkdownEditorForwarded: ForwardRefRenderFunction<UnconnectedCodeEditor, Props> = ({
+  mode,
+  placeholder,
+  defaultPreviewMode,
+  className,
+  tall,
+  defaultValue,
+  onChange,
+}, ref): ReactElement => {
+  const [markdown, setMarkdown] = useState(defaultValue); // this was we are cutting the flow of prop change event after the initial rendering
+  const classes = classnames('react-tabs', 'markdown-editor', 'outlined', className, {
+    'markdown-editor--dynamic-height': !tall,
+  });
 
-@autoBindMethodsForReact(AUTOBIND_CFG)
-export class MarkdownEditor extends PureComponent<Props, State> {
-  _editor: UnconnectedCodeEditor | null = null;
+  const handleChange = (markdown: string): void => {
+    onChange(markdown);
+    setMarkdown(markdown);
+  };
 
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      markdown: props.defaultValue,
-    };
-  }
+  return (
+    <Tabs className={classes} defaultIndex={defaultPreviewMode ? 1 : 0}>
+      <TabList>
+        <Tab tabIndex="-1">
+          <Button value="Write">Write</Button>
+        </Tab>
+        <Tab tabIndex="-1">
+          <Button value="Preview">Preview</Button>
+        </Tab>
+      </TabList>
+      <TabPanel className="react-tabs__tab-panel markdown-editor__edit">
+        <div className="form-control form-control--outlined">
+          <CodeEditor
+            ref={ref}
+            hideGutters
+            hideLineNumbers
+            dynamicHeight={!tall}
+            manualPrettify
+            noStyleActiveLine
+            enableNunjucks
+            mode={mode || 'text/x-markdown'}
+            placeholder={placeholder}
+            debounceMillis={300}
+            defaultValue={markdown}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="txt-sm italic faint">Styling with Markdown is supported</div>
+      </TabPanel>
+      <TabPanel className="react-tabs__tab-panel markdown-editor__preview">
+        <MarkdownPreview markdown={markdown} />
+      </TabPanel>
+    </Tabs>
+  );
+};
 
-  _handleChange(markdown: string) {
-    this.props.onChange(markdown);
-    this.setState({
-      markdown,
-    });
-  }
-
-  _setEditorRef(editor: UnconnectedCodeEditor) {
-    this._editor = editor;
-  }
-
-  focusEnd() {
-    this._editor?.focusEnd();
-  }
-
-  focus() {
-    this._editor?.focus();
-  }
-
-  render() {
-    const {
-      mode,
-      placeholder,
-      defaultPreviewMode,
-      className,
-      tall,
-    } = this.props;
-    const { markdown } = this.state;
-    const classes = classnames('react-tabs', 'markdown-editor', 'outlined', className, {
-      'markdown-editor--dynamic-height': !tall,
-    });
-    return (
-      <Tabs className={classes} defaultIndex={defaultPreviewMode ? 1 : 0}>
-        <TabList>
-          <Tab tabIndex="-1">
-            <Button value="Write">Write</Button>
-          </Tab>
-          <Tab tabIndex="-1">
-            <Button value="Preview">Preview</Button>
-          </Tab>
-        </TabList>
-        <TabPanel className="react-tabs__tab-panel markdown-editor__edit">
-          <div className="form-control form-control--outlined">
-            <CodeEditor
-              ref={this._setEditorRef}
-              hideGutters
-              hideLineNumbers
-              dynamicHeight={!tall}
-              manualPrettify
-              noStyleActiveLine
-              enableNunjucks
-              mode={mode || 'text/x-markdown'}
-              placeholder={placeholder}
-              defaultValue={markdown}
-              onChange={this._handleChange}
-            />
-          </div>
-          <div className="txt-sm italic faint">Styling with Markdown is supported</div>
-        </TabPanel>
-        <TabPanel className="react-tabs__tab-panel markdown-editor__preview">
-          <MarkdownPreview markdown={markdown} />
-        </TabPanel>
-      </Tabs>
-    );
-  }
-}
+export const MarkdownEditor = forwardRef(MarkdownEditorForwarded);
