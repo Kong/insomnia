8b5dfdb3796e0e826927a67eb25e66732cac42c0
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-07 22:16:55 +0300

    Remove onDisabledClick prop

--- packages/insomnia/src/ui/components/base/button.tsx
+++ packages/insomnia/src/ui/components/base/button.tsx
@@ -3,7 +3,6 @@ export interface ButtonProps<T> {
   children: ReactNode;
   value?: T;
   className?: string;
-  onDisabledClick?: (event: React.MouseEvent<HTMLButtonElement>, value?: T) => void;
   onClick?: (event: React.MouseEvent<HTMLButtonElement>, value?: T) => void;
   disabled?: boolean;
   tabIndex?: number;
@@ -26,22 +25,13 @@ export const Button = <T, >(props: ButtonProps<T>) => {
     style,
     title,
     onClick,
-    onDisabledClick
   } = props;
 
   /**
    * This function fires when user clicks the button
    * @param e The mouse click event
    */
-  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    const fn = disabled ? onDisabledClick : onClick;
-
-    if (props.hasOwnProperty('value')) {
-      fn?.(e, value);
-    } else {
-      fn?.(e);
-    }
-  };
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => onClick?.(e, value);
 
   return (
     <button
