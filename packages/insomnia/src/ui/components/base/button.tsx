20f6038b3e2f90ebcdaf209584bcde5ca56658d5
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-07 22:16:01 +0300

    Refactored the button component to be function component

--- packages/insomnia/src/ui/components/base/button.tsx
+++ packages/insomnia/src/ui/components/base/button.tsx
@@ -1,8 +1,4 @@
-import { autoBindMethodsForReact } from 'class-autobind-decorator';
-import React, { ButtonHTMLAttributes, CSSProperties, PureComponent, ReactNode } from 'react';
-
-import { AUTOBIND_CFG } from '../../../common/constants';
-
+import React, { ButtonHTMLAttributes, CSSProperties, ReactNode } from 'react';
 export interface ButtonProps<T> {
   children: ReactNode;
   value?: T;
@@ -17,34 +13,48 @@ export interface ButtonProps<T> {
   style?: CSSProperties;
 }
 
-@autoBindMethodsForReact(AUTOBIND_CFG)
-export class Button<T> extends PureComponent<ButtonProps<T>> {
-  _handleClick(event: React.MouseEvent<HTMLButtonElement>) {
-    const { onClick, onDisabledClick, disabled, value } = this.props;
+export const Button = <T, >(props: ButtonProps<T>) => {
+  // Distract all the available properties.
+  const {
+    value,
+    children,
+    disabled,
+    tabIndex,
+    className,
+    type,
+    id,
+    style,
+    title,
+    onClick,
+    onDisabledClick
+  } = props;
+
+  /**
+   * This function fires when user clicks the button
+   * @param e The mouse click event
+   */
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     const fn = disabled ? onDisabledClick : onClick;
 
-    if (this.props.hasOwnProperty('value')) {
-      fn?.(event, value);
+    if (props.hasOwnProperty('value')) {
+      fn?.(e, value);
     } else {
-      fn?.(event);
+      fn?.(e);
     }
-  }
+  };
 
-  render() {
-    const { children, disabled, tabIndex, className, type, id, style, title } = this.props;
-    return (
-      <button
-        disabled={disabled}
-        id={id}
-        type={type}
-        tabIndex={tabIndex}
-        className={className}
-        onClick={this._handleClick}
-        style={style}
-        title={title}
-      >
-        {children}
-      </button>
-    );
-  }
-}
+  return (
+    <button
+      disabled={disabled}
+      id={id}
+      type={type}
+      tabIndex={tabIndex}
+      className={className}
+      onClick={handleClick}
+      style={style}
+      title={title}
+    >
+      {children}
+    </button>
+  );
+};
