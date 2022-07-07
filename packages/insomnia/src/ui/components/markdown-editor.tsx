5ba18e9d82898ecffcb1e050c52ed851f4146f8a
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-07 21:40:15 +0300

    Used the new Tabs component and remove the button from children from markdown-editor

--- packages/insomnia/src/ui/components/markdown-editor.tsx
+++ packages/insomnia/src/ui/components/markdown-editor.tsx
@@ -1,7 +1,7 @@
 import classnames from 'classnames';
 import React, { forwardRef, ReactElement, useCallback, useState } from 'react';
-import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 
+import { Tab, TabList, TabPanel, Tabs } from './base/tabs';
 import { CodeEditor, CodeEditorHandle } from './codemirror/code-editor';
 import { MarkdownPreview } from './markdown-preview';
 
@@ -15,58 +15,71 @@ interface Props {
   tall?: boolean;
 }
 
-export const MarkdownEditor = forwardRef<CodeEditorHandle, Props>(({
-  mode,
-  placeholder,
-  defaultPreviewMode,
-  className,
-  tall,
-  defaultValue,
-  onChange,
-}, ref): ReactElement => {
-  // default value is added here to capture the original class component's behavior, but this way cuts the flow of prop change event after the initial rendering
-  const [markdown, setMarkdown] = useState(defaultValue);
-  const classes = classnames('react-tabs', 'markdown-editor', 'outlined', className, {
-    'markdown-editor--dynamic-height': !tall,
+export const MarkdownEditor = forwardRef<CodeEditorHandle, Props>(
+  (
+    {
+      mode,
+      placeholder,
+      defaultPreviewMode,
+      className,
+      tall,
+      defaultValue,
+      onChange,
+    },
+    ref
+  ): ReactElement => {
+    // default value is added here to capture the original class component's behavior, but this way cuts the flow of prop change event after the initial rendering
+    const [markdown, setMarkdown] = useState(defaultValue);
+    const classes = classnames(
+      'react-tabs',
+      'markdown-editor',
+      'outlined',
+      className,
+      {
+        'markdown-editor--dynamic-height': !tall,
+      }
+    );
+
+    const handleChange = useCallback(
+      (markdown: string) => {
+        onChange(markdown);
+        setMarkdown(markdown);
+      },
+      [onChange]
+    );
+
+    return (
+      <Tabs className={classes} defaultIndex={defaultPreviewMode ? 1 : 0}>
+        <TabList>
+          <Tab tabIndex="-1">
+            <button value="Write">Write</button>
+          </Tab>
+          <Tab tabIndex="-1">
+            <button value="Preview">Preview</button>
+          </Tab>
+        </TabList>
+        <TabPanel className="react-tabs__tab-panel markdown-editor__edit">
+          <div className="form-control form-control--outlined">
+            <CodeEditor
+              ref={ref}
+              hideGutters
+              hideLineNumbers
+              dynamicHeight={!tall}
+              showPrettifyButton
+              noStyleActiveLine
+              enableNunjucks
+              mode={mode || 'text/x-markdown'}
+              placeholder={placeholder}
+              defaultValue={markdown}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="txt-sm italic faint">Styling with Markdown is supported</div>
+        </TabPanel>
+        <TabPanel className="react-tabs__tab-panel markdown-editor__preview">
+          <MarkdownPreview markdown={markdown} />
+        </TabPanel>
+      </Tabs>
+    );
   });
-
-  const handleChange = useCallback((markdown: string) => {
-    onChange(markdown);
-    setMarkdown(markdown);
-  }, [onChange]);
-
-  return (
-    <Tabs className={classes} defaultIndex={defaultPreviewMode ? 1 : 0}>
-      <TabList>
-        <Tab tabIndex="-1">
-          <button value="Write">Write</button>
-        </Tab>
-        <Tab tabIndex="-1">
-          <button value="Preview">Preview</button>
-        </Tab>
-      </TabList>
-      <TabPanel className="react-tabs__tab-panel markdown-editor__edit">
-        <div className="form-control form-control--outlined">
-          <CodeEditor
-            ref={ref}
-            hideGutters
-            hideLineNumbers
-            dynamicHeight={!tall}
-            showPrettifyButton
-            noStyleActiveLine
-            enableNunjucks
-            mode={mode || 'text/x-markdown'}
-            placeholder={placeholder}
-            defaultValue={markdown}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="txt-sm italic faint">Styling with Markdown is supported</div>
-      </TabPanel>
-      <TabPanel className="react-tabs__tab-panel markdown-editor__preview">
-        <MarkdownPreview markdown={markdown} />
-      </TabPanel>
-    </Tabs>
-  );
-});
 MarkdownEditor.displayName = 'MarkdownEditor';
