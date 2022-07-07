9003a5bec56f2b62b4ca47f6a025e5fb1c3e163e
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-07 21:35:15 +0300

    Used the new Tabs component and remove the button from children from grpc-tabbed-messages

--- packages/insomnia/src/ui/components/viewers/grpc-tabbed-messages.tsx
+++ packages/insomnia/src/ui/components/viewers/grpc-tabbed-messages.tsx
@@ -1,8 +1,8 @@
 import classnames from 'classnames';
 import React, { FunctionComponent } from 'react';
-import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 
 import { HandleGetRenderContext, HandleRender } from '../../../common/render';
+import { Tab, TabList, TabPanel, Tabs } from '../base/tabs';
 import { GRPCEditor } from '../editors/grpc-editor';
 
 interface Message {
@@ -37,20 +37,17 @@ export const GrpcTabbedMessages: FunctionComponent<Props> = ({
   const shouldShowBody = !!handleBodyChange;
   const orderedMessages = messages?.sort((a, b) => a.created - b.created) || [];
   return (
-    <Tabs key={uniquenessKey} className={classnames('react-tabs', 'react-tabs--nested')}>
-      <div className="tab-action-wrapper">
-        <div className="tab-action-tabs">
+    <Tabs
+      key={uniquenessKey}
+      className={classnames('react-tabs', 'react-tabs--nested')}
+    >
+      <div className='tab-action-wrapper'>
+        <div className='tab-action-tabs'>
           <TabList>
-            {shouldShowBody && (
-              <Tab>
-                <button>Body</button>
-              </Tab>
-            )}
+            {shouldShowBody && <Tab>Body</Tab>}
             {messages?.map((m, index) => (
               <Tab key={m.id}>
-                <button>
-                  {tabNamePrefix} {index + 1}
-                </button>
+                {tabNamePrefix} {index + 1}
               </Tab>
             ))}
           </TabList>
@@ -59,33 +56,30 @@ export const GrpcTabbedMessages: FunctionComponent<Props> = ({
           <>
             {handleStream && (
               <button
-                className="btn btn--compact btn--clicky margin-sm bg-default"
+                className='btn btn--compact btn--clicky margin-sm bg-default'
                 onClick={handleStream}
               >
-                Stream <i className="fa fa-plus" />
+                Stream <i className='fa fa-plus' />
               </button>
             )}
             {handleCommit && (
               <button
-                className="btn btn--compact btn--clicky margin-sm bg-surprise"
+                className='btn btn--compact btn--clicky margin-sm bg-surprise'
                 onClick={handleCommit}
               >
-                Commit <i className="fa fa-arrow-right" />
+                Commit <i className='fa fa-arrow-right' />
               </button>
             )}
           </>
         )}
       </div>
       {shouldShowBody && (
-        <TabPanel className="react-tabs__tab-panel editor-wrapper">
-          <GRPCEditor
-            content={bodyText}
-            handleChange={handleBodyChange}
-          />
+        <TabPanel className='react-tabs__tab-panel editor-wrapper'>
+          <GRPCEditor content={bodyText} handleChange={handleBodyChange} />
         </TabPanel>
       )}
       {orderedMessages.map(m => (
-        <TabPanel key={m.id} className="react-tabs__tab-panel editor-wrapper">
+        <TabPanel key={m.id} className='react-tabs__tab-panel editor-wrapper'>
           <GRPCEditor content={m.text} readOnly />
         </TabPanel>
       ))}
