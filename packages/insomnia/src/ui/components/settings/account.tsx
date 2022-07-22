4d82d4ddb78711dabbc9087c18c5645a443f4f05
Jack Kavanagh <jackkav@gmail.com>
2022-07-22 11:25:00 +0200

    account class-fc (#4988)
    * first pass
    
    * simplify
    
    * use FormData as state
    
    * address review comments

--- packages/insomnia/src/ui/components/settings/account.tsx
+++ packages/insomnia/src/ui/components/settings/account.tsx
@@ -1,10 +1,7 @@
-import { autoBindMethodsForReact } from 'class-autobind-decorator';
-import React, { Fragment, PureComponent } from 'react';
-import { connect } from 'react-redux';
+import React, { FC, Fragment, useCallback, useState } from 'react';
+import { useSelector } from 'react-redux';
 
 import * as session from '../../../account/session';
-import { AUTOBIND_CFG } from '../../../common/constants';
-import { RootState } from '../../redux/modules';
 import { selectSettings } from '../../redux/selectors';
 import { Link } from '../base/link';
 import { PromptButton } from '../base/prompt-button';
@@ -12,267 +9,180 @@ import { HelpTooltip } from '../help-tooltip';
 import { hideAllModals, showModal } from '../modals/index';
 import { LoginModal } from '../modals/login-modal';
 
-type ReduxProps = ReturnType<typeof mapStateToProps>;
+export const Account: FC = () => {
+  const { disablePaidFeatureAds } = useSelector(selectSettings);
+  const [codeSent, setCodeSent] = useState(false);
+  const [error, setError] = useState('');
+  const [showChangePassword, setShowChangePassword] = useState(false);
+  const [finishedResetting, setFinishedResetting] = useState(false);
 
-type Props = ReduxProps;
-
-interface State {
-  code: string;
-  password: string;
-  password2: string;
-  showChangePassword: boolean;
-  codeSent: boolean;
-  error: string;
-  finishedResetting: boolean;
-}
-
-@autoBindMethodsForReact(AUTOBIND_CFG)
-class UnconnectedAccount extends PureComponent<Props, State> {
-  state: State = {
-    code: '',
-    password: '',
-    password2: '',
-    codeSent: false,
-    showChangePassword: false,
-    error: '',
-    finishedResetting: false,
-  };
-
-  async _handleShowChangePasswordForm() {
-    this.setState(state => ({
-      showChangePassword: !state.showChangePassword,
-      finishedResetting: false,
-    }));
-  }
-
-  _handleChangeCode(event: React.SyntheticEvent<HTMLInputElement>) {
-    this.setState({
-      code: event.currentTarget.value,
-    });
-  }
-
-  _handleChangePassword(event: React.SyntheticEvent<HTMLInputElement>) {
-    this.setState({
-      password: event.currentTarget.value,
-    });
-  }
-
-  _handleChangePassword2(event: React.SyntheticEvent<HTMLInputElement>) {
-    this.setState({
-      password2: event.currentTarget.value,
-    });
-  }
-
-  async _handleSubmitPasswordChange(event: React.SyntheticEvent<HTMLFormElement>) {
-    event.preventDefault();
-    this.setState({
-      error: '',
-    });
-    const { password, password2, code } = this.state;
-    let error = '';
-
-    if (password !== password2) {
-      error = 'Passwords did not match';
-    } else if (!code) {
-      error = 'Code was not provided';
-    }
-
-    if (error) {
-      this.setState({
-        error,
-      });
-      return;
-    }
-
-    try {
-      await session.changePasswordWithToken(password, code);
-    } catch (err) {
-      this.setState({
-        error: err.message,
-      });
-      return;
-    }
-
-    this.setState({
-      error: '',
-      finishedResetting: true,
-      showChangePassword: false,
-    });
-  }
-
-  async _handleLogout() {
-    await session.logout();
-    this.forceUpdate();
-  }
-
-  static _handleLogin(event: React.SyntheticEvent<HTMLAnchorElement>) {
+  const handleLogin = useCallback((event: React.SyntheticEvent<HTMLAnchorElement>) => {
     event.preventDefault();
     hideAllModals();
     showModal(LoginModal);
-  }
+  }, []);
 
-  async _sendCode() {
-    try {
-      await session.sendPasswordChangeCode();
-    } catch (err) {
-      this.setState({
-        error: err.message,
-      });
+  const handleSubmitPasswordChange = useCallback(async (event: React.SyntheticEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const password = formData.get('password') as string;
+    const confirmPassword = formData.get('confirmPassword') as string;
+    const code = formData.get('code') as string;
+    setError('');
+    if (!password) {
+      setError('Password was not provided');
       return;
     }
-
-    this.setState({
-      codeSent: true,
-    });
-  }
-
-  async _handleSendCode(event: React.SyntheticEvent<HTMLAnchorElement>) {
-    event.preventDefault();
-    await this._sendCode();
-  }
-
-  renderUpgrade() {
-    const { disablePaidFeatureAds } = this.props;
-
-    const logInButton = (
-      <a href="#" onClick={Account._handleLogin} className="theme--link">
-        Log In
-      </a>
-    );
-
-    if (disablePaidFeatureAds) {
-      return logInButton;
+    if (password !== confirmPassword) {
+      setError('Passwords did not match');
+      return;
     }
+    if (!code) {
+      setError('Code was not provided');
+      return;
+    }
+    try {
+      await session.changePasswordWithToken(password, code);
+    } catch (err) {
+      setError(err.message);
+      return;
+    }
+    setError('');
+    setFinishedResetting(true);
+    setShowChangePassword(false);
+  }, []);
 
-    return (
-      <Fragment>
-        <div className="notice pad surprise">
-          <h1 className="no-margin-top">Try Insomnia Plus!</h1>
-          <p>
-            Sync your data across devices or with a team
-            <br />
-            Keep synced data safe with end-to-end encryption
-            <br />
-            Prioritized email support
-            <br />
-          </p>
-          <br />
-          <div className="pad">
-            <Link button className="btn btn--clicky" href="https://insomnia.rest/pricing">
-              View plans <i className="fa fa-external-link" />
-            </Link>
-          </div>
-        </div>
+  const emailCode = useCallback(async event => {
+    event.preventDefault();
+    try {
+      session.sendPasswordChangeCode();
+    } catch (err) {
+      setError(err.message);
+      return;
+    }
+    setCodeSent(true);
+    setError('');
+  }, []);
+
+  const openChangePasswordDialog = useCallback(() => {
+    setShowChangePassword(!showChangePassword);
+    setFinishedResetting(false);
+  }, [showChangePassword]);
+
+  const logout = useCallback(() => session.logout(), []);
+
+  return session.isLoggedIn() ? (
+    <Fragment>
+      <div>
+        <h2 className="no-margin-top">Welcome {session.getFirstName()}!</h2>
         <p>
-          Or{' '}{logInButton}
+          You are currently logged in as{' '}
+          <code className="code--compact">{session.getEmail()}</code>
         </p>
-      </Fragment>
-    );
-  }
+        <br />
+        <Link button href="https://app.insomnia.rest" className="btn btn--clicky">
+          Manage Account
+        </Link>
+        <PromptButton className="space-left btn btn--clicky" onClick={logout}>
+          Sign Out
+        </PromptButton>
+        <button
+          className="space-left btn btn--clicky"
+          onClick={openChangePasswordDialog}
+        >
+          Change Password
+        </button>
+      </div>
 
-  renderAccount() {
-    const {
-      code,
-      password,
-      password2,
-      codeSent,
-      showChangePassword,
-      error,
-      finishedResetting,
-    } = this.state;
-    return (
-      <Fragment>
-        <div>
-          <h2 className="no-margin-top">Welcome {session.getFirstName()}!</h2>
-          <p>
-            You are currently logged in as{' '}
-            <code className="code--compact">{session.getEmail()}</code>
-          </p>
+      {finishedResetting && (
+        <p className="notice surprise">Your password was changed successfully</p>
+      )}
+
+      {showChangePassword && (
+        <form onSubmit={handleSubmitPasswordChange} className="pad-top">
+          <hr />
+          {error && <p className="notice error">{error}</p>}
+          <div className="form-control form-control--outlined">
+            <label>
+              New Password
+              <input
+                name="password"
+                type="password"
+                placeholder="•••••••••••••••••"
+              />
+            </label>
+          </div>
+          <div className="form-control form-control--outlined">
+            <label>
+              Confirm Password
+              <input
+                name="confirmPassword"
+                type="password"
+                placeholder="•••••••••••••••••"
+              />
+            </label>
+          </div>
+          <div className="form-control form-control--outlined">
+            <label>
+              Confirmation Code{' '}
+              <HelpTooltip>A confirmation code has been sent to your email address</HelpTooltip>
+              <input
+                name="code"
+                type="text"
+                placeholder="aa8b0d1ea9"
+              />
+            </label>
+          </div>
+          <div className="row-spaced row--top">
+            <div>
+              {codeSent ? 'A code was sent to your email' : 'Looking for a code?'}{' '}
+              <Link
+                href="#"
+                onClick={emailCode}
+              >
+                Email Me a Code
+              </Link>
+            </div>
+            <div className="text-right">
+              <button
+                type="submit"
+                className="btn btn--clicky"
+              >
+                Submit Change
+              </button>
+            </div>
+          </div>
+        </form>
+      )}
+    </Fragment>
+  ) : (disablePaidFeatureAds ? (
+    <a href="#" onClick={handleLogin} className="theme--link">
+      Log In
+    </a>
+  ) : (
+    <Fragment>
+      <div className="notice pad surprise">
+        <h1 className="no-margin-top">Try Insomnia Plus!</h1>
+        <p>
+          Sync your data across devices or with a team
           <br />
-          <Link button href="https://app.insomnia.rest" className="btn btn--clicky">
-            Manage Account
+          Keep synced data safe with end-to-end encryption
+          <br />
+          Prioritized email support
+          <br />
+        </p>
+        <br />
+        <div className="pad">
+          <Link button className="btn btn--clicky" href="https://insomnia.rest/pricing">
+            View plans <i className="fa fa-external-link" />
           </Link>
-          <PromptButton className="space-left btn btn--clicky" onClick={this._handleLogout}>
-            Sign Out
-          </PromptButton>
-          <button
-            className="space-left btn btn--clicky"
-            onClick={this._handleShowChangePasswordForm}
-          >
-            Change Password
-          </button>
         </div>
-
-        {finishedResetting && (
-          <p className="notice surprise">Your password was changed successfully</p>
-        )}
-
-        {showChangePassword && (
-          <form onSubmit={this._handleSubmitPasswordChange} className="pad-top">
-            <hr />
-            {error && <p className="notice error">{error}</p>}
-            <div className="form-control form-control--outlined">
-              <label>
-                New Password
-                <input
-                  type="password"
-                  placeholder="•••••••••••••••••"
-                  onChange={this._handleChangePassword}
-                />
-              </label>
-            </div>
-            <div className="form-control form-control--outlined">
-              <label>
-                Confirm Password
-                <input
-                  type="password"
-                  placeholder="•••••••••••••••••"
-                  onChange={this._handleChangePassword2}
-                />
-              </label>
-            </div>
-            <div className="form-control form-control--outlined">
-              <label>
-                Confirmation Code{' '}
-                <HelpTooltip>A confirmation code has been sent to your email address</HelpTooltip>
-                <input
-                  type="text"
-                  defaultValue={code}
-                  placeholder="aa8b0d1ea9"
-                  onChange={this._handleChangeCode}
-                />
-              </label>
-            </div>
-            <div className="row-spaced row--top">
-              <div>
-                {codeSent ? 'A code was sent to your email' : 'Looking for a code?'}{' '}
-                <Link href="#" onClick={this._handleSendCode}>
-                  Email Me a Code
-                </Link>
-              </div>
-              <div className="text-right">
-                <button
-                  type="submit"
-                  className="btn btn--clicky"
-                  disabled={!code || !password || password !== password2}
-                >
-                  Submit Change
-                </button>
-              </div>
-            </div>
-          </form>
-        )}
-      </Fragment>
-    );
-  }
-
-  render() {
-    return session.isLoggedIn() ? this.renderAccount() : this.renderUpgrade();
-  }
-}
-
-const mapStateToProps = (state: RootState) => ({
-  disablePaidFeatureAds: selectSettings(state).disablePaidFeatureAds,
-});
-
-export const Account = connect(mapStateToProps)(UnconnectedAccount);
+      </div>
+      <p>
+        Or{' '}<a href="#" onClick={handleLogin} className="theme--link">
+          Log In
+        </a>
+      </p>
+    </Fragment>)
+  );
+};
