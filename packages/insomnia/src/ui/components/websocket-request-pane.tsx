3e3ef58b5a62503708ae0ddbed6fc2ef7d2eee46
Mark Kim <mark.kim@konghq.com>
2022-08-04 10:43:37 -0400

    import name change

--- packages/insomnia/src/ui/components/websocket-request-pane.tsx
+++ packages/insomnia/src/ui/components/websocket-request-pane.tsx
@@ -1,11 +1,11 @@
-import React, { FC } from 'react';
+import React, { FunctionComponent } from 'react';
 
 import { WebsocketActionBar } from './websockets/action-bar';
 
 interface Props {
   requestId: string;
 }
-export const WebSocketRequestPane: FC<Props> = ({ requestId }) => {
+export const WebSocketRequestPane: FunctionComponent<Props> = ({ requestId }) => {
   return (
     <div>
       <WebsocketActionBar requestId={requestId} />
