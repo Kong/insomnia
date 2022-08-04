b34e190ecef936f14efc5a18566de0ffcf7bdcd8
James Gatz <jamesgatzos@gmail.com>
2022-08-04 15:25:39 +0200

    Websockets IPC API (#5044)
    * add url to ws-request model
    
    * fix webSocketRequest typo and add url
    
    * add websocket api and expose it through preload ipc
    
    * add typings
    
    Co-authored-by: Mark Kim <mark.kim@konghq.com>

---
+++ packages/insomnia/src/main/network/websocket.ts
@@ -0,0 +1,199 @@
+import { ipcMain } from 'electron';
+import { v4 as uuidV4 } from 'uuid';
+import {
+  CloseEvent,
+  ErrorEvent,
+  Event as OpenEvent,
+  MessageEvent,
+  WebSocket,
+} from 'ws';
+
+import { websocketRequest } from '../../models';
+
+export interface WebSocketConnection extends WebSocket {
+  _id: string;
+  requestId: string;
+}
+
+export type WebsocketOpenEvent = Omit<OpenEvent, 'target'> & {
+  _id: string;
+  requestId: string;
+};
+
+export type WebsocketMessageEvent = Omit<MessageEvent, 'target'> & {
+  _id: string;
+  requestId: string;
+  direction: 'OUTGOING' | 'INCOMING';
+};
+
+export type WebsocketErrorEvent = Omit<ErrorEvent, 'target'> & {
+  _id: string;
+  requestId: string;
+};
+
+export type WebsocketCloseEvent = Omit<CloseEvent, 'target'> & {
+  _id: string;
+  requestId: string;
+};
+
+export type WebsocketEvent =
+  | WebsocketOpenEvent
+  | WebsocketMessageEvent
+  | WebsocketErrorEvent
+  | WebsocketCloseEvent;
+
+export type WebSocketEventLog = WebsocketEvent[];
+
+// @TODO: Volatile state for now, later we might want to persist event logs.
+const WebSocketConnections = new Map<string, WebSocket>();
+const WebSocketEventLogs = new Map<string, WebSocketEventLog>();
+
+async function createWebSocketConnection(
+  event: Electron.IpcMainInvokeEvent,
+  options: { requestId: string }
+) {
+  const existingConnection = WebSocketConnections.get(options.requestId);
+
+  if (existingConnection) {
+    console.warn('Connection still open to ' + existingConnection.url);
+    return;
+  }
+
+  try {
+    const request = await websocketRequest.getById(options.requestId);
+
+    if (!request?.url) {
+      throw new Error('No URL specified');
+    }
+
+    const eventChannel = `webSocketRequest.connection.${request._id}.event`;
+    const readyStateChannel = `webSocketRequest.connection.${request._id}.readyState`;
+
+    const ws = new WebSocket(request?.url);
+
+    event.sender.send(readyStateChannel, ws.readyState);
+    WebSocketConnections.set(options.requestId, ws);
+
+    ws.addEventListener('open', ({ type }) => {
+      const openEvent: WebsocketOpenEvent = {
+        _id: uuidV4(),
+        requestId: options.requestId,
+        type,
+      };
+
+      WebSocketEventLogs.set(options.requestId, [openEvent]);
+
+      event.sender.send(eventChannel, openEvent);
+      event.sender.send(readyStateChannel, ws.readyState);
+    });
+
+    ws.addEventListener('message', ({ data, type }) => {
+      const msgs = WebSocketEventLogs.get(options.requestId) || [];
+      const messageEvent: WebsocketMessageEvent = {
+        _id: uuidV4(),
+        requestId: options.requestId,
+        data,
+        type,
+        direction: 'INCOMING',
+      };
+
+      WebSocketEventLogs.set(options.requestId, [...msgs, messageEvent]);
+      event.sender.send(eventChannel, messageEvent);
+    });
+
+    ws.addEventListener('close', ({ code, reason, type, wasClean }) => {
+      const msgs = WebSocketEventLogs.get(options.requestId) || [];
+      const closeEvent: WebsocketCloseEvent = {
+        _id: uuidV4(),
+        requestId: options.requestId,
+        code,
+        reason,
+        type,
+        wasClean,
+      };
+
+      WebSocketEventLogs.set(options.requestId, [...msgs, closeEvent]);
+      WebSocketConnections.delete(options.requestId);
+
+      event.sender.send(eventChannel, closeEvent);
+      event.sender.send(readyStateChannel, ws.readyState);
+    });
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+}
+
+function getWebSocketReadyState(
+  _event: Electron.IpcMainInvokeEvent,
+  options: { requestId: string }
+): WebSocketConnection['readyState'] {
+  return WebSocketConnections.get(options.requestId)?.readyState ?? 0;
+}
+
+async function sendWebSocketEvent(
+  event: Electron.IpcMainInvokeEvent,
+  options: { message: string; requestId: string }
+) {
+  const ws = WebSocketConnections.get(options.requestId);
+
+  if (!ws) {
+    console.warn('No websocket found for requestId: ' + options.requestId);
+    return;
+  }
+
+  ws.send(options.message, error => {
+    // @TODO: We might want to set a status in the WebsocketMessageEvent
+    // and update it here based on the error. e.g. status = 'sending' | 'sent' | 'error'
+    if (error) {
+      console.error(error);
+    } else {
+      console.log('Message sent');
+    }
+  });
+
+  const connectionMessages = WebSocketEventLogs.get(options.requestId) || [];
+
+  const lastMessage: WebsocketMessageEvent = {
+    _id: uuidV4(),
+    requestId: options.requestId,
+    data: options.message,
+    direction: 'OUTGOING',
+    type: 'message',
+  };
+
+  WebSocketEventLogs.set(options.requestId, [
+    ...connectionMessages,
+    lastMessage,
+  ]);
+  const eventChannel = `webSocketRequest.connection.${options.requestId}.event`;
+  event.sender.send(eventChannel, lastMessage);
+}
+
+async function closeWebSocketConnection(
+  _event: Electron.IpcMainInvokeEvent,
+  options: { requestId: string }
+) {
+  const ws = WebSocketConnections.get(options.requestId);
+  if (!ws) {
+    console.warn('No websocket found for requestId: ' + options.requestId);
+    return;
+  }
+  ws.close();
+}
+
+async function getWebSocketConnectionEvents(
+  _event: Electron.IpcMainInvokeEvent,
+  options: { requestId: string }
+) {
+  const connectionMessages = WebSocketEventLogs.get(options.requestId) || [];
+  return connectionMessages;
+}
+
+export function registerWebSocketHandlers() {
+  ipcMain.handle('webSocketRequest.connection.create', createWebSocketConnection);
+  ipcMain.handle('webSocketRequest.connection.readyState', getWebSocketReadyState);
+  ipcMain.handle('webSocketRequest.connection.event.send', sendWebSocketEvent);
+  ipcMain.handle('webSocketRequest.connection.close', closeWebSocketConnection);
+  ipcMain.handle('webSocketRequest.connection.event.findMany', getWebSocketConnectionEvents);
+}
