be0f5db4ff7803c27c6b8f7924b26b41fe2b663c
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2022-08-03 23:24:02 -0400

    [INS-1697] Create WebSocket Request (#5041)
    * wip
    
    * add fail safe
    
    Co-authored-by: Mark Kim <mark.kim@konghq.com>

---
+++ packages/insomnia/src/ui/components/websocket-request-pane.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from 'react';
+
+export const WebSocketRequestPane: FC = () => {
+  return <div>WebSocket Request</div>;
+};
---
+++ packages/insomnia/src/ui/components/websocket-response-pane.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from 'react';
+
+export const WebSocketResponsePane: FC = () => {
+  return <div>WebSocket Response</div>;
+};
