bcba03d1350f8ee1fc70a420a6a7c67a1884f5d3
Mark Kim <mark.kim@konghq.com>
2022-08-09 13:07:50 -0400

    clean up

---
+++ packages/insomnia/src/ui/context/websocket-client/test-utils.ts
@@ -0,0 +1,15 @@
+import EventEmitter from 'events';
+
+export const getMockWsClient = (onChangeEvent: EventEmitter) => {
+  return {
+    create: jest.fn(),
+    close: jest.fn(),
+    send: jest.fn(),
+    onReadyState: (options: { requestId: string }, listener: (readyState: WebSocket['readyState']) => any) => {
+      const channel = `webSocketRequest.connection.${options.requestId}.readyState`;
+      onChangeEvent.on(channel, listener);
+      return () => onChangeEvent.removeListener(channel, listener);
+    },
+    getReadyState: jest.fn(),
+  };
+};
