714a6b26a8f5d0c6cb3a6f0f53bb12a121d73164
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2022-08-12 11:15:50 -0400

    updates test for new changes

--- packages/insomnia-importers/src/importers/curl.test.ts
+++ packages/insomnia-importers/src/importers/curl.test.ts
@@ -4,6 +4,11 @@ import { quote } from 'shell-quote';
 import { convert } from './curl';
 
 describe('curl', () => {
+  const url = 'http://example.com';
+  const method = '-X POST';
+  const mimeType = 'application/x-www-form-urlencoded';
+  const header = `-H 'Content-Type: ${mimeType}'`;
+
   describe('encoding', () => {
     it.each([
       { input: ' ', expected: ' ' },
@@ -16,13 +21,9 @@ describe('curl', () => {
       { input: '}', expected: '}' },
       { input: '|', expected: '|' },
       { input: '^', expected: '^' },
-      { input: '%', expected: '%' },
+      { input: '%3d', expected: '=' },
       { input: '"', expected: '"' },
     ])('encodes %p correctly', ({ input, expected }: { input: string; expected: string }) => {
-      const url = 'http://example.com';
-      const method = '-X POST';
-      const mimeType = 'application/x-www-form-urlencoded';
-      const header = `-H 'Content-Type: ${mimeType}'`;
       const quoted = quote([input]);
       const rawData = `curl ${method} ${url} ${header} --data ${quoted} --data-urlencode ${quoted}`;
 
@@ -30,10 +31,28 @@ describe('curl', () => {
         body: {
           params: [
             { name: expected, value: '' },
-            { name: expected, value: '' },
+            { name: input, value: '' },
           ],
         },
       }]);
+    });
+
+    it('handles & correctly', () => {
+      const rawData = `curl ${method} ${url} ${header} --data a=1\\&b=2 --data-urlencode c=3\\&d=4`;
+      expect(convert(rawData)).toMatchObject([{
+        body: {
+          params: [
+            { name: 'a', value: '1' },
+            { name: 'b', value: '2' },
+            { name: 'c=3&d=4', value: '' },
+          ],
+        },
+      }]);
+    });
+
+    it('throws on invalid url encoding', () => {
+      const rawData = `curl ${method} ${url} ${header} --data %`;
+      expect(() => convert(rawData)).toThrow('URI malformed');
     });
   });
 });
