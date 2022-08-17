d52610c605fc614c6fdd651a11e9842cf3fa11aa
Jack Kavanagh <jackkav@gmail.com>
2022-08-17 15:37:31 +0200

    Update packages/insomnia/src/ui/components/dropdowns/websocket-request-actions-dropdown.tsx

--- packages/insomnia/src/ui/components/dropdowns/websocket-request-actions-dropdown.tsx
+++ packages/insomnia/src/ui/components/dropdowns/websocket-request-actions-dropdown.tsx
@@ -71,4 +71,4 @@ export const WebSocketRequestActionsDropdown = forwardRef<DropdownHandle, Props>
   );
 });
 
-WebSocketRequestActionsDropdown.displayName = 'RequestActionsDropdown';
+WebSocketRequestActionsDropdown.displayName = 'WebSocketRequestActionsDropdown';
