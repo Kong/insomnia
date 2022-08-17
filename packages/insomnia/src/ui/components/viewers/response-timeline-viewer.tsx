623f1d5c38733779d22635c8c5f33fd0302b51da
jackkav <jackkav@gmail.com>
2022-08-17 22:43:44 +0200

    simplify ResponseTimelineViewer

--- packages/insomnia/src/ui/components/viewers/response-timeline-viewer.tsx
+++ packages/insomnia/src/ui/components/viewers/response-timeline-viewer.tsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { FC } from 'react';
 
 import { clickLink } from '../../../common/electron-helpers';
 import type { ResponseTimelineEntry } from '../../../main/network/libcurl-promise';
@@ -11,84 +11,41 @@ interface Props {
   response: Response;
 }
 
-interface State {
-  timeline: ResponseTimelineEntry[];
-  timelineKey: string;
-}
+export const ResponseTimelineViewer: FC<Props> = ({ showBody, response }) => {
+  const timeline = models.response.getTimeline(response, showBody);
+  const rows = timeline
+    .map(({ name, value }, i, all) => {
+      const prefixLookup: Record<ResponseTimelineEntry['name'], string> = {
+        HeaderIn: '< ',
+        DataIn: '| ',
+        SslDataIn: '<< ',
+        HeaderOut: '> ',
+        DataOut: '| ',
+        SslDataOut: '>> ',
+        Text: '* ',
+      };
+      const prefix: string = prefixLookup[name] || '* ';
+      const lines = (value + '').replace(/\n$/, '').split('\n');
+      const newLines = lines.filter(l => !l.match(/^\s*$/)).map(l => `${prefix}${l}`);
+      // Prefix each section with a newline to separate them
+      const previousName = i > 0 ? all[i - 1].name : '';
 
-export class ResponseTimelineViewer extends PureComponent<Props, State> {
-  state: State = {
-    timeline: [],
-    timelineKey: '',
-  };
-
-  componentDidMount() {
-    this.refreshTimeline();
-  }
-
-  componentDidUpdate(prevProps: Props) {
-    const { response } = this.props;
-
-    if (response._id !== prevProps.response._id) {
-      this.refreshTimeline();
-    }
-  }
-
-  async refreshTimeline() {
-    const { response, showBody } = this.props;
-    const timeline = models.response.getTimeline(response, showBody);
-
-    this.setState({
-      timeline,
-      timelineKey: response._id,
-    });
-  }
-
-  renderRow(row: ResponseTimelineEntry, i: number, all: ResponseTimelineEntry[]) {
-    const { name, value } = row;
-    const previousName = i > 0 ? all[i - 1].name : '';
-    const prefixLookup: Record<ResponseTimelineEntry['name'], string> = {
-      HeaderIn: '< ',
-      DataIn: '| ',
-      SslDataIn: '<< ',
-      HeaderOut: '> ',
-      DataOut: '| ',
-      SslDataOut: '>> ',
-      Text: '* ',
-    };
-    const prefix: string = prefixLookup[name] || '* ';
-
-    const lines = (value + '').replace(/\n$/, '').split('\n');
-    const newLines = lines.filter(l => !l.match(/^\s*$/)).map(l => `${prefix}${l}`);
-    let leadingSpace = '';
-
-    // Prefix each section with a newline to separate them
-    if (previousName !== name) {
-      leadingSpace = '\n';
-    }
-
-    // Join all lines together
-    return leadingSpace + newLines.join('\n');
-  }
-
-  render() {
-    const { timeline, timelineKey } = this.state;
-    const rows = timeline
-      .map(this.renderRow)
-      .filter(r => r !== null)
-      .join('\n')
-      .trim();
-
-    return (
-      <CodeEditor
-        key={timelineKey}
-        hideLineNumbers
-        readOnly
-        onClickLink={clickLink}
-        defaultValue={rows}
-        className="pad-left"
-        mode="curl"
-      />
-    );
-  }
-}
+      const hasNameChanged = previousName !== name;
+      // Join all lines together
+      return (hasNameChanged ? '\n' : '') + newLines.join('\n');
+    })
+    .filter(r => r !== null)
+    .join('\n')
+    .trim();
+  return (
+    <CodeEditor
+      key={response._id}
+      hideLineNumbers
+      readOnly
+      onClickLink={clickLink}
+      defaultValue={rows}
+      className="pad-left"
+      mode="curl"
+    />
+  );
+};
