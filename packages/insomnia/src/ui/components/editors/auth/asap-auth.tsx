bbb61f5a4e8913c61cf83663c0f53bedd63060bf
Mark Kim <103070941+marckong@users.noreply.github.com>
2022-08-29 10:41:47 -0400

    [INS-1805] Add Auth Header Tab (#5115)
    * add minimal change to the auth flow
    
    * add disable state
    
    * adding dropdown disable
    
    * simplify reducer
    
    * fix lint
    
    Co-authored-by: jackkav <jackkav@gmail.com>

--- packages/insomnia/src/ui/components/editors/auth/asap-auth.tsx
+++ packages/insomnia/src/ui/components/editors/auth/asap-auth.tsx
@@ -3,9 +3,11 @@ import React, { FC } from 'react';
 import { AuthInputRow } from './components/auth-input-row';
 import { AuthPrivateKeyRow } from './components/auth-private-key-row';
 import { AuthTableBody } from './components/auth-table-body';
+import { AuthToggleRow } from './components/auth-toggle-row';
 
 export const AsapAuth: FC = () => (
   <AuthTableBody>
+    <AuthToggleRow label="Enabled" property="disabled" invert />
     <AuthInputRow label='Issuer (iss)' property='issuer' />
     <AuthInputRow label='Subject (sub)' property='subject' />
     <AuthInputRow label='Audience (aud)' property='audience' />
--- packages/insomnia/src/ui/components/editors/auth/digest-auth.tsx
+++ packages/insomnia/src/ui/components/editors/auth/digest-auth.tsx
@@ -2,10 +2,12 @@ import React, { FC } from 'react';
 
 import { AuthInputRow } from './components/auth-input-row';
 import { AuthTableBody } from './components/auth-table-body';
+import { AuthToggleRow } from './components/auth-toggle-row';
 
-export const DigestAuth: FC = () => (
+export const DigestAuth: FC<{ disabled?: boolean }> = ({ disabled = false }) => (
   <AuthTableBody>
-    <AuthInputRow label='Username' property='username' />
-    <AuthInputRow label='Password' property='password' mask />
+    <AuthToggleRow label="Enabled" property="disabled" invert disabled={disabled} />
+    <AuthInputRow label='Username' property='username' disabled={disabled} />
+    <AuthInputRow label='Password' property='password' mask disabled={disabled} />
   </AuthTableBody>
 );
