192a4ab31f3e8026e16d356340b0bd89f1c38374
Jack Kavanagh <jackkav@gmail.com>
2022-09-11 09:25:05 +0200

    workspace duplicate simplify

--- packages/insomnia/src/ui/components/modals/workspace-duplicate-modal.tsx
+++ packages/insomnia/src/ui/components/modals/workspace-duplicate-modal.tsx
@@ -1,18 +1,15 @@
-import { autoBindMethodsForReact } from 'class-autobind-decorator';
-import React, { createRef, FC, forwardRef, ForwardRefRenderFunction, PureComponent, useImperativeHandle, useRef, useState } from 'react';
+import React, { FC, forwardRef, useImperativeHandle, useRef, useState } from 'react';
 import { useCallback } from 'react';
 import { useForm } from 'react-hook-form';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { AUTOBIND_CFG } from '../../../common/constants';
 import { getWorkspaceLabel } from '../../../common/get-workspace-label';
 import { strings } from '../../../common/strings';
 import * as models from '../../../models';
 import { ApiSpec } from '../../../models/api-spec';
-import getWorkspaceName from '../../../models/helpers/get-workspace-name';
 import * as workspaceOperations from '../../../models/helpers/workspace-operations';
 import { isDefaultProject, isLocalProject, isRemoteProject, Project } from '../../../models/project';
-import { Workspace } from '../../../models/workspace';
+import { isDesign, Workspace } from '../../../models/workspace';
 import { initializeLocalBackendProjectAndMarkForSync } from '../../../sync/vcs/initialize-backend-project';
 import { VCS } from '../../../sync/vcs/vcs';
 import { activateWorkspace } from '../../redux/modules/workspace';
@@ -21,21 +18,10 @@ import { Modal, ModalProps } from '../base/modal';
 import { ModalBody } from '../base/modal-body';
 import { ModalFooter } from '../base/modal-footer';
 import { ModalHeader } from '../base/modal-header';
-import { showModal } from '.';
-
-interface Options {
-  workspace: Workspace;
-  apiSpec: ApiSpec;
-  onDone?: () => void;
-}
 
 interface FormFields {
   newName: string;
   projectId: string;
-}
-
-interface InnerProps extends Options, Props {
-  hide: () => void;
 }
 
 const ProjectOption: FC<Project> = project => (
@@ -44,86 +30,13 @@ const ProjectOption: FC<Project> = project => (
   </option>
 );
 
-const WorkspaceDuplicateModalInternalWithRef: ForwardRefRenderFunction<Modal, InnerProps> = ({ workspace, apiSpec, onDone, hide, vcs }, ref) => {
-  const dispatch = useDispatch();
-
-  const projects = useSelector(selectProjects);
-  const activeProject = useSelector(selectActiveProject);
-  const isLoggedIn = useSelector(selectIsLoggedIn);
-
-  const title = `Duplicate ${getWorkspaceLabel(workspace).singular}`;
-  const defaultWorkspaceName = getWorkspaceName(workspace, apiSpec);
-
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: {
-      errors,
-    } } = useForm<FormFields>({
-      defaultValues: {
-        newName: defaultWorkspaceName,
-        projectId: activeProject._id,
-      },
-    });
-
-  const onSubmit = useCallback(async ({ projectId, newName }: FormFields) => {
-    const duplicateToProject = projects.find(project => project._id === projectId);
-    if (!duplicateToProject) {
-      throw new Error('Project could not be found');
-    }
-
-    const newWorkspace = await workspaceOperations.duplicate(workspace, { name: newName, parentId: projectId });
-    await models.workspace.ensureChildren(newWorkspace);
-
-    // Mark for sync if logged in and in the expected project
-    if (isLoggedIn && vcs && isRemoteProject(duplicateToProject)) {
-      await initializeLocalBackendProjectAndMarkForSync({ vcs: vcs.newInstance(), workspace: newWorkspace });
-    }
-
-    dispatch(activateWorkspace({ workspace: newWorkspace }));
-    hide();
-    onDone?.();
-  }, [dispatch, hide, isLoggedIn, onDone, projects, vcs, workspace]);
-
-  return <Modal ref={ref} onShow={reset}>
-    <ModalHeader>{title}</ModalHeader>
-    <ModalBody className="wide">
-      <form className="wide pad" onSubmit={handleSubmit(onSubmit)}>
-        <div className="form-control form-control--wide form-control--outlined">
-          <label>
-            New Name
-            <input {...register('newName', { validate: v => Boolean(v.trim()) || 'Should not be blank' })} />
-            {errors.newName && <div className="font-error space-top">{errors.newName.message}</div>}
-          </label>
-        </div>
-        <div className="form-control form-control--outlined">
-          <label>
-            {strings.project.singular} to duplicate into
-            <select {...register('projectId')}>
-              {projects.map(ProjectOption)}
-            </select>
-          </label>
-        </div>
-      </form>
-    </ModalBody>
-    <ModalFooter>
-      <button className="btn" onClick={handleSubmit(onSubmit)}>
-        Duplicate
-      </button>
-    </ModalFooter>
-  </Modal>;
-};
-
-const WorkspaceDuplicateModalInternal = forwardRef(WorkspaceDuplicateModalInternalWithRef);
-
 type Props = ModalProps & {
   vcs?: VCS;
 };
 
 interface ErrorModalOptions {
-  workspace: Workspace;
-  apiSpec: ApiSpec;
+  workspace?: Workspace;
+  apiSpec?: ApiSpec;
   onDone?: () => void;
 }
 export interface WorkspaceDuplicateModalHandle {
@@ -143,17 +56,72 @@ export const WorkspaceDuplicateModal = forwardRef<WorkspaceDuplicateModalHandle,
       modalRef.current?.show();
     },
   }), []);
-  if (state) {
-    return <WorkspaceDuplicateModalInternal
-      ref={modalRef}
-      workspace={state.workspace}
-      apiSpec={state.apiSpec}
-      onDone={state.onDone}
-      vcs={vcs}
-      hide={() => modalRef.current?.hide()}
-    />;
-  } else {
+  const dispatch = useDispatch();
+
+  const projects = useSelector(selectProjects);
+  const activeProject = useSelector(selectActiveProject);
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors } } = useForm<FormFields>({
+      defaultValues: {
+        newName: state.workspace ? isDesign(state.workspace) ? (state.apiSpec?.fileName || '') : state.workspace.name : '',
+        projectId: activeProject._id,
+      },
+    });
+
+  const onSubmit = useCallback(async ({ projectId, newName }: FormFields) => {
+    const duplicateToProject = projects.find(project => project._id === projectId);
+    if (!duplicateToProject) {
+      throw new Error('Project could not be found');
+    }
+    if (!state.workspace) {
+      throw new Error('Workspace could not be found');
+    }
+    const newWorkspace = await workspaceOperations.duplicate(state.workspace, { name: newName, parentId: projectId });
+    await models.workspace.ensureChildren(newWorkspace);
+    // Mark for sync if logged in and in the expected project
+    if (isLoggedIn && vcs && isRemoteProject(duplicateToProject)) {
+      await initializeLocalBackendProjectAndMarkForSync({ vcs: vcs.newInstance(), workspace: newWorkspace });
+    }
+    dispatch(activateWorkspace({ workspace: newWorkspace }));
+    modalRef.current?.hide();
+    state?.onDone?.();
+  }, [dispatch, isLoggedIn, projects, vcs, state]);
+  if (!state.workspace) {
     return null;
   }
+  return (
+    <Modal ref={modalRef} onShow={reset}>
+      <ModalHeader>{`Duplicate ${getWorkspaceLabel(state.workspace).singular}`}</ModalHeader>
+      <ModalBody className="wide">
+        <form className="wide pad" onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-control form-control--wide form-control--outlined">
+            <label>
+              New Name
+              <input {...register('newName', { validate: v => Boolean(v.trim()) || 'Should not be blank' })} />
+              {errors.newName && <div className="font-error space-top">{errors.newName.message}</div>}
+            </label>
+          </div>
+          <div className="form-control form-control--outlined">
+            <label>
+              {strings.project.singular} to duplicate into
+              <select {...register('projectId')}>
+                {projects.map(ProjectOption)}
+              </select>
+            </label>
+          </div>
+        </form>
+      </ModalBody>
+      <ModalFooter>
+        <button className="btn" onClick={handleSubmit(onSubmit)}>
+          Duplicate
+        </button>
+      </ModalFooter>
+    </Modal>
+  );
 });
 WorkspaceDuplicateModal.displayName = 'WorkspaceDuplicateModal';
