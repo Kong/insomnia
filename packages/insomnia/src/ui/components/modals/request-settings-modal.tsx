51e8807fe1bbade472232d90a781ccd6a2152844
jackkav <jackkav@gmail.com>
2022-09-11 09:51:39 +0200

    flatten request settings

--- packages/insomnia/src/ui/components/modals/request-settings-modal.tsx
+++ packages/insomnia/src/ui/components/modals/request-settings-modal.tsx
@@ -242,80 +242,6 @@ export class UnconnectedRequestSettingsModal extends PureComponent<Props, State>
     );
   }
 
-  _renderRequestSettings() {
-    const { request } = this.state;
-
-    // GrpcRequests do not have any request settings (yet)
-    // When the time comes, explore creating a standalone request settings modal for gRPC
-    if (!request || isGrpcRequest(request)) {
-      return null;
-    }
-
-    return (
-      <>
-        <div className="pad-top pad-bottom">
-          <div className="form-control form-control--thin">
-            <label>
-              Send cookies automatically
-              {this.renderCheckboxInput('settingSendCookies')}
-            </label>
-          </div>
-          <div className="form-control form-control--thin">
-            <label>
-              Store cookies automatically
-              {this.renderCheckboxInput('settingStoreCookies')}
-            </label>
-          </div>
-          <div className="form-control form-control--thin">
-            <label>
-              Automatically encode special characters in URL
-              {this.renderCheckboxInput('settingEncodeUrl')}
-              <HelpTooltip position="top" className="space-left">
-                Automatically encode special characters at send time (does not apply to query
-                parameters editor)
-              </HelpTooltip>
-            </label>
-          </div>
-          <div className="form-control form-control--thin">
-            <label>
-              Skip rendering of request body
-              {this.renderCheckboxInput('settingDisableRenderRequestBody')}
-              <HelpTooltip position="top" className="space-left">
-                Disable rendering of environment variables and tags for the request body
-              </HelpTooltip>
-            </label>
-          </div>
-          <div className="form-control form-control--thin">
-            <label>
-              Rebuild path dot sequences
-              <HelpTooltip position="top" className="space-left">
-                This instructs libcurl to squash sequences of "/../" or "/./" that may exist in the
-                URL's path part and that is supposed to be removed according to RFC 3986 section
-                5.2.4
-              </HelpTooltip>
-              {this.renderCheckboxInput('settingRebuildPath')}
-            </label>
-          </div>
-        </div>
-        <div className="form-control form-control--outlined">
-          <label>
-            Follow redirects <span className="txt-sm faint italic">(overrides global setting)</span>
-            <select
-              // @ts-expect-error -- TSCONVERSION this setting only exists for a Request not GrpcRequest
-              defaultValue={this.state.request?.settingFollowRedirects}
-              name="settingFollowRedirects"
-              onChange={this._updateRequestSettingString}
-            >
-              <option value={'global'}>Use global setting</option>
-              <option value={'off'}>Don't follow redirects</option>
-              <option value={'on'}>Follow redirects</option>
-            </select>
-          </label>
-        </div>
-      </>
-    );
-  }
-
   _renderDescription() {
     const {
     } = this.props;
@@ -361,106 +287,175 @@ export class UnconnectedRequestSettingsModal extends PureComponent<Props, State>
     return null;
   }
 
-  _renderMoveCopy() {
+  render() {
+    const { request, showDescription, defaultPreviewMode, activeWorkspaceIdToCopyTo, justMoved, justCopied, workspace } = this.state;
     const { workspacesForActiveProject } = this.props;
-    const { activeWorkspaceIdToCopyTo, justMoved, justCopied, workspace, request } = this.state;
-
-    // Don't show move/copy items if it doesn't exist, or if it is a gRPC request
-    if (!request || isGrpcRequest(request)) {
-      return null;
-    }
-
-    return (
-      <div className="form-row">
-        <div className="form-control form-control--outlined">
-          <label>
-            Move/Copy to Workspace
-            <HelpTooltip position="top" className="space-left">
-              Copy or move the current request to a new workspace. It will be placed at the root of
-              the new workspace's folder structure.
-            </HelpTooltip>
-            <select
-              value={activeWorkspaceIdToCopyTo || '__NULL__'}
-              onChange={this._handleUpdateMoveCopyWorkspace}
-            >
-              <option value="__NULL__">-- Select Workspace --</option>
-              {workspacesForActiveProject.map(w => {
-                if (workspace && workspace._id === w._id) {
-                  return null;
-                }
-
-                return (
-                  <option key={w._id} value={w._id}>
-                    {w.name}
-                  </option>
-                );
-              })}
-            </select>
-          </label>
-        </div>
-        <div className="form-control form-control--no-label width-auto">
-          <button
-            disabled={justCopied || !activeWorkspaceIdToCopyTo}
-            className="btn btn--clicky"
-            onClick={this._handleCopyToWorkspace}
-          >
-            {justCopied ? 'Copied!' : 'Copy'}
-          </button>
-        </div>
-        <div className="form-control form-control--no-label width-auto">
-          <button
-            disabled={justMoved || !activeWorkspaceIdToCopyTo}
-            className="btn btn--clicky"
-            onClick={this._handleMoveToWorkspace}
-          >
-            {justMoved ? 'Moved!' : 'Move'}
-          </button>
-        </div>
-      </div>
-    );
-  }
-
-  renderModalBody() {
-    const { request } = this.state;
-
     if (!request) {
       return null;
     }
 
-    return (
-      <div>
-        <div className="form-control form-control--outlined">
-          <label>
-            Name{' '}
-            <span className="txt-sm faint italic">(also rename by double-clicking in sidebar)</span>
-            <DebouncedInput
-              delay={500}
-              // @ts-expect-error -- TSCONVERSION props expand into an input but are difficult to type
-              type="text"
-              placeholder={request.url || 'My Request'}
-              defaultValue={request.name}
-              onChange={this._handleNameChange}
-            />
-          </label>
-        </div>
-        {this._renderDescription()}
-        {this._renderRequestSettings()}
-        <hr />
-        {this._renderMoveCopy()}
-        {this._renderFeatureRequestPrompt()}
-      </div>
-    );
-  }
-
-  render() {
-    const { request } = this.state;
     return (
       <Modal ref={this._setModalRef} freshState>
         <ModalHeader>
           Request Settings{' '}
           <span className="txt-sm selectable faint monospace">{request ? request._id : ''}</span>
         </ModalHeader>
-        <ModalBody className="pad">{this.renderModalBody()}</ModalBody>
+        <ModalBody className="pad">
+          <div>
+            <div className="form-control form-control--outlined">
+              <label>
+                Name{' '}
+                <span className="txt-sm faint italic">(also rename by double-clicking in sidebar)</span>
+                <DebouncedInput
+                  delay={500}
+                  // @ts-expect-error -- TSCONVERSION props expand into an input but are difficult to type
+                  type="text"
+                  placeholder={request.url || 'My Request'}
+                  defaultValue={request.name}
+                  onChange={this._handleNameChange}
+                />
+              </label>
+            </div>
+            {isGrpcRequest(request)
+              ? (
+                <p className="faint italic">
+                  Are there any gRPC settings you expect to see? Create a{' '}
+                  <a href={'https://github.com/Kong/insomnia/issues/new/choose'}>feature request</a>!
+                </p>
+              )
+              : (
+                <>
+                  <>
+                    {showDescription ? (
+                      <MarkdownEditor
+                        ref={this._editorRef}
+                        className="margin-top"
+                        defaultPreviewMode={defaultPreviewMode}
+                        placeholder="Write a description"
+                        defaultValue={request.description}
+                        onChange={this._handleDescriptionChange}
+                      />
+                    ) : (
+                      <button
+                        onClick={this._handleAddDescription}
+                        className="btn btn--outlined btn--super-duper-compact"
+                      >
+                        Add Description
+                      </button>
+                    )}
+                  </>
+                  <>
+                    <div className="pad-top pad-bottom">
+                      <div className="form-control form-control--thin">
+                        <label>
+                          Send cookies automatically
+                          {this.renderCheckboxInput('settingSendCookies')}
+                        </label>
+                      </div>
+                      <div className="form-control form-control--thin">
+                        <label>
+                          Store cookies automatically
+                          {this.renderCheckboxInput('settingStoreCookies')}
+                        </label>
+                      </div>
+                      <div className="form-control form-control--thin">
+                        <label>
+                          Automatically encode special characters in URL
+                          {this.renderCheckboxInput('settingEncodeUrl')}
+                          <HelpTooltip position="top" className="space-left">
+                            Automatically encode special characters at send time (does not apply to query
+                            parameters editor)
+                          </HelpTooltip>
+                        </label>
+                      </div>
+                      <div className="form-control form-control--thin">
+                        <label>
+                          Skip rendering of request body
+                          {this.renderCheckboxInput('settingDisableRenderRequestBody')}
+                          <HelpTooltip position="top" className="space-left">
+                            Disable rendering of environment variables and tags for the request body
+                          </HelpTooltip>
+                        </label>
+                      </div>
+                      <div className="form-control form-control--thin">
+                        <label>
+                          Rebuild path dot sequences
+                          <HelpTooltip position="top" className="space-left">
+                            This instructs libcurl to squash sequences of "/../" or "/./" that may exist in the
+                            URL's path part and that is supposed to be removed according to RFC 3986 section
+                            5.2.4
+                          </HelpTooltip>
+                          {this.renderCheckboxInput('settingRebuildPath')}
+                        </label>
+                      </div>
+                    </div>
+                    <div className="form-control form-control--outlined">
+                      <label>
+                        Follow redirects <span className="txt-sm faint italic">(overrides global setting)</span>
+                        <select
+                          // @ts-expect-error -- TSCONVERSION this setting only exists for a Request not GrpcRequest
+                          defaultValue={this.state.request?.settingFollowRedirects}
+                          name="settingFollowRedirects"
+                          onChange={this._updateRequestSettingString}
+                        >
+                          <option value={'global'}>Use global setting</option>
+                          <option value={'off'}>Don't follow redirects</option>
+                          <option value={'on'}>Follow redirects</option>
+                        </select>
+                      </label>
+                    </div>
+                  </>
+                  <hr />
+                  <div className="form-row">
+                    <div className="form-control form-control--outlined">
+                      <label>
+                        Move/Copy to Workspace
+                        <HelpTooltip position="top" className="space-left">
+                          Copy or move the current request to a new workspace. It will be placed at the root of
+                          the new workspace's folder structure.
+                        </HelpTooltip>
+                        <select
+                          value={activeWorkspaceIdToCopyTo || '__NULL__'}
+                          onChange={this._handleUpdateMoveCopyWorkspace}
+                        >
+                          <option value="__NULL__">-- Select Workspace --</option>
+                          {workspacesForActiveProject.map(w => {
+                            if (workspace && workspace._id === w._id) {
+                              return null;
+                            }
+
+                            return (
+                              <option key={w._id} value={w._id}>
+                                {w.name}
+                              </option>
+                            );
+                          })}
+                        </select>
+                      </label>
+                    </div>
+                    <div className="form-control form-control--no-label width-auto">
+                      <button
+                        disabled={justCopied || !activeWorkspaceIdToCopyTo}
+                        className="btn btn--clicky"
+                        onClick={this._handleCopyToWorkspace}
+                      >
+                        {justCopied ? 'Copied!' : 'Copy'}
+                      </button>
+                    </div>
+                    <div className="form-control form-control--no-label width-auto">
+                      <button
+                        disabled={justMoved || !activeWorkspaceIdToCopyTo}
+                        className="btn btn--clicky"
+                        onClick={this._handleMoveToWorkspace}
+                      >
+                        {justMoved ? 'Moved!' : 'Move'}
+                      </button>
+                    </div>
+                  </div>
+                </>)
+            }
+          </div>
+        </ModalBody>
       </Modal>
     );
   }
