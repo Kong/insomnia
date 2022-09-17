3a849339f05671ab015d320df29d4d0fb734c9bd
Carlos Eduardo Marques Freitas <carlosmfreitas05@gmail.com>
2022-09-17 15:48:45 -0300

    refactor: add makeNewAuth function to authentication common

--- packages/insomnia/src/ui/components/dropdowns/auth-dropdown.tsx
+++ packages/insomnia/src/ui/components/dropdowns/auth-dropdown.tsx
@@ -1,15 +1,12 @@
 import React, { FC, useCallback } from 'react';
 import { useSelector } from 'react-redux';
 
+import { makeNewAuth } from '../../../common/authentication';
 import {
   AuthType,
   getAuthTypeName,
-  HAWK_ALGORITHM_SHA256,
 } from '../../../common/constants';
 import { update } from '../../../models/helpers/request-operations';
-import { RequestAuthentication } from '../../../models/request';
-import { SIGNATURE_METHOD_HMAC_SHA1 } from '../../../network/o-auth-1/constants';
-import { GRANT_TYPE_AUTHORIZATION_CODE } from '../../../network/o-auth-2/constants';
 import { selectActiveRequest } from '../../redux/selectors';
 import { Dropdown } from '../base/dropdown/dropdown';
 import { DropdownButton } from '../base/dropdown/dropdown-button';
@@ -30,92 +27,6 @@ const defaultTypes: AuthType[] = [
   'asap',
   'netrc',
 ];
-
-function makeNewAuth(type: string, oldAuth: RequestAuthentication = {}): RequestAuthentication {
-  switch (type) {
-    // No Auth
-    case 'none':
-      return {};
-
-    // HTTP Basic Authentication
-    case 'basic':
-      return {
-        type,
-        useISO88591: oldAuth.useISO88591 || false,
-        disabled: oldAuth.disabled || false,
-        username: oldAuth.username || '',
-        password: oldAuth.password || '',
-      };
-
-    case 'digest':
-    case 'ntlm':
-      return {
-        type,
-        disabled: oldAuth.disabled || false,
-        username: oldAuth.username || '',
-        password: oldAuth.password || '',
-      };
-
-    case 'oauth1':
-      return {
-        type,
-        disabled: false,
-        signatureMethod: SIGNATURE_METHOD_HMAC_SHA1,
-        consumerKey: '',
-        consumerSecret: '',
-        tokenKey: '',
-        tokenSecret: '',
-        privateKey: '',
-        version: '1.0',
-        nonce: '',
-        timestamp: '',
-        callback: '',
-      };
-
-    // OAuth 2.0
-    case 'oauth2':
-      return {
-        type,
-        grantType: GRANT_TYPE_AUTHORIZATION_CODE,
-      };
-
-    // Aws IAM
-    case 'iam':
-      return {
-        type,
-        disabled: oldAuth.disabled || false,
-        accessKeyId: oldAuth.accessKeyId || '',
-        secretAccessKey: oldAuth.secretAccessKey || '',
-        sessionToken: oldAuth.sessionToken || '',
-      };
-
-    // Hawk
-    case 'hawk':
-      return {
-        type,
-        algorithm: HAWK_ALGORITHM_SHA256,
-      };
-
-    // Atlassian ASAP
-    case 'asap':
-      return {
-        type,
-        issuer: '',
-        subject: '',
-        audience: '',
-        additionalClaims: '',
-        keyId: '',
-        privateKey: '',
-      };
-
-    // Types needing no defaults
-    case 'netrc':
-    default:
-      return {
-        type,
-      };
-  }
-}
 
 const AuthItem: FC<{
   type: AuthType;
