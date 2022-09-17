d325881bb14380c3cc574e2deb691b85fdadaca1
Carlos Eduardo Marques Freitas <carlosmfreitas05@gmail.com>
2022-09-17 15:47:08 -0300

    refactor: add help tooltip to auth toggle and auth input component

--- packages/insomnia/src/ui/components/modals/workspace-preferences/components/auth-input-row.tsx
+++ packages/insomnia/src/ui/components/modals/workspace-preferences/components/auth-input-row.tsx
@@ -1,18 +1,34 @@
-import React, { FC, useCallback } from 'react';
+import React, { ComponentProps, FC, useCallback } from 'react';
+import { useSelector } from 'react-redux';
+import { useToggle } from 'react-use';
 
 import { toKebabCase } from '../../../../../common/misc';
-import { WorkspaceAuthentication } from '../../../../../models/workspace';
 import { useActiveWorkspace } from '../../../../hooks/use-active-workspace';
+import { selectSettings } from '../../../../redux/selectors';
+import { Button } from '../../../base/button';
 import { OneLineEditor } from '../../../codemirror/one-line-editor';
+import { HelpTooltip } from '../../../help-tooltip';
 import { AuthRow } from './auth-row';
 
-interface AuthInputRowProps {
+type OneLineEditorProps = Pick<ComponentProps<typeof OneLineEditor>, 'getAutocompleteConstants'>;
+
+interface AuthInputRowProps extends OneLineEditorProps {
   label: string;
-  property: keyof Omit<WorkspaceAuthentication, 'disabled' | 'type'>;
+  property: string;
+  mode?: string;
+  mask?: boolean;
+  help?: string;
 }
 
-export const AuthInputRow: FC<AuthInputRowProps> = ({ label, property }) => {
+export const AuthInputRow: FC<AuthInputRowProps> = ({ label, property, mode, mask, help, getAutocompleteConstants }) => {
+  const { showPasswords } = useSelector(selectSettings);
   const { activeWorkspace: { authentication }, patchAuth } = useActiveWorkspace();
+
+  const [masked, toggleMask] = useToggle(true);
+  const canBeMasked = !showPasswords && mask;
+  const isMasked = canBeMasked && masked;
+
+  const onClick = useCallback(() => toggleMask(), [toggleMask]);
 
   const onChange = useCallback((value: string) => patchAuth({ [property]: value }), [patchAuth, property]);
 
@@ -20,13 +36,32 @@ export const AuthInputRow: FC<AuthInputRowProps> = ({ label, property }) => {
 
   return (
     <AuthRow>
-      <label htmlFor={id}>{label}</label>
+      <div className="row" style={{ gap: 'var(--padding-sm)' }}>
+        <label className="no-margin-top" htmlFor={id}>{label}</label>
+        {help ? <HelpTooltip>{help}</HelpTooltip> : null}
+      </div>
 
-      <OneLineEditor
-        id={id}
-        defaultValue={authentication[property] || ''}
-        onChange={onChange}
-      />
+      <div className="flex">
+        <OneLineEditor
+          id={id}
+          type={isMasked ? 'password' : 'text'}
+          mode={mode}
+          onChange={onChange}
+          disabled={authentication.disabled}
+          defaultValue={authentication[property] || ''}
+          getAutocompleteConstants={getAutocompleteConstants}
+        />
+
+        {canBeMasked ? (
+          <Button
+            className="btn btn--super-duper-compact pointer"
+            onClick={onClick}
+            value={isMasked}
+          >
+            {isMasked ? <i className="fa fa-eye" data-testid="reveal-password-icon" /> : <i className="fa fa-eye-slash" data-testid="mask-password-icon" />}
+          </Button>
+        ) : null}
+      </div>
     </AuthRow>
   );
 };
