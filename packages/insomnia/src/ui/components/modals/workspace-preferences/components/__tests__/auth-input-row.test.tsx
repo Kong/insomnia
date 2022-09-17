22724312c5e52f08b8c44bec5dedc0eb2fcf9b62
Carlos Eduardo Marques Freitas <carlosmfreitas05@gmail.com>
2022-09-17 16:38:42 -0300

    test: add workspace auth components tests

---
+++ packages/insomnia/src/ui/components/modals/workspace-preferences/components/__tests__/auth-input-row.test.tsx
@@ -0,0 +1,106 @@
+import { beforeEach, describe, expect, it, jest } from '@jest/globals';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React, { FC } from 'react';
+
+import { globalBeforeEach } from '../../../../../../__jest__/before-each';
+import { createMockStoreWithWorkspace } from '../../../../../../__jest__/create-mock-store-with-active-workspace';
+import { MockCodeEditor } from '../../../../../../__jest__/mock-code-editor';
+import { withReduxStore } from '../../../../../../__jest__/with-redux-store';
+import * as models from '../../../../../../models';
+import { AuthInputRow } from '../auth-input-row';
+
+jest.mock('../../../../codemirror/code-editor', () => ({
+  CodeEditor: MockCodeEditor,
+}));
+
+const Row: FC = ({ children }) => <div>{children}</div>;
+
+describe('<AuthInputRow />', () => {
+  beforeEach(globalBeforeEach);
+
+  it('should mask and toggle the input', async () => {
+    // Arrange
+    await models.settings.patch({ showPasswords: false });
+    const { store } = await createMockStoreWithWorkspace();
+
+    // Render with mask enabled
+    const { findByLabelText, findByTestId } = render(
+      <AuthInputRow label='inputLabel' property='inputProperty' mask />,
+      { wrapper: withReduxStore(store, Row) }
+    );
+
+    let input = await findByLabelText('inputLabel');
+    expect(input).toHaveAttribute('type', 'password');
+
+    // Act
+    await userEvent.click(await findByTestId('reveal-password-icon'));
+
+    // Assert
+    input = await findByLabelText('inputLabel');
+    expect(input).toHaveAttribute('type', 'text');
+
+    // Act
+    await userEvent.click(await findByTestId('mask-password-icon'));
+
+    // Assert
+    input = await findByLabelText('inputLabel');
+    expect(input).toHaveAttribute('type', 'password');
+  });
+
+  it('should not show masking toggle if all passwords are shown', async () => {
+    // Arrange
+    await models.settings.patch({ showPasswords: true });
+    const { store } = await createMockStoreWithWorkspace();
+
+    // Render with mask enabled
+    const { findByLabelText, queryAllByTestId } = render(
+      <AuthInputRow label='inputLabel' property='inputProperty' mask />,
+      { wrapper: withReduxStore(store, Row) }
+    );
+
+    // Assert
+    const input = await findByLabelText('inputLabel');
+    expect(input).toHaveAttribute('type', 'text');
+    expect(queryAllByTestId('reveal-password-icon')).toHaveLength(0);
+    expect(queryAllByTestId('mask-password-icon')).toHaveLength(0);
+  });
+
+  it('should update the authentication property on typing', async () => {
+    // Arrange
+    const { store, workspaceId } = await createMockStoreWithWorkspace();
+
+    const { findByLabelText } = render(
+      <AuthInputRow label='inputLabel' property='inputProperty' />,
+      { wrapper: withReduxStore(store, Row) }
+    );
+
+    // This is a function because the underlying component changes as the element gets focus
+    const getInput = () => findByLabelText('inputLabel');
+
+    // Act
+    expect(await getInput()).not.toHaveFocus();
+    await userEvent.click(await getInput());
+    expect(await getInput()).toHaveFocus();
+
+    // NOTE: we are typing into a mocked CodeEditor component.
+    await userEvent.type(await getInput(), 'inputValue');
+
+    // Assert
+    const workspace = await models.workspace.getById(workspaceId);
+    expect(workspace?.authentication.inputProperty).toEqual('inputValue');
+  });
+
+  it('should pre-fill existing authentication property value', async () => {
+    // Arrange
+    const { store } = await createMockStoreWithWorkspace({ workspacePatch: { authentication: { inputProperty: 'existing' } } });
+
+    const { findByLabelText } = render(
+      <AuthInputRow label='inputLabel' property='inputProperty' />,
+      { wrapper: withReduxStore(store, Row) }
+    );
+
+    // Assert
+    expect(await findByLabelText('inputLabel')).toHaveValue('existing');
+  });
+});
