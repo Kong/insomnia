72f6911d16baeecebc9a26478c247e28dd989eb7
Mark Kim <mark.kim@konghq.com>
2022-09-30 13:22:00 -0400

    remove console

--- packages/insomnia/src/ui/components/websockets/disconnect-dropdown.tsx
+++ packages/insomnia/src/ui/components/websockets/disconnect-dropdown.tsx
@@ -17,16 +17,14 @@ const Button = styled(DropdownButton)({
     filter: 'brightness(0.8)',
   },
 });
-
 Button.displayName = DROPDOWN_BUTTON_DISPLAY_NAME;
+
 export const DisconnectDropdown: FC<{ requestId: string }> = ({ requestId }) => {
   const dropdownRef = useRef<DropdownHandle>();
   const handleCloseThisRequest = () => {
-    console.log('close');
     window.main.webSocket.close({ requestId });
   };
   const handleCloseAllRequests = () => {
-    console.log('close all');
     window.main.webSocket.closeAll();
   };
   return (
