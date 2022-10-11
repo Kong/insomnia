0a32da89f08e8b5ea8d522132c2ffbc3ca0b47a9
gatzjames <jamesgatzos@gmail.com>
2022-10-11 16:11:31 +0200

    remove unused switch component

--- packages/insomnia/src/ui/insomnia-components/index.ts
+++ packages/insomnia/src/ui/insomnia-components/index.ts
@@ -8,4 +8,3 @@ export { Header, type  HeaderProps } from './header';
 export { MultiSwitch } from './multi-switch';
 export { RadioButtonGroup, type RadioButtonGroupProps } from './radio-button-group';
 export { SvgIcon, type SvgIconProps, type IconEnum, type ThemeEnum, type IconId } from './svg-icon';
-export { Switch, type SwitchProps, type SwitchItem } from './switch';
