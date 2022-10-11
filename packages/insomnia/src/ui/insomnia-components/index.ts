fbd8187100694f45298e356313c6b3cb1d92a0dc
gatzjames <jamesgatzos@gmail.com>
2022-10-11 16:28:21 +0200

    Remove unused multi-switch and radio-group

--- packages/insomnia/src/ui/insomnia-components/index.ts
+++ packages/insomnia/src/ui/insomnia-components/index.ts
@@ -2,6 +2,4 @@ export * from './button';
 export * from './dropdown';
 export * from './list-group';
 
-export { MultiSwitch } from './multi-switch';
-export { RadioButtonGroup, type RadioButtonGroupProps } from './radio-button-group';
 export { SvgIcon, type SvgIconProps, type IconEnum, type ThemeEnum, type IconId } from './svg-icon';
