1f7db6b212682f78df152f0b4f301e7436876062
gatzjames <jamesgatzos@gmail.com>
2022-10-11 16:21:43 +0200

    move header to app-header

--- packages/insomnia/src/ui/insomnia-components/index.ts
+++ packages/insomnia/src/ui/insomnia-components/index.ts
@@ -4,7 +4,6 @@ export * from './list-group';
 
 export { CardContainer } from './card-container';
 export { Card, type  CardProps } from './card';
-export { Header, type  HeaderProps } from './header';
 export { MultiSwitch } from './multi-switch';
 export { RadioButtonGroup, type RadioButtonGroupProps } from './radio-button-group';
 export { SvgIcon, type SvgIconProps, type IconEnum, type ThemeEnum, type IconId } from './svg-icon';
