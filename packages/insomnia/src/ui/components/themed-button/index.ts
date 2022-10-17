3aac55d2b2a9f07b7d86706d32810746ffaac48a
gatzjames <jamesgatzos@gmail.com>
2022-10-17 14:53:28 +0200

    remove unused circle button

--- packages/insomnia/src/ui/components/themed-button/index.ts
+++ packages/insomnia/src/ui/components/themed-button/index.ts
@@ -1,3 +1,2 @@
 export { Button, type ButtonProps, type ButtonSizeEnum, type ButtonThemeEnum, type ButtonVariantEnum } from './button';
 export { AsyncButton, type AsyncButtonProps } from './async-button';
-export { CircleButton, type CircleButtonProps } from './circle-button';
