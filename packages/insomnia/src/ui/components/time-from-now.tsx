f9f7faa8ba8719e429c70663dc34deefe9a5fd3a
jackkav <jackkav@gmail.com>
2022-10-23 10:10:54 +0100

    time from now

--- packages/insomnia/src/ui/components/time-from-now.tsx
+++ packages/insomnia/src/ui/components/time-from-now.tsx
@@ -1,8 +1,7 @@
-import { autoBindMethodsForReact } from 'class-autobind-decorator';
 import { differenceInMinutes, formatDistanceToNowStrict } from 'date-fns';
-import React, { PureComponent } from 'react';
+import React, { FC, useState } from 'react';
+import { useInterval } from 'react-use';
 
-import { AUTOBIND_CFG } from '../../common/constants';
 import { toTitleCase } from '../../common/misc';
 
 interface Props {
@@ -12,56 +11,24 @@ interface Props {
   titleCase?: boolean;
 }
 
-interface State {
-  text: string;
-}
-
-@autoBindMethodsForReact(AUTOBIND_CFG)
-export class TimeFromNow extends PureComponent<Props, State> {
-  _interval: NodeJS.Timeout | null = null;
-
-  state: State = {
-    text: '',
-  };
-
-  _update() {
-    const { timestamp, titleCase } = this.props;
+export const TimeFromNow: FC<Props> = ({ className, timestamp, titleCase, intervalSeconds }) => {
+  const [text, setText] = useState('');
+  useInterval(() => {
     const date = new Date(timestamp);
-
     let text = formatDistanceToNowStrict(date, { addSuffix: true });
-
     const lessThanOneMinuteAgo = differenceInMinutes(Date.now(), date) < 1;
     if (lessThanOneMinuteAgo) {
       text = 'just now';
     }
-
     if (titleCase) {
       text = toTitleCase(text);
     }
+    setText(text);
+  }, (intervalSeconds || 5) * 1000);
 
-    this.setState({ text });
-  }
-
-  componentDidMount() {
-    const intervalSeconds = this.props.intervalSeconds || 5;
-    this._interval = setInterval(this._update, intervalSeconds * 1000);
-
-    this._update();
-  }
-
-  componentWillUnmount() {
-    if (this._interval !== null) {
-      clearInterval(this._interval);
-    }
-  }
-
-  render() {
-    const { className, timestamp } = this.props;
-    const { text } = this.state;
-    return (
-      <span title={new Date(timestamp).toLocaleString()} className={className}>
-        {text}
-      </span>
-    );
-  }
-}
+  return (
+    <span title={new Date(timestamp).toLocaleString()} className={className}>
+      {text}
+    </span>
+  );
+};
