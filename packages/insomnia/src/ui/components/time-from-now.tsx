30f03e90a13a73c6a19174647b59423f401d3b24
Mark Kim <mark.kim@konghq.com>
2022-10-24 10:39:37 -0400

    add fix for the timenow delay

--- packages/insomnia/src/ui/components/time-from-now.tsx
+++ packages/insomnia/src/ui/components/time-from-now.tsx
@@ -11,21 +11,41 @@ interface Props {
   titleCase?: boolean;
 }
 
-export const TimeFromNow: FC<Props> = ({ className, timestamp, titleCase, intervalSeconds }) => {
-  const [text, setText] = useState('');
+function getTimeFromNow(timestamp: string | number | Date, titleCase: boolean): string {
+  const date = new Date(timestamp);
+  let text = formatDistanceToNowStrict(date, { addSuffix: true });
+  const lessThanOneMinuteAgo = differenceInMinutes(Date.now(), date) < 1;
+  if (lessThanOneMinuteAgo) {
+    text = 'just now';
+  }
+  if (titleCase) {
+    text = toTitleCase(text);
+  }
+  return text;
+}
+
+function useTimeNowLabel(
+  timestamp: number | Date | string,
+  titleCase?: boolean,
+  intervalSeconds?: number
+): string {
+  const [text, setText] = useState(getTimeFromNow(timestamp, Boolean(titleCase)));
+
   useInterval(() => {
-    const date = new Date(timestamp);
-    let text = formatDistanceToNowStrict(date, { addSuffix: true });
-    const lessThanOneMinuteAgo = differenceInMinutes(Date.now(), date) < 1;
-    if (lessThanOneMinuteAgo) {
-      text = 'just now';
-    }
-    if (titleCase) {
-      text = toTitleCase(text);
-    }
-    setText(text);
+    const newText = getTimeFromNow(timestamp, Boolean(titleCase));
+    setText(newText);
   }, (intervalSeconds || 5) * 1000);
 
+  return text;
+}
+
+export const TimeFromNow: FC<Props> = ({
+  className,
+  timestamp,
+  titleCase,
+  intervalSeconds,
+}) => {
+  const text = useTimeNowLabel(timestamp, titleCase, intervalSeconds);
   return (
     <span title={new Date(timestamp).toLocaleString()} className={className}>
       {text}
