d2d28ce34093803c9aeecb6e5d1736d71f0e651c
jackkav <jackkav@gmail.com>
2022-11-04 22:07:04 +0100

    duplicate all that noise

--- plugins/insomnia-plugin-request/index.js
+++ plugins/insomnia-plugin-request/index.js
@@ -1,9 +1,82 @@
 const { jarFromCookies } = require('insomnia-cookies');
 const { format, parse } = require('url');
+
+/**
+ * URL encode a string in a flexible way
+ * @param str string to encode
+ * @param ignore characters to ignore
+ */
+const flexibleEncodeComponent = (str = '', ignore = '') => {
+  // Sometimes spaces screw things up because of url.parse
+  str = str.replace(/%20/g, ' ');
+
+  // Handle all already-encoded characters so we don't touch them
+  str = str.replace(/%([0-9a-fA-F]{2})/g, '__ENC__$1');
+
+  // Do a special encode of ignored chars, so they aren't touched.
+  // This first pass, surrounds them with a special tag (anything unique
+  // will work), so it can change them back later
+  // Example: will replace %40 with __LEAVE_40_LEAVE__, and we'll change
+  // it back to %40 at the end.
+  for (const c of ignore) {
+    const code = encodeURIComponent(c).replace('%', '');
+    const escaped = c.replace(ESCAPE_REGEX_MATCH, '\\$&');
+    const re2 = new RegExp(escaped, 'g');
+    str = str.replace(re2, `__RAW__${code}`);
+  }
+
+  // Encode it
+  str = encodeURIComponent(str);
+
+  // Put back the raw version of the ignored chars
+  for (const match of str.match(/__RAW__([0-9a-fA-F]{2})/g) || []) {
+    const code = match.replace('__RAW__', '');
+    str = str.replace(match, decodeURIComponent(`%${code}`));
+  }
+
+  // Put back the encoded version of the ignored chars
+  for (const match of str.match(/__ENC__([0-9a-fA-F]{2})/g) || []) {
+    const code = match.replace('__ENC__', '');
+    str = str.replace(match, `%${code}`);
+  }
+
+  return str;
+};
+/**
+ * Build a querystring parameter from a param object
+ */
+const buildQueryParameter = (
+  param,
+
+  /** allow empty names and values */
+  strict,
+) => {
+  strict = strict === undefined ? true : strict;
+
+  // Skip non-name ones in strict mode
+  if (strict && !param.name) {
+    return '';
+  }
+
+  // Cast number values to strings
+  if (typeof param.value === 'number') {
+    param.value = String(param.value);
+  }
+
+  if (!strict || param.value) {
+    // Don't encode ',' in values
+    const value = flexibleEncodeComponent(param.value || '').replace(/%2C/gi, ',');
+    const name = flexibleEncodeComponent(param.name || '');
+
+    return `${name}=${value}`;
+  } else {
+    return flexibleEncodeComponent(param.name);
+  }
+};
 /**
  * Build a querystring from a list of name/value pairs
  */
-export const buildQueryStringFromParams = (
+const buildQueryStringFromParams = (
   parameters,
   /** allow empty names and values */
   strict,
@@ -19,7 +92,11 @@ export const buildQueryStringFromParams = (
   }
   return items.join('&');
 };
-export const joinUrlAndQueryString = (url, qs) => {
+const getJoiner = (url) => {
+  url = url || '';
+  return url.indexOf('?') === -1 ? '?' : '&';
+};
+const joinUrlAndQueryString = (url, qs) => {
   if (!qs) {
     return url;
   }
@@ -33,12 +110,86 @@ export const joinUrlAndQueryString = (url, qs) => {
   const hash = hashes.length ? `#${hashes.join('#')}` : '';
   return `${baseUrl}${joiner}${qs}${hash}`;
 };
+const setDefaultProtocol = (url, defaultProto) => {
+  const trimmedUrl = url.trim();
+  defaultProto = defaultProto || 'http:';
+
+  // If no url, don't bother returning anything
+  if (!trimmedUrl) {
+    return '';
+  }
+
+  // Default the proto if it doesn't exist
+  if (trimmedUrl.indexOf('://') === -1) {
+    return `${defaultProto}//${trimmedUrl}`;
+  }
+
+  return trimmedUrl;
+};
+/**
+ * Deconstruct a querystring to name/value pairs
+ * @param [qs] {string}
+ * @param [strict=true] {boolean} - allow empty names and values
+ * @returns {{name: string, value: string}[]}
+ */
+const deconstructQueryStringToParams = (
+  qs,
+
+  /** allow empty names and values */
+  strict,
+) => {
+  strict = strict === undefined ? true : strict;
+  const pairs = [];
+
+  if (!qs) {
+    return pairs;
+  }
+
+  const stringPairs = qs.split('&');
+
+  for (const stringPair of stringPairs) {
+    // NOTE: This only splits on first equals sign. '1=2=3' --> ['1', '2=3']
+    const [encodedName, ...encodedValues] = stringPair.split('=');
+    const encodedValue = encodedValues.join('=');
+
+    let name = '';
+    try {
+      name = decodeURIComponent(encodedName || '');
+    } catch (error) {
+      // Just leave it
+      name = encodedName;
+    }
+
+    let value = '';
+    try {
+      value = decodeURIComponent(encodedValue || '');
+    } catch (error) {
+      // Just leave it
+      value = encodedValue;
+    }
+
+    if (strict && !name) {
+      continue;
+    }
+
+    pairs.push({ name, value });
+  }
+
+  return pairs;
+};
+const ESCAPE_REGEX_MATCH = /[-[\]/{}()*+?.\\^$|]/g;
+/** see list of allowed characters https://datatracker.ietf.org/doc/html/rfc3986#section-2.2 */
+const RFC_3986_GENERAL_DELIMITERS = ':@'; // (unintentionally?) missing: /?#[]
+/** see list of allowed characters https://datatracker.ietf.org/doc/html/rfc3986#section-2.2 */
+const RFC_3986_SUB_DELIMITERS = '$+,;='; // (unintentionally?) missing: !&'()*
+/** see list of allowed characters https://datatracker.ietf.org/doc/html/rfc3986#section-2.2 */
+const URL_PATH_CHARACTER_WHITELIST = `${RFC_3986_GENERAL_DELIMITERS}${RFC_3986_SUB_DELIMITERS}`;
 /**
  * Automatically encode the path and querystring components
  * @param url url to encode
  * @param encode enable encoding
  */
-export const smartEncodeUrl = (url, encode) => {
+const smartEncodeUrl = (url, encode) => {
   // Default autoEncode = true if not passed
   encode = encode === undefined ? true : encode;
   const urlWithProto = setDefaultProtocol(url);
