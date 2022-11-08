29a50e36564d410c9aa3e5dabbc1a1a9173457ec
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-11-08 19:39:16 +0200

    Use react-aria tabs instead of react-tabs

--- packages/insomnia/src/ui/components/base/tabs.tsx
+++ packages/insomnia/src/ui/components/base/tabs.tsx
@@ -1,29 +1,93 @@
-import { Tab as UnstyledTab, TabList, TabPanel, Tabs } from 'react-tabs';
+import { Node, Orientation } from '@react-types/shared';
+import classnames from 'classnames';
+import React, { createRef, FC, ReactNode } from 'react';
+import { AriaTabListProps, AriaTabPanelProps, useTab, useTabList, useTabPanel } from 'react-aria';
+import { Item, ItemProps, TabListState, useTabListState } from 'react-stately';
 import styled from 'styled-components';
 
-/**
- * Create a wrapper styled Tab component.
- */
-const Tab = styled(UnstyledTab).attrs({
-  selectedClassName: 'selected',
-  tabIndex: '0',
-})`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: var(--line-height-sm);
-  border: 1px solid transparent;
-  border-bottom: 1px solid var(--hl-md);
-  border-top: 0;
-  white-space: nowrap;
-  position: relative;
-  padding: 0 var(--padding-md);
-  color: var(--hl);
+type TabItemProps = ItemProps<{
+  className: string;
+}>;
 
-  &.selected {
-    border-left: 1px solid var(--hl-md);
-    border-right: 1px solid var(--hl-md);
-  }
-`;
+interface TabProps {
+  item: Node<TabItemProps>;
+  state: TabListState<TabItemProps>;
+  orientation?: Orientation;
+}
 
-export { Tab, TabList, TabPanel, Tabs };
+// const Tab: FC<TabProps> = ({ item, state, orientation }) => {
+const Tab: FC<TabProps> = ({ item, state }) => {
+  const { key, rendered } = item;
+  const ref = createRef<HTMLDivElement>();
+  // const { tabProps, isSelected, isDisabled } = useTab({ key }, state, ref);
+  const { tabProps } = useTab({ key }, state, ref);
+
+  return (
+    <div {...tabProps} ref={ref}>
+      {rendered}
+    </div>
+  );
+};
+
+interface TabPanelProps extends AriaTabPanelProps {
+  state: TabListState<TabItemProps>;
+}
+
+const TabPanel: FC<TabPanelProps> = ({ state, ...props }) => {
+  const ref = createRef<HTMLDivElement>();
+  const { tabPanelProps } = useTabPanel(props, state, ref);
+
+  return (
+    <div {...tabPanelProps} ref={ref}>
+      {state.selectedItem?.props.children}
+    </div>
+  );
+};
+
+const Tabs: FC<AriaTabListProps<TabItemProps>> = props => {
+  const state = useTabListState(props);
+  const ref = createRef<HTMLDivElement>();
+  const { tabListProps } = useTabList(props, state, ref);
+
+  return (
+    <div className={classnames('tabs', props.orientation || '')}>
+      <div {...tabListProps} ref={ref}>
+        {[...state.collection].map((item: Node<TabItemProps>) => (
+          <Tab
+            key={item.key}
+            item={item}
+            state={state}
+            orientation={props.orientation}
+          />
+        ))}
+      </div>
+      <TabPanel
+        key={state.selectedItem?.key}
+        state={state}
+      />
+    </div>
+  );
+};
+
+interface PanelContainerProps {
+  className?: string;
+  children: ReactNode;
+}
+
+const StyledPanelContainer = styled.div({
+  width: '100%',
+  height: '100%',
+  position: 'relative',
+  boxSizing: 'border-box',
+  overflow: 'auto',
+
+  '&::-webkit-scrollbar': {
+    display: 'none',
+  },
+});
+
+const PanelContainer: FC<PanelContainerProps> = ({ className, children }) => {
+  return (<StyledPanelContainer className={className}>{children}</StyledPanelContainer>);
+};
+
+export { Tabs, Item as TabItem, PanelContainer };
