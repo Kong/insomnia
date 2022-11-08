ce07e7e85ff6068a6655fcee9f359893238ecd80
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-11-08 19:54:43 +0200

    Use new tabs in grpc-tabbed-messages

--- packages/insomnia/src/ui/components/viewers/grpc-tabbed-messages.tsx
+++ packages/insomnia/src/ui/components/viewers/grpc-tabbed-messages.tsx
@@ -1,8 +1,7 @@
-import classnames from 'classnames';
 import React, { FunctionComponent } from 'react';
 
 import { HandleGetRenderContext, HandleRender } from '../../../common/render';
-import { Tab, TabList, TabPanel, Tabs } from '../base/tabs';
+import { TabItem, Tabs } from '../base/tabs';
 import { GRPCEditor } from '../editors/grpc-editor';
 
 interface Message {
@@ -37,20 +36,18 @@ export const GrpcTabbedMessages: FunctionComponent<Props> = ({
   const shouldShowBody = !!handleBodyChange;
   const orderedMessages = messages?.sort((a, b) => a.created - b.created) || [];
   return (
-    <Tabs
-      key={uniquenessKey}
-      className={classnames('react-tabs', 'react-tabs--nested')}
-    >
+    <Tabs key={uniquenessKey}>
       <div className='tab-action-wrapper'>
         <div className='tab-action-tabs'>
-          <TabList>
-            {shouldShowBody && <Tab>Body</Tab>}
-            {messages?.map((m, index) => (
-              <Tab key={m.id}>
-                {tabNamePrefix} {index + 1}
-              </Tab>
-            ))}
-          </TabList>
+          {shouldShowBody &&
+            <TabItem key="body" title="Body">
+              <GRPCEditor content={bodyText} handleChange={handleBodyChange} />
+            </TabItem>}
+          {orderedMessages?.map((m, index) => (
+            <TabItem key={m.id} title={`${tabNamePrefix} ${index + 1}`}>
+              <GRPCEditor content={m.text} readOnly />
+            </TabItem>
+          ))}
         </div>
         {showActions && (
           <>
@@ -73,16 +70,6 @@ export const GrpcTabbedMessages: FunctionComponent<Props> = ({
           </>
         )}
       </div>
-      {shouldShowBody && (
-        <TabPanel className='react-tabs__tab-panel editor-wrapper'>
-          <GRPCEditor content={bodyText} handleChange={handleBodyChange} />
-        </TabPanel>
-      )}
-      {orderedMessages.map(m => (
-        <TabPanel key={m.id} className='react-tabs__tab-panel editor-wrapper'>
-          <GRPCEditor content={m.text} readOnly />
-        </TabPanel>
-      ))}
     </Tabs>
   );
 };
