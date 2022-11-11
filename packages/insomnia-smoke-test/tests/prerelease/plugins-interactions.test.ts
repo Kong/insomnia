40d08f996ffd5d3e4d7bf87074e4eefb300d2504
Filipe Freire <livrofubia@gmail.com>
2022-11-11 10:02:28 +0000

    Add failing test for the Declarative config and kong kubernetes

--- packages/insomnia-smoke-test/tests/prerelease/plugins-interactions.test.ts
+++ packages/insomnia-smoke-test/tests/prerelease/plugins-interactions.test.ts
@@ -17,5 +17,27 @@ test.describe('Plugins', async () => {
     await expect(page.locator('.app')).toContainText('insomnia-plugin-demo-example');
   });
 
+  test('Check Declarative Config and Kong Kubernetes config', async ({ page }) => {
+    // Switch to design tab
+    await page.click('text=Design');
+
+    // Set example OpenAPI spec
+    await page.click('text=start from an example');
+    await expect(page.locator('.app')).toContainText('This is a sample server Petstore server');
+  
+    // Open declarative config
+    await page.click('button:has-text("New Document")');
+    await page.click('button:has-text("Declarative Config")');
+    
+    // Check for declarative config contents
+    await page.click('text=/.*"_format_version".*/');
+    
+    // Switch to Kong for Kubernetes tab
+    await page.click('div[role="tab"]:has-text("Kong for Kubernetes")');
+    
+    // Check for Kong for Kubernetes contents
+    await page.click('text=apiVersion: networking.k8s.io/v1');
+  });
+
   // TODO: more scenarios will be added in follow-up iterations of increasing test coverage
 });
