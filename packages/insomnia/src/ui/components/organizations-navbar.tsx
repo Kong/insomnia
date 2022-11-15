473624a45e2e30ec85750151fd2028a0ace22924
James Gatz <jamesgatzos@gmail.com>
2022-11-15 17:27:30 +0100

    better initials for organization names and proper detection of icons and emojis (#5418)

--- packages/insomnia/src/ui/components/organizations-navbar.tsx
+++ packages/insomnia/src/ui/components/organizations-navbar.tsx
@@ -118,6 +118,26 @@ const SignupModal: FC<{onHide: () => void}> = ({
   );
 };
 
+const getNameInitials = (name: string) => {
+  // Split on whitespace and take first letter of each word
+  const words = name.toUpperCase().split(' ');
+  const firstWord = words[0];
+  const lastWord = words[words.length - 1];
+
+  // If there is only one word, just take the first letter
+  if (words.length === 1) {
+    return firstWord.charAt(0);
+  }
+
+  // If the first word is an emoji or an icon then just use that
+  const iconMatch = firstWord.match(/\p{Extended_Pictographic}/u);
+  if (iconMatch) {
+    return iconMatch[0];
+  }
+
+  return `${firstWord.charAt(0)}${lastWord ? lastWord.charAt(0) : ''}`;
+};
+
 export const OrganizationsNav: FC = () => {
   const { organizations } = useLoaderData() as RootLoaderData;
   const { organizationId } = useParams() as { organizationId:string };
@@ -134,7 +154,7 @@ export const OrganizationsNav: FC = () => {
                     to={`/organization/${organization._id}`}
                     $isActive={organizationId === organization._id}
                   >
-                    {isDefaultOrganization(organization) ? (<i className='fa fa-home' />) : organization.name.charAt(0).toUpperCase() + organization.name.slice(1).charAt(0).toUpperCase()}
+                    {isDefaultOrganization(organization) ? (<i className='fa fa-home' />) : getNameInitials(organization.name)}
                   </NavbarItem>
                 </Tooltip>
               </li>
