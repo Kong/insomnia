0f31588c54efc7481153f50e51c1926593b12a36
Filipe Freire <livrofubia@gmail.com>
2022-11-21 14:34:14 +0000

    Add prerelease tests for Unit Test feature

--- packages/insomnia-smoke-test/tests/prerelease/design-interactions.test.ts
+++ packages/insomnia-smoke-test/tests/prerelease/design-interactions.test.ts
@@ -1,9 +1,9 @@
 import { expect } from '@playwright/test';
 
+import { loadFixture } from '../../playwright/paths';
 import { test } from '../../playwright/test';
 
 test.describe('Design interactions', async () => {
-  // TODO(filfreire): add a few scenarios ported from the release checklist
 
   test.fixme('Requests are auto-generated when switching tabs', async ({ page }) => {
     // TODO implement
@@ -23,16 +23,48 @@ test.describe('Design interactions', async () => {
     await expect(true).toBeTruthy();
   });
 
-  test.fixme('Unit Test interactions', async ({ page }) => {
-    // TODO implement
-    // switch to the test tab
-    // create a new test suite
-    // add a new test
-    // use the autocomplete inside the test code
-    // run tests and check the results
-    // rename the test
-    // rename the test suite
+  test('Unit Test interactions', async ({ app, page }) => {
+    // Setup
     await page.click('[data-testid="project"] >> text=Insomnia');
-    await expect(true).toBeTruthy();
+    await page.click('text=Create');
+    const text = await loadFixture('unit-test.yaml');
+    await app.evaluate(async ({ clipboard }, text) => clipboard.writeText(text), text);
+    await page.click('button:has-text("Clipboard")');
+    await page.click('text=unit-test.yamljust now');
+
+    // Switch to Test tab
+    await page.click('a:has-text("Test")');
+
+    // Run tests and check results
+    await page.click('#wrapper >> text=Run Tests');
+    await expect(page.locator('.app')).toContainText('Request A is found');
+    await expect(page.locator('.app')).toContainText('Request B is not found');
+    await expect(page.locator('.app')).toContainText('Tests Passed 2/2');
+
+    // Create a new test suite
+    await page.click('text=New Test Suite');
+    await page.click('text=Create Suite');
+    await page.click('button:has-text("New Suite")');
+
+    // Add a new test
+    await page.locator('text=New Test').nth(1).click();
+    await page.click('div[role="dialog"] button:has-text("New Test")');
+    await page.locator('select[name="request"]').selectOption('req_17ca8bbd46374144a089f891e32842d6');
+
+    await page.click('#wrapper >> text=Run Tests');
+    await expect(page.locator('.app')).toContainText('Tests Passed 1/1');
+
+    // Rename a test
+    // TODO(filipe) - add this in another PR
+
+    // Rename a test suite
+    await page.click('button:has-text("Existing Test Suite")');
+    await page.click('span:has-text("Existing Test Suite")');
+    await page.locator('text=New TestRun Tests >> input[type="text"]').fill('Renamed');
+    await page.locator('text=New TestRun Tests >> input[type="text"]').press('Enter');
+    await page.click('button:has-text("Renamed")');
+
+    // Use autocomplete inside the test code
+    // TODO(filipe) - add this in another PR
   });
 });
