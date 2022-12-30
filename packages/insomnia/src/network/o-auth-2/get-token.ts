8d26dd36fdf639efb35b96e1046901b50fd42b64
jackkav <jackkav@gmail.com>
2022-12-30 20:37:07 +0000

    flatten refresh token

--- packages/insomnia/src/network/o-auth-2/get-token.ts
+++ packages/insomnia/src/network/o-auth-2/get-token.ts
@@ -2,7 +2,9 @@ import * as models from '../../models';
 import type { OAuth2Token } from '../../models/o-auth-2-token';
 import type { RequestAuthentication, RequestHeader } from '../../models/request';
 import type { Response } from '../../models/response';
+import { invariant } from '../../utils/invariant';
 import { setDefaultProtocol } from '../../utils/url/protocol';
+import { getBasicAuthHeader } from '../basic-auth/get-header';
 import { sendWithSettings } from '../network';
 import {
   GRANT_TYPE_AUTHORIZATION_CODE,
@@ -16,7 +18,7 @@ import { grantAuthCodeParams } from './grant-authorization-code';
 import { grantClientCreds } from './grant-client-credentials';
 import { grantImplicitUrl } from './grant-implicit';
 import { grantPassword } from './grant-password';
-import { getOAuthSession, tryToParse } from './misc';
+import { getOAuthSession, insertAuthKeyIf, tryToParse } from './misc';
 import { refreshAccessToken } from './refresh-token';
 
 export const oauthResponseToAccessToken = (accessTokenUrl: string, response: Response) => {
@@ -86,7 +88,7 @@ export const getOAuth2Token = async (
         access_token: data.access_token || data._id_token,
       };
       const old = await models.oAuth2Token.getOrCreateByParentId(requestId);
-      return models.oAuth2Token.update(old, transformNewAccessTokenToOauthModal(newToken));
+      return models.oAuth2Token.update(old, transformNewAccessTokenToOauthModel(newToken));
     }
   } else {
     let params: RequestHeader[] = [];
@@ -100,15 +102,16 @@ export const getOAuth2Token = async (
     const response = await sendAccessTokenRequest(requestId, authentication.accessTokenUrl, params, authentication.origin);
     newToken = oauthResponseToAccessToken(authentication.accessTokenUrl, response);
     const old = await models.oAuth2Token.getOrCreateByParentId(requestId);
-    return models.oAuth2Token.update(old, transformNewAccessTokenToOauthModal(newToken));
+    return models.oAuth2Token.update(old, transformNewAccessTokenToOauthModel(newToken));
   }
   return null;
 };
 
-const transformNewAccessTokenToOauthModal = (accessToken: Record<string, string>): OAuth2Token => {
+const transformNewAccessTokenToOauthModel = (accessToken: Record<string, string>): OAuth2Token => {
+  const expiry = accessToken.expires_in ? +accessToken.expires_in : 0;
   return {
     // Calculate expiry date
-    expiresAt: accessToken.expires_in ? Date.now() + +accessToken.expires_in * 1000 : null,
+    expiresAt: accessToken.expires_in ? Date.now() + expiry * 1000 : null,
     refreshToken: accessToken.refresh_token || null,
     accessToken: accessToken.access_token || null,
     identityToken: accessToken.id_token || null,
@@ -154,13 +157,77 @@ async function _getExisingAccessTokenAndRefreshIfExpired(
   if (!token.refreshToken) {
     return null;
   }
+  const {
+    accessTokenUrl,
+    credentialsInBody,
+    clientId,
+    clientSecret,
+    scope,
+  } = authentication;
+  const params = [
+    {
+      name: 'grant_type',
+      value: 'refresh_token',
+    },
+    {
+      name: 'refresh_token',
+      value: token.refreshToken,
+    },
+    ...insertAuthKeyIf(scope, 'scope'),
+    ...(credentialsInBody ? [{
+      name: 'client_id',
+      value: clientId,
+    }, {
+      name: 'client_secret',
+      value: clientSecret,
+    }] : [getBasicAuthHeader(clientId, clientSecret)]),
+  ], ;
+  const response = await sendAccessTokenRequest(requestId, authentication.accessTokenUrl, params, authentication.origin);
 
-  const newToken = await refreshAccessToken(
-    requestId,
-    token.refreshToken,
-    authentication
-  );
+  const statusCode = response.statusCode || 0;
+  const bodyBuffer = models.response.getBodyBuffer(response);
 
+  if (statusCode === 401) {
+    // If the refresh token was rejected due an unauthorized request, we will
+    // return a null access_token to trigger an authentication request to fetch
+    // brand new refresh and access tokens.
+    newToken = { access_token: null };
+  } else if (statusCode < 200 || statusCode >= 300) {
+    if (bodyBuffer && statusCode === 400) {
+      const body = tryToParse(bodyBuffer.toString());
+      // If the refresh token was rejected due an oauth2 invalid_grant error, we will
+      // return a null access_token to trigger an authentication request to fetch
+      // brand new refresh and access tokens.
+      if (body?.error === 'invalid_grant') {
+        console.log(`[oauth2] Refresh token rejected due to invalid_grant error: ${body.error_description}`);
+        newToken = { access_token: null };
+      }
+    }
+
+    throw new Error(`[oauth2] Failed to refresh token url=${accessTokenUrl} status=${statusCode}`);
+  } else {
+    invariant(bodyBuffer, `[oauth2] No body returned from ${accessTokenUrl}`);
+    const data = tryToParse(bodyBuffer.toString());
+    const keys =
+      [
+        'access_token',
+        'id_token',
+        'refresh_token',
+        'expires_in',
+        'token_type',
+        'scope',
+        'error',
+        'error_uri',
+        'error_description',
+      ];
+
+    const results = Object.fromEntries(keys.map(key => [key, data?.[key] !== undefined ? data[key] : null]));
+
+    if (results.refresh_token !== undefined) {
+      results.refresh_token = token.refreshToken;
+    }
+    newToken = results;
+  }
   // If we didn't receive an access token it means the refresh token didn't succeed,
   // so we tell caller to fetch brand new access and refresh tokens.
   if (!newToken.access_token) {
@@ -171,18 +238,5 @@ async function _getExisingAccessTokenAndRefreshIfExpired(
   // Update the DB //
   // ~~~~~~~~~~~~~ //
   const old = await models.oAuth2Token.getOrCreateByParentId(requestId);
-  const expiry = newToken.expires_in ? +newToken.expires_in : 0;
-  return models.oAuth2Token.update(old, {
-    // Calculate expiry date
-    expiresAt: expiry ? Date.now() + expiry * 1000 : null,
-    refreshToken: newToken.refresh_token || null,
-    accessToken: newToken.access_token || null,
-    identityToken: newToken.id_token || null,
-    error: newToken.error || null,
-    errorDescription: newToken.error_description || null,
-    errorUri: newToken.error_uri || null,
-    // Special Cases
-    xResponseId: newToken[X_RESPONSE_ID] || null,
-    xError: newToken[X_ERROR] || null,
-  });
+  return models.oAuth2Token.update(old, transformNewAccessTokenToOauthModel(newToken));
 }
