ac7490f70cf6ffed6d487a61387d0299b9662d19
jackkav <jackkav@gmail.com>
2023-01-02 15:30:39 +0100

    use abort controller for cancellation

--- packages/insomnia/src/network/cancellations.ts
+++ packages/insomnia/src/network/cancellations.ts
@@ -7,26 +7,38 @@ export async function cancelRequestById(requestId: string) {
   }
   console.log(`[network] Failed to cancel req=${requestId} because cancel function not found`);
 }
-export const cancellableCurlRequest = (requestOptions: CurlRequestOptions) => {
+export const cancellableCurlRequest = async (requestOptions: CurlRequestOptions) => {
   const requestId = requestOptions.requestId;
-  return new Promise<CurlRequestOutput | { statusMessage: string; error: string }>(async resolve => {
-    try {
-      cancelRequestFunctionMap.set(requestId, async () => {
-        cancelRequestFunctionMap.delete(requestId);
-        window.main.cancelCurlRequest(requestId);
-        return resolve({ statusMessage: 'Cancelled', error: 'Request was cancelled' });
-      });
-
-      // NOTE: conditionally use ipc bridge, renderer cannot import native modules directly
-      const nodejsCurlRequest = process.type === 'renderer'
-        ? window.main.curlRequest
-        : (await import('../main/network/libcurl-promise')).curlRequest;
-      return resolve(nodejsCurlRequest(requestOptions));
-
-    } catch (err) {
-      console.log('[network] Error', err);
-      cancelRequestFunctionMap.delete(requestId);
-      return resolve({ statusMessage: 'Error', error: err.message || 'Something went wrong' });
+  const controller = new AbortController();
+  const cancelRequest = () => {
+    window.main.cancelCurlRequest(requestId);
+    controller.abort();
+  };
+  cancelRequestFunctionMap.set(requestId, cancelRequest);
+  try {
+    const result = await cancellablePromise({ signal: controller.signal, fn: window.main.curlRequest(requestOptions) });
+    return result as CurlRequestOutput;
+  } catch (err) {
+    cancelRequestFunctionMap.delete(requestId);
+    if (err.name === 'AbortError') {
+      return { statusMessage: 'Cancelled', error: 'Request was cancelled' };
     }
+    console.log('[network] Error', err);
+    return { statusMessage: 'Error', error: err.message || 'Something went wrong' };
+  }
+};
+const cancellablePromise = ({ signal, fn }: { signal: AbortSignal; fn: Promise<any> }) => {
+  if (signal?.aborted) {
+    return Promise.reject(new DOMException('Aborted', 'AbortError'));
+  }
+  return new Promise((resolve, reject) => {
+    const abortHandler = () => {
+      reject(new DOMException('Aborted', 'AbortError'));
+    };
+    fn.then(res => {
+      resolve(res);
+      signal?.removeEventListener('abort', abortHandler);
+    }, reject);
+    signal?.addEventListener('abort', abortHandler);
   });
 };
