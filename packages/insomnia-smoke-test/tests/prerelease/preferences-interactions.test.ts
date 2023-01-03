535a70c379b09a89f302352ab3c847a51e2bf43c
kreosus <kristofer.huffman@konghq.com>
2023-01-03 16:17:18 -0600

    fixed selectors

--- packages/insomnia-smoke-test/tests/prerelease/preferences-interactions.test.ts
+++ packages/insomnia-smoke-test/tests/prerelease/preferences-interactions.test.ts
@@ -5,7 +5,7 @@ import { test } from '../../playwright/test';
 test('Preferences through click', async ({ page }) => {
 
   await page.locator('[data-testid="settings-button"]').click();
-  await page.locator('text=Insomnia Preferences – v2022.7.0-alpha.0GeneralDataThemesKeyboardAccountPluginsU >> button').first().click();
+  await page.locator('text=Insomnia Preferences ').first().click();
 
   expect(true).toBeTruthy();
 });
@@ -13,8 +13,7 @@ test('Preferences through click', async ({ page }) => {
 test('Preferences through keyboard shortcut', async ({ page }) => {
 
   await page.locator('text=Preferences (Ctrl + ,)');
-  await page.locator('[data-testid="settings-button"]');
-  await page.locator('text=Insomnia Preferences – v2022.7.0-alpha.0GeneralDataThemesKeyboardAccountPluginsU >> button').first();
-
+  await page.locator('[data-testid="settings-button"]').click();
+  await page.locator('text=Insomnia Preferences').first().click();
   // TODO: more preferences scenarios will be added in the followup iterations of increasing app test coverage
 });
