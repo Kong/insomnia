eae2db943afc4ceebe82c4391df84f978aee0d8d
kreosus <kristofer.huffman@konghq.com>
2023-01-04 07:24:58 -0600

    fixed test selectors

---
+++ packages/insomnia-smoke-test/tests/prerelease/debug-head.test.ts
@@ -0,0 +1,6 @@
+import { expect } from '@playwright/test';
+
+import { test } from '../../playwright/test';
+
+test('Basic interactions', async ({ }) => {
+  // TODO: add a few scenarios ported from the release checklist
