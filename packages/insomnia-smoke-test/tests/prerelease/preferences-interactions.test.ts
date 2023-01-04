638d7e59e248d686aeb31b8fc06b963f27a18aff
Filipe Freire <livrofubia@gmail.com>
2023-01-04 16:57:55 +0000

    Improve a bit the preferences tests

--- packages/insomnia-smoke-test/tests/prerelease/preferences-interactions.test.ts
+++ packages/insomnia-smoke-test/tests/prerelease/preferences-interactions.test.ts
@@ -1,19 +1,15 @@
-import { expect } from '@playwright/test';
-
 import { test } from '../../playwright/test';
 
 test('Preferences through click', async ({ page }) => {
-
   await page.locator('[data-testid="settings-button"]').click();
   await page.locator('text=Insomnia Preferences').first().click();
-
-  expect(true).toBeTruthy();
 });
 
 test('Preferences through keyboard shortcut', async ({ page }) => {
-
-  await page.locator('text=Preferences (Ctrl + ,)');
-  await page.locator('[data-testid="settings-button"]').click();
+  if (process.platform === 'darwin') {
+    await page.locator('.app').press('Meta+,');
+  } else {
+    await page.locator('.app').press('Control+,');
+  }
   await page.locator('text=Insomnia Preferences').first().click();
-  // TODO: more preferences scenarios will be added in the followup iterations of increasing app test coverage
 });
