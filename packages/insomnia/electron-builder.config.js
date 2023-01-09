db44ea29f9e2709fdfc156d066763e8dc4a1a148
Filipe Freire <livrofubia@gmail.com>
2023-01-09 11:41:55 +0000

    Try to fix app-builder-lib

--- packages/insomnia/electron-builder.config.js
+++ packages/insomnia/electron-builder.config.js
@@ -26,7 +26,7 @@ const config = {
   ],
   publish: null,
   afterSign: './scripts/afterSignHook.js',
-  afterPack: './scripts/afterPack.js',
+  // afterPack: './scripts/afterPack.js',
   extraResources: [
     {
       from: './bin',
@@ -45,7 +45,7 @@ const config = {
   fileAssociations: [],
   mac: {
     // disabling notarize on electron-builder, we use afterSignHook.js
-    notarize: false,
+    // notarize: false,
     hardenedRuntime: true,
     category: 'public.app-category.developer-tools',
     entitlements: './build/static/entitlements.mac.inherit.plist',
