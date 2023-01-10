73b33982e6d4038a4ced47957f4594d869facc5d
Pavlos Koutoglou <pkoutoglou@gmail.com>
2023-01-10 22:58:09 +0200

    Add title

--- packages/insomnia/src/ui/components/cookie-list.tsx
+++ packages/insomnia/src/ui/components/cookie-list.tsx
@@ -101,7 +101,7 @@ export const CookieList: FC<CookieListProps> = ({
           >
             <Dropdown
               triggerButton={
-                <DropdownButton className="btn btn--super-duper-compact btn--outlined txt-md">
+                <DropdownButton title="Add cookie" className="btn btn--super-duper-compact btn--outlined txt-md">
                   Actions <i className="fa fa-caret-down" />
                 </DropdownButton>
               }
