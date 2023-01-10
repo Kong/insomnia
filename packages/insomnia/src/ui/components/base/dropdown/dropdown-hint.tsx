753a7a53821a0afd91df62daf0df86911a81e453
Pavlos Koutoglou <pkoutoglou@gmail.com>
2023-01-10 23:43:33 +0200

    Remove insomnia-common imports

--- packages/insomnia/src/ui/components/base/dropdown/dropdown-hint.tsx
+++ packages/insomnia/src/ui/components/base/dropdown/dropdown-hint.tsx
@@ -1,7 +1,7 @@
-import { PlatformKeyCombinations } from 'insomnia-common';
 import React from 'react';
 import styled from 'styled-components';
 
+import { PlatformKeyCombinations } from '../../../../common/settings';
 import { Hotkey } from '../../hotkey';
 
 const StyledHotkey = styled(Hotkey)({
