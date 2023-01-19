bda59c62536550d54ef0cdc1438b7e11b6a2f707
Kristofer Huffman <kristofer.huffman@konghq.com>
2023-01-19 09:28:32 -0600

    Git-Sync-Test

--- packages/insomnia-smoke-test/tests/prerelease/git-sync-interactions.test.ts
+++ packages/insomnia-smoke-test/tests/prerelease/git-sync-interactions.test.ts
@@ -19,28 +19,19 @@ test('Clone Repo with bad values', async ({ page }) => {
   await page.locator('[data-testid="CodeEditor"] >> text=description').first().click();
   await page.locator('text=openapi').click();
   await page.locator('#root button:has-text("main")').click();
-  await page.locator('button:has-text("History (44)")').click();
-  await page.locator('text=Git History (44)').click();
+  await page.locator('button:has-text("History")').click();
+  await page.locator('text=Git History').click();
   await page.locator('text=Done').click();
-
+  await page.getByRole('button', { name: 'main ' }).click();
+  await page.getByRole('button', { name: ' Branches' }).click();
+  await page.getByPlaceholder('testing-branch').fill('test123');
+  await page.getByRole('button', { name: '+ Create' }).click();
+  await page.getByRole('button', { name: 'Done' }).click();
+  await page.getByRole('button', { name: 'test123 ' }).click();
+  await page.getByRole('button', { name: ' Push' }).click();
   await expect(true).toBeTruthy();
 });
 
-test('Clone new repo', async ({ page }) => {
-  await page.locator('.fa').first().click();
-  await page.locator('text=Create').click();
-  await page.locator('button:has-text("Git Clone")').click();
-  await page.locator('#react-aria7161960595-6-tab-custom').click();
-  await page.locator('[placeholder="https\\:\\/\\/github\\.com\\/org\\/repo\\.git"]').fill('https://github.com/Kong/dino-park-insomnia.git');
-  await page.locator('[placeholder="Name"]').click();
-  await page.locator('[placeholder="Name"]').fill('filipe freire');
-  await page.locator('[placeholder="Email"]').click();
-  await page.locator('[placeholder="Email"]').fill('filipe.freire@konghq.com');
-  await page.locator('[placeholder="MyUser"]').click();
-  await page.locator('[placeholder="MyUser"]').fill('kreosus');
-  await page.locator('[placeholder="\\38 8e7ee63b254e4b0bf047559eafe86ba9dd49507"]').click();
-  await page.locator('[data-testid="git-repository-settings-modal__sync-btn"]').click();
-
-});
+// TODO(kreosus): more preferences will be added for secrets to be used
 
 // TODO(kreosus): more preferences scenarios will be added in the followup iterations of increasing app test coverage
