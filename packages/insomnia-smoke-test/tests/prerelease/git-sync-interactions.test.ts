3a5eb3866c431ddc7363c15e8845cb8ee479a9e0
Filipe Freire <livrofubia@gmail.com>
2023-01-20 14:06:57 +0000

    improve a bit the locators

--- packages/insomnia-smoke-test/tests/prerelease/git-sync-interactions.test.ts
+++ packages/insomnia-smoke-test/tests/prerelease/git-sync-interactions.test.ts
@@ -4,34 +4,53 @@ import { test } from '../../playwright/test';
 
 test('Clone Repo with bad values', async ({ page }) => {
   await page.click('[data-testid="project"] >> text=Insomnia');
-  await page.locator('text=Create').click();
+  await page.getByRole('button', { name: 'Create' }).click();
   await page.getByRole('button', { name: 'Git Clone' }).click();
   await page.getByRole('tab', { name: 'Git' }).nth(2).click();
-  await page.getByPlaceholder('https://github.com/org/repo.git').fill('https://github.com/kong/dino-park-insomnia');
-  await page.locator('[placeholder="Name"]').fill('test');
-  await page.locator('[placeholder="Email"]').click();
-  await page.locator('[placeholder="Email"]').fill('test');
-  await page.locator('[placeholder="MyUser"]').click();
-  await page.locator('[placeholder="MyUser"]').fill('test');
-  await page.locator('[placeholder="\\38 8e7ee63b254e4b0bf047559eafe86ba9dd49507"]').click();
-  await page.locator('[placeholder="\\38 8e7ee63b254e4b0bf047559eafe86ba9dd49507"]').fill('test');
-  await page.locator('[placeholder="\\38 8e7ee63b254e4b0bf047559eafe86ba9dd49507"]').press('Enter');
-  await page.locator('[data-testid="CodeEditor"] >> text=description').first().click();
-  await page.locator('text=openapi').click();
-  await page.locator('#root button:has-text("main")').click();
-  await page.locator('button:has-text("History")').click();
+
+  // Fill in Git Sync details and clone repository
+  await page.getByText('Git URI (https)').fill('https://github.com/kong/dino-park-insomnia');
+  await page.getByText('Author Name').fill('test');
+  await page.getByText('Author Email').fill('test');
+  await page.getByText('Username').fill('test');
+  await page.getByText('Authentication Token').fill('test');
+  await page.getByRole('button', { name: 'Clone' }).click();
+
+  // Basic check repository data is loaded
+  // Design doc
+  await page.getByText('"Our Dino Park API"').click();
+  await page.getByRole('link', { name: 'Debug' }).click();
+  // Requests
+  await page.getByRole('button', { name: 'POST Turn on/off the electricity of the fences' }).click();
+  // Environments
+  await page.getByRole('button', { name: 'No Environment' }).click();
+  await page.getByRole('button', { name: 'Use Via Kong GW' }).click();
+  // Tests
+  await page.getByRole('link', { name: 'Test' }).click();
+  await page.getByRole('heading', { name: 'Check status' }).click();
+
+  // Check branch history
+  await page.getByRole('button', { name: 'main' }).click();
+  await page.getByRole('button', { name: 'History' }).click();
   await page.locator('text=Git History').click();
+
+  // Check a recent and old commit show up on history
+  await page.getByRole('cell', { name: 'bump to OpenAPI v3.1' }).click();
+  await page.getByRole('cell', { name: 'initial commit' }).click();
   await page.locator('text=Done').click();
-  await page.getByRole('button', { name: 'main ' }).click();
-  await page.getByRole('button', { name: ' Branches' }).click();
+
+  // Create a branch and try to push with bad Git token
+  await page.getByRole('button', { name: 'main' }).click();
+  await page.getByRole('button', { name: 'Branches' }).click();
   await page.getByPlaceholder('testing-branch').fill('test123');
   await page.getByRole('button', { name: '+ Create' }).click();
+  await page.getByRole('cell', { name: 'test123(current)' }).click();
   await page.getByRole('button', { name: 'Done' }).click();
-  await page.getByRole('button', { name: 'test123 ' }).click();
-  await page.getByRole('button', { name: ' Push' }).click();
-  await expect(true).toBeTruthy();
+  await page.getByRole('button', { name: 'test123' }).click();
+  await page.getByRole('button', { name: 'Push' }).click();
+  await expect(page.locator('.app')).toContainText('Error Pushing Repository');
 });
 
-// TODO(kreosus): more preferences will be added for secrets to be used
+// TODO(kreosus): more git sync prerelease tests will be added when gh auth secrets are properly setup for CI use
 
 // TODO(kreosus): more preferences scenarios will be added in the followup iterations of increasing app test coverage
