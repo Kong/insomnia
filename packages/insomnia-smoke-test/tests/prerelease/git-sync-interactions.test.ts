ab5e86d1fb6e5dc767ffcabb78f76f901b2710b9
Filipe Freire <livrofubia@gmail.com>
2023-02-09 15:07:47 +0000

    Add test reproducing issue #5740

--- packages/insomnia-smoke-test/tests/prerelease/git-sync-interactions.test.ts
+++ packages/insomnia-smoke-test/tests/prerelease/git-sync-interactions.test.ts
@@ -51,6 +51,36 @@ test('Clone Repo with bad values', async ({ page }) => {
   await expect(page.locator('.app')).toContainText('Error Pushing Repository');
 });
 
+test('Clone an empty repository', async ({ page }) => {
+  // Clone an empty repository
+  await page.click('[data-testid="project"] >> text=Insomnia');
+  await page.getByRole('button', { name: 'Create' }).click();
+  await page.getByRole('menuitem', { name: 'Git Clone' }).click();
+  await page.getByRole('tab', { name: 'Git' }).nth(2).click();
+  // TODO(Filipe) - replace with kong/empty-repo
+  await page.getByPlaceholder('https://github.com/org/repo.git').fill('https://github.com/filfreire/empty-repo');
+  await page.getByText('Author Name').fill('test');
+  await page.getByText('Author Email').fill('test');
+  await page.getByText('Username').fill('test');
+  await page.getByText('Authentication Token').fill('test');
+  await page.getByRole('button', { name: 'Clone' }).click();
+
+  // Get no document found dialogue
+  await page.getByText('No document found', { exact: true }).click();
+  await page.getByText('No document found in the repository for import. Would you like to create a new one?').click();
+
+  // Create a new Design Doc
+  await page.getByRole('button', { name: 'Yes' }).click();
+  await page.getByText('Create New Design Document').click();
+  await page.getByRole('button', { name: 'Create', exact: true }).click();
+
+  // Check branch history
+  await page.getByRole('button', { name: 'main' }).click();
+  await page.getByRole('menuitem', { name: 'History' }).click();
+  await page.locator('text=Git History').click();
+  // TODO(Filipe) add check for empty git history
+});
+
 // TODO(kreosus): more git sync prerelease tests will be added when gh auth secrets are properly setup for CI use
 
 // TODO(kreosus): more preferences scenarios will be added in the followup iterations of increasing app test coverage
