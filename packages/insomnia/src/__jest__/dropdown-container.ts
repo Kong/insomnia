ad4d2119483824af6baa9673b62b5ba454cdbb35
Dave Nicolson <david.nicolson@gmail.com>
2023-03-19 16:41:19 +0100

    Remove unused code

--- packages/insomnia/src/__jest__/dropdown-container.ts
+++ packages/insomnia/src/__jest__/dropdown-container.ts
@@ -1,8 +1,5 @@
-import { dropdownsContainerId } from '../ui/components/base/dropdown/dropdown';
-
 export const getDropdownContainer = () => {
   const container = document.createElement('div');
-  container.setAttribute('id', dropdownsContainerId);
   document.body.appendChild(container);
 
   return container;
