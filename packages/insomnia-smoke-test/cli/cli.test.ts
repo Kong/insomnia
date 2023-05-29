ecc3ffb1b38723f34cdcddb2bba6bc8b1c3b304f
jackkav <jackkav@gmail.com>
2023-05-29 23:42:24 +0200

    update execa api

--- packages/insomnia-smoke-test/cli/cli.test.ts
+++ packages/insomnia-smoke-test/cli/cli.test.ts
@@ -5,12 +5,9 @@ import path from 'path';
 const binPath = path.resolve('../insomnia-inso/binaries/inso');
 
 describe('inso basic features', () => {
-  const inso = (...args: string[]) => execa.sync(binPath, args);
-  // const res = await execa(binPath, ['-h']);
-  console.log(binPath);
   describe('run test', () => {
-    it('should not fail running tests', () => {
-      const { failed } = inso(
+    it('should not fail running tests', async () => {
+      const { failed } = await execa(binPath, [
         'run',
         'test',
         '--src',
@@ -19,49 +16,49 @@ describe('inso basic features', () => {
         'Dev',
         'Echo Test Suite',
         '--verbose',
-      );
+      ]);
 
       expect(failed).toBe(false);
     });
   });
 
   describe('generate config', () => {
-    it('should not fail generating config', () => {
-      const { failed } = inso(
+    it('should not fail generating config', async () => {
+      const { failed } = await execa(binPath, [
         'generate',
         'config',
         '--src',
         'fixtures/inso-nedb',
         'Smoke Test API server 1.0.0',
-      );
+      ]);
 
       expect(failed).toBe(false);
     });
   });
 
   describe('lint spec', () => {
-    it('should not fail linting spec', () => {
-      const { failed } = inso(
+    it('should not fail linting spec', async () => {
+      const { failed } = await execa(binPath, [
         'lint',
         'spec',
         '--src',
         'fixtures/inso-nedb',
         'Smoke Test API server 1.0.0',
-      );
+      ]);
 
       expect(failed).toBe(false);
     });
   });
 
   describe('export spec', () => {
-    it('should not fail linting spec', () => {
-      const { failed } = inso(
+    it('should not fail linting spec', async () => {
+      const { failed } = await execa(binPath, [
         'export',
         'spec',
         '--src',
         'fixtures/inso-nedb',
         'Smoke Test API server 1.0.0',
-      );
+      ]);
 
       expect(failed).toBe(false);
     });
