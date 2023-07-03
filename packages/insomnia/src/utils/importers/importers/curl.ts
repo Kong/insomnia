7221518bb38e78326e8a25201967a58fd2df9d29
Jack Kavanagh <jackkav@gmail.com>
2023-07-03 10:46:55 +0200

    fix: curl transform - allow header without value (#6069)
    * allow header without value
    
    * tidy
    
    * fix test

--- packages/insomnia/src/utils/importers/importers/curl.ts
+++ packages/insomnia/src/utils/importers/importers/curl.ts
@@ -123,6 +123,13 @@ const importCommand = (parseEntries: ParseEntry[]): ImportRequest => {
     ...((pairsByName.H as string[] | undefined) || []),
   ].map(header => {
     const [name, value] = header.split(/:(.*)$/);
+    // remove final colon from header name if present
+    if (!value) {
+      return {
+        name: name.trim().replace(/;$/, ''),
+        value: '',
+      };
+    }
     return {
       name: name.trim(),
       value: value.trim(),
