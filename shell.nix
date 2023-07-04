c0e25d0490726c2abcbeb32fe520bfaa5b54373c
Jack Kavanagh <jackkav@gmail.com>
2023-07-04 22:55:47 +0200

    Bump/electron-25 (#6081)
    * bump
    
    * lockfiles
    
    * upgrade doc

--- shell.nix
+++ shell.nix
@@ -3,9 +3,9 @@ with import <nixpkgs> { };
 mkShell {
   nativeBuildInputs = [
     nodejs-18_x
-    electron_23
+    electron_25
     stdenv.cc.cc.lib
   ];
   LD_LIBRARY_PATH = "${stdenv.cc.cc.lib}/lib64:$LD_LIBRARY_PATH";
-  ELECTRON_OVERRIDE_DIST_PATH = "${electron_23}/bin/";
+  ELECTRON_OVERRIDE_DIST_PATH = "${electron_25}/bin/";
 }
