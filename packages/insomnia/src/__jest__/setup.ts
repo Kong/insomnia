a90f5c6fba4db5e03832829df292ebc3aa7f9c56
jackkav <jackkav@gmail.com>
2023-07-07 00:30:19 +0200

    lock files and setup

--- packages/insomnia/src/__jest__/setup.ts
+++ packages/insomnia/src/__jest__/setup.ts
@@ -1,17 +1,50 @@
-// global.requestAnimationFrame = (callback: FrameRequestCallback) => {
-//   process.nextTick(callback);
-//   // note: the spec indicates that the return of this function (the request id) is a non-zero number.  hopefully returning 0 here will indicate that this is a mock if the return is ever to be used accidentally.
-//   return 0;
-// };
+import type { Config } from 'jest';
+const localStorageMock: Storage = (function () {
+  let store: Record<string, string> = {};
+  return {
+    get length() {
+      return Object.keys(store).length;
+    },
 
-// global.require = require;
+    clear() {
+      store = {};
+    },
 
-// // Don't console log real logs that start with a tag (eg. [db] ...). It's annoying
-// const log = console.log;
+    getItem(key: string) {
+      return store[key];
+    },
 
-// global.console.log = (...args) => {
-//   if (!(typeof args[0] === 'string' && args[0][0] === '[')) {
-//     log(...args);
-//   }
-// };
-export { };
+    key() {
+      return null;
+    },
+
+    removeItem(key: string) {
+      delete store[key];
+    },
+
+    setItem(key: string, value: string) {
+      store[key] = value.toString();
+    },
+  };
+})();
+const log = console.log;
+const config: Config = {
+  globals: {
+    __DEV__: false,
+    localStorage: localStorageMock,
+    requestAnimationFrame: (callback: FrameRequestCallback) => {
+      process.nextTick(callback);
+      // note: the spec indicates that the return of this function (the request id) is a non-zero number.  hopefully returning 0 here will indicate that this is a mock if the return is ever to be used accidentally.
+      return 0;
+    },
+    global: { require },
+    require,
+    'console.log ': (...args: any[]) => {
+      if (!(typeof args[0] === 'string' && args[0][0] === '[')) {
+        log(...args);
+      }
+    },
+  },
+};
+
+export default config;
