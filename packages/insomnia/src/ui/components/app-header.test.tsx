0d321c5664a5a29b8e54aff53088717a42ee3c82
Filipe Freire <livrofubia@gmail.com>
2023-07-11 16:01:11 +0100

    fix lint

--- packages/insomnia/src/ui/components/app-header.test.tsx
+++ packages/insomnia/src/ui/components/app-header.test.tsx
@@ -1,15 +1,8 @@
-import { describe, expect, test } from '@jest/globals';
-import { render, screen } from '@testing-library/react';
-import React from 'react';
-import { Provider } from 'react-redux';
 import configureMockStore from 'redux-mock-store';
 import { MockStoreEnhanced } from 'redux-mock-store';
 import thunk from 'redux-thunk';
 
-import { globalBeforeEach } from '../../__jest__/before-each';
-import { getDropdownContainer } from '../../__jest__/dropdown-container';
 import * as models from '../../models';
-import { AppHeader } from './app-header';
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
