97138fcdddb41731061c520632bf4746f2d65829
Sebastian Jonasson <sebastianjonasson@gmail.com>
2023-07-14 11:15:02 +0200

    gRPC: Support JSONPath in response (#5996)

--- packages/insomnia/src/ui/components/editors/grpc-editor.tsx
+++ packages/insomnia/src/ui/components/editors/grpc-editor.tsx
@@ -1,5 +1,10 @@
 import React, { FunctionComponent } from 'react';
+import { useSelector } from 'react-redux';
 
+import * as models from '../../../models';
+import type { Response } from '../../../models/response';
+import { updateRequestMetaByParentId } from '../../hooks/create-request';
+import { selectActiveResponse } from '../../redux/selectors';
 import { CodeEditor } from '../codemirror/code-editor';
 
 interface Props {
@@ -12,8 +17,27 @@ export const GRPCEditor: FunctionComponent<Props> = ({
   content,
   handleChange,
   readOnly,
-}) => (
-  <CodeEditor
+}) => {
+  const response = useSelector(selectActiveResponse) as Response | null;
+  const handleSetFilter = async (responseFilter: string) => {
+    if (!response) {
+      return;
+    }
+    const requestId = response.parentId;
+    await updateRequestMetaByParentId(requestId, { responseFilter });
+    const meta = await models.requestMeta.getByParentId(requestId);
+    if (!meta) {
+      return;
+    }
+    const responseFilterHistory = meta.responseFilterHistory.slice(0, 10);
+    // Already in history or empty?
+    if (!responseFilter || responseFilterHistory.includes(responseFilter)) {
+      return;
+    }
+    responseFilterHistory.unshift(responseFilter);
+    updateRequestMetaByParentId(requestId, { responseFilterHistory });
+  };
+  return (<CodeEditor
     defaultValue={content}
     onChange={handleChange}
     mode="application/json"
@@ -21,5 +45,6 @@ export const GRPCEditor: FunctionComponent<Props> = ({
     readOnly={readOnly}
     autoPrettify={readOnly}
     showPrettifyButton={!readOnly}
-  />
-);
+    updateFilter={handleSetFilter}
+  />);
+};
