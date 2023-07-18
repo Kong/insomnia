2f570a5544b70bfcdaa11a05ce3ba3fdd419912b
Peter C <petoc@users.noreply.github.com>
2023-07-18 11:52:39 +0200

    import all available header properties from Postman

--- packages/insomnia/src/utils/importers/importers/postman.ts
+++ packages/insomnia/src/utils/importers/importers/postman.ts
@@ -136,9 +136,11 @@ export class ImportPostman {
       url: this.importUrl(request.url),
       parameters: parameters,
       method: request.method || 'GET',
-      headers: headers.map(({ key, value }) => ({
+      headers: headers.map(({ key, value, disabled, description }) => ({
         name: key,
         value,
+        ...(disabled ? { disabled } : {}),
+        ...(typeof description !== 'undefined' ? { description } : {}),
       })),
       body: this.importBody(request.body, headers.find(({ key }) => key === 'Content-Type')?.value),
       authentication,
