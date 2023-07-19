9b72c9ac8fa5d12a839afbeacd4fabb6ed3ee378
gatzjames <jamesgatzos@gmail.com>
2023-07-19 15:48:43 +0200

    tailwind

---
+++ packages/insomnia/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
