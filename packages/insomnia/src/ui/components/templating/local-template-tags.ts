6e6e572c7ee223804683f83cf8a01d6fd8020b4c
jackkav <jackkav@gmail.com>
2023-07-24 17:25:49 +0200

    eat json path

--- packages/insomnia/src/ui/components/templating/local-template-tags.ts
+++ packages/insomnia/src/ui/components/templating/local-template-tags.ts
@@ -250,4 +250,43 @@ export const localTemplateTags: TemplateTag[] = [
       },
     },
   },
+  {
+    templateTag: {
+      displayName: 'JSONPath',
+      name: 'jsonpath',
+      description: 'pull data from JSON strings with JSONPath',
+      args: [
+        {
+          displayName: 'JSON string',
+          type: 'string',
+        },
+        {
+          displayName: 'JSONPath Filter',
+          encoding: 'base64', // So it doesn't cause syntax errors
+          type: 'string',
+        },
+      ],
+      run(context, jsonString, filter) {
+        let body;
+        try {
+          body = JSON.parse(jsonString);
+        } catch (err) {
+          throw new Error(`Invalid JSON: ${err.message}`);
+        }
+
+        let results;
+        try {
+          results = JSONPath({ json: body, path: filter });
+        } catch (err) {
+          throw new Error(`Invalid JSONPath query: ${filter}`);
+        }
+
+        if (results.length === 0) {
+          throw new Error(`JSONPath query returned no results: ${filter}`);
+        }
+
+        return results[0];
+      },
+    },
+  },
 ];
