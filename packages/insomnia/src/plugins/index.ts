74de78fdc91f0824e58f95c1ccde0b6230528e88
jackkav <jackkav@gmail.com>
2023-07-24 22:40:53 +0200

    eat core themes

--- packages/insomnia/src/plugins/index.ts
+++ packages/insomnia/src/plugins/index.ts
@@ -394,7 +394,26 @@ export async function getResponseHooks(): Promise<ResponseHook[]> {
 }
 
 export async function getThemes(): Promise<Theme[]> {
-  let extensions: Theme[] = [];
+  let extensions: Theme[] = [
+    require('./themes/default'),
+    require('./themes/legacy'),
+    require('./themes/studio-light'),
+    require('./themes/studio-dark'),
+    require('./themes/material'),
+    require('./themes/simple-light'),
+    require('./themes/simple-dark'),
+    require('./themes/one-light'),
+    require('./themes/one-dark'),
+    require('./themes/purple'),
+    require('./themes/gruvbox'),
+    require('./themes/hyper'),
+    require('./themes/railscasts'),
+    require('./themes/solarized-light'),
+    require('./themes/solarized'),
+    require('./themes/solarized-dark'),
+    require('./themes/high-contrast-light'),
+    require('./themes/colorblind-dark'),
+  ];
 
   for (const plugin of await getActivePlugins()) {
     const themes = plugin.module.themes || [];
