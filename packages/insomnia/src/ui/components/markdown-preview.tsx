7f731c2f09665bdf70d8c7d1c3fc2392570bc125
jackkav <jackkav@gmail.com>
2023-07-28 16:23:16 +0200

    put the markdown compiler back

--- packages/insomnia/src/ui/components/markdown-preview.tsx
+++ packages/insomnia/src/ui/components/markdown-preview.tsx
@@ -1,7 +1,8 @@
 import classnames from 'classnames';
 import highlight from 'highlight.js/lib/common';
-import React, { FC, useLayoutEffect, useRef } from 'react';
+import React, { FC, useEffect, useLayoutEffect, useRef, useState } from 'react';
 
+import { markdownToHTML } from '../../common/markdown-to-html';
 import { HandleRender } from '../../common/render';
 
 interface Props {
@@ -13,6 +14,26 @@ interface Props {
 
 export const MarkdownPreview: FC<Props> = ({ markdown, className, heading }) => {
   const divRef = useRef<HTMLDivElement>(null);
+  const [compiled, setCompiled] = useState('');
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    let shouldUpdate = true;
+    const fn = async () => {
+      try {
+        const compiled = markdownToHTML(markdown);
+        shouldUpdate && setCompiled(compiled);
+        shouldUpdate && setError('');
+      } catch (err) {
+        shouldUpdate && setCompiled('');
+        shouldUpdate && setError(err.message);
+      }
+    };
+    fn();
+    return () => {
+      shouldUpdate = false;
+    };
+  }, [markdown]);
   useLayoutEffect(() => {
     if (!divRef.current) {
       return;
@@ -27,7 +48,7 @@ export const MarkdownPreview: FC<Props> = ({ markdown, className, heading }) => 
       a.removeEventListener('click', _handleClickLink);
       a.addEventListener('click', _handleClickLink);
     }
-  }, []);
+  }, [compiled]);
   const _handleClickLink = (event: any) => {
     event.preventDefault();
     window.main.openInBrowser(event.target.getAttribute('href'));
@@ -35,9 +56,10 @@ export const MarkdownPreview: FC<Props> = ({ markdown, className, heading }) => 
 
   return (
     <div ref={divRef} className={classnames('markdown-preview', className)}>
+      {error ? <p className="notice error no-margin">Failed to render: {error}</p> : null}
       <div className="selectable">
         {heading ? <h1 className="markdown-preview__content-title">{heading}</h1> : null}
-        <div className="markdown-preview__content" dangerouslySetInnerHTML={{ __html: markdown }} />
+        <div className="markdown-preview__content" dangerouslySetInnerHTML={{ __html: compiled }} />
       </div>
     </div>
   );
