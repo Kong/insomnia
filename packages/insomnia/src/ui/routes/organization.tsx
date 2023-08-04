c17190b26c1f0062134ee395db3f517317170db1
gatzjames <jamesgatzos@gmail.com>
2023-08-04 20:43:34 +0300

    move modals to root

--- packages/insomnia/src/ui/routes/organization.tsx
+++ packages/insomnia/src/ui/routes/organization.tsx
@@ -1,4 +1,3 @@
-import { IpcRendererEvent } from 'electron';
 import React, { Fragment, useEffect, useState } from 'react';
 import {
   Breadcrumbs,
@@ -28,15 +27,10 @@ import {
   getLastName,
   isLoggedIn,
 } from '../../account/session';
-import { getAppWebsiteBaseURL, isDevelopment } from '../../common/constants';
+import { getAppWebsiteBaseURL } from '../../common/constants';
 import * as models from '../../models';
 import { Settings } from '../../models/settings';
 import { isDesign, isScratchpad } from '../../models/workspace';
-import { reloadPlugins } from '../../plugins';
-import { createPlugin } from '../../plugins/create';
-import { setTheme } from '../../plugins/misc';
-import { exchangeCodeForToken } from '../../sync/git/github-oauth-provider';
-import { exchangeCodeForGitLabToken } from '../../sync/git/gitlab-oauth-provider';
 import FileSystemDriver from '../../sync/store/drivers/file-system-driver';
 import { MergeConflict } from '../../sync/types';
 import { getVCS, initVCS } from '../../sync/vcs/vcs';
@@ -46,15 +40,9 @@ import { GitHubStarsButton } from '../components/github-stars-button';
 import { Hotkey } from '../components/hotkey';
 import { Icon } from '../components/icon';
 import { InsomniaAILogo } from '../components/insomnia-icon';
-import { showError, showModal } from '../components/modals';
-import { AlertModal } from '../components/modals/alert-modal';
-import { AskModal } from '../components/modals/ask-modal';
-import { ImportModal } from '../components/modals/import-modal';
+import { showModal } from '../components/modals';
 import {
-  SettingsModal,
   showSettingsModal,
-  TAB_INDEX_PLUGINS,
-  TAB_INDEX_THEMES,
 } from '../components/modals/settings-modal';
 import { SyncMergeModal } from '../components/modals/sync-merge-modal';
 import { PresentUsers } from '../components/present-users';
@@ -63,8 +51,6 @@ import { AppHooks } from '../containers/app-hooks';
 import { AIProvider } from '../context/app/ai-context';
 import { PresenceProvider } from '../context/app/presence-context';
 import { NunjucksEnabledProvider } from '../context/nunjucks/nunjucks-enabled-context';
-import { useSettingsPatcher } from '../hooks/use-request';
-import Modals from './modals';
 import { WorkspaceLoaderData } from './workspace';
 
 const getNameInitials = (name: string) => {
@@ -239,162 +225,7 @@ const OrganizationRoute = () => {
   const workspaceData = useRouteLoaderData(
     ':workspaceId',
   ) as WorkspaceLoaderData | null;
-  const [importUri, setImportUri] = useState('');
-  const patchSettings = useSettingsPatcher();
-  const actionFetcher = useFetcher();
   const logoutFetcher = useFetcher();
-
-  useEffect(() => {
-    return window.main.on(
-      'shell:open',
-      async (_: IpcRendererEvent, url: string) => {
-        // Get the url without params
-        let parsedUrl;
-        try {
-          parsedUrl = new URL(url);
-        } catch (err) {
-          console.log('Invalid args, expected insomnia://x/y/z', url);
-          return;
-        }
-        let urlWithoutParams = url.substring(0, url.indexOf('?')) || url;
-        const params = Object.fromEntries(parsedUrl.searchParams);
-        // Change protocol for dev redirects to match switch case
-        if (isDevelopment()) {
-          urlWithoutParams = urlWithoutParams.replace(
-            'insomniadev://',
-            'insomnia://',
-          );
-        }
-        switch (urlWithoutParams) {
-          case 'insomnia://app/alert':
-            showModal(AlertModal, {
-              title: params.title,
-              message: params.message,
-            });
-            break;
-
-          case 'insomnia://app/auth/login':
-            actionFetcher.submit(
-              {},
-              {
-                action: '/auth/logout',
-                method: 'POST',
-              },
-            );
-            break;
-
-          case 'insomnia://app/import':
-            setImportUri(params.uri);
-            break;
-
-          case 'insomnia://plugins/install':
-            showModal(AskModal, {
-              title: 'Plugin Install',
-              message: (
-                <>
-                  Do you want to install <code>{params.name}</code>?
-                </>
-              ),
-              yesText: 'Install',
-              noText: 'Cancel',
-              onDone: async (isYes: boolean) => {
-                if (isYes) {
-                  try {
-                    await window.main.installPlugin(params.name);
-                    showModal(SettingsModal, { tab: TAB_INDEX_PLUGINS });
-                  } catch (err) {
-                    showError({
-                      title: 'Plugin Install',
-                      message: 'Failed to install plugin',
-                      error: err.message,
-                    });
-                  }
-                }
-              },
-            });
-            break;
-
-          case 'insomnia://plugins/theme':
-            const parsedTheme = JSON.parse(decodeURIComponent(params.theme));
-            showModal(AskModal, {
-              title: 'Install Theme',
-              message: (
-                <>
-                  Do you want to install <code>{parsedTheme.displayName}</code>?
-                </>
-              ),
-              yesText: 'Install',
-              noText: 'Cancel',
-              onDone: async (isYes: boolean) => {
-                if (isYes) {
-                  const mainJsContent = `module.exports.themes = [${JSON.stringify(
-                    parsedTheme,
-                    null,
-                    2,
-                  )}];`;
-                  await createPlugin(
-                    `theme-${parsedTheme.name}`,
-                    '0.0.1',
-                    mainJsContent,
-                  );
-                  patchSettings({ theme: parsedTheme.name });
-                  await reloadPlugins();
-                  await setTheme(parsedTheme.name);
-                  showModal(SettingsModal, { tab: TAB_INDEX_THEMES });
-                }
-              },
-            });
-            break;
-
-          case 'insomnia://oauth/github/authenticate': {
-            const { code, state } = params;
-            await exchangeCodeForToken({ code, state }).catch(
-              (error: Error) => {
-                showError({
-                  error,
-                  title: 'Error authorizing GitHub',
-                  message: error.message,
-                });
-              },
-            );
-            break;
-          }
-
-          case 'insomnia://oauth/gitlab/authenticate': {
-            const { code, state } = params;
-            await exchangeCodeForGitLabToken({ code, state }).catch(
-              (error: Error) => {
-                showError({
-                  error,
-                  title: 'Error authorizing GitLab',
-                  message: error.message,
-                });
-              },
-            );
-            break;
-          }
-
-          case 'insomnia://app/auth/finish': {
-            actionFetcher.submit(
-              {
-                code: params.box,
-              },
-              {
-                action: '/auth/authorize',
-                method: 'POST',
-                encType: 'application/json',
-              },
-            );
-            break;
-          }
-
-          default: {
-            console.log(`Unknown deep link: ${url}`);
-          }
-        }
-      },
-    );
-  }, [actionFetcher, patchSettings]);
 
   const { organizationId, projectId, workspaceId } = useParams() as {
     organizationId: string;
@@ -447,16 +278,6 @@ const OrganizationRoute = () => {
         <NunjucksEnabledProvider>
           <AppHooks />
           <div className="app">
-            <Modals />
-            {/* triggered by insomnia://app/import */}
-            {importUri && (
-              <ImportModal
-                onHide={() => setImportUri('')}
-                projectName="Insomnia"
-                organizationId={organizationId}
-                from={{ type: 'uri', defaultValue: importUri }}
-              />
-            )}
             <div className="w-full h-full divide-x divide-solid divide-y divide-[--hl-md] grid-template-app-layout grid relative bg-[--color-bg]">
               <header className="[grid-area:Header] grid grid-cols-3 items-center">
                 <div className="flex items-center">
