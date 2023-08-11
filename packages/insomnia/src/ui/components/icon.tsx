f9b80e2b5defd5093d86151d886e44b6ecd01627
gatzjames <jamesgatzos@gmail.com>
2023-08-11 20:18:46 +0200

    fa brand icons

--- packages/insomnia/src/ui/components/icon.tsx
+++ packages/insomnia/src/ui/components/icon.tsx
@@ -1,4 +1,5 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import {
@@ -7,7 +8,7 @@ import {
 } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
-library.add(fas, far);
+library.add(fas, far, fab);
 
 export const Icon = (props: FontAwesomeIconProps) => (
   <FontAwesomeIcon {...props} />
