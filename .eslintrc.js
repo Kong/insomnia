9ea0fb8c4dfb5ca34dd98e95b5e50faec3cb27d9
jackkav <jackkav@gmail.com>
2023-08-12 01:51:31 +0200

    first pass

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
   plugins: [
     '@typescript-eslint',
