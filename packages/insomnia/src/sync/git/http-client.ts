ee6a4246b7cff9e7409267f84556916c2d3c4e9c
James Gatz <jamesgatzos@gmail.com>
2023-08-11 15:13:34 +0200

    Return error response from axios (#6261)
    * return error response from axios
    
    * fix 401 issue
    
    * redirect to existing when cloning a second time
    
    * fix types
    
    ---------
    
    Co-authored-by: jackkav <jackkav@gmail.com>

--- packages/insomnia/src/sync/git/http-client.ts
+++ packages/insomnia/src/sync/git/http-client.ts
@@ -1,37 +1,23 @@
 /** This is a client for isomorphic-git {@link https://isomorphic-git.org/docs/en/http} */
 export const httpClient = {
   request: async (config: any) => {
-    let response;
-
     if (config.headers && !config.headers.Accept) {
       config.headers.Accept = '*/*';
     }
-
-    try {
-      // hosted-git-info was adding git+ to the beginning of the url which isn't supported by axios after 0.27.0
-      const withoutGitPlus = config.url.replace(/^git\+/, '');
-      response = await window.main.axiosRequest({
-        url: withoutGitPlus,
-        method: config.method,
-        headers: config.headers,
-        data: config.body,
-        responseType: 'arraybuffer',
-        maxRedirects: 10,
-      });
-    } catch (err) {
-      if (!err.response) {
-        console.log('[git-http-client] Error thrown', err.message);
-        // NOTE: config.url is unreachable
-        throw err;
-      }
-      console.log('[git-http-client] Ignored Error', err.response);
-      response = err.response;
-    }
-
+    // hosted-git-info was adding git+ to the beginning of the url which isn't supported by axios after 0.27.0
+    const withoutGitPlus = config.url.replace(/^git\+/, '');
+    const response = await window.main.axiosRequest({
+      url: withoutGitPlus,
+      method: config.method,
+      headers: config.headers,
+      data: config.body,
+      responseType: 'arraybuffer',
+      maxRedirects: 10,
+    });
     return {
       url: config.url,
       method: config.method,
-      headers: response.headers,
+      headers: response.headers as Record<string, string>,
       body: [response.data],
       statusCode: response.status,
       statusMessage: response.statusText,
