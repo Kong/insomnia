9646812fd20100f3116f1f3ca747fe5efb59dce4
jackkav <jackkav@gmail.com>
2023-08-13 10:45:47 +0200

    remove plugin ignore code

--- packages/insomnia/src/network/unit-test-feature.ts
+++ packages/insomnia/src/network/unit-test-feature.ts
@@ -1,37 +1,33 @@
 import { RENDER_PURPOSE_SEND } from '../common/render';
 import { stats } from '../models';
 import { getBodyBuffer } from '../models/response';
-import * as plugins from '../plugins';
 import { fetchRequestData, responseTransform, sendCurlAndWriteTimeline, tryToInterpolateRequest, tryToTransformRequestWithPlugins } from './network';
 
 export function getSendRequestCallback() {
   return async function sendRequest(requestId: string) {
     stats.incrementExecutedRequests();
-    try {
-      // NOTE: unit tests will use the UI selected environment
-      const { request,
-        environment,
-        settings,
-        clientCertificates,
-        caCert,
-        activeEnvironmentId } = await fetchRequestData(requestId);
+    // NOTE: unit tests will use the UI selected environment
+    const { request,
+      environment,
+      settings,
+      clientCertificates,
+      caCert,
+      activeEnvironmentId } = await fetchRequestData(requestId);
 
-      const renderResult = await tryToInterpolateRequest(request, environment._id, RENDER_PURPOSE_SEND);
-      const renderedRequest = await tryToTransformRequestWithPlugins(renderResult);
-      const response = await sendCurlAndWriteTimeline(
-        renderedRequest,
-        clientCertificates,
-        caCert,
-        settings,
-      );
-      const res = await responseTransform(response, activeEnvironmentId, renderedRequest, renderResult.context);
-      const { statusCode: status, statusMessage, headers: headerArray, elapsedTime: responseTime } = res;
-      const headers = headerArray?.reduce((acc, { name, value }) => ({ ...acc, [name.toLowerCase() || '']: value || '' }), []);
-      const bodyBuffer = await getBodyBuffer(res) as Buffer;
-      const data = bodyBuffer ? bodyBuffer.toString('utf8') : undefined;
-      return { status, statusMessage, data, headers, responseTime };
-    } finally {
-      plugins.clearIgnores();
-    }
+    const renderResult = await tryToInterpolateRequest(request, environment._id, RENDER_PURPOSE_SEND);
+    const renderedRequest = await tryToTransformRequestWithPlugins(renderResult);
+    const response = await sendCurlAndWriteTimeline(
+      renderedRequest,
+      clientCertificates,
+      caCert,
+      settings,
+    );
+    const res = await responseTransform(response, activeEnvironmentId, renderedRequest, renderResult.context);
+    const { statusCode: status, statusMessage, headers: headerArray, elapsedTime: responseTime } = res;
+    const headers = headerArray?.reduce((acc, { name, value }) => ({ ...acc, [name.toLowerCase() || '']: value || '' }), []);
+    const bodyBuffer = await getBodyBuffer(res) as Buffer;
+    const data = bodyBuffer ? bodyBuffer.toString('utf8') : undefined;
+    return { status, statusMessage, data, headers, responseTime };
+
   };
 }
--- packages/insomnia/src/plugins/index.ts
+++ packages/insomnia/src/plugins/index.ts
@@ -105,25 +105,12 @@ export type ColorScheme = 'default' | 'light' | 'dark';
 
 let plugins: Plugin[] | null | undefined = null;
 
-let ignorePlugins: string[] = [];
-
 export async function init() {
-  clearIgnores();
   await reloadPlugins();
 }
 
-export function ignorePlugin(name: string) {
-  if (!ignorePlugins.includes(name)) {
-    ignorePlugins.push(name);
-  }
-}
-
-export function clearIgnores() {
-  ignorePlugins = [];
-}
-
 async function _traversePluginPath(
-  pluginMap: Record<string, any>,
+  pluginMap: Record<string, Plugin>,
   allPaths: string[],
   allConfigs: PluginConfigMap,
 ) {
@@ -169,8 +156,16 @@ async function _traversePluginPath(
         // Delete require cache entry and re-require
         const module = global.require(modulePath);
 
-        const pluginName = pluginJson.name;
-        pluginMap[pluginName] = _initPlugin(pluginJson || {}, module, allConfigs, modulePath);
+        pluginMap[pluginJson.name] = {
+          name: pluginJson.name,
+          description: pluginJson.description || pluginJson.insomnia.description || '',
+          version: pluginJson.version || 'unknown',
+          directory: modulePath || '',
+          config: allConfigs.hasOwnProperty(pluginJson.name)
+            ? allConfigs[pluginJson.name]
+            : { disabled: false },
+          module: module,
+        };
         console.log(`[plugin] Loaded ${modulePath}`);
       } catch (err) {
         showError({
@@ -399,30 +394,4 @@ export async function getThemes(): Promise<Theme[]> {
   }
 
   return extensions;
-}
-
-const _defaultPluginConfig: PluginConfig = {
-  disabled: false,
-};
-
-function _initPlugin(
-  packageJSON: Record<string, any>,
-  module: any,
-  allConfigs: PluginConfigMap,
-  path?: string | null,
-): Plugin {
-  const meta = packageJSON.insomnia || {};
-  const name = packageJSON.name || meta.name;
-  // Find config
-  const config: PluginConfig = allConfigs.hasOwnProperty(name)
-    ? allConfigs[name]
-    : _defaultPluginConfig;
-  return {
-    name,
-    description: packageJSON.description || meta.description || '',
-    version: packageJSON.version || 'unknown',
-    directory: path || '',
-    config,
-    module: module,
-  };
 }
