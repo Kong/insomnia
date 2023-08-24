6de4af9b6aefc23e20699e5b63c8ecd01b471e25
jackkav <jackkav@gmail.com>
2023-08-24 19:44:14 +0200

    restore check

--- packages/insomnia/src/main/export.ts
+++ packages/insomnia/src/main/export.ts
@@ -28,6 +28,10 @@ export async function restoreBackup(version: string) {
     const dataPath = process.env['INSOMNIA_DATA_PATH'] || electron.app.getPath('userData');
     const versionPath = path.join(dataPath, 'backups', version);
     const files = await readdir(versionPath);
+    if (!files.length) {
+      console.log('No backup found at:', versionPath);
+      return;
+    }
     files.forEach(async (file: string) => {
       if (file.endsWith('.db')) {
         await copyFile(path.join(versionPath, file), path.join(dataPath, file));
