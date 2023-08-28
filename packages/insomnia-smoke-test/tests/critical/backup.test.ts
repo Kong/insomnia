0f4eeb56e62aea829f49c1910c5cf62833bbd66e
Filipe Freire <livrofubia@gmail.com>
2023-08-28 11:06:44 +0100

    check for backups folder contents

--- packages/insomnia-smoke-test/tests/critical/backup.test.ts
+++ packages/insomnia-smoke-test/tests/critical/backup.test.ts
@@ -8,14 +8,17 @@ import { test } from '../../playwright/test';
 test('can backup data on new version available', async ({ app, page }) => {
 
     const dataPath = await app.evaluate(async ({ app }) => app.getPath('userData'));
-
     let foundBackups = false;
     // retry 5 times
     for (let i = 0; i < 5; i++) {
         console.log('Retry', i);
         if (fs.existsSync(path.join(dataPath, 'backups'))) {
             console.log('Backups exists!');
-            foundBackups = true;
+            const rootBackupsFolder = fs.readdirSync(path.join(dataPath, 'backups'));
+            const backupDir = fs.readdirSync(path.join(dataPath, 'backups', rootBackupsFolder[0]));
+            const hasFilesInsideBackup = backupDir.length > 0;
+            const hasProjectDbFile = backupDir.includes('insomnia.Project.db');
+            foundBackups = hasFilesInsideBackup && hasProjectDbFile;
             break;
         } else {
             console.log('backups not found. Waiting 5 seconds...');
