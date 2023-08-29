a9f32d924150de1bab0a55c76527d97914d8fc43
James Gatz <jamesgatzos@gmail.com>
2023-08-29 13:29:13 +0200

    update to document rename tests (#6413)

--- packages/insomnia-smoke-test/tests/prerelease/design-document-naming.test.ts
+++ packages/insomnia-smoke-test/tests/prerelease/design-document-naming.test.ts
@@ -1,11 +1,12 @@
+import { expect } from '@playwright/test';
+
 import { test } from '../../playwright/test';
 
 test('can name design documents', async ({ page }) => {
     await page.getByRole('button', { name: ' New Document' }).click();
     await page.getByPlaceholder('my-spec.yaml').fill('jurassic park');
     await page.getByPlaceholder('my-spec.yaml').press('Enter');
-    await page.getByLabel('jurassic park').click();
-    await page.getByRole('button', { name: 'jurassic park ' }).press('Escape');
+    expect(page.getByRole('button', { name: 'jurassic park' })).toHaveText('jurassic park');
     await page.getByTestId('project').click();
     await page.getByLabel('jurassic park').click();
 });
