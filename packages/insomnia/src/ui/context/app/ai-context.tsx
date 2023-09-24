543cb852c26b2a9d9b94475e2488a4177f2b3b77
gatzjames <jamesgatzos@gmail.com>
2023-09-24 16:27:56 +0200

    re-enable ai check

--- packages/insomnia/src/ui/context/app/ai-context.tsx
+++ packages/insomnia/src/ui/context/app/ai-context.tsx
@@ -57,7 +57,7 @@ export const AIProvider: FC<PropsWithChildren> = ({ children }) => {
     }
   }, [aiAccessFetcher, organizationId, loggedIn]);
 
-  // const isAIEnabled = Boolean(aiAccessFetcher.data?.enabled);
+  const isAIEnabled = Boolean(aiAccessFetcher.data?.enabled);
 
   const aiGenerateTestsProgressStream = aiGenerateTestsFetcher.data as TransformStream;
 
@@ -105,7 +105,7 @@ export const AIProvider: FC<PropsWithChildren> = ({ children }) => {
           });
         },
         access: {
-          enabled: true,
+          enabled: isAIEnabled,
           loading: aiAccessFetcher.state !== 'idle',
         },
       }}
