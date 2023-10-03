b24f507ac53416f30b1f4b4c02cf0a0314ec7f50
Jack Kavanagh <jackkav@gmail.com>
2023-10-03 17:29:45 +0200

    Analytics after login (#6633)
    * add export all event
    
    * add navigate to scratch pad event
    
    * add events to all export workspaces

--- packages/insomnia/src/ui/routes/auth.login.tsx
+++ packages/insomnia/src/ui/routes/auth.login.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { Button, Dialog, DialogTrigger, Heading, Modal, ModalOverlay } from 'react-aria-components';
-import { ActionFunction, Link, LoaderFunction, redirect, useFetcher, useLoaderData } from 'react-router-dom';
+import { ActionFunction, Link, LoaderFunction, redirect, useFetcher, useLoaderData, useNavigate } from 'react-router-dom';
 
 import { getAppWebsiteBaseURL } from '../../common/constants';
 import { exportAllData } from '../../common/export-all-data';
 import { shouldRunMigration } from '../../sync/vcs/migrate-to-cloud-projects';
+import { SegmentEvent } from '../analytics';
 import { getLoginUrl } from '../auth-session-provider';
 import { Icon } from '../components/icon';
 import { showAlert } from '../components/modals';
@@ -65,7 +66,7 @@ const Login = () => {
   const loginFetcher = useFetcher();
   const [isMigrationModalOpen, setIsMigrationModalOpen] = useState(false);
   const [selectedProvider, setSelectedProvider] = useState('email');
-
+  const navigate = useNavigate();
   const login = (provider: string) => {
     if (data.hasProjectsToMigrate) {
       setIsMigrationModalOpen(true);
@@ -169,9 +170,14 @@ const Login = () => {
       </p>
 
       <div className='flex gap-[--padding-md] justify-between'>
-        <Link
+        <Button
+          onPress={() => {
+            window.main.trackSegmentEvent({
+              event: SegmentEvent.selectScratchpad,
+            });
+            navigate('/organization/org_scratchpad/project/proj_scratchpad/workspace/wrk_scratchpad/debug');
+          }}
           aria-label='Use the Scratch Pad'
-          to={'/organization/org_scratchpad/project/proj_scratchpad/workspace/wrk_scratchpad/debug'}
           className='flex outline-none transition-colors justify-center text-[rgba(var(--color-font-rgb),0.8)] text-sm gap-[--padding-xs] hover:text-[--color-font] focus:text-[--color-font]'
         >
           <div>
@@ -180,7 +186,7 @@ const Login = () => {
           <span>
             Use the local Scratch Pad
           </span>
-        </Link>
+        </Button>
         <DialogTrigger>
           <Button
             aria-label='Export data and more'
@@ -274,6 +280,9 @@ const Login = () => {
                         showAlert({
                           title: 'Export Complete',
                           message: 'All your data have been successfully exported',
+                        });
+                        window.main.trackSegmentEvent({
+                          event: SegmentEvent.exportAllCollections,
                         });
                       }}
                       aria-label='Export all data'
@@ -527,6 +536,9 @@ const Login = () => {
                             showAlert({
                               title: 'Export Complete',
                               message: 'All your data have been successfully exported',
+                            });
+                            window.main.trackSegmentEvent({
+                              event: SegmentEvent.exportAllCollections,
                             });
                           }}
                           className='focus:text-[--color-font] hover:text-[--color-font] font-bold transition-colors'
