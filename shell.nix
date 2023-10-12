b42ab0decb9557041a6bcfb679792b092d2c8f7e
Filipe Freire <livrofubia@gmail.com>
2023-10-12 14:38:15 +0100

    bump electron 27

--- shell.nix
+++ shell.nix
@@ -3,9 +3,9 @@ with import <nixpkgs> { };
 mkShell {
   nativeBuildInputs = [
     nodejs-18_x
-    electron_25
+    electron_27
     stdenv.cc.cc.lib
   ];
   LD_LIBRARY_PATH = "${stdenv.cc.cc.lib}/lib64:$LD_LIBRARY_PATH";
-  ELECTRON_OVERRIDE_DIST_PATH = "${electron_25}/bin/";
+  ELECTRON_OVERRIDE_DIST_PATH = "${electron_27}/bin/";
 }
