75ac45d344370e763785b6e0e8a4c0e05317bbbd
Filipe Freire <livrofubia@gmail.com>
2023-10-25 16:56:03 +0100

    rm custom certificate url parsing

--- packages/insomnia/src/network/url-matches-cert-host.ts
+++ packages/insomnia/src/network/url-matches-cert-host.ts
@@ -2,17 +2,15 @@ import { parse as urlParse } from 'url';
 
 import { escapeRegex } from '../common/misc';
 import { setDefaultProtocol } from '../utils/url/protocol';
-import certificateUrlParse from './certificate-url-parse';
 
 const DEFAULT_PORT = 443;
 
 export function urlMatchesCertHost(certificateHost: string, requestUrl: string) {
   const cHostWithProtocol = setDefaultProtocol(certificateHost, 'https:');
   const { hostname, port } = urlParse(requestUrl);
-  const { hostname: cHostname, port: cPort } = certificateUrlParse(cHostWithProtocol);
+  const { hostname: cHostname, port: cPort } = new URL(cHostWithProtocol);
   // @ts-expect-error -- TSCONVERSION `parseInt(null)` returns `NaN`
   const assumedPort = parseInt(port) || DEFAULT_PORT;
-  // @ts-expect-error -- TSCONVERSION `parseInt(null)` returns `NaN`
   const assumedCPort = parseInt(cPort) || DEFAULT_PORT;
   const cHostnameRegex = escapeRegex(cHostname || '').replace(/\\\*/g, '.*');
   const cPortRegex = escapeRegex(cPort || '').replace(/\\\*/g, '.*');
