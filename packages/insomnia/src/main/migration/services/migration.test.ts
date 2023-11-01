01025e9a815927db94ea486f987de0f04e1c60c8
Mark Kim <mark.kim@konghq.com>
2023-11-01 01:18:28 -0400

    add empty test cases

---
+++ packages/insomnia/src/main/migration/services/migration.test.ts
@@ -0,0 +1,10 @@
+describe('MigrationService', async () => {
+    it('should run successfully with local selection', async () => { });
+    it('should run successfully with remote selection', async () => { });
+    it('should fail when network call fails', async () => { });
+    it('should fail when conflict exists', async () => { });
+    it('should fail when migration did not complete fully for files', async () => { });
+    it('should fail when migration did not complete fully for projects', async () => { });
+    it('should log activities', async () => { });
+    it('should publish status updates', async () => { });
+});
