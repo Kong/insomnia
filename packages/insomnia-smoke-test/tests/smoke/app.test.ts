4d2a8354341fc8743d51ac76e2ebc09599867d19
jackkav <jackkav@gmail.com>
2023-11-02 18:29:22 +0100

    re order test

--- packages/insomnia-smoke-test/tests/smoke/app.test.ts
+++ packages/insomnia-smoke-test/tests/smoke/app.test.ts
@@ -2,48 +2,7 @@ import { expect } from '@playwright/test';
 
 import { getFixturePath, loadFixture } from '../../playwright/paths';
 import { test } from '../../playwright/test';;
-test.only('can send request with server and client certificates', async ({ app, page }) => {
-  const text = await loadFixture('smoke-test-collection.yaml');
-  await app.evaluate(async ({ clipboard }, text) => clipboard.writeText(text), text);
 
-  await page.getByRole('button', { name: 'Create in project' }).click();
-  await page.getByRole('menuitemradio', { name: 'Import' }).click();
-  await page.locator('[data-test-id="import-from-clipboard"]').click();
-  await page.getByRole('button', { name: 'Scan' }).click();
-  await page.getByRole('dialog').getByRole('button', { name: 'Import' }).click();
-  await page.getByText('CollectionSmoke testsjust now').click();
-
-  await page.getByLabel('Request Collection').getByTestId('sends request with certs').press('Enter');
-
-  await page.getByRole('button', { name: 'Send' }).click();
-  await page.getByText('Error: SSL peer certificate or SSH remote key was not OK').click();
-
-  // add server and client certs
-  await page.getByTestId('workspace-context-dropdown').click();
-  await page.getByRole('menuitemradio', { name: 'Settings' }).click();
-  const workspaceId = await page.getByTestId('workspace-id').textContent();
-  const fixturePath = getFixturePath('certificates');
-  await page.evaluate(async ({ workspaceId, fixturePath }) => {
-    window.models.caCertificate.create({
-      parentId: workspaceId,
-      path: fixturePath + '/rootCA.pem',
-    });
-    window.models.clientCertificate.create({
-      parentId: workspaceId,
-      cert: fixturePath + '/client.localhost-client.pem',
-      key: fixturePath + '/client.localhost-client-key.pem',
-      host: 'localhost',
-    });
-
-  }, { workspaceId, fixturePath });
-  await page.getByRole('button', { name: '' }).click();
-
-  // test request with certs
-  await page.getByRole('button', { name: 'Send' }).click();
-  await page.getByText('200 OK').click();
-  await page.locator('pre').filter({ hasText: '"id": "1"' }).click();
-
-});
 test('can send requests', async ({ app, page }) => {
   test.slow(process.platform === 'darwin' || process.platform === 'win32', 'Slow app start on these platforms');
   const statusTag = page.locator('[data-testid="response-status-tag"]:visible');
@@ -144,4 +103,46 @@ test('can cancel requests', async ({ app, page }) => {
 
   await page.getByRole('button', { name: 'Cancel Request' }).click();
   await page.click('text=Request was cancelled');
+});
+
+test('can send request with server and client certificates', async ({ app, page }) => {
+  const text = await loadFixture('smoke-test-collection.yaml');
+  await app.evaluate(async ({ clipboard }, text) => clipboard.writeText(text), text);
+
+  await page.getByRole('button', { name: 'Create in project' }).click();
+  await page.getByRole('menuitemradio', { name: 'Import' }).click();
+  await page.locator('[data-test-id="import-from-clipboard"]').click();
+  await page.getByRole('button', { name: 'Scan' }).click();
+  await page.getByRole('dialog').getByRole('button', { name: 'Import' }).click();
+  await page.getByText('CollectionSmoke testsjust now').click();
+
+  await page.getByLabel('Request Collection').getByTestId('sends request with certs').press('Enter');
+
+  await page.getByRole('button', { name: 'Send' }).click();
+  await page.getByText('Error: SSL peer certificate or SSH remote key was not OK').click();
+
+  // add server and client certs
+  await page.getByTestId('workspace-context-dropdown').click();
+  await page.getByRole('menuitemradio', { name: 'Settings' }).click();
+  const workspaceId = await page.getByTestId('workspace-id').textContent();
+  const fixturePath = getFixturePath('certificates');
+  await page.evaluate(async ({ workspaceId, fixturePath }) => {
+    window.models.caCertificate.create({
+      parentId: workspaceId,
+      path: fixturePath + '/rootCA.pem',
+    });
+    window.models.clientCertificate.create({
+      parentId: workspaceId,
+      cert: fixturePath + '/client.localhost-client.pem',
+      key: fixturePath + '/client.localhost-client-key.pem',
+      host: 'localhost',
+    });
+
+  }, { workspaceId, fixturePath });
+  await page.getByRole('button', { name: '' }).click();
+
+  // test request with certs
+  await page.getByRole('button', { name: 'Send' }).click();
+  await page.getByText('200 OK').click();
+  await page.locator('pre').filter({ hasText: '"id": "1"' }).click();
 });
