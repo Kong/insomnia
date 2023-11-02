abc35e10777a2aee1fcf883d50c1a8b15c302820
jackkav <jackkav@gmail.com>
2023-11-02 18:26:54 +0100

    use import to avoid url onChange timing issue

--- packages/insomnia-smoke-test/tests/smoke/app.test.ts
+++ packages/insomnia-smoke-test/tests/smoke/app.test.ts
@@ -2,11 +2,19 @@ import { expect } from '@playwright/test';
 
 import { getFixturePath, loadFixture } from '../../playwright/paths';
 import { test } from '../../playwright/test';;
-test.only('can send request with server and client certificates', async ({ page }) => {
-  await page.getByRole('button', { name: ' New Collection' }).click();
-  await page.getByRole('button', { name: 'Create', exact: true }).click();
-  await page.getByRole('button', { name: 'New HTTP Request' }).click();
-  await page.getByRole('textbox').fill('https://localhost/pets/1');
+test.only('can send request with server and client certificates', async ({ app, page }) => {
+  const text = await loadFixture('smoke-test-collection.yaml');
+  await app.evaluate(async ({ clipboard }, text) => clipboard.writeText(text), text);
+
+  await page.getByRole('button', { name: 'Create in project' }).click();
+  await page.getByRole('menuitemradio', { name: 'Import' }).click();
+  await page.locator('[data-test-id="import-from-clipboard"]').click();
+  await page.getByRole('button', { name: 'Scan' }).click();
+  await page.getByRole('dialog').getByRole('button', { name: 'Import' }).click();
+  await page.getByText('CollectionSmoke testsjust now').click();
+
+  await page.getByLabel('Request Collection').getByTestId('sends request with certs').press('Enter');
+
   await page.getByRole('button', { name: 'Send' }).click();
   await page.getByText('Error: SSL peer certificate or SSH remote key was not OK').click();
 
