a617b79bc12750264df37c82733146ab5f6de17e
Mark Kim <mark.kim@konghq.com>
2023-11-08 11:13:23 -0500

    only initialize if not versioned already

--- packages/insomnia/src/sync/vcs/vcs.ts
+++ packages/insomnia/src/sync/vcs/vcs.ts
@@ -1541,17 +1541,9 @@ export class VCS {
     return matchedBackendProjects;
   }
 
-  async resetVersion(rootDocumentId: string) {
+  async checkIfAlreadyVersioned(rootDocumentId: string): Promise<boolean> {
     const versionedFiles = await this._getAllBackendProjectsByRootDocument(rootDocumentId);
-
-    if (!versionedFiles?.length) {
-      return;
-    }
-
-    // remove all the versioned files locally
-    for (const file of versionedFiles) {
-      await this._removeProject(file);
-    }
+    return Boolean(versionedFiles?.length);
   }
 }
 
