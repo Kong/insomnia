ee759216bc60b41880f0ed1e30ed8800ce2a03cc
Mark Kim <mark.kim@konghq.com>
2023-11-08 11:00:07 -0500

    add tests

---
+++ packages/insomnia/src/sync/vcs/__tests__/migrate-projects-into-organizatino.test.ts
@@ -0,0 +1,73 @@
+import { afterAll, beforeEach, describe, expect, it, jest } from '@jest/globals';
+
+import { globalBeforeEach } from '../../../__jest__/before-each';
+import * as models from '../../../models';
+import MemoryDriver from '../../store/drivers/memory-driver';
+import { pushSnapshotOnInitialize } from '../initialize-backend-project';
+import { VCS } from '../vcs';
+
+describe('migrate-projects-into-organizatino', () => {
+  beforeEach(globalBeforeEach);
+
+  describe('pushSnapshotOnInitialize()', () => {
+    const vcs = new VCS(new MemoryDriver());
+
+    const pushSpy = jest.spyOn(vcs, 'push');
+
+    beforeEach(() => {
+      jest.resetAllMocks();
+    });
+
+    afterAll(() => {
+      pushSpy.mockClear();
+    });
+
+    it('should not push if no active project', async () => {
+      const project = await models.project.create({ remoteId: null });
+      const workspace = await models.workspace.create({ parentId: project._id });
+      const workspaceMeta = await models.workspaceMeta.create({ parentId: workspace._id });
+      vcs.clearBackendProject();
+
+      await pushSnapshotOnInitialize({ vcs, project, workspace });
+
+      expect(pushSpy).not.toHaveBeenCalled();
+      await expect(models.workspaceMeta.getByParentId(workspace._id)).resolves.toStrictEqual(workspaceMeta);
+    });
+
+    it('should not push snapshot if not remote project', async () => {
+      const project = await models.project.create({ remoteId: null });
+      const workspace = await models.workspace.create({ parentId: project._id });
+      const workspaceMeta = await models.workspaceMeta.create({ parentId: workspace._id });
+      vcs.switchAndCreateBackendProjectIfNotExist(workspace._id, workspace.name);
+
+      await pushSnapshotOnInitialize({ vcs, project, workspace });
+
+      expect(pushSpy).not.toHaveBeenCalled();
+      await expect(models.workspaceMeta.getByParentId(workspace._id)).resolves.toStrictEqual(workspaceMeta);
+    });
+
+    it('should not push snapshot if workspace not in project', async () => {
+      const project = await models.project.create({ remoteId: 'abc' });
+      const anotherProject = await models.project.create({ remoteId: 'def' });
+      const workspace = await models.workspace.create({ parentId: anotherProject._id });
+      vcs.switchAndCreateBackendProjectIfNotExist(workspace._id, workspace.name);
+
+      await pushSnapshotOnInitialize({ vcs, project, workspace });
+
+      expect(pushSpy).not.toHaveBeenCalled();
+    });
+
+    it('should push snapshot if conditions are met', async () => {
+      const project = await models.project.create({ remoteId: 'abc', parentId: 'team_abc' });
+      const workspace = await models.workspace.create({ parentId: project._id });
+      await models.workspaceMeta.create({ parentId: workspace._id, pushSnapshotOnInitialize: true });
+      vcs.switchAndCreateBackendProjectIfNotExist(workspace._id, workspace.name);
+
+      await pushSnapshotOnInitialize({ vcs, project, workspace });
+
+      expect(pushSpy).toHaveBeenCalledWith({ teamId: 'team_abc', teamProjectId: project.remoteId });
+      const updatedMeta = await models.workspaceMeta.getByParentId(workspace._id);
+      expect(updatedMeta?.pushSnapshotOnInitialize).toBe(false);
+    });
+  });
+});
