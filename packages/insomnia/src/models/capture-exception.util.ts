716de4e5e82f1f9aeabe52b8a32ed4b3650d65fe
Mark Kim <mark.kim@konghq.com>
2023-11-29 15:43:19 -0500

    chore: add comments

--- packages/insomnia/src/models/capture-exception.util.ts
+++ packages/insomnia/src/models/capture-exception.util.ts
@@ -1,3 +1,8 @@
+/**
+ * This is a HACK to work around inso cli using the database module used for Insomnia desktop client.
+ * Now this is getting coupled with Electron side, and CLI should not be really related to the electron at all.
+ * That is another tech debt.
+ */
 export type ExceptionCallback = (exception: unknown, captureContext?: unknown) => string;
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
