093927e84aef1d3b3ec3684f3f833fab48e8c1ee
gatzjames <jamesgatzos@gmail.com>
2023-12-11 15:11:15 +0100

    add warning in project for due payments of organization

--- packages/insomnia/src/ui/routes/project.tsx
+++ packages/insomnia/src/ui/routes/project.tsx
@@ -54,7 +54,7 @@ import { ApiSpec } from '../../models/api-spec';
 import { CaCertificate } from '../../models/ca-certificate';
 import { ClientCertificate } from '../../models/client-certificate';
 import { sortProjects } from '../../models/helpers/project';
-import { isOwnerOfOrganization, isScratchpadOrganizationId } from '../../models/organization';
+import { isOwnerOfOrganization, isPersonalOrganization, isScratchpadOrganizationId } from '../../models/organization';
 import { Organization } from '../../models/organization';
 import {
   isRemoteProject,
@@ -706,6 +706,10 @@ const ProjectRoute: FC = () => {
     },
   ];
 
+  const organization = organizations.find(o => o.id === organizationId);
+  const isUserOwner = organization && accountId && isOwnerOfOrganization({ organization, accountId });
+  const isPersonalOrg = organization && isPersonalOrganization(organization);
+
   return (
     <ErrorBoundary>
       <Fragment>
@@ -1009,6 +1013,15 @@ const ProjectRoute: FC = () => {
           }
           renderPaneOne={
             <div className="w-full h-full flex flex-col overflow-hidden">
+              <div className='p-[--padding-md] pb-0'>
+                <div className='flex flex-wrap justify-between items-center gap-2 p-[--padding-sm] border border-solid border-[--hl-md] bg-opacity-80 bg-[rgba(var(--color-warning-rgb),var(--tw-bg-opacity))] text-[--color-font-warning] rounded'>
+                  <p className='text-base'>
+                    <Icon icon="exclamation-triangle" className='mr-2' />
+                    {isUserOwner ? `Your ${isPersonalOrg ? 'personal account' : 'organization'} has unpaid past invoices. Please enter a new payment method to continue using Insomnia.` : 'This organization has unpaid past invoices. Please ask the organization owner to enter a new payment method to continue using Insomnia.'}
+                  </p>
+                  <Button className="px-4 text-[--color-bg] bg-opacity-100 bg-[rgba(var(--color-font-rgb),var(--tw-bg-opacity))] py-1 font-semibold border border-solid border-[--hl-md] flex items-center justify-center gap-2 aria-pressed:opacity-80 rounded-sm hover:bg-opacity-80 focus:ring-inset ring-1 ring-transparent focus:ring-[--hl-md] transition-all text-sm">Update payment method</Button>
+                </div>
+              </div>
               <div className="flex justify-between w-full gap-1 p-[--padding-md]">
                 <SearchField
                   aria-label="Workspaces filter"
