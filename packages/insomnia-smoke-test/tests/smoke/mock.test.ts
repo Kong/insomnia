b479d3ec4dab7a8fb79553cfa46d4c0a3b7d3414
jackkav <jackkav@gmail.com>
2024-01-17 11:41:23 +0100

    ui test

---
+++ packages/insomnia-smoke-test/tests/smoke/mock.test.ts
@@ -0,0 +1,12 @@
+import { test } from '../../playwright/test';
+
+test('can make a mock route', async ({ page }) => {
+  test.slow(process.platform === 'darwin' || process.platform === 'win32', 'Slow app start on these platforms');
+  await page.getByLabel('New Mock Server').click();
+  await page.getByRole('button', { name: 'Create', exact: true }).click();
+  await page.getByRole('button', { name: 'New Mock Route' }).click();
+  await page.getByText('GET/').click();
+  await page.getByTestId('CodeEditor').getByRole('textbox').fill('123');
+  await page.getByRole('button', { name: 'Test' }).click();
+  await page.getByRole('button', { name: 'Ok' }).click();
+});
