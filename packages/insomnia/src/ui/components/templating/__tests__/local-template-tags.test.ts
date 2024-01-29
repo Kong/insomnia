2d0042a83c06de13ebe8516f73ed466782233a7a
Nick Graham <nbgraham@users.noreply.github.com>
2024-01-29 07:20:25 -0600

    feat: template tag to encode hex to base64 (#6211)
    * feat: template tag to encode hex to base64
    
    * review, flatten, simplify
    
    ---------
    
    Co-authored-by: jackkav <jackkav@gmail.com>

---
+++ packages/insomnia/src/ui/components/templating/__tests__/local-template-tags.test.ts
@@ -0,0 +1,24 @@
+import { type PluginTemplateTagContext } from '../../../../templating/extensions';
+import { invariant } from '../../../../utils/invariant';
+import { localTemplateTags } from '../local-template-tags';
+describe('base64 tag', () => {
+  describe('encoder', () => {
+    const base64EncoderTag = localTemplateTags.find(p => p.templateTag.name === 'base64')?.templateTag;
+    invariant(base64EncoderTag, 'missing tag in localTemplateTags');
+    it('encodes from normal', () => {
+      const encoded = base64EncoderTag.run({} as PluginTemplateTagContext, 'encode', 'normal', 'hello');
+      expect(encoded).toBe('aGVsbG8=');
+    });
+    it('encodes from hex', () => {
+      const encoded = base64EncoderTag.run({} as PluginTemplateTagContext, 'encode', 'hex', 'abc123');
+      expect(encoded).toBe('q8Ej');
+    });
+    it('errors on invalid action', () => {
+      expect(() => base64EncoderTag.run({} as PluginTemplateTagContext, 'transform', 'normal', 'hello')).toThrowError();
+    });
+    it('errors on invalid kind', () => {
+      expect(() => base64EncoderTag.run({} as PluginTemplateTagContext, 'encode', 'klingon', 'hello')).toThrowError();
+    });
+  });
+
+});
