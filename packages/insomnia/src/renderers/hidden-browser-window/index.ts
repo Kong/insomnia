f07b1947fdb60b2924a21ceda341a62c8a3d3fd0
jackkav <jackkav@gmail.com>
2024-02-08 18:09:39 +0100

    simplify

--- packages/insomnia/src/renderers/hidden-browser-window/index.ts
+++ packages/insomnia/src/renderers/hidden-browser-window/index.ts
@@ -1,5 +1,4 @@
 import type { Request } from '../../models/request';
-import { invariant } from '../../utils/invariant';
 
 declare global {
   interface Window {
@@ -17,55 +16,45 @@ export interface HiddenBrowserWindowBridgeAPI {
   runPreRequestScript: (options: { script: string; context: RequestContext }) => Promise<RequestContext>;
 };
 
-const work: HiddenBrowserWindowBridgeAPI = {
-  runPreRequestScript: async ({ script, context }) => {
-    console.log(script);
-    const executionContext = {
-      request: {
-        addHeader: (v: string) => context.request.headers.push({ name: v.split(':')[0], value: v.split(':')[1] }),
-      },
-    };
+const runPreRequestScript = async ({ script, context }: { script: string; context: RequestContext }): Promise<RequestContext> => {
+  console.log(script);
+  const executionContext = {
+    request: {
+      addHeader: (v: string) => context.request.headers.push({ name: v.split(':')[0], value: v.split(':')[1] }),
+    },
+  };
 
-    const log = [];
-    const consolePolyfill = {
-      log: (...args: any[]) => log.push(JSON.stringify({ value: args.map(a => JSON.stringify(a)).join('\n'), name: 'Text', timestamp: Date.now() }) + '\n'),
-    };
+  const log: string[] = [];
+  const consolePolyfill = {
+    log: (...args: any[]) => log.push(JSON.stringify({ value: args.map(a => JSON.stringify(a)).join('\n'), name: 'Text', timestamp: Date.now() }) + '\n'),
+  };
 
-    const AsyncFunction = (async () => { }).constructor;
-    const executeScript = AsyncFunction(
-      'insomnia',
-      'require',
-      'console',
-      `
-                        const $ = insomnia, pm = insomnia;
-                        // const originalConsole = console;
-                        // const console = {
-                        //     log: (...args) => {
-                        //         originalConsole.log(...args);
-                        //         insomnia.log.push(args.join(' '));
-                        //     },
-                        //   };
-                        //   console = originalConsole;
-                         ${script};
-                        return insomnia;
-                    `
-    );
+  const AsyncFunction = (async () => { }).constructor;
+  const executeScript = AsyncFunction(
+    'insomnia',
+    'require',
+    'console',
+    `
+      const $ = insomnia, pm = insomnia;
+       ${script};
+      return insomnia;`
+  );
 
-    const mutated = await executeScript(executionContext, window.bridge.requirePolyfill, consolePolyfill);
-    await window.bridge.requirePolyfill('fs').promises.writeFile(context.timelinePath, log.join('\n'));
-    return context;
-  },
+  const mutatedInsomniaObject = await executeScript(executionContext,
+    window.bridge.requirePolyfill,
+    consolePolyfill);
+  await window.bridge.requirePolyfill('fs').promises.writeFile(context.timelinePath, log.join('\n'));
+  console.log('mutatedInsomniaObject', mutatedInsomniaObject);
+  console.log('context', context);
+  return context;
 };
+
 window.bridge.on('renderer-listener', async (event: MessageEvent) => {
   const [port] = event.ports;
   console.log('opened port to insomnia renderer');
   port.onmessage = async event => {
     try {
-      // TODO: consider removing this early abstraction
-      invariant(event.data.type, 'Missing work type');
-      const workType: 'runPreRequestScript' = event.data.type;
-      invariant(work[workType], `Unknown work type ${workType}`);
-      const result = await work[workType](event.data);
+      const result = await runPreRequestScript(event.data);
       console.log('got', { input: event.data, result });
       port.postMessage(result);
     } catch (err) {
