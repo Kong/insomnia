039c12160c98e295213fb2a4bc08ffa7790da697
Jack Kavanagh <jackkav@gmail.com>
2024-02-13 10:01:32 +0100

    E2e/mock-test-fixture (#7086)
    * fix export
    
    * add fixture
    
    * use fixture

--- packages/insomnia-smoke-test/tests/smoke/mock.test.ts
+++ packages/insomnia-smoke-test/tests/smoke/mock.test.ts
@@ -1,7 +1,17 @@
+import { loadFixture } from '../../playwright/paths';
 import { test } from '../../playwright/test';
 
-test('can make a mock route', async ({ page }) => {
+test('can make a mock route', async ({ app, page }) => {
   test.slow(process.platform === 'darwin' || process.platform === 'win32', 'Slow app start on these platforms');
+  const text = await loadFixture('smoke-test-collection.yaml');
+  await app.evaluate(async ({ clipboard }, text) => clipboard.writeText(text), text);
+
+  await page.getByRole('button', { name: 'Create in project' }).click();
+  await page.getByRole('menuitemradio', { name: 'Import' }).click();
+  await page.locator('[data-test-id="import-from-clipboard"]').click();
+  await page.getByRole('button', { name: 'Scan' }).click();
+  await page.getByRole('dialog').getByRole('button', { name: 'Import' }).click();
+
   await page.getByLabel('New Mock Server').click();
   await page.getByRole('button', { name: 'Create', exact: true }).click();
   await page.getByRole('button', { name: 'New Mock Route' }).click();
