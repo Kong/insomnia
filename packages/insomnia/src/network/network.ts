9c164f131a9c7b6a026b074892a561a8e94763a4
jackkav <jackkav@gmail.com>
2024-02-13 10:17:39 +0100

    remove folder check

--- packages/insomnia/src/network/network.ts
+++ packages/insomnia/src/network/network.ts
@@ -67,7 +67,6 @@ export const fetchRequestData = async (requestId: string) => {
   const responseId = generateId('res');
   const responsesDir = pathJoin((process.type === 'renderer' ? window : require('electron')).app.getPath('userData'), 'responses');
   const timelinePath = pathJoin(responsesDir, responseId + '.timeline');
-  await fs.promises.mkdir(responsesDir, { recursive: true });
   return { request, environment, settings, clientCertificates, caCert, activeEnvironmentId, timelinePath, responseId };
 };
 
