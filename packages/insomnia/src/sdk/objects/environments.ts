648c2654ea95904cd80ff08659dc5fc22a2742df
George He <hexxa@outlook.com>
2024-02-21 11:34:21 +0800

    feat(hidden-window): add globals, iterationData and variables interfaces in pre-request scripting

--- packages/insomnia/src/sdk/objects/environments.ts
+++ packages/insomnia/src/sdk/objects/environments.ts
@@ -34,3 +34,79 @@ export class Environment {
         return Object.fromEntries(this.kvs.entries());
     };
 }
+
+export class Variables {
+    // TODO: support vars for all levels
+    private globals: Environment;
+    private collection: Environment;
+    private environment: Environment;
+    private data: Environment;
+    private local: Environment;
+
+    constructor(
+        args: {
+            globals: Environment;
+            collection: Environment;
+            environment: Environment;
+            data: Environment;
+        },
+    ) {
+        this.globals = args.globals;
+        this.collection = args.collection;
+        this.environment = args.environment;
+        this.data = args.data;
+        this.local = new Environment({});
+    }
+
+    has = (variableName: string) => {
+        const globalsHas = this.globals.has(variableName);
+        const collectionHas = this.collection.has(variableName);
+        const environmentHas = this.environment.has(variableName);
+        const dataHas = this.data.has(variableName);
+        const localHas = this.local.has(variableName);
+
+        return globalsHas || collectionHas || environmentHas || dataHas || localHas;
+    };
+
+    get = (variableName: string) => {
+        let finalVal: boolean | number | string | object | undefined = undefined;
+        [
+            this.local,
+            this.data,
+            this.environment,
+            this.collection,
+            this.globals,
+        ].forEach(vars => {
+            const value = vars.get(variableName);
+            if (!finalVal && value) {
+                finalVal = value;
+            }
+        });
+
+        return finalVal;
+    };
+
+    set = (variableName: string, variableValue: boolean | number | string) => {
+        this.local.set(variableName, variableValue);
+    };
+
+    // replaceIn = (template: string) => {
+    //     const context = this.toObject();
+    //     return getIntepolator().render(template, context);
+    // };
+
+    toObject = () => {
+        return [
+            this.globals,
+            this.collection,
+            this.environment,
+            this.data,
+            this.local,
+        ].map(
+            vars => vars.toObject()
+        ).reduce(
+            (ctx, obj) => ({ ...ctx, ...obj }),
+            {},
+        );
+    };
+}
