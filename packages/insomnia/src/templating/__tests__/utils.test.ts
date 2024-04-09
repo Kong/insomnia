898e44d19a874374e13f7ecedde3d0d4a696dc72
Curry Yang <1019yanglu@gmail.com>
2024-04-09 11:25:04 +0800

    fix: change test name

--- packages/insomnia/src/templating/__tests__/utils.test.ts
+++ packages/insomnia/src/templating/__tests__/utils.test.ts
@@ -322,7 +322,7 @@ describe('decodeEncoding()', () => {
 describe('extractVariableKey()', () => {
   beforeEach(globalBeforeEach);
 
-  it('encodes things', () => {
+  it('extract nunjucks variable key', () => {
     expect(utils.extractVariableKey('{{name}}', 1, 1)).toBe('name');
     expect(utils.extractVariableKey('aaaaaa{{name}}', 1, 7)).toBe('name');
     expect(utils.extractVariableKey('{{name}}\n\n{{age}}', 3, 1)).toBe('age');
