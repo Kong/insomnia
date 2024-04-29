33243aea25ffaa9a9310e015bd0988a0fd1103df
jackkav <jackkav@gmail.com>
2024-04-29 22:50:30 +0200

    group protocols

--- packages/insomnia/src/main/api.protocol.ts
+++ packages/insomnia/src/main/api.protocol.ts
@@ -6,72 +6,50 @@ export interface RegisterProtocolOptions {
 }
 
 const insomniaStreamScheme = 'insomnia-event-source';
+const insomniaAPIScheme = 'insomnia-api';
+const externalScheme = 'external-api';
 
-export async function registerInsomniaStreamProtocol() {
+export async function registerInsomniaProtocols() {
   protocol.registerSchemesAsPrivileged([{
     scheme: insomniaStreamScheme,
     privileges: { secure: true, standard: true, supportFetchAPI: true },
+  }, {
+      scheme: insomniaAPIScheme,
+      privileges: { secure: true, standard: true, supportFetchAPI: true },
+    }, {
+      scheme: externalScheme,
+      privileges: { secure: true, standard: true, supportFetchAPI: true },
   }]);
 
   await app.whenReady();
 
-  if (protocol.isProtocolHandled(insomniaStreamScheme)) {
-    return;
+  if (!protocol.isProtocolHandled(insomniaStreamScheme)) {
+    protocol.handle(insomniaStreamScheme, async request => {
+      const apiURL = getApiBaseURL();
+      const url = new URL(`${apiURL}/${request.url.replace(`${insomniaStreamScheme}://`, '')}`);
+      const sessionId = new URLSearchParams(url.search).get('sessionId');
+      request.headers.append('X-Session-Id', sessionId || '');
+
+      return net.fetch(url.toString(), request);
+    });
+  }
+  if (!protocol.isProtocolHandled(insomniaAPIScheme)) {
+    protocol.handle(insomniaAPIScheme, async request => {
+      const origin = request.headers.get('X-Origin') || getApiBaseURL();
+
+      const path = request.url.replace(`${insomniaAPIScheme}://insomnia`, '');
+      const url = new URL(path, origin);
+      console.log('Fetching', url.toString());
+      return net.fetch(url.toString(), request);
+    });
+  }
+  if (!protocol.isProtocolHandled(externalScheme)) {
+    protocol.handle(externalScheme, async request => {
+      const path = request.url.replace(`${externalScheme}://insomnia/`, 'https://');
+      const url = new URL(path);
+      console.log('Fetching external', url.toString());
+      return net.fetch(url.toString(), request);
+    });
   }
 
-  protocol.handle(insomniaStreamScheme, async request => {
-    const apiURL = getApiBaseURL();
-    const url = new URL(`${apiURL}/${request.url.replace(`${insomniaStreamScheme}://`, '')}`);
-    const sessionId = new URLSearchParams(url.search).get('sessionId');
-    request.headers.append('X-Session-Id', sessionId || '');
-
-    return net.fetch(url.toString(), request);
-  });
-}
-
-const insomniaAPIScheme = 'insomnia-api';
-
-export async function registerInsomniaAPIProtocol() {
-  protocol.registerSchemesAsPrivileged([{
-    scheme: insomniaAPIScheme,
-    privileges: { secure: true, standard: true, supportFetchAPI: true },
-  }]);
-
-  await app.whenReady();
-
-  if (protocol.isProtocolHandled(insomniaAPIScheme)) {
-    return;
-  }
-
-  protocol.handle(insomniaAPIScheme, async request => {
-    const origin = request.headers.get('X-Origin') || getApiBaseURL();
-
-    const path = request.url.replace(`${insomniaAPIScheme}://insomnia`, '');
-    const url = new URL(path, origin);
-    console.log('Fetching', url.toString());
-    return net.fetch(url.toString(), request);
-  });
-}
-
-const externalScheme = 'external-api';
-
-export async function registerExternalProtocol() {
-  protocol.registerSchemesAsPrivileged([{
-    scheme: externalScheme,
-    privileges: { secure: true, standard: true, supportFetchAPI: true },
-  }]);
-
-  await app.whenReady();
-
-  if (protocol.isProtocolHandled(externalScheme)) {
-    return;
-  }
-
-  protocol.handle(externalScheme, async request => {
-    const origin = request.headers.get('X-Origin') || getApiBaseURL();
-    const path = request.url.replace(`${externalScheme}://insomnia`, '');
-    const url = new URL(path, origin);
-    console.log('Fetching', url.toString());
-    return net.fetch(url.toString(), request);
-  });
 }
