7c9e9fbdb627fcd634c33d229fa24e274ea8edbf
jackkav <jackkav@gmail.com>
2024-05-02 11:12:44 +0200

    bump electron

--- shell.nix
+++ shell.nix
@@ -3,10 +3,10 @@ with import <nixpkgs> { };
 mkShell {
   nativeBuildInputs = [
     nodejs-20_x
-    electron_29
+    electron_30
     stdenv.cc.cc.lib
   ];
   LD_LIBRARY_PATH = "${stdenv.cc.cc.lib}/lib64:$LD_LIBRARY_PATH";
-  ELECTRON_OVERRIDE_DIST_PATH = "${electron_29}/bin/";
+  ELECTRON_OVERRIDE_DIST_PATH = "${electron_30}/bin/";
   ELECTRON_SKIP_BINARY_DOWNLOAD = 1;
 }
