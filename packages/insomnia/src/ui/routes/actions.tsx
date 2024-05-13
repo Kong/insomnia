52fd3bd911660876cac8953aab12721e0049da6a
jackkav <jackkav@gmail.com>
2024-05-13 10:25:42 +0200

    fix typo

--- packages/insomnia/src/ui/routes/actions.tsx
+++ packages/insomnia/src/ui/routes/actions.tsx
@@ -24,7 +24,7 @@ import { VCSInstance } from '../../sync/vcs/insomnia-sync';
 import { insomniaFetch } from '../../ui/insomniaFetch';
 import { invariant } from '../../utils/invariant';
 import { SegmentEvent } from '../analytics';
-import { SpectralRunner } from '../worker/spetral-run';
+import { SpectralRunner } from '../worker/spectral-run';
 
 // Project
 export const createNewProjectAction: ActionFunction = async ({ request, params }) => {
