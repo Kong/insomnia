9f288ea547a9f0db68208fb4a1a67f03c414d7ec
George He <hexxa@outlook.com>
2024-06-21 11:40:55 +0800

    test: refactor and add some tests

--- packages/insomnia/src/utils/importers/convert.test.ts
+++ packages/insomnia/src/utils/importers/convert.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from '@jest/globals';
 import { fail } from 'assert';
 
-import { convert } from './convert';
+import { checkInvalidPeriod, convert } from './convert';
 
 describe('Import errors', () => {
   it('fail to find importer', async () => {
@@ -11,5 +11,41 @@ describe('Import errors', () => {
     } catch (err) {
       expect(err.message).toBe('No importers found for file');
     }
+  });
+});
+
+describe('test checkInvalidPeriod', () => {
+  [
+    {
+      input: {
+        '.hehe': 'haha',
+      },
+      noError: false,
+    },
+    {
+      input: {
+        '.': '',
+        'arr': [''],
+      },
+      noError: false,
+    },
+    {
+      input: [
+        '',
+        1,
+      ],
+      noError: true,
+    },
+  ].forEach(testCase => {
+    it(`check: ${testCase.input}`, () => {
+      let e: Error | undefined = undefined;
+
+      try {
+        checkInvalidPeriod(testCase.input);
+      } catch (ex) {
+        e = ex;
+      }
+      expect(e === undefined).toBe(testCase.noError);
+    });
   });
 });
--- packages/insomnia/src/utils/importers/convert.ts
+++ packages/insomnia/src/utils/importers/convert.ts
@@ -54,14 +54,14 @@ export const convert = async (rawData: string) => {
 };
 
 // this checks invalid keys ahead, or nedb would return an error in importing.
-function checkInvalidPeriod(entity: Record<string, any> | any[]) {
+export function checkInvalidPeriod(entity: Record<string | number, any>) {
   for (const key in entity) {
     const containsPeriod = key.indexOf('.') !== -1;
     if (containsPeriod) {
       throw new Error(`Detected invalid key "${key}", which contains '.'. Please update it in the original tool and re-import it.`);
     }
 
-    const value = typeof entity === 'object' ? (entity as Record<string, any>)[key] : entity[key];
+    const value = entity[key];
     const shouldRecurse = Array.isArray(value) || typeof value === 'object';
     if (shouldRecurse) {
       checkInvalidPeriod(value);
