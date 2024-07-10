07f24d5c72aaa2049b0c0c3419055adc2441964e
Curry Yang <1019yanglu@gmail.com>
2024-07-10 11:50:46 +0800

    add comment

--- packages/insomnia/src/ui/sentry.ts
+++ packages/insomnia/src/ui/sentry.ts
@@ -24,6 +24,8 @@ export function initializeSentry() {
     ...SENTRY_OPTIONS,
     // enable sentry tracing
     integrations: [Sentry.browserTracingIntegration()],
+    // set 0.5 sample rate for traces, and check whether the limit is exceeded
+    // https://konghq.sentry.io/settings/billing/overview/?category=transactions
     tracesSampleRate: 0.5,
   });
   sentryWatchUserInfo();
