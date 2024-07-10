9db34768dee745bb68f875e5002767f9da0c26ba
Curry Yang <163384738+CurryYangxx@users.noreply.github.com>
2024-07-10 16:14:10 +0800

    chore: enable sentry tracing (#7688)
    * chore: enable sentry tracing
    
    * add comment
    
    * change to 10 percent
    
    ---------
    
    Co-authored-by: jackkav <jackkav@gmail.com>

--- packages/insomnia/src/ui/sentry.ts
+++ packages/insomnia/src/ui/sentry.ts
@@ -20,6 +20,13 @@ function sentryWatchUserInfo() {
 }
 
 export function initializeSentry() {
-  Sentry.init(SENTRY_OPTIONS);
+  Sentry.init({
+    ...SENTRY_OPTIONS,
+    // enable sentry tracing
+    integrations: [Sentry.browserTracingIntegration()],
+    // set 0.1 sample rate for traces, only send 10% of traces, and check whether the limit is exceeded
+    // https://konghq.sentry.io/settings/billing/overview/?category=transactions
+    tracesSampleRate: 0.1,
+  });
   sentryWatchUserInfo();
 }
