bd525b8220a0c0f50e309de1ff3b278771bccd58
garug <allefff@gmail.com>
2024-07-23 21:32:55 -0300

    2024-07-23.2

--- packages/insomnia/src/utils/prettify/edn.ts
+++ packages/insomnia/src/utils/prettify/edn.ts
@@ -5,73 +5,76 @@ const delimitersData = [
   ["[", "]"]
 ]
 
-const delimiters = delimitersData.flatMap(e => e)
-
-const [startDelimiters, endDelimiters] = delimitersData.reduce((acc, e) => {
-  acc[0].push(e[0])
-  acc[1].push(e[1])
-  return acc
-}, [[], []] as typeof delimitersData)
+const [startDelimiters, endDelimiters] = delimitersData.reduce(
+  (acc, e) => {
+    acc[0].push(e[0]);
+    acc[1].push(e[1]);
+    return acc;
+  },
+  [[], []] as typeof delimitersData
+);
 
 function spacesOnLeft(spaces: number[]) {
-  const length = spaces.reduce((acc, e) => acc + e)
+  const length = spaces.reduce((acc, e) => acc + e, 0);
 
-  return Array
-    .from({ length })
-    .map(() => " ").join("");
+  return Array.from({ length })
+    .map(() => " ")
+    .join("");
 }
 
 export const ednPrettify = (edn: string, _filter?: string) => {
   let insideString = false;
 
-  let tokens: string[] = []
+  let tokens: string[] = [];
 
-  let symbol = ""
+  let symbol = "";
 
   // let newSymbol = false
 
-  for (const [i, c] of [...edn].entries()) {
+  for (const c of edn) {
     if (!insideString) {
       // Ignore when
-      if (c === "," || (c === " " && !symbol)) {
-        continue
+      if (c === "," || c === "\n" || (c === " " && !symbol)) {
+        symbol = "";
+        continue;
       } else if (c === '"') {
-        insideString = true
-        symbol += c
+        insideString = true;
+        symbol += c;
       } else if (c === " " && symbol) {
-        tokens.push(symbol)
-        symbol = ""
+        tokens.push(symbol);
+        symbol = "";
       } else if (startDelimiters.includes(symbol + c)) {
-        tokens.push(symbol + c)
-        symbol = ""
+        tokens.push(symbol + c);
+        symbol = "";
       } else if (endDelimiters.includes(c)) {
         if (symbol) {
-          tokens.push(symbol)
+          tokens.push(symbol);
         }
-        tokens.push(c)
-
-        symbol = ""
+        tokens.push(c);
+        symbol = "";
       } else {
-        symbol += c
+        symbol += c;
       }
     } else {
       if (c === '"' && symbol !== "\\") {
-        insideString = false
-        tokens.push(symbol + c)
-        symbol = ""
+        insideString = false;
+        tokens.push(symbol + c);
+        symbol = "";
       } else {
-        symbol += c
+        symbol += c;
       }
     }
   }
 
-  let lines: string[] = []
+  let lines: string[] = [];
 
-  let currentLine: string[] = []
+  let currentLine: string[] = [];
 
-  let keyValue: string[] = []
+  let currentLineStartStructure = false;
 
-  let spaces: number[] = []
+  let keyValue: string[] = [];
+
+  let spaces: number[] = [];
 
   let tokenUsed = false;
 
@@ -79,11 +82,11 @@ export const ednPrettify = (edn: string, _filter?: string) => {
     // If this token already been used, continue for next token
     if (tokenUsed) {
       tokenUsed = false;
-      continue
+      continue;
     }
 
     // Adds this token to current line
-    currentLine.push(t)
+    currentLine.push(t);
 
     // If its a delimiter, will continue for next token
     const startDelimiter = startDelimiters.includes(t);
@@ -91,7 +94,13 @@ export const ednPrettify = (edn: string, _filter?: string) => {
 
     if (startDelimiter || endDelimiter) {
       if (startDelimiter) {
-        keyValue = []
+        keyValue = [];
+        currentLineStartStructure = true;
+        console.log({ currentLine, spaces });
+        spaces.push(
+          currentLine.map((e) => e.length).reduce((acc, e) => acc + e) -
+            spaces.reduce((acc, e) => acc + e, 0)
+        );
       }
       continue;
     }
@@ -99,14 +108,14 @@ export const ednPrettify = (edn: string, _filter?: string) => {
     // Token can be a key, value or metadata, only key and value are valid for line count
     // Metadata are tokens started with # like #uuid
     if (!t.startsWith("#")) {
-      keyValue.push(t)
+      keyValue.push(t);
     }
 
     let endLine = false;
 
     // If the line already contains a key and value, go to next line
     if (keyValue.length === 2) {
-      keyValue = []
+      keyValue = [];
       endLine = true;
     }
 
@@ -115,31 +124,39 @@ export const ednPrettify = (edn: string, _filter?: string) => {
 
     // If line is not ending and next token is not a closing delimiter, continue for next token
     if (!endLine && !nextEnding) {
-      currentLine.push(" ")
-      continue
+      currentLine.push(" ");
+      continue;
     }
 
     // If the next token is a close delimiter, use this delimiter instead of key/value for end of line
     if (nextEnding) {
       currentLine.push(nextToken);
       tokenUsed = true;
+      console.log("element removed, ", spaces.pop());
     }
 
     // Create line, add to lines and prepare for next line
-    const line = currentLine.join("")
-    lines.push(line)
-    currentLine = []
+    const line = currentLine.join("");
+    // console.log({ spaces, currentLineStartStructure, line });
+    lines.push(line);
+    currentLineStartStructure = false;
+    console.log({ spaces, line });
+    currentLine = [spacesOnLeft(spaces)];
   }
 
-  console.log({ tokens, lines, currentLine })
-
-  return lines.join("\n") + currentLine.join("");
+  return lines.join("\n") + currentLine.join("").trim();
 };
 
-console.log(ednPrettify(`{:name #uuid "garug"
-:age 20
-:enabled true
-:likes #{{:name "Paris"}
-{:name "Londres"}}}`));
-// console.log(ednPrettify(`{:name #uuid "garug" :age 20 :enabled true :likes #{"dogs" "cats"}}`))
-// console.log(ednPrettify(`{:prop1 {:prop2 {:prop3 :b}}}`))
+// console.log(ednPrettify(`{:name #uuid "garug"
+// :age 20
+// :enabled true
+// :likes #{{:name "Paris"}
+// {:name "Londres"}}}`));
+
+// console.log(
+//   ednPrettify(
+//     `{:name #uuid "garug" :age 20 :enabled true :likes #{"dogs" "cats"}}`
+//   )
+// );
+
+// console.log(ednPrettify(`{:prop1 {:prop2 {:prop3 :b}}}`));
