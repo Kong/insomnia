9aab12593679e19819cfd0f6b2637fd5564c87e8
jackkav <jackkav@gmail.com>
2024-07-25 14:12:02 +0200

    remove skipped logs

--- packages/insomnia-inso/vitest.config.ts
+++ packages/insomnia-inso/vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    hideSkippedTests: true,
     env: {
       DEFAULT_APP_NAME: process.env.DEFAULT_APP_NAME || 'insomnia-app',
     },
