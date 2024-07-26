ac077ccb5938193ee336755375be9db53ef69942
Allef Santana <allefff@gmail.com>
2024-07-25 22:03:32 -0300

    abstracted functions

--- packages/insomnia/src/utils/prettify/edn.ts
+++ packages/insomnia/src/utils/prettify/edn.ts
@@ -1,6 +1,6 @@
 const delimitersData = [
   // Procedence matters
-  ["#{", "}"],
+  ["#{", '}'],
   ["{", "}"],
   ["[", "]"]
 ]
@@ -14,15 +14,7 @@ const [startDelimiters, endDelimiters] = delimitersData.reduce(
   [[], []] as typeof delimitersData
 );
 
-function spacesOnLeft(spaces: number[]) {
-  const length = spaces.reduce((acc, e) => acc + e, 0);
-
-  return Array.from({ length })
-    .map(() => " ")
-    .join("");
-}
-
-export const ednPrettify = (edn: string, _filter?: string) => {
+function tokenize(edn: string) {
   let insideString = false;
 
   let tokens: string[] = [];
@@ -56,19 +48,33 @@ export const ednPrettify = (edn: string, _filter?: string) => {
       } else {
         symbol += c;
       }
-    } else {
-      if (c === '"' && symbol.at(-1) !== "\\") {
-        insideString = false;
-        tokens.push(symbol + c);
-        symbol = "";
-      } else if (c === "\n") {
-        symbol += "\\n"
-      } else {
-        symbol += c;
-      }
+      continue
     }
+
+    if (c === '"' && symbol.at(-1) !== "\\") {
+      insideString = false;
+      tokens.push(symbol + c);
+      symbol = "";
+    } else if (c === "\n") {
+      symbol += "\\n"
+    } else {
+      symbol += c;
+    }
+
   }
 
+  return tokens;
+}
+
+function spacesOnLeft(spaces: number[]) {
+  const length = spaces.reduce((acc, e) => acc + e, 0);
+
+  return Array.from({ length })
+    .map(() => " ")
+    .join("");
+}
+
+function tokensToLines(tokens: ReturnType<typeof tokenize>) {
   let lines: string[][] = [];
 
   let currentLine: string[] = [];
@@ -92,7 +98,7 @@ export const ednPrettify = (edn: string, _filter?: string) => {
       }
       tokenUsed = false;
       continue;
-    }    
+    }
 
     const startDelimiter = startDelimiters.includes(t);
     const endDelimiter = endDelimiters.includes(t);
@@ -154,19 +160,12 @@ export const ednPrettify = (edn: string, _filter?: string) => {
 
   lines.push(currentLine)
 
-  return lines.map(l => l.join("")).join("\n");
+  return lines.map(l => l.join(""));
+}
+
+export const ednPrettify = (edn: string, _filter?: string) => {
+  const tokens = tokenize(edn);
+  const lines = tokensToLines(tokens)
+
+  return lines.join("\n");
 };
-
-console.log(ednPrettify(`{:experiment-name "some name", :ecomm-verticals #{:tablet :notebooks :most_sold :eletronics :cell_phone :smartphone :television :computing :electro :best_blackfriday :childrens_day}, :search {:enabled true}, :experiment-inclusion-groups #{:rollout_awin :control_mega_experiment}, :sort-idx 3, :categories #{:ecommerce}, :terms-and-conditions-url "https://nubank.com.br/termos-de-uso", :kyc-fields #{}, :status :active, :id #uuid "44f3bc25-db7d-45cf-9532-62189cb2ae8d", :help-url "nuapp://nuhelp/", :version 52, :target-url "https://nubank.com.br/samsung-staging", :seller {:id #uuid "5a2cf25e-dcaf-48d8-9e10-82c7669b92de"}, :detail {:description "No site faça login usando seu email e o\n código 101010 e pague com Nubank.", :selling-points-icon-type :number, :partnership-text "Este serviço é uma parceria com a Samsung e o processo continuará em seu site", :name "Samsung", :terms-and-conditions-title "Termos e Condições", :selling-points #{{:name "Selling point 2", :explanation "Para acessar o site faça seu login usando seu email e use o código 101010 (BIN).", :order 1, :icon "nuds_v2_icon.smartphone_lock"} {:name "Selling point 1", :explanation "Pagando com Nubank você pode parcelar o valor em até 24 vezes sem juros.", :order 2, :icon "nuds_v2_icon.smartphone"} {:name "Selling point 3", :explanation "Benefício válido para pagamentos feitos com a opção \\"Pagar com Nubank\\" (NuPay).", :order 3, :icon "nuds_v2_icon.money_in"}}, :base-url "http://parcerias.samsung.com.br/nubank", :benefit "Até 40% de desconto", :handover-description "Este serviço é uma parceria com a Samsung e o processo continuará em seu site", :id #uuid "ab6e1597-696e-4047-8390-f6f77c749ded", :transition-steps #{{:step 2, :text "Aplicando o seu benefício", :action {:sleep-ms 500}} {:step 1, :text "Carregando produtos", :action {:mutation "engagementToken"}} {:step 3, :text "Indo para\nLoja", :action {:sleep-ms 500}}}, :query-params #{{:name "utm_source", :value "nubank-samsung"} {:name "utm_campaign", :value "nubank-samsung"}}, :engagement-token {:query-param-name "token"}, :terms-and-conditions-text "As ofertas apresentadas nesta página são oferecidas por nossos parceiros. Ao continuar você reconhece que esses parceiros são responsáveis por suas ofertas e pelo atendimento referente a elas. Você pode entrar em contato com a gente a qualquer momento para entender mais sobre nossas parcerias.", :handover-title "Indo para Samsung", :handover-action-text "Continuar para loja", :action-text "action text detail", :handover-enabled? false}}`));
-
-// console.log(ednPrettify(`{:name "Benefício válido para pagamentos feitos com a opção \\"Pagar com Nubank\\" (NuPay)."}`))
-
-// console.log(ednPrettify('{:name "ga\nrug"}'))
-
-// console.log(
-//   ednPrettify(
-//     `{:name #uuid "garug" :age 20 :enabled true :likes #{"dogs" "cats"}}`
-//   )
-// );
-
-// console.log(ednPrettify(`{:prop1 {:prop2 {:prop3 :b}}}`));
