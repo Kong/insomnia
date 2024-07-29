5085103bcc3fc1bb5a54a1201965189e0c020a76
jackkav <jackkav@gmail.com>
2024-07-29 17:14:34 +0200

    prepare test harness

--- packages/insomnia-inso/src/bundle.test.ts
+++ packages/insomnia-inso/src/bundle.test.ts
@@ -3,29 +3,23 @@ import { exec, ExecException } from 'child_process';
 import path from 'path';
 import { describe, expect, it } from 'vitest';
 describe('inso dev bundle', () => {
-  it('console logs a script', async () => {
+  it('logs response and timeline with verbose', async () => {
     const input = '$PWD/packages/insomnia-inso/bin/inso run collection -w packages/insomnia-inso/src/examples/minimal.yml wrk_5b5ab6 --verbose';
-    const result = await cli(input);
+    const result = await runCliFromRoot(input);
     if (result.code !== 0) {
       console.log(result);
     }
-    expect(result.stdout).toContain('foo bar baz');
+    // logs response object
+    expect(result.stdout).toContain('status: 200');
+    // logs timeline
+    expect(result.stdout).toContain('Preparing request to http://127.0.0.1:4010/');
+    // expect(result.stdout).toContain('foo bar baz');
   });
 
 });
-const cli = (input: string): Promise<{ code: number; error: ExecException | null; stdout: string; stderr: string }> => {
-  return new Promise(resolve => {
-    exec(input,
-      {
-        cwd: path.resolve(__dirname, '../../..'),
-      },
-      (error, stdout, stderr) => {
-        resolve({
-          code: error && error.code ? error.code : 0,
-          error,
-          stdout,
-          stderr,
-        });
-      });
-  });
+
+// Execute the command in the root directory of the project
+export const runCliFromRoot = (input: string): Promise<{ code: number; error: ExecException | null; stdout: string; stderr: string }> => {
+  return new Promise(resolve => exec(input, { cwd: path.resolve(__dirname, '../../..') },
+    (error, stdout, stderr) => resolve({ code: error?.code || 0, error, stdout, stderr, })));
 };
