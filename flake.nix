1ac22ed31f2eddb37c2efc61ac7e30b9d8241e20
Jack Kavanagh <jackkav@gmail.com>
2024-09-02 15:26:39 +0200

    use unstable (#7902)

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     electron-31-nixpkgs.url = "github:nixos/nixpkgs/a04eac9c5aa7f82e02d6e9e0b203b6eb5704c141";
-    node-20-16-nixpkgs.url = "github:nixos/nixpkgs/8a977702729f2d763adc3e214513d517dcb94d9b";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
   outputs = { self, nixpkgs, ... }@inputs:
     let
@@ -16,7 +16,7 @@
           in
           pkgs.mkShell {
             buildInputs = [
-              inputs.node-20-16-nixpkgs.legacyPackages.${system}.nodejs_20
+              inputs.nixpkgs-unstable.legacyPackages.${system}.nodejs_20
               inputs.electron-31-nixpkgs.legacyPackages.${system}.electron
             ];
 
