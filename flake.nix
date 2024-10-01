2fdd6b19695b8e6c0fa1f4e4e98ee5de2654abed
jackkav <jackkav@gmail.com>
2024-10-01 21:51:14 +0200

    flake.nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
   inputs = {
-    electron-31-nixpkgs.url = "github:nixos/nixpkgs/a04eac9c5aa7f82e02d6e9e0b203b6eb5704c141";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
   outputs = { self, nixpkgs, ... }@inputs:
@@ -13,17 +12,15 @@
         (system:
           let
             pkgs = nixpkgs.legacyPackages.${system};
+            unstable =inputs.nixpkgs-unstable.legacyPackages.${system};
           in
           pkgs.mkShell {
             buildInputs = [
-              inputs.nixpkgs-unstable.legacyPackages.${system}.nodejs_20
-              inputs.electron-31-nixpkgs.legacyPackages.${system}.electron
+              unstable.nodejs_20
+              pkgs.yarn
             ];
 
-            packages = [
-
-            ];
-            ELECTRON_OVERRIDE_DIST_PATH = "${inputs.electron-31-nixpkgs.legacyPackages.${system}.electron}/bin/";
+            ELECTRON_OVERRIDE_DIST_PATH = "${unstable.electron_31}/bin/";
             ELECTRON_SKIP_BINARY_DOWNLOAD = 1;
             LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib64:$LD_LIBRARY_PATH";
           });
