2a2ce4d81a99724ca568b03873dd98d3b59023ab
Filipe Freire <livrofubia@gmail.com>
2024-10-15 13:45:29 +0100

    prep customSign

--- packages/insomnia/customSign.js
+++ packages/insomnia/customSign.js
@@ -1,3 +1,34 @@
+const { exec } = require('child_process');
+const util = require('util');
+const execAsync = util.promisify(exec);
+
 exports.default = async function(configuration) {
-    console.log('[customSign] debug, file left to sign before packaging:', configuration.path);
+    console.log('[customSign] Debug - file to sign before packaging:', configuration.path);
+    const { ES_USERNAME, ES_PASSWORD, ES_CREDENTIAL_ID, ES_TOTP_SECRET } = process.env;
+
+    if (!ES_USERNAME || !ES_PASSWORD || !ES_CREDENTIAL_ID || !ES_TOTP_SECRET) {
+        throw new Error('[customSign] Missing required environment variables.');
+    }
+    // meant to be run on Windows host with docker
+    const inputFilePath = configuration.path.replace(/\\/g, '/');
+    const dockerCommand = `docker run -it --rm \
+        -e USERNAME="${USERNAME}" \
+        -e PASSWORD="${PASSWORD}" \
+        -e CREDENTIAL_ID="${CREDENTIAL_ID}" \
+        -e TOTP_SECRET="${TOTP_SECRET}" \
+        ghcr.io/sslcom/codesigner-win:latest sign \
+        -input_file_path="${inputFilePath}" -override`;
+    try {
+        const { stdout, stderr } = await execAsync(dockerCommand);
+
+        console.log('[customSign] Docker command output:', stdout);
+        if (stderr) {
+            console.error('[customSign] Docker command error output:', stderr);
+        }
+
+        console.log('[customSign] File signed successfully.');
+    } catch (error) {
+        console.error('[customSign] Error executing Docker command:', error);
+        throw error;
+    }
 };
