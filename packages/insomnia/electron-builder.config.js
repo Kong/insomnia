099c7c3594161a3205ebc62723cf97de6552037c
Filipe Freire <livrofubia@gmail.com>
2024-10-15 16:16:49 +0100

    fix

--- packages/insomnia/electron-builder.config.js
+++ packages/insomnia/electron-builder.config.js
@@ -101,7 +101,6 @@ const config = {
   },
   portable: {
     artifactName: `${BINARY_PREFIX}-\${version}-portable.\${ext}`,
-    sign: false,
   },
   linux: {
     artifactName: `${BINARY_PREFIX}-\${version}.\${ext}`,
