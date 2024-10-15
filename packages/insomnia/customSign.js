d8f96fec1e5ba0e6f405745d198e403dd5fe05d2
Filipe Freire <livrofubia@gmail.com>
2024-10-15 16:32:11 +0100

    workaround

--- packages/insomnia/customSign.js
+++ packages/insomnia/customSign.js
@@ -5,7 +5,7 @@ const execAsync = util.promisify(exec);
 
 exports.default = async function(configuration) {
     // skip signing if not windows squirrel
-    if (configuration.options.target[0].target !== 'squirrel') {
+    if (!configuration.options.target || configuration.options.target !== 'squirrel' || configuration.options.target[0].target !== 'squirrel') {
         console.log('[customSign] Skipping signing because target is not windows squirrel.');
         return;
     }
