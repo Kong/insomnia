5462a5b240ffcbf545ce297f106328d7f3c667e3
Kent Wang <kent.wang@konghq.com>
2024-11-26 10:42:27 +0800

    1.add a smoke test for env table view

--- packages/insomnia-smoke-test/tests/smoke/environment-editor-interactions.test.ts
+++ packages/insomnia-smoke-test/tests/smoke/environment-editor-interactions.test.ts
@@ -1,3 +1,5 @@
+import { expect } from '@playwright/test';
+
 import { loadFixture } from '../../playwright/paths';
 import { test } from '../../playwright/test';
 
@@ -111,4 +113,58 @@ test.describe('Environment Editor', async () => {
     // await page.locator('pre').filter({ hasText: '| Gandalf' }).click();
 
   });
+
+  test('Switch to table view and edit environment', async ({ page }) => {
+    await page.getByRole('button', { name: 'Manage Environments' }).click();
+    await page.getByRole('button', { name: 'Manage collection environments' }).click();
+    // switch table view
+    await page.getByRole('button', { name: 'Table Edit' }).click();
+    const kvTable = await page.getByRole('listbox', { name: 'Environment Key Value Pair' });
+    // disable row
+    await page.getByRole('button', { name: 'Disable Row' }).first().click();
+    let firstRow = await kvTable.getByRole('option').first();
+    // check row has been disabled
+    await expect(firstRow).toHaveCSS('opacity', '0.4');
+    // delete all items
+    await page.getByRole('button', { name: 'Delete All' }).dblclick();
+
+    firstRow = await kvTable.getByRole('option').first();
+    await firstRow.getByTestId('OneLineEditor').first().click();
+    await page.keyboard.type('exampleString');
+    await firstRow.getByTestId('OneLineEditor').nth(1).click();
+    await page.keyboard.type('kvstring');
+    // wait for editor update
+    await page.waitForTimeout(1000);
+    // add one more row
+    await page.getByRole('button', { name: 'Add Row' }).click();
+    const secondRow = await kvTable.getByRole('option').nth(1);
+    await secondRow.getByTestId('OneLineEditor').first().click();
+    await page.keyboard.type('exampleObject');
+    // change type to json
+    await secondRow.getByRole('button', { name: 'Type Selection' }).click();
+    await page.getByRole('menuitemradio', { name: 'JSON' }).click();
+    await secondRow.getByRole('button', { name: 'Edit JSON' }).click();
+    // wait for modal to show
+    await page.waitForTimeout(500);
+    const bodyEditor = await page.getByTestId('CodeEditor').getByRole('textbox');
+    // move cursor right and input json string
+    await bodyEditor.focus();
+    await bodyEditor.press('ArrowRight');
+    await bodyEditor.fill('"anotherString":"kvAnotherStr","anotherNumber": 12345');
+    await page.getByRole('button', { name: 'Modal Submit' }).click();
+    // Let debounce finish
+    await page.waitForTimeout(1500);
+
+    // Open request
+    await page.getByRole('button', { name: 'Close' }).click();
+    await page.getByLabel('Manage collection environments').press('Escape');
+    await page.getByLabel('Request Collection').getByTestId('New Request').press('Enter');
+    await page.getByRole('button', { name: 'Send' }).click();
+    await page.getByRole('tab', { name: 'Console' }).click();
+    // check new environment value
+    await page.getByText('kvstring').click();
+    await page.getByText('kvAnotherStr').click();
+    await page.getByText('12345').click();
+  });
+
 });
