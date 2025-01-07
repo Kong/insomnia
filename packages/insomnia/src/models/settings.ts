da76f5168fcff73b193606b78b4339c08bfda3f7
Kent Wang <kent.wang@konghq.com>
2025-01-07 14:33:40 +0800

    1.add enableVaultInScripts settings to allow using vault in script 2.fix editor bug

--- packages/insomnia/src/models/settings.ts
+++ packages/insomnia/src/models/settings.ts
@@ -73,6 +73,7 @@ export function init(): BaseSettings {
     saveVaultKeyLocally: true,
     enableVaultInScripts: false,
     saveVaultKeyToOSSecretManager: true,
+    enableVaultInScripts: false,
   };
 }
 
