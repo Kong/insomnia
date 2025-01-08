8ae06366e950aafee5f68a5e110ba1436a587138
Curry Yang <1019yanglu@gmail.com>
2025-01-08 16:13:10 +0800

    feat: close connections when active environment change

--- packages/insomnia/src/ui/hooks/use-close-connection.ts
+++ packages/insomnia/src/ui/hooks/use-close-connection.ts
@@ -4,11 +4,28 @@ import * as models from '../../models';
 import { isGrpcRequestId } from '../../models/grpc-request';
 import { isEventStreamRequest, isGraphqlSubscriptionRequest, isRequestId } from '../../models/request';
 import { isWebSocketRequestId } from '../../models/websocket-request';
-import uiEventBus, { UIEventType } from '../eventBus';
+import { useInsomniaTabContext } from '../context/app/insomnia-tab-context';
+import uiEventBus from '../eventBus';
 
 export const useCloseConnection = () => {
+
+  const closeConnectionById = async (id: string) => {
+    if (isGrpcRequestId(id)) {
+      window.main.grpc.cancel(id);
+    } else if (isWebSocketRequestId(id)) {
+      window.main.webSocket.close({ requestId: id });
+    } else if (isRequestId(id)) {
+      const request = await models.request.getById(id);
+      if (request && isEventStreamRequest(request)) {
+        window.main.curl.close({ requestId: id });
+      } else if (request && isGraphqlSubscriptionRequest(request)) {
+        window.main.webSocket.close({ requestId: id });
+      }
+    }
+  };
+
   // close websocket&grpc&SSE connections
-  const closeConnection = useCallback((_: string, ids: 'all' | string[]) => {
+  const handleTabClose = useCallback((_: string, ids: 'all' | string[]) => {
     if (ids === 'all') {
       window.main.webSocket.closeAll();
       window.main.grpc.closeAll();
@@ -17,26 +34,28 @@ export const useCloseConnection = () => {
     }
 
     ids.forEach(async id => {
-      if (isGrpcRequestId(id)) {
-        window.main.grpc.cancel(id);
-      } else if (isWebSocketRequestId(id)) {
-        window.main.webSocket.close({ requestId: id });
-      } else if (isRequestId(id)) {
-        const request = await models.request.getById(id);
-        if (request && isEventStreamRequest(request)) {
-          window.main.curl.close({ requestId: id });
-        } else if (request && isGraphqlSubscriptionRequest(request)) {
-          window.main.webSocket.close({ requestId: id });
-        }
-      }
+      await closeConnectionById(id);
     });
   }, []);
 
+  const { currentOrgTabs } = useInsomniaTabContext();
+
+  const handleActiveEnvironmentChange = useCallback((workspaceId: string) => {
+    const { tabList } = currentOrgTabs;
+    const tabs = tabList.filter(tab => tab.workspaceId === workspaceId);
+    tabs.forEach(async tab => {
+      const id = tab.id;
+      await closeConnectionById(id);
+    });
+  }, [currentOrgTabs]);
+
   useEffect(() => {
-    uiEventBus.on(UIEventType.CLOSE_TAB, closeConnection);
+    uiEventBus.on('CLOSE_TAB', handleTabClose);
+    uiEventBus.on('CHANGE_ACTIVE_ENV', handleActiveEnvironmentChange);
 
     return () => {
-      uiEventBus.off(UIEventType.CLOSE_TAB, closeConnection);
+      uiEventBus.off('CLOSE_TAB', handleTabClose);
+      uiEventBus.off('CHANGE_ACTIVE_ENV', handleActiveEnvironmentChange);
     };
-  }, [closeConnection]);
+  }, [handleTabClose, handleActiveEnvironmentChange]);
 };
