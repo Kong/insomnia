5049ee87e33b445b3ced781873e53ac5127b7caa
yaoweiprc <6896642+yaoweiprc@users.noreply.github.com>
2025-03-18 14:40:05 +0800

    Use import-from-clipboard instead of import-from-file in smoke test

--- packages/insomnia-smoke-test/tests/smoke/import.test.ts
+++ packages/insomnia-smoke-test/tests/smoke/import.test.ts
@@ -1,14 +1,16 @@
-import path from 'node:path';
-
 import { expect } from '@playwright/test';
 
+import { loadFixture } from '../../playwright/paths';
 import { test } from '../../playwright/test';
 
-test('can import multiple workspaces from single file', async ({ page }) => {
+test('can import multiple workspaces from single file', async ({ app, page }) => {
+  const text = await loadFixture('multiple-workspaces.yaml');
+  await app.evaluate(async ({ clipboard }, text) => clipboard.writeText(text), text);
   await page.getByLabel('Import').click();
-  await page.locator('[data-test-id="import-file-input"]').setInputFiles(path.join(__dirname, '../..', 'fixtures', 'multiple-workspaces.yaml'));
+  await page.locator('[data-test-id="import-from-clipboard"]').click();
   await page.getByRole('button', { name: 'Scan' }).click();
   await page.getByRole('dialog').getByRole('button', { name: 'Import' }).click();
+  // Have two collections in current project
   await expect(page.getByLabel('Collection 1')).toBeAttached();
   await expect(page.getByLabel('Collection 2')).toBeAttached();
 });
