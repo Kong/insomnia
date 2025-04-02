82a7d45af1c089b9194d3ad638eda373d3c644b2
jackkav <jackkav@gmail.com>
2025-04-02 16:11:47 +0200

    remove config helpers

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,3 @@
-const {
-  ERROR,
-  OFF,
-  SUCCESSOR,
-  TYPESCRIPT_CONVERSION,
-  TYPESCRIPT_EXTENSION,
-  UNKNOWN,
-} = require('eslint-config-helpers');
-
 /** @type { import('eslint').Linter.Config } */
 module.exports = {
   settings: {
@@ -54,35 +45,31 @@ module.exports = {
     {
       files: ['*.js'],
       rules: {
-        '@typescript-eslint/no-var-requires': OFF(UNKNOWN),
+        '@typescript-eslint/no-var-requires': 'off',
       },
     },
   ],
   rules: {
-    'array-bracket-spacing': ERROR,
-    'brace-style': SUCCESSOR(TYPESCRIPT_EXTENSION),
-    'block-spacing': ERROR,
-    'comma-dangle': [ERROR, 'always-multiline'],
-    'comma-spacing': ERROR,
-    'curly': ERROR,
-    'default-case': ERROR,
-    'default-case-last': ERROR,
-    'eol-last': [ERROR, 'always'],
-    'eqeqeq': [ERROR, 'smart'],
-    'arrow-parens': [ERROR, 'as-needed'],
-    'arrow-spacing': ERROR,
-    'keyword-spacing': SUCCESSOR(TYPESCRIPT_EXTENSION),
-    'no-else-return': ERROR,
-    'no-var': ERROR,
-    'no-trailing-spaces': ERROR,
-    'no-multiple-empty-lines': [ERROR, { 'max': 1, 'maxEOF': 0 }],
-    'object-curly-spacing': [ERROR, 'always'],
-    'semi': SUCCESSOR(TYPESCRIPT_EXTENSION),
-    'space-before-function-paren': [ERROR, { anonymous: 'ignore', named: 'ignore', asyncArrow: 'always' }],
-    'space-infix-ops': SUCCESSOR(TYPESCRIPT_EXTENSION),
-    'space-unary-ops': ERROR,
-    'space-in-parens': ERROR,
-    'spaced-comment': [ERROR, 'always', {
+    'array-bracket-spacing': 'error',
+    'block-spacing': 'error',
+    'comma-dangle': ['error', 'always-multiline'],
+    'comma-spacing': 'error',
+    'curly': 'error',
+    'default-case': 'error',
+    'default-case-last': 'error',
+    'eol-last': ['error', 'always'],
+    'eqeqeq': ['error', 'smart'],
+    'arrow-parens': ['error', 'as-needed'],
+    'arrow-spacing': 'error',
+    'no-else-return': 'error',
+    'no-var': 'error',
+    'no-trailing-spaces': 'error',
+    'no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 0 }],
+    'object-curly-spacing': ['error', 'always'],
+    'space-before-function-paren': ['error', { anonymous: 'ignore', named: 'ignore', asyncArrow: 'always' }],
+    'space-unary-ops': 'error',
+    'space-in-parens': 'error',
+    'spaced-comment': ['error', 'always', {
       exceptions: ['/', '*', '-', '* '], // for ASCII art :)
       markers: [
         '/', // for TypeScript directives, doxygen, vsdoc, etc. (which use `///`)
@@ -90,44 +77,44 @@ module.exports = {
       ],
     }],
 
-    'react/no-unescaped-entities': OFF(TYPESCRIPT_CONVERSION),
-    'react/jsx-first-prop-new-line': [ERROR, 'multiline'],
-    'react/jsx-max-props-per-line': [ERROR, { maximum: 1, when: 'multiline' }],
-    'react/jsx-uses-react': ERROR,
-    'react/jsx-uses-vars': ERROR,
-    'react/jsx-indent-props': [ERROR, 2],
-    'react/prop-types': OFF(UNKNOWN),
-    'react/function-component-definition': [ERROR, {
+    'react/no-unescaped-entities': 'off', // TODO: Enable this rule
+    'react/jsx-first-prop-new-line': ['error', 'multiline'],
+    'react/jsx-max-props-per-line': ['error', { maximum: 1, when: 'multiline' }],
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react/jsx-indent-props': ['error', 2],
+    'react/prop-types': 'off',
+    'react/function-component-definition': ['error', {
       'namedComponents': 'arrow-function',
       'unnamedComponents': 'arrow-function',
     }],
-    'react/jsx-closing-bracket-location': [ERROR, 'line-aligned'],
-    'react/prefer-stateless-function': ERROR,
-    'react/jsx-key': [ERROR, { 'checkFragmentShorthand': true }],
-    'react/no-array-index-key': ERROR,
-    'react/self-closing-comp': ERROR,
+    'react/jsx-closing-bracket-location': ['error', 'line-aligned'],
+    'react/prefer-stateless-function': 'error',
+    'react/jsx-key': ['error', { 'checkFragmentShorthand': true }],
+    'react/no-array-index-key': 'error',
+    'react/self-closing-comp': 'error',
 
-    'react-hooks/exhaustive-deps': [ERROR, {
+    'react-hooks/exhaustive-deps': ['error', {
       // From react-use https://github.com/streamich/react-use/issues/1703#issuecomment-770972824
       'additionalHooks': '^use(Async|AsyncFn|AsyncRetry|Debounce|UpdateEffect|IsomorphicLayoutEffect|DeepCompareEffect|ShallowCompareEffect)$',
     }],
-    'react-hooks/rules-of-hooks': ERROR,
+    'react-hooks/rules-of-hooks': 'error',
 
-    '@typescript-eslint/array-type': [ERROR, { default: 'array', readonly: 'array' }],
-    '@typescript-eslint/ban-types': OFF(UNKNOWN),
-    '@typescript-eslint/consistent-type-definitions': [ERROR, 'interface'],
-    '@typescript-eslint/no-empty-interface': [ERROR, { 'allowSingleExtends': true }],
-    '@typescript-eslint/no-empty-object-type': OFF(TYPESCRIPT_CONVERSION),
-    '@typescript-eslint/no-namespace': [ERROR, { allowDeclarations: true }],
-    '@typescript-eslint/no-redeclare': ERROR,
-    '@typescript-eslint/no-require-imports': OFF(UNKNOWN),
-    '@typescript-eslint/no-wrapper-object-types': OFF(TYPESCRIPT_CONVERSION),
-    '@typescript-eslint/no-unsafe-function-type': OFF(TYPESCRIPT_CONVERSION),
-    '@typescript-eslint/no-unused-expressions': OFF(TYPESCRIPT_CONVERSION),
-    '@typescript-eslint/no-unused-vars': OFF(TYPESCRIPT_CONVERSION),
+    '@typescript-eslint/array-type': ['error', { default: 'array', readonly: 'array' }],
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    '@typescript-eslint/no-empty-interface': ['error', { 'allowSingleExtends': true }],
+    '@typescript-eslint/no-empty-object-type': 'off', // TODO: Enable this rule
+    '@typescript-eslint/no-namespace': ['error', { allowDeclarations: true }],
+    '@typescript-eslint/no-redeclare': 'error',
+    '@typescript-eslint/no-require-imports': 'off',
+    '@typescript-eslint/no-wrapper-object-types': 'off', // TODO: Enable this rule
+    '@typescript-eslint/no-unsafe-function-type': 'off', // TODO: Enable this rule
+    '@typescript-eslint/no-unused-expressions': 'off', // TODO: Enable this rule
+    '@typescript-eslint/no-unused-vars': 'off', // TODO: Enable this rule
 
-    'simple-import-sort/imports': ERROR,
-    '@typescript-eslint/no-use-before-define': OFF(TYPESCRIPT_CONVERSION),
-    '@typescript-eslint/no-explicit-any': OFF(TYPESCRIPT_CONVERSION),
+    'simple-import-sort/imports': 'error',
+    '@typescript-eslint/no-use-before-define': 'off', // TODO: Enable this rule
+    '@typescript-eslint/no-explicit-any': 'off', // TODO: Enable this rule
   },
 };
