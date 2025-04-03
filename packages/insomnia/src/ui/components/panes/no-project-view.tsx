d2c3a86cdd1ef9d13393ee6284fc181e20719d71
Pavlos Koutoglou <pkoutoglou@gmail.com>
2025-04-03 03:09:54 +0200

    feat: Add NoProjectView component to guide users in creating new projects

---
+++ packages/insomnia/src/ui/components/panes/no-project-view.tsx
@@ -0,0 +1,23 @@
+import type { FC } from 'react';
+import React from 'react';
+
+import { ORG_STORAGE_RULE } from '../../routes/organization';
+import { ProjectSettingsForm } from '../project/project-settings-form';
+
+interface Props {
+  storageRule: ORG_STORAGE_RULE;
+  isGitSyncEnabled: boolean;
+}
+
+export const NoProjectView: FC<Props> = ({
+  storageRule,
+  isGitSyncEnabled,
+}) => {
+  return (
+    <div className='flex flex-col items-center pt-[15%] w-full h-full text-center gap-3 opacity-[calc(var(--opacity-subtle)*0.8)]'>
+      <span className='font-semibold text-xl'>Welcome to your organization!</span>
+      <span className='text-md'>Create a new project to get started</span>
+      <ProjectSettingsForm storageRule={storageRule} isGitSyncEnabled={isGitSyncEnabled} defaultProjectName="My first project" />
+    </div>
+  );
+};
