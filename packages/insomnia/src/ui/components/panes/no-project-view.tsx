16ea543d5f7d0177e850dfc1c824082d288876e5
Pavlos Koutoglou <pkoutoglou@gmail.com>
2025-04-10 22:01:06 +0200

    refactor: Update NoProjectView to use StorageRules type and adjust ProjectSettingsForm prop

--- packages/insomnia/src/ui/components/panes/no-project-view.tsx
+++ packages/insomnia/src/ui/components/panes/no-project-view.tsx
@@ -1,23 +1,23 @@
 import type { FC } from 'react';
 import React from 'react';
 
-import type { ORG_STORAGE_RULE } from '../../routes/organization';
 import { ProjectSettingsForm } from '../project/project-settings-form';
+import type { StorageRules } from '../../routes/organization';
 
 interface Props {
-  storageRule: ORG_STORAGE_RULE;
+  storageRules: StorageRules;
   isGitSyncEnabled: boolean;
 }
 
 export const NoProjectView: FC<Props> = ({
-  storageRule,
+  storageRules,
   isGitSyncEnabled,
 }) => {
   return (
     <div className='flex flex-col items-center pt-[15%] w-full h-full text-center gap-3'>
       <span className='font-semibold text-xl'>Welcome to your organization!</span>
       <span className='text-md'>Create a new project to get started</span>
-      <ProjectSettingsForm storageRule={storageRule} isGitSyncEnabled={isGitSyncEnabled} defaultProjectName="My first project" />
+      <ProjectSettingsForm storageRules={storageRules} isGitSyncEnabled={isGitSyncEnabled} defaultProjectName="My first project" />
     </div>
   );
 };
