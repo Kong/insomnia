ae45255bac1396b30ce285ea8637983d5536046b
George He <hexxa@outlook.com>
2025-04-15 17:23:46 +0800

    docs(sdk): add docs for collection

--- packages/insomnia-scripting-environment/src/objects/collection.ts
+++ packages/insomnia-scripting-environment/src/objects/collection.ts
@@ -1,3 +1,18 @@
+
+/**
+ * This module exports various types and interfaces related to the Insomnia scripting environment.
+ *
+ * - `RequestAuth`: Represents authentication details for a request.
+ * - `Certificate`: Represents SSL/TLS certificate settings details.
+ * - `Cookie`, `CookieList`: Represents HTTP cookies and a list of cookies.
+ * - `Header`, `HeaderList`: Represents HTTP headers and a list of headers.
+ * - `Property`, `PropertyBase`, `PropertyList`: Represents generic properties, their base structure, and a list of properties.
+ * - `ProxyConfig`, `ProxyConfigList`: Represents proxy configuration details and a list of proxy configurations.
+ * - `FormParam`, `Request`, `RequestBody`: Represents form parameters, HTTP requests, and request bodies.
+ * - `Response`: Represents an HTTP response.
+ * - `QueryParam`, `Url`, `UrlMatchPattern`, `UrlMatchPatternList`: Represents query parameters, URLs, URL match patterns, and a list of URL match patterns.
+ * - `Variable`, `VariableList`: Represents variables and a list of variables.
+ * ```*/
 export { RequestAuth } from './auth';
 export { Certificate } from './certificates';
 export { Cookie, CookieList } from './cookies';
