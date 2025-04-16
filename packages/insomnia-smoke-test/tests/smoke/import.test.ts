9d38dcbe3703ecddda3b0d0c56431d8a60e78c9c
yaoweiprc <6896642+yaoweiprc@users.noreply.github.com>
2025-04-16 15:27:22 +0800

    Generate content-type header for imported requests [INS-5281] (#8610)
    * generate content-type header for imported requests. update playwright version
    
    * update vitest snapshot

--- packages/insomnia-smoke-test/tests/smoke/import.test.ts
+++ packages/insomnia-smoke-test/tests/smoke/import.test.ts
@@ -3,8 +3,8 @@ import { expect } from '@playwright/test';
 import { loadFixture } from '../../playwright/paths';
 import { test } from '../../playwright/test';
 
-test('can import multiple workspaces from single file', async ({ app, page }) => {
-  const text = await loadFixture('multiple-workspaces.yaml');
+test('Can import multiple workspaces from single file', async ({ app, page }) => {
+  const text = await loadFixture('import/multiple-workspaces.yaml');
   await app.evaluate(async ({ clipboard }, text) => clipboard.writeText(text), text);
   await page.getByLabel('Import').click();
   await page.locator('[data-test-id="import-from-clipboard"]').click();
@@ -13,4 +13,18 @@ test('can import multiple workspaces from single file', async ({ app, page }) =>
   // Have two collections in current project
   await expect(page.getByLabel('Collection 1')).toBeAttached();
   await expect(page.getByLabel('Collection 2')).toBeAttached();
+});
+
+test('Can generate content-type header from imported postman file', async ({ app, page }) => {
+  const text = await loadFixture('import/import-content-type-from-postman.json');
+  await app.evaluate(async ({ clipboard }, text) => clipboard.writeText(text), text);
+  await page.getByLabel('Import').click();
+  await page.locator('[data-test-id="import-from-clipboard"]').click();
+  await page.getByRole('button', { name: 'Scan' }).click();
+  await page.getByRole('dialog').getByRole('button', { name: 'Import' }).click();
+  // Have generated content-type of application/x-www-form-urlencoded for the request
+  await page.getByRole('link', { name: 'New Collection' }).click();
+  await page.getByTestId('New Request').click();
+  await page.locator('[data-key="headers"]').click();
+  await expect.soft(page.getByText('application/x-www-form-urlencoded')).toBeAttached();
 });
