21753352642d14806186fc8ca37582daf201701c
George He <hexxa@outlook.com>
2025-04-17 10:50:52 +0800

    chore: update links

--- packages/insomnia-scripting-environment/src/objects/collection.ts
+++ packages/insomnia-scripting-environment/src/objects/collection.ts
@@ -2,22 +2,22 @@
  *
  * This module exports various types and interfaces related to the Insomnia scripting environment.
  *
- * - `RequestAuth`: Represents authentication details for a request.
- * - `Certificate`: Represents SSL/TLS certificate settings details.
- * - `Cookie`, `CookieList`: Represents HTTP cookies and a list of cookies.
- * - `Header`, `HeaderList`: Represents HTTP headers and a list of headers.
- * - `Property`, `PropertyBase`, `PropertyList`: Represents generic properties, their base structure, and a list of properties.
- * - `ProxyConfig`, `ProxyConfigList`: Represents proxy configuration details and a list of proxy configurations.
- * - `FormParam`, `Request`, `RequestBody`: Represents form parameters, HTTP requests, and request bodies.
- * - `Response`: Represents an HTTP response.
- * - `QueryParam`, `Url`, `UrlMatchPattern`, `UrlMatchPatternList`: Represents query parameters, URLs, URL match patterns, and a list of URL match patterns.
- * - `Variable`, `VariableList`: Represents variables and a list of variables.
+ * - {@link RequestAuth}: Represents authentication details for a request.
+ * - {@link Certificate}: Represents SSL/TLS certificate settings details.
+ * - {@link Cookie}, {@link CookieList} : Represents HTTP cookies and a list of cookies.
+ * - {@link Header}, {@link HeaderList} : Represents HTTP headers and a list of headers.
+ * - {@link Property}, {@link PropertyBase}, {@link PropertyList} : Represents generic properties, their base structure, and a list of properties.
+ * - {@link ProxyConfig}, {@link ProxyConfigList} : Represents proxy configuration details and a list of proxy configurations.
+ * - {@link FormParam}, {@link Request}, {@link RequestBody} : Represents form parameters, HTTP requests, and request bodies.
+ * - {@link Response}: Represents an HTTP response.
+ * - {@link QueryParam}, {@link Url}, {@link UrlMatchPattern}, {@link UrlMatchPatternList} : Represents query parameters, URLs, URL match patterns, and a list of URL match patterns.
+ * - {@link Variable}, {@link VariableList} : Represents variables and a list of variables.
  *
  * @example
  * ```javascript
- * const uuid = require('uuid');
- * insomnia.environment.set('user_id', uuid.v4());
- * console.log(insomnia.environment.get('user_id'));
+ * const { Cookie } = require("insomnia-collection");
+ * const cookie = new Cookie({ key: "expire", value: "1024" });
+ * console.log(cookie);
  * ```
  * @module
  **/
