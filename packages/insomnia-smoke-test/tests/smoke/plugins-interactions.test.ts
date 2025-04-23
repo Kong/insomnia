575cdab12036cd15103d695a1aada9f25767a0b5
Pavlos Koutoglou <pkoutoglou@gmail.com>
2025-04-23 21:50:10 +0200

    fix: Correct import order in plugins interactions test

--- packages/insomnia-smoke-test/tests/smoke/plugins-interactions.test.ts
+++ packages/insomnia-smoke-test/tests/smoke/plugins-interactions.test.ts
@@ -1,5 +1,5 @@
-import { test } from '../../playwright/test';
 import { expect } from '@playwright/test';
+import { test } from '../../playwright/test';
 
 test('Plugins', async ({ page }) => {
   // Opening settings
