19eaa949fd63ccb638fe7a465a1e022d59d9a1d8
Kent Wang <kent.wang@konghq.com>
2025-05-12 22:23:26 +0800

    Support auto restart electron when main processor related code changes

--- packages/insomnia/esbuild.main.ts
+++ packages/insomnia/esbuild.main.ts
@@ -1,18 +1,21 @@
+import { type ChildProcess, spawn } from 'node:child_process';
 import { builtinModules } from 'node:module';
 import path from 'node:path';
 
-import esbuild from 'esbuild';
+import esbuild, { type BuildOptions, type Plugin } from 'esbuild';
 
 import pkg from './package.json';
 
 interface Options {
   mode?: 'development' | 'production';
+  watch?: boolean;
 }
 
 export default async function build(options: Options) {
   const mode = options.mode || 'production';
   const __DEV__ = mode !== 'production';
   const PORT = pkg.dev['dev-server-port'];
+  const watch = options.watch || false;
 
   const outdir = __DEV__ ? path.join(__dirname, 'src') : path.join(__dirname, 'build');
 
@@ -29,7 +32,8 @@ export default async function build(options: Options) {
         'process.env.INSOMNIA_ENV': JSON.stringify('production'),
         'process.env.BUILD_DATE': JSON.stringify(new Date()),
       };
-  const preload = esbuild.build({
+
+  const preloadBuildOptions: BuildOptions = {
     entryPoints: ['./src/preload.ts'],
     outfile: path.join(outdir, 'preload.js'),
     target: 'esnext',
@@ -38,9 +42,9 @@ export default async function build(options: Options) {
     sourcemap: true,
     format: 'cjs',
     external: ['electron'],
-  });
+  };
 
-  const hiddenBrowserWindowPreload = esbuild.build({
+  const hiddenBrowserWindowPreloadBuildOptions: BuildOptions = {
     entryPoints: ['./src/hidden-window-preload.ts'],
     outfile: path.join(outdir, 'hidden-window-preload.js'),
     target: 'esnext',
@@ -52,9 +56,9 @@ export default async function build(options: Options) {
     loader: {
       '.node': 'copy',
     },
-  });
+  };
 
-  const main = esbuild.build({
+  const mainBuildOptions: BuildOptions = {
     entryPoints: ['./src/main.development.ts'],
     outfile: path.join(outdir, 'main.min.js'),
     bundle: true,
@@ -69,9 +73,77 @@ export default async function build(options: Options) {
       ...Object.keys(pkg.dependencies),
       ...Object.keys(builtinModules),
     ],
-  });
+  };
 
-  return Promise.all([main, preload, hiddenBrowserWindowPreload]);
+  if (__DEV__ && watch) {
+    // build script with auto reload
+    console.log('[Dev Build] Watching for main process changes...');
+    let buildCount = 0;
+    let electronProcess: ChildProcess;
+    const restartElectronPlugin = (scriptName: string): Plugin => ({
+      name: 'restart-electron',
+      setup: build => {
+        build.onStart(() => {
+          console.log(`[Dev Build] Detecting changes, rebuild ${scriptName}`);
+        });
+        build.onEnd(() => {
+          buildCount++;
+          if (buildCount > 3) {
+            console.log(`[Dev Build] Finish rebuilding ${scriptName}, restarting Electron`);
+            restartElectronProcess();
+          } else {
+            console.log(`[Dev Build] Skip restarting Electron for ${scriptName} since it is the first rebuild`);
+          }
+        });
+      },
+    });
+    const preloadContext = await esbuild.context({
+      ...preloadBuildOptions,
+      plugins: [restartElectronPlugin('preload')],
+    });
+    const mainContext = await esbuild.context({
+      ...mainBuildOptions,
+      plugins: [restartElectronPlugin('main')],
+    });
+    const hiddenPreloadContext = await esbuild.context({
+      ...hiddenBrowserWindowPreloadBuildOptions,
+      plugins: [restartElectronPlugin('hidden-browser-window-preload')],
+    });
+
+    const startElectron = () => {
+      electronProcess = spawn('electron', ['--inspect=5858', '.'], {
+        stdio: 'inherit',
+        env: process.env,
+        shell: true,
+      });
+    };
+
+    const restartElectronProcess = () => {
+      console.log('[Dev Build] Start restarting Electron');
+
+      if (electronProcess) {
+        electronProcess.once('exit', () => {
+          console.log('[Dev Build] Electron exited');
+          startElectron();
+        });
+
+        //Shutdown electron first. Existing debugger inspector will be closed in quit event of the app.
+        electronProcess.kill();
+      }
+    };
+
+    startElectron();
+    const preloadWatch = await preloadContext.watch();
+    const mainWatch = await mainContext.watch();
+    const hiddenWindowWatch = await hiddenPreloadContext.watch();
+    return Promise.all([preloadWatch, mainWatch, hiddenWindowWatch]);
+  }
+  const preload = esbuild.build(preloadBuildOptions);
+  const hiddenBrowserWindowPreload = esbuild.build(hiddenBrowserWindowPreloadBuildOptions);
+  const main = esbuild.build(mainBuildOptions);
+  return Promise.all([main, preload, hiddenBrowserWindowPreload]).catch(err => {
+    console.error('[Build] Build failed:', err);
+  });
 }
 
 // Build if ran as a cli script
@@ -79,5 +151,6 @@ const isMain = require.main === module;
 
 if (isMain) {
   const mode = process.env.NODE_ENV === 'development' ? 'development' : 'production';
-  build({ mode });
+  const watch = process.argv.includes('--watch');
+  build({ mode, watch });
 }
