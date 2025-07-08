4e462f1c66cd93c42f34f215fcd2fb5928a8e637
Pavlos Koutoglou <pkoutoglou@gmail.com>
2025-07-08 16:28:45 +0200

    feat(CloudSyncProjectBar): add CloudSyncProjectBar component for cloud synchronization indication

---
+++ packages/insomnia/src/ui/components/dropdowns/cloud-sync-project-bar.tsx
@@ -0,0 +1,27 @@
+import type { FC } from 'react';
+import React from 'react';
+import { Button, Tooltip, TooltipTrigger } from 'react-aria-components';
+
+import { Icon } from '../icon';
+
+export const CloudSyncProjectBar: FC = () => {
+  return (
+    <div className="flex h-[--line-height-sm] w-full items-center justify-between px-[--padding-md] text-sm text-[--color-font] ring-1 ring-transparent transition-all">
+      <div className="flex items-center gap-2">
+        <Icon icon="earth-americas" />
+        <span>Securely synced with Insomnia</span>
+      </div>
+      <TooltipTrigger delay={0}>
+        <Button className="cursor-default">
+          <Icon icon="question-circle" />
+        </Button>
+        <Tooltip
+          offset={8}
+          className="max-h-[85vh] max-w-xs select-none overflow-y-auto rounded-md border border-solid border-[--hl-sm] bg-[--color-bg] px-4 py-2 text-sm text-[--color-font] shadow-lg focus:outline-none"
+        >
+          Encrypted and synced securely to the cloud. Ideal for out of the box collaboration.
+        </Tooltip>
+      </TooltipTrigger>
+    </div>
+  );
+};
