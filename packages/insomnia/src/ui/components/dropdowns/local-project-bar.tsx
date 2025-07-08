c13de5ff9330828ca372208f16fd7db768f9122c
Pavlos Koutoglou <pkoutoglou@gmail.com>
2025-07-08 16:28:53 +0200

    fix(LocalProjectBar): remove unnecessary class from project text span

--- packages/insomnia/src/ui/components/dropdowns/local-project-bar.tsx
+++ packages/insomnia/src/ui/components/dropdowns/local-project-bar.tsx
@@ -9,7 +9,7 @@ export const LocalProjectBar: FC = () => {
     <div className="flex h-[--line-height-sm] w-full items-center justify-between px-[--padding-md] text-sm text-[--color-font] ring-1 ring-transparent transition-all">
       <div className="flex items-center gap-2">
         <Icon icon="laptop" />
-        <span className="local-project-bar__text">Local-only project</span>
+        <span>Local-only project</span>
       </div>
       <TooltipTrigger delay={0}>
         <Button className="cursor-default">
