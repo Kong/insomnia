9b7d06528d773f17a2765fe7dd0874f605e2c7dc
jackkav <jackkav@gmail.com>
2025-07-15 10:30:51 +0200

    fix process env

--- packages/insomnia/send-request/electron/index.js
+++ packages/insomnia/send-request/electron/index.js
@@ -1,8 +1,15 @@
 // This file implements just enough of the electron module to get sending requests to work
 module.exports = {
   app: {
-    getPath: (/** @type {string} */ name) =>
-      name === 'temp' ? require('os').tmpdir() : require('path').join(require('os').tmpdir(), 'insomnia-send-request'),
+    getPath: (/** @type {string} */ name) => {
+      if (name === 'temp') {
+        return require('os').tmpdir();
+      }
+      if (name === 'userData') {
+        return process.env.INSOMNIA_DATA_PATH || require('path').join(require('os').tmpdir(), 'insomnia-send-request');
+      }
+      throw new Error(`Unknown app path: ${name}`);
+    }
   },
   ipcMain: {
     on: () => {
