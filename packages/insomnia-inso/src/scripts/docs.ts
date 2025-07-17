d487318f4b08153e308d6645b248f544e7400c52
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2025-07-17 10:53:51 -0300

    Fix linting issues

--- packages/insomnia-inso/src/scripts/docs.ts
+++ packages/insomnia-inso/src/scripts/docs.ts
@@ -1,7 +1,7 @@
+import fs from 'node:fs';
 import path from 'node:path';
 
-import * as commander from 'commander';
-import fs from 'fs';
+import type * as commander from 'commander';
 
 import { version } from '../../package.json';
 
