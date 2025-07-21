4425c155ed7613a69a481bfb9a93146d5f5db4b6
Kent Wang <kent.wang@konghq.com>
2025-07-21 14:56:39 +0800

    fix github actions

--- packages/insomnia/bin/yarn-standalone.js
+++ packages/insomnia/bin/yarn-standalone.js
@@ -94521,10 +94521,8 @@ function envReplace(value, env = process.env) {
     if (esc.length && esc.length % 2) {
       return match;
     }
-    console.log(JSON.stringify(env));
-    console.log(`envVarName: ${envVarName} match ${match}`);
     if (undefined === env[envVarName]) {
-      throw new Error('Failed to replace env in config: ' + match);
+      throw new Error('Failed to replace env in config: ' + match + JSON.stringify(env) + `envVarName: ${envVarName}`);
     }
     return env[envVarName] || '';
   });
