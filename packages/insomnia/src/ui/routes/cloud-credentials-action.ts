5a98da240193564fb00cad8969df85911faf2ae5
Curry Yang <1019yanglu@gmail.com>
2025-07-24 17:23:47 +0800

    feat: add cloud credential action

---
+++ packages/insomnia/src/ui/routes/cloud-credentials-action.ts
@@ -0,0 +1,80 @@
+import type { ActionFunction } from 'react-router';
+
+import * as models from '../../models';
+import type { BaseCloudCredential } from '../../models/cloud-credential';
+import { invariant } from '../../utils/invariant';
+
+export const createCloudCredentialAction: ActionFunction = async ({ request }) => {
+  const patch = await request.json();
+  const { name, provider, credentials, isAuthenticated } = patch as BaseCloudCredential & { isAuthenticated?: boolean };
+  invariant(typeof name === 'string', 'Name is required');
+  invariant(provider, 'Cloud Provier name is required');
+  if (name && provider && credentials) {
+    if (isAuthenticated) {
+      // find credential with same name for oauth authenticated cloud service
+      const existingCredential = await models.cloudCredential.getByName(name, provider);
+      if (existingCredential.length === 0) {
+        await models.cloudCredential.create(patch);
+      } else {
+        await models.cloudCredential.update(existingCredential[0], patch);
+      }
+      return credentials;
+    }
+    const authenciateResponse = await window.main.cloudService.authenticate({ provider, credentials });
+    const { success, error, result } = authenciateResponse!;
+    if (success) {
+      if (provider === 'hashicorp') {
+        // update access token and expires_at
+        const { access_token, expires_at } = result as { access_token: string; expires_at: number };
+        patch.credentials['access_token'] = access_token;
+        patch.credentials['expires_at'] = expires_at;
+      }
+      await models.cloudCredential.create(patch);
+    } else {
+      return {
+        error: error?.errorMessage,
+      };
+    }
+    return result;
+  }
+  return { error: 'Invalid paramters for creating cloud credential' };
+};
+
+export const updateCloudCredentialAction: ActionFunction = async ({ request, params }) => {
+  const { cloudCredentialId } = params;
+  invariant(typeof cloudCredentialId === 'string', 'Credential ID is required');
+  const patch = await request.json();
+  const { name, provider, credentials } = patch;
+  invariant(typeof name === 'string', 'Name is required');
+  invariant(provider, 'Cloud Provier name is required');
+  if (name && provider && credentials) {
+    const authenciateResponse = await window.main.cloudService.authenticate({ provider, credentials });
+    const { success, error, result } = authenciateResponse!;
+    if (success) {
+      const originCredential = await models.cloudCredential.getById(cloudCredentialId);
+      invariant(originCredential, 'No Cloud Credential found');
+      if (provider === 'hashicorp') {
+        // update access token and expires_at
+        const { access_token, expires_at } = result as { access_token: string; expires_at: number };
+        patch.credentials['access_token'] = access_token;
+        patch.credentials['expires_at'] = expires_at;
+      }
+      await models.cloudCredential.update(originCredential, patch);
+    } else {
+      return {
+        error: error?.errorMessage,
+      };
+    }
+    return result;
+  }
+  return { error: 'Invalid paramters for updating cloud credential' };
+};
+
+export const deleteCloudCredentialAction: ActionFunction = async ({ params }) => {
+  const { cloudCredentialId } = params;
+  invariant(typeof cloudCredentialId === 'string', 'Cloud Credential ID is required');
+  const cloudCredential = await models.cloudCredential.getById(cloudCredentialId);
+  invariant(cloudCredential, 'Cloud Credential not found');
+  await models.cloudCredential.remove(cloudCredential);
+  return null;
+};
