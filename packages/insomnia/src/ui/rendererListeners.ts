9206bf12dbd127b96496f3a73cb5876b5f898440
jackkav <jackkav@gmail.com>
2025-08-19 15:23:49 +0200

    remove db file delete widgets

--- packages/insomnia/src/ui/rendererListeners.ts
+++ packages/insomnia/src/ui/rendererListeners.ts
@@ -1,71 +1,13 @@
-import { isDevelopment } from '../common/constants';
-import { database } from '../common/database';
 import * as models from '../models';
 import * as plugins from '../plugins';
 import * as themes from '../plugins/misc';
 import * as templating from '../templating';
 import { showModal } from './components/modals';
-import { AskModal } from './components/modals/ask-modal';
-import { SelectModal } from './components/modals/select-modal';
 import { SettingsModal, TAB_INDEX_SHORTCUTS } from './components/modals/settings-modal';
 
 window.main.on('toggle-preferences', () => {
   showModal(SettingsModal);
 });
-
-if (isDevelopment()) {
-  window.main.on('clear-model', () => {
-    const options = models
-      .types()
-      .filter(t => t !== models.settings.type) // don't clear settings
-      .map(t => ({ name: t, value: t }));
-
-    showModal(SelectModal, {
-      title: 'Clear a model',
-      message: 'Select a model to clear; this operation cannot be undone.',
-      value: options[0].value,
-      options,
-      onDone: async (type: string | null) => {
-        if (!type || !models.isValidType(type)) {
-          console.error(`[developer] invalid model type: ${type}`);
-          return;
-        }
-        const bufferId = await database.bufferChanges();
-        console.log(`[developer] clearing all "${type}" entities`);
-        const allEntities = await database.find(type);
-        // TODO: this could be done with a removeMany
-        await database.batchModifyDocs({ remove: allEntities });
-        database.flushChanges(bufferId);
-      },
-    });
-  });
-
-  window.main.on('clear-all-models', () => {
-    showModal(AskModal, {
-      title: 'Clear all models',
-      message: 'Are you sure you want to clear all models? This operation cannot be undone.',
-      yesText: 'Yes',
-      noText: 'No',
-      onDone: async (yes: boolean) => {
-        if (yes) {
-          const bufferId = await database.bufferChanges();
-          const promises = models
-            .types()
-            .filter(t => t !== models.settings.type) // don't clear settings
-            .reverse()
-            .map(async type => {
-              console.log(`[developer] clearing all "${type}" entities`);
-              const allEntities = await database.find(type);
-              // TODO: this could be done with a removeMany
-              await database.batchModifyDocs({ remove: allEntities });
-            });
-          await Promise.all(promises);
-          database.flushChanges(bufferId);
-        }
-      },
-    });
-  });
-}
 
 window.main.on('reload-plugins', async () => {
   const settings = await models.settings.get();
