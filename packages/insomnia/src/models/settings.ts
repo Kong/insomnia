53cc3cb8f916ac285ce8c5eb9b27ee7ce3f2f5ee
David <43302190+SpechtD@users.noreply.github.com>
2025-09-03 15:05:27 +0000

    fix: missing comma

--- packages/insomnia/src/models/settings.ts
+++ packages/insomnia/src/models/settings.ts
@@ -73,7 +73,7 @@ export function init(): BaseSettings {
     saveVaultKeyLocally: true,
     enableVaultInScripts: false,
     saveVaultKeyToOSSecretManager: true,
-    importOpenApiPathParamsAsVars: true
+    importOpenApiPathParamsAsVars: true,
     // The duration in mins for which the external vault secret is cached
     vaultSecretCacheDuration: 30,
     dataFolders: [],
