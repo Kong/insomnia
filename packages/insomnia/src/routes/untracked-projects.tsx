2967ec36df85671ef8ae0f69b1dbba84d90be294
Bingbing <ZxBing0066@gmail.com>
2025-09-10 13:58:11 +0800

    fix

--- packages/insomnia/src/routes/untracked-projects.tsx
+++ packages/insomnia/src/routes/untracked-projects.tsx
@@ -1,6 +1,6 @@
 import { database } from '~/common/database';
 import { userSession } from '~/models';
-import { type Organization, SCRATCHPAD_ORGANIZATION_ID } from '~/models/organization';
+import { type Organization } from '~/models/organization';
 import type { Project } from '~/models/project';
 import type { Workspace } from '~/models/workspace';
 import { createFetcherLoadHook } from '~/utils/router';
