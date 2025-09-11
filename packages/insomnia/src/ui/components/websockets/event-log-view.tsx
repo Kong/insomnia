31dcf83da1d0f8eb451fe4a0e5834b903fbddcf5
Kent Wang <kent.wang@konghq.com>
2025-09-11 16:19:16 +0800

    feat: Add basic response pane for MCP (#9131)
    * New List page
    * Add a common utility function
    * Add response pane ui
    * Support events and logs

--- packages/insomnia/src/ui/components/websockets/event-log-view.tsx
+++ packages/insomnia/src/ui/components/websockets/event-log-view.tsx
@@ -4,26 +4,30 @@ import React, { type FC, useEffect, useRef } from 'react';
 import { Cell, Column, Row, Table, TableBody, TableHeader } from 'react-aria-components';
 
 import type { CurlEvent } from '../../../main/network/curl';
+import type { McpEvent } from '../../../main/network/mcp';
 import type { SocketIOEvent } from '../../../main/network/socket-io';
 import type { WebSocketEvent } from '../../../main/network/websocket';
 import { type IconId, SvgIcon } from '../svg-icon';
 
+type EventTypes = WebSocketEvent | CurlEvent | SocketIOEvent | McpEvent;
 const Timestamp: FC<{ time: Date | number }> = ({ time }) => {
   const date = format(time, 'HH:mm:ss');
   return <>{date}</>;
 };
 
 interface Props {
-  events: (WebSocketEvent | CurlEvent | SocketIOEvent)[];
+  events: EventTypes[];
   selectionId?: string;
-  onSelect: (event: WebSocketEvent | CurlEvent | SocketIOEvent) => void;
+  onSelect: (event: EventTypes) => void;
 }
 
-const isSocketIOEvent = (event: WebSocketEvent | CurlEvent | SocketIOEvent): event is SocketIOEvent => {
+const isSocketIOEvent = (event: EventTypes): event is SocketIOEvent => {
   return 'eventName' in event && typeof event.eventName === 'string';
 };
 
-function getIcon(event: WebSocketEvent | CurlEvent | SocketIOEvent): IconId {
+const isMcpEvent = (event: EventTypes): event is McpEvent => event._id.toString().startsWith('mcp-');
+
+function getIcon(event: EventTypes): IconId {
   switch (event.type) {
     case 'message': {
       if (event.direction === 'OUTGOING') {
@@ -55,7 +59,7 @@ function getIcon(event: WebSocketEvent | CurlEvent | SocketIOEvent): IconId {
   }
 }
 
-const getMessage = (event: WebSocketEvent | CurlEvent | SocketIOEvent): string | JSX.Element => {
+const getMessage = (event: EventTypes): string | JSX.Element => {
   switch (event.type) {
     case 'message': {
       if (isSocketIOEvent(event)) {
@@ -70,6 +74,11 @@ const getMessage = (event: WebSocketEvent | CurlEvent | SocketIOEvent): string |
             )}
           </div>
         );
+      }
+      if (isMcpEvent(event)) {
+        if ('method' in event) {
+          return <pre className="whitespace-pre-wrap">{event.method}</pre>;
+        }
       }
       if ('data' in event && typeof event.data === 'object') {
         return 'Binary data';
