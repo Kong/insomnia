06bea7fad86f19497666ae7d7ca75daac8b3f5cf
Kent Wang <kent.wang@konghq.com>
2025-09-24 11:12:32 +0800

    feat: Change observer pattern for mcp events and enable auto selection (#9165)
    * change polling method
    * auto activate latest event for mcp
    * support showing error details for mcp

--- packages/insomnia/src/common/constants.ts
+++ packages/insomnia/src/common/constants.ts
@@ -552,3 +552,9 @@ export const RESPONSE_CODE_REASONS: Record<number, string> = {
 
 // (ms) curently server timeout is 30s
 export const INSOMNIA_FETCH_TIME_OUT = 30_000;
+
+// channel names for real time events (websocket/socket-io/mcp)
+export const REALTIME_EVENTS_CHANNELS = {
+  READY_STATE: 'readyState',
+  NEW_EVENT: 'newEventReceived',
+};
