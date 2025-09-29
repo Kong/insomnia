04eb342a9dca36fa2efa22b7d4c93f773a715680
Ryan Willis <ryan.willis@konghq.com>
2025-09-29 12:17:11 -0700

    fix openpath type send method

--- packages/insomnia/src/entry.preload.ts
+++ packages/insomnia/src/entry.preload.ts
@@ -195,7 +195,7 @@ const app: Window['app'] = {
 };
 const shell: Window['shell'] = {
   showItemInFolder: options => ipcRenderer.send('showItemInFolder', options),
-  openPath: options => ipcRenderer.send('openPath', options),
+  openPath: options => ipcRenderer.sendSync('openPath', options),
 };
 const clipboard: Window['clipboard'] = {
   readText: () => ipcRenderer.sendSync('readText'),
