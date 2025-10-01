a3baba5a46265156ee476752325efdf849c11b41
shelby-moore <13246465+shelby-moore@users.noreply.github.com>
2025-10-01 11:23:56 -0700

    revert unnecessary change

--- packages/insomnia/vite.config.ts
+++ packages/insomnia/vite.config.ts
@@ -92,11 +92,11 @@ export default defineConfig(({ mode }) => {
       target: 'esnext',
       sourcemap: true,
       rollupOptions: {
-        external: ['@getinsomnia/node-libcurl', '@kong-insomnia/insomnia-plugin-ai'],
+        external: ['@getinsomnia/node-libcurl'],
       },
     },
     optimizeDeps: {
-      exclude: ['@getinsomnia/node-libcurl', '@kong-insomnia/insomnia-plugin-ai'],
+      exclude: ['@getinsomnia/node-libcurl'],
       force: true, // wipe vite cache
       include: ['codemirror-graphql/utils/SchemaReference', '@stoplight/spectral-core', 'isomorphic-git'],
     },
