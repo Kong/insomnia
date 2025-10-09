b7d4dff6d752c407a4635cbc8ef32a3143a72c1f
Kent Wang <kent.wang@konghq.com>
2025-10-09 14:35:10 +0800

    fix issues

--- packages/insomnia/src/ui/components/mcp/mcp-request-pane.tsx
+++ packages/insomnia/src/ui/components/mcp/mcp-request-pane.tsx
@@ -339,7 +339,7 @@ export const McpRequestPane: FC<Props> = ({
                 </div>
               </Panel>
               <PanelResizeHandle className="h-[1px] w-full bg-[--hl-md]" />
-              {selectedPrimitiveItem?.type === 'tools' && (
+              {selectedPrimitiveItem?.type !== 'resources' && selectedPrimitiveItem?.type !== 'resourceTemplates' && (
                 <Panel minSize={20}>
                   <div className="flex h-full flex-col">
                     <Heading className="p-4 text-xs font-bold text-[--hl]">Parameter Overview</Heading>
