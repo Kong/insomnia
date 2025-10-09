3cee6448ec1570f1249157a73944cb2670e9ffd1
Ryan Willis <ryan.willis@konghq.com>
2025-10-08 19:18:48 -0700

    fix: include mockgen script in build (#9222)

--- packages/insomnia/scripts/build.ts
+++ packages/insomnia/scripts/build.ts
@@ -44,6 +44,10 @@ export const start = async () => {
   await copyFiles('../src/static', path.join(buildFolder, 'static'));
   await copyFiles('../src/icons', buildFolder);
   await copyFiles('../src/main/lint-process.mjs', path.join(buildFolder, 'main/lint-process.mjs'));
+  await copyFiles(
+    '../src/main/mock-generation-process.mjs',
+    path.join(buildFolder, 'main/mock-generation-process.mjs'),
+  );
   await copyFiles('../src/hidden-window.html', path.join(buildFolder, 'hidden-window.html'));
 
   console.log('[build] Complete!');
