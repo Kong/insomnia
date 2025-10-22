bdb882db7c4f22069957fe3943f7f693097f1a22
Bingbing <ZxBing0066@gmail.com>
2025-10-22 16:42:10 +0800

    code clean

--- packages/insomnia/src/main/ipc/electron.ts
+++ packages/insomnia/src/main/ipc/electron.ts
@@ -176,8 +176,7 @@ export type RendererOnChannels =
   | 'toggle-sidebar'
   | 'show-oauth-authorization-modal'
   | 'hide-oauth-authorization-modal'
-  | 'mcp-auth-confirmation'
-  | 'updaterStatus';
+  | 'mcp-auth-confirmation';
 
 export const ipcMainOn = (
   channel: MainOnChannels,
