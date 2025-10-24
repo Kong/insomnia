725480ef5fa839130c3c845eaf1d293191154839
Curry Yang <1019yanglu@gmail.com>
2025-10-24 16:58:25 +0800

    feat: form component

---
+++ packages/insomnia/src/ui/components/base/input.tsx
@@ -0,0 +1,27 @@
+import cn from 'classnames';
+import type { TextFieldProps, ValidationResult } from 'react-aria-components';
+import { Input as RaInput, Label, TextField as RaTextField } from 'react-aria-components';
+
+interface CustomInputFieldProps extends TextFieldProps {
+  label?: string;
+  placeholder?: string;
+  errorMessage?: string | ((validation: ValidationResult) => string);
+}
+
+export const Input = ({ label, errorMessage, ...props }: CustomInputFieldProps) => {
+  return (
+    <RaTextField className="flex flex-col text-[--color-font]" {...props}>
+      {label && <Label className="mb-2 pt-0">{label}</Label>}
+      <RaInput
+        className={({ isFocused, isFocusVisible, isInvalid }) =>
+          cn('h-[30px] w-full rounded-sm border border-solid bg-[--color-bg] p-2', {
+            'border-[--hl-lg]': isFocused,
+            'ring-2 ring-[--hl-md] ring-offset-1': isFocusVisible,
+            'border-red-500': isInvalid,
+            'border-[--hl-sm]': !isFocused && !isInvalid,
+          })
+        }
+      />
+    </RaTextField>
+  );
+};
