0754d2536a42b685d59004d77c226d18224aff2b
jackkav <jackkav@gmail.com>
2025-10-29 16:55:05 +0100

    merge files

--- packages/insomnia/src/main/updates.ts
+++ packages/insomnia/src/main/updates.ts
@@ -2,6 +2,8 @@ import { spawn } from 'node:child_process';
 import path from 'node:path';
 
 import { app, autoUpdater, BrowserWindow, dialog } from 'electron';
+import log from 'electron-log';
+import { autoUpdater as electronUpdater } from 'electron-updater';
 
 import type { Settings } from '~/models/settings';
 
@@ -13,9 +15,8 @@ import { delay } from '../common/misc';
 import * as models from '../models/index';
 import { invariant } from '../utils/invariant';
 import { ipcMainOn } from './ipc/electron';
-import { initNsisUpdater, isNsisInstaller } from './nsisUpdate';
 
-export const isUpdateSupported = () => {
+const isUpdateSupported = () => {
   if (process.platform === 'linux') {
     console.log('[updater] Not supported on this platform', process.platform);
     showUpdateStatusToast('Updates disabled on linux');
@@ -53,7 +54,7 @@ export const getUpdateUrl = (updateChannel: string): string | null => {
   return fullUrl.toString();
 };
 
-export const showUpdateStatusToast = (title: string, description?: string) => {
+const showUpdateStatusToast = (title: string, description?: string) => {
   for (const window of BrowserWindow.getAllWindows()) {
     window.webContents.send('show-toast', {
       content: {
@@ -150,6 +151,75 @@ const createListeners = () => {
             windowsHide: true,
           });
           app.quit();
+        }
+      });
+  });
+};
+const isNsisInstaller = async () => {
+  if (process.platform !== 'win32') {
+    return false;
+  }
+  try {
+    const installDir = path.dirname(process.execPath);
+    // we inject this file(nsisInstall.nsh) during the NSIS build process to indicate the installer type
+    const flagFilePath = path.join(installDir, 'installer-info.json');
+
+    const content = await fs.promises.readFile(flagFilePath, 'utf-8');
+    const json = JSON.parse(content);
+    console.log('installer type', json.installer);
+    return json.installer === 'nsis';
+  } catch (err) {
+    console.warn('Failed to read installer-info.json:', err);
+    return false;
+  }
+};
+const initNsisUpdater = () => {
+  electronUpdater.logger = log;
+  electronUpdater.disableDifferentialDownload = true;
+  createNSISListeners();
+
+  return (settings: Settings) => {
+    try {
+      console.log(`[NSIS updater] Checking for updates`);
+      // set auto-update channel
+      electronUpdater.channel = settings.updateChannel;
+      electronUpdater.checkForUpdates();
+    } catch (err) {
+      console.warn('[NSIS updater] Failed to check for updates:', err.message);
+      showUpdateStatusToast('Update Error');
+    }
+  };
+};
+
+const createNSISListeners = () => {
+  electronUpdater.on('error', error => {
+    console.warn(`[updater] Error: ${error.message}`);
+    showUpdateStatusToast('Update Error', error.message);
+  });
+  electronUpdater.on('update-not-available', () => {
+    console.log('[updater] Not Available');
+    showUpdateStatusToast(`Up to Date`, packageJSON.version);
+  });
+  electronUpdater.on('update-available', () => {
+    console.log('[updater] Update Available');
+    showUpdateStatusToast('Downloading update...');
+  });
+  electronUpdater.on('update-downloaded', async ({ version }) => {
+    console.log(`[NSIS updater] Downloaded ${version}`);
+    showUpdateStatusToast('Performing backup...');
+    showUpdateStatusToast(`Downloaded ${version}`, 'Restart to apply the updates.');
+    // documented: https://www.electronjs.org/docs/latest/tutorial/updates#step-3-notifying-users-when-updates-are-available
+    dialog
+      .showMessageBox({
+        type: 'info',
+        buttons: ['Restart', 'Later'],
+        title: 'Application Update',
+        message: `New version: ${version}`,
+        detail: 'A new version of Insomnia has been downloaded. Restart the application to apply the updates.',
+      })
+      .then(returnValue => {
+        if (returnValue.response === 0) {
+          electronUpdater.quitAndInstall();
         }
       });
   });
