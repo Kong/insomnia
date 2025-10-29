dc8d3767fa391229dbd3c1b41a7a8bceb669f0ed
xiaodemen <fuq_pr@163.com>
2025-10-29 18:06:50 +0800

    fix: fix source

--- packages/insomnia/src/ui/components/upgrade-notice.tsx
+++ packages/insomnia/src/ui/components/upgrade-notice.tsx
@@ -19,9 +19,7 @@ export const UpgradeNotice = (props: UpgradeNoticeProps) => {
     : 'Please contact the organization owner to upgrade the plan.';
   const message = `${featureName} is only enabled for ${planDetail}.`;
   const handleUpgradePlan = () => {
-    window.main.openInBrowser(
-      `${getAppWebsiteBaseURL()}/app/subscription/update?plan=team&source=app_feature_cloud_credentials`,
-    );
+    window.main.openInBrowser(`${getAppWebsiteBaseURL()}/app/subscription/update?plan=team&source=app_${featureName}`);
   };
 
   return (
