a774ffb98e101a9aae79beef3dd3b916961cc9e9
yaoweiprc <6896642+yaoweiprc@users.noreply.github.com>
2025-10-29 16:08:27 +0800

    fix type

--- packages/insomnia/src/ui/hooks/theme.ts
+++ packages/insomnia/src/ui/hooks/theme.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import * as reactUse from 'react-use';
 
 import { useRootLoaderData } from '~/root';
