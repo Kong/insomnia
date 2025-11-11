d9c0f8082678b6c96e590129c86087c7829b877b
Kent Wang <kent.wang@konghq.com>
2025-11-11 15:38:20 +0800

    initial check-in

---
+++ packages/insomnia-component-docs/docusaurus.config.ts
@@ -0,0 +1,80 @@
+import type * as Preset from '@docusaurus/preset-classic';
+import type { Config } from '@docusaurus/types';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'Insomnia Component Docs',
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: 'https://insomnia.rest/',
+  baseUrl: '/',
+
+  onBrokenLinks: 'throw',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+        },
+        theme: {
+          customCss: ['./src/css/default.css'],
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  plugins: [
+    async function myPlugin(_context, _options) {
+      return {
+        name: 'docusaurus-tailwindcss',
+        configurePostCss(postcssOptions) {
+          // Appends TailwindCSS and AutoPrefixer.
+          postcssOptions.plugins.push(require('tailwindcss'));
+          postcssOptions.plugins.push(require('autoprefixer'));
+          return postcssOptions;
+        },
+      };
+    },
+  ],
+  themeConfig: {
+    colorMode: {
+      defaultMode: 'dark',
+    },
+    // Replace with your project's social card
+    navbar: {
+      title: 'Insomnia',
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Components',
+        },
+        {
+          href: 'https://github.com/kong/insomnia',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {},
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
