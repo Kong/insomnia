e13771a45fd59c99a8342a6bcf913899622314d7
Kent Wang <kent.wang@konghq.com>
2025-11-12 17:38:12 +0800

    update comment

--- packages/insomnia-component-docs/docusaurus.config.ts
+++ packages/insomnia-component-docs/docusaurus.config.ts
@@ -1,8 +1,6 @@
 import type * as Preset from '@docusaurus/preset-classic';
 import type { Config } from '@docusaurus/types';
 
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-
 const config: Config = {
   title: 'Insomnia Component Docs',
 
@@ -11,15 +9,11 @@ const config: Config = {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
-  // Set the production url of your site here
   url: 'https://insomnia.rest/',
   baseUrl: '/',
 
   onBrokenLinks: 'throw',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -62,7 +56,6 @@ const config: Config = {
         playgroundPosition: 'bottom',
       },
     },
-    // Replace with your project's social card
     navbar: {
       title: 'Insomnia',
       items: [
