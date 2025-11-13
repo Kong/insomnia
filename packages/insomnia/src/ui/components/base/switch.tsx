720b305fcc8d7541a3946c22884d1fe2a7598f3b
Curry Yang <1019yanglu@gmail.com>
2025-11-13 11:33:24 +0800

    feat: use twMerge to support class overide

--- packages/insomnia/src/ui/components/base/switch.tsx
+++ packages/insomnia/src/ui/components/base/switch.tsx
@@ -1,24 +1,24 @@
-import cn from 'classnames';
 import type { SwitchProps } from 'react-aria-components';
 import { Switch as RaSwitch } from 'react-aria-components';
+import { twMerge } from 'tailwind-merge';
 
-interface CustomSwitchProps extends Omit<SwitchProps, 'children'> {
+interface CustomSwitchProps extends Omit<SwitchProps, 'children' | 'className'> {
   children?: React.ReactNode;
+  className?: string;
 }
 
-export const Switch = ({ children, ...props }: CustomSwitchProps) => {
+export const Switch = ({ children, className, ...props }: CustomSwitchProps) => {
   return (
     <RaSwitch className="flex h-full cursor-pointer items-center p-0" {...props}>
       {({ isSelected, isDisabled }) => {
         return (
           <div
-            className={cn(
+            className={twMerge(
               "h-4.5 w-[30px] rounded-full border-[1px] border-solid border-[--hl] bg-[--color-bg] transition-all duration-200 before:m-0.5 before:block before:h-3.5 before:w-3.5 before:rounded-full before:transition-all before:duration-200 before:content-['']",
-              {
-                'bg-[--color-surprise] before:translate-x-[100%] before:bg-[--color-bg]': isSelected,
-                'before:bg-[--color-surprise]': !isSelected,
-                'cursor-not-allowed border-[--hl] before:bg-[--hl]': isDisabled,
-              },
+              isSelected && 'bg-[--color-surprise] before:translate-x-[100%] before:bg-[--color-bg]',
+              !isSelected && 'before:bg-[--color-surprise]',
+              isDisabled && 'cursor-not-allowed border-[--hl] before:bg-[--hl]',
+              className,
             )}
           />
         );
