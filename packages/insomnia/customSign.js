7f9cbece5dd1e350e723f3666839eeb8b1edcece
Balu George <balu.george@konghq.com>
2025-12-12 12:08:15 +0530

    Modify the windows app signing process to use the new certificate

--- packages/insomnia/customSign.js
+++ packages/insomnia/customSign.js
@@ -13,46 +13,32 @@ exports.default = async function (configuration) {
     return;
   }
 
-  const { USERNAME, PASSWORD, CREDENTIAL_ID, TOTP_SECRET } = process.env;
-  if (!USERNAME || !PASSWORD || !CREDENTIAL_ID || !TOTP_SECRET) {
-    console.log('[customSign] Skipping signing,  Missing required environment variables.');
+  const { SM_KEYPAIR_ALIAS } = process.env;
+  if (!SM_KEYPAIR_ALIAS) {
+    console.log('[customSign] Skipping signing,  Missing required environment variable: SM_KEYPAIR_ALIAS');
     return;
   }
 
   // Note: Avoid changing the lines below. Risk of breaking the windows code-signing process.
   // Feedback loop > 15 mins. Requires a branch on origin, a PR, and a separate dummy release pipeline to test changes.
-  // sslcom/codesigner-win has large image size (>1GB) and requires docker within windows-latest host.
   const rawPath = configuration.path.replace(/(\r\n|\n|\r)/gm, ''); // remove /n and other crap from path
   console.log('[customSign] File to sign before final packaging:', rawPath);
   const absolutePath = path.resolve(rawPath); // C:\Users\...\Update.exe
   const fixedAbsolutePath = absolutePath.replace(/\\/g, '/'); // C:/Users/.../Update.exe
-  const lastSlashIndex = fixedAbsolutePath.lastIndexOf('/'); // index of last / slash
-  const directoryPath = fixedAbsolutePath.substring(0, lastSlashIndex); // C:/Users/...
-  const inputFileName = path.basename(absolutePath); // Update.exe
-  const codeSignPath = 'C:/CodeSignTool/Insomnia'; // path inside docker container
-  const dockerInputFilePath = path.join(codeSignPath, inputFileName); // C:/CodeSignTool/Insomnia/Update.exe
-  const dockerCommand = `docker run --rm \
-        -v "${directoryPath}:${codeSignPath}" \
-        -e USERNAME="${USERNAME}" \
-        -e PASSWORD="${PASSWORD}" \
-        -e CREDENTIAL_ID="${CREDENTIAL_ID}" \
-        -e TOTP_SECRET="${TOTP_SECRET}" \
-        ghcr.io/sslcom/codesigner-win:latest sign \
-        \`\`-input_file_path="${dockerInputFilePath}" \`\`-override`;
+  const smctlSignCommand = `smctl sign --simple --keypair-alias ${SM_KEYPAIR_ALIAS} --input ${fixedAbsolutePath}`;
 
   try {
-    console.log('[customSign] Docker command:', dockerCommand);
-    console.log('[customSign] Starting to run sign cmd via docker...');
-    const { stdout, stderr } = await execAsync(dockerCommand);
+    console.log('[customSign] Starting to run smctl sign cmd...');
+    const { stdout, stderr } = await execAsync(smctlSignCommand);
 
-    console.log('[customSign] Docker command output:', stdout);
+    console.log('[customSign] smctl sign command output:', stdout);
     if (stderr) {
-      console.error('[customSign] Docker command error output:', stderr);
+      console.error('[customSign] smctl sign command error output:', stderr);
     }
 
     console.log('[customSign] File signed successfully.');
   } catch (error) {
-    console.error('[customSign] Error executing Docker command:', error);
+    console.error('[customSign] Error executing smctl sign command:', error);
     throw error;
   }
 };
