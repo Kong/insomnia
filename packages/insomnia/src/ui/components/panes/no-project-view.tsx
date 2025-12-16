0a7abdaa031e7adc7ea080dea27814fbcb54e01b
James Gatz <jamesgatzos@gmail.com>
2025-12-16 15:53:02 +0100

    feat: enhance NoProjectView layout and update heading style

--- packages/insomnia/src/ui/components/panes/no-project-view.tsx
+++ packages/insomnia/src/ui/components/panes/no-project-view.tsx
@@ -1,4 +1,5 @@
 import React, { type FC } from 'react';
+import { Heading } from 'react-aria-components';
 
 import type { StorageRules } from '~/models/organization';
 
@@ -11,18 +12,16 @@ interface Props {
 
 export const NoProjectView: FC<Props> = ({ storageRules, isGitSyncEnabled }) => {
   return (
-    <div className="h-full overflow-y-auto px-8 pt-16">
-      <div className="max-h-full text-center">
-        <p className="mb-3 text-xl font-semibold">Welcome to your organization!</p>
-        <p className="mb-3">Create a new project to get started</p>
-        <div className="mx-auto flex max-w-2xl justify-center pb-16">
-          <ProjectCreateForm
-            storageRules={storageRules}
-            isGitSyncEnabled={isGitSyncEnabled}
-            defaultProjectName="My first project"
-          />
-        </div>
+    <div className="grid w-[min(var(--container-xl),100%)] grid-rows-[min-content_1fr_min-content] place-items-stretch items-stretch gap-4 self-center overflow-hidden p-16">
+      <div>
+        <p className="mb-3 text-3xl font-semibold">Welcome to your organization!</p>
+        <Heading className="mb-3">Create a new project to get started</Heading>
       </div>
+      <ProjectCreateForm
+        storageRules={storageRules}
+        isGitSyncEnabled={isGitSyncEnabled}
+        defaultProjectName="My first project"
+      />
     </div>
   );
 };
