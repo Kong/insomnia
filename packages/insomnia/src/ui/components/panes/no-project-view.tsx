49cc241aa80e76efd803315b25fb6463ee74f88e
Curry Yang <1019yanglu@gmail.com>
2026-01-05 11:15:04 +0800

    fix: empty view ui

--- packages/insomnia/src/ui/components/panes/no-project-view.tsx
+++ packages/insomnia/src/ui/components/panes/no-project-view.tsx
@@ -22,7 +22,7 @@ export const NoProjectView: FC<Props> = ({ storageRules, isGitSyncEnabled }) => 
 
   const { credentials = [], providers = [] } = credentialsFetcher.data || {};
   return (
-    <div className="grid w-[min(var(--container-xl),100%)] grid-rows-[min-content_1fr_min-content] place-items-stretch items-stretch gap-4 self-center overflow-hidden p-16">
+    <div className="grid grid-rows-[min-content_1fr_min-content] place-items-stretch items-stretch gap-4 self-center overflow-hidden p-16">
       <div>
         <p className="mb-3 text-3xl font-semibold">Welcome to your organization!</p>
         <Heading className="mb-3">Create a new project to get started</Heading>
