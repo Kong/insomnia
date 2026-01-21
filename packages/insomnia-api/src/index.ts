551defed6db0a48bfb7ff4f9995650f89e0c7a5a
Curry Yang <1019yanglu@gmail.com>
2026-01-21 17:34:15 +0800

    refactor: migrate invite api

--- packages/insomnia-api/src/index.ts
+++ packages/insomnia-api/src/index.ts
@@ -3,5 +3,6 @@ export * from './vault';
 export * from './enterprise';
 export * from './trial';
 export * from './project';
+export * from './invite';
 
 export { configureFetch, type FetchConfig, ResponseFailError, isApiError } from './fetch';
