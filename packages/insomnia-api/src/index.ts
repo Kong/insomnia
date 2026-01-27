71c15fe4a94eda471fa503f0819291bd01033fe8
Curry Yang <1019yanglu@gmail.com>
2026-01-27 14:39:18 +0800

    refactor: migrate collaborator api

--- packages/insomnia-api/src/index.ts
+++ packages/insomnia-api/src/index.ts
@@ -3,5 +3,6 @@ export * from './vault';
 export * from './enterprise';
 export * from './trial';
 export * from './project';
+export * from './collaborators';
 
 export { configureFetch, type FetchConfig, ResponseFailError, isApiError } from './fetch';
