14a59c0c45108675e8a602362acfa7333fcd9dd5
Vivek Thuravupala <2700229+godfrzero@users.noreply.github.com>
2026-03-19 08:15:12 -0700

    Position Invite collaborators modal a fixed distance from the top rather than centering vertically

--- packages/insomnia/src/ui/components/modals/invite-modal/invite-modal.tsx
+++ packages/insomnia/src/ui/components/modals/invite-modal/invite-modal.tsx
@@ -155,7 +155,7 @@ const InviteModal: FC<{
       isDismissable={false}
       isOpen={true}
       onOpenChange={setIsOpen}
-      className="theme--transparent-overlay fixed top-0 left-0 z-10 flex h-(--visual-viewport-height) w-full items-center justify-center bg-(--color-bg)"
+      className="theme--transparent-overlay fixed top-0 left-0 z-10 flex h-(--visual-viewport-height) w-full items-start justify-center bg-(--color-bg) pt-[70px]"
     >
       <Modal className="theme--dialog flex max-h-[calc(var(--visual-viewport-height)-140px)] w-full max-w-[900px] flex-col overflow-hidden rounded-md border border-solid border-(--hl-sm) bg-(--color-bg) p-[32px] text-(--color-font)">
         <Dialog className="relative flex h-full flex-1 flex-col overflow-hidden outline-hidden">
