bd08258303f636eb5efda264bf3286c8cf3ea7cd
Curry Yang <163384738+CurryYangxx@users.noreply.github.com>
2026-04-22 16:49:54 +0800

    fix: refresh ui after sync (#9848)

--- packages/insomnia/src/ui/hooks/use-vcs-version.ts
+++ packages/insomnia/src/ui/hooks/use-vcs-version.ts
@@ -1,12 +1,13 @@
 import { useProjectIndexLoaderData } from '../../routes/organization.$organizationId.project.$projectId._index';
 import { useWorkspaceLoaderData } from '../../routes/organization.$organizationId.project.$projectId.workspace.$workspaceId';
 
-// We use this hook to determine if the active workspace has been updated from the Git VCS
-// For example, by pulling a new version from the remote, switching branches, etc.
+// We use this hook to determine if the active workspace should remount editors due to
+// Git/VCS changes or a successful FS watcher sync of the backing YAML file.
 export function useGitVCSVersion() {
   const workspaceData = useWorkspaceLoaderData();
   const projectData = useProjectIndexLoaderData();
   const gitRepository = workspaceData?.gitRepository || projectData?.activeProjectGitRepository;
+  const gitFileLastSyncTime = workspaceData?.activeWorkspaceMeta?.gitFileLastSyncTime;
 
-  return `${gitRepository?.cachedGitLastCommitTime}:${gitRepository?.cachedGitRepositoryBranch}`;
+  return `${gitRepository?.cachedGitLastCommitTime}:${gitRepository?.cachedGitRepositoryBranch}:${gitFileLastSyncTime}`;
 }
