df19ac25bd54a5e7814b0b4f3dee72884b9117df
jackkav <jackkav@gmail.com>
2026-04-27 21:04:41 +0200

    fix handlerId

--- packages/insomnia/src/network/url-matches-cert-host.ts
+++ packages/insomnia/src/network/url-matches-cert-host.ts
@@ -1,3 +1,5 @@
+import { parse as urlParse, URL } from 'node:url';
+
 import { escapeRegex } from '../common/misc';
 import { setDefaultProtocol } from '../utils/url/protocol';
 
@@ -5,17 +7,16 @@ const DEFAULT_PORT = 443;
 
 export function urlMatchesCertHost(certificateHost: string, requestUrl: string, needCheckPort = true) {
   const cHostWithProtocol = setDefaultProtocol(certificateHost, 'https:');
-  let requestUrlWithProtocol: URL;
-  let certificateHostWithProtocol: URL;
+  const { hostname, port } = urlParse(requestUrl);
+  let certificateHostWithProtocol = new URL('https://example.com');
   try {
-    requestUrlWithProtocol = new URL(requestUrl);
     certificateHostWithProtocol = new URL(cHostWithProtocol);
   } catch {
-    // Return false early if either URL is invalid.
+    // return false early if the certificate host is invalid
     return false;
   }
-  const { hostname, port } = requestUrlWithProtocol;
   const { hostname: cHostname, port: cPort } = certificateHostWithProtocol;
+  // @ts-expect-error -- TSCONVERSION `parseInt(null)` returns `NaN`
   const assumedPort = Number.parseInt(port) || DEFAULT_PORT;
   const assumedCPort = Number.parseInt(cPort) || DEFAULT_PORT;
   const cHostnameRegex = escapeRegex(cHostname || '').replace(/\\\*/g, '.*');
