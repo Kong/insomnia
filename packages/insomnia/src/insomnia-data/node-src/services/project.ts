b99e1c788d32b0baa58600d8afcb052fa0b8788d
Bingbing <ZxBing0066@gmail.com>
2026-05-13 17:13:36 +0800

    fix

--- packages/insomnia/src/insomnia-data/node-src/services/project.ts
+++ packages/insomnia/src/insomnia-data/node-src/services/project.ts
@@ -1,4 +1,4 @@
-import type { Project } from '~/insomnia-data';
+import type { Project, Query } from '~/insomnia-data';
 import { database as db, models } from '~/insomnia-data';
 
 const { type } = models.project;
@@ -8,12 +8,13 @@ export function create(patch: Partial<Project> = {}) {
 }
 
 export function list(options?: { gitRepositoryIds?: string[]; organizationId?: string }) {
-  const query: Record<string, unknown> = {};
+  const query: Query<Project> = {};
   if (options?.organizationId) {
     query.parentId = options.organizationId;
   }
   if (options?.gitRepositoryIds) {
-    query.gitRepositoryId = { $in: options.gitRepositoryIds };
+    const queryIds = options.gitRepositoryIds.flatMap(id => models.project.getQueryableGitRepositoryIds(id));
+    query.gitRepositoryId = { $in: queryIds };
   }
   return db.find<Project>(type, query);
 }
@@ -30,7 +31,7 @@ const getProjectByIdOrProject = async (idOrProject: string | Project) => {
   const project = typeof idOrProject === 'string' ? await get(idOrProject) : idOrProject;
   if (!project) {
     throw new Error(
-      `Project not found: ${typeof idOrProject === 'string' ? idOrProject : idOrProject._id + idOrProject.name}`,
+      `Project not found: ${typeof idOrProject === 'string' ? idOrProject : `_id=${idOrProject._id}, name=${idOrProject.name}`}`,
     );
   }
   return project;
