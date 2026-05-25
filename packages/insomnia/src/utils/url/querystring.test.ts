394cbdf4afc70d4b6a1a82beb1b77160faa0f98a
Bingbing <ZxBing0066@gmail.com>
2026-05-25 15:24:26 +0800

    fix

--- packages/insomnia/src/utils/url/querystring.test.ts
+++ packages/insomnia/src/utils/url/querystring.test.ts
@@ -3,7 +3,6 @@ import { describe, expect, it } from 'vitest';
 import {
   buildQueryParameter,
   buildQueryStringFromParams,
-  deconstructQueryStringToParams,
   getJoiner,
   joinUrlAndQueryString,
   smartEncodeUrl,
@@ -158,49 +157,6 @@ describe('querystring', () => {
       );
 
       expect(str).toBe('foo=bar%3F%3F&foo1=&foo2&hello&hi%20there=bar%3F%3F');
-    });
-  });
-
-  describe('deconstructToParams()', () => {
-    it('builds from params', () => {
-      const str = deconstructQueryStringToParams('foo=bar%3F%3F&hello&hi%20there=bar%3F%3F&=&=val');
-
-      expect(str).toEqual([
-        { name: 'foo', value: 'bar??' },
-        { name: 'hello', value: '' },
-        { name: 'hi there', value: 'bar??' },
-      ]);
-    });
-    it('builds from params with =', () => {
-      const str = deconstructQueryStringToParams('foo=bar&1=2=3=4&hi');
-
-      expect(str).toEqual([
-        { name: 'foo', value: 'bar' },
-        { name: '1', value: '2=3=4' },
-        { name: 'hi', value: '' },
-      ]);
-    });
-
-    it('builds from params not strict', () => {
-      const str = deconstructQueryStringToParams('foo=bar%3F%3F&hello&hi%20there=bar%3F%3F&=&=val', false);
-
-      expect(str).toEqual([
-        { name: 'foo', value: 'bar??' },
-        { name: 'hello', value: '' },
-        { name: 'hi there', value: 'bar??' },
-        { name: '', value: '' },
-        { name: '', value: 'val' },
-      ]);
-    });
-
-    it('builds from params with strictNullHandle', () => {
-      const str = deconstructQueryStringToParams('foo=bar&foo1&foo2=', true, { strictNullHandling: true });
-
-      expect(str).toEqual([
-        { name: 'foo', value: 'bar' },
-        { name: 'foo1', value: null },
-        { name: 'foo2', value: '' },
-      ]);
     });
   });
 
