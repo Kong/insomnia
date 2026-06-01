6185c3fb32ac18d31e24adb522f39dd8a175aa88
jackkav <jackkav@gmail.com>
2026-06-01 21:39:55 +0200

    restore node require plugin

--- packages/insomnia/src/plugins/index.ts
+++ packages/insomnia/src/plugins/index.ts
@@ -1,6 +1,8 @@
 import fs from 'node:fs';
 import path from 'node:path';
 
+import electron from 'electron';
+
 import type { Request, RequestGroup, Workspace } from '~/insomnia-data';
 import { database as db, models, services } from '~/insomnia-data';
 import type { PluginConfigMap } from '~/insomnia-data/common';
@@ -127,7 +129,7 @@ export async function getPlugins(force = false): Promise<Plugin[]> {
 
     // Make sure the default directories exist
     // imported lazy becuase this should only be read in plugin hidden window
-    const electron = await import('electron');
+
     const pluginPath = path.resolve(process.env['INSOMNIA_DATA_PATH'] || electron.app.getPath('userData'), 'plugins');
 
     // Also look in node_modules folder in each directory
@@ -295,7 +297,7 @@ export function getPluginCommonContext({
     ...pluginStore.init(plugin),
     ...pluginNetwork.init(),
     util: {
-      openInBrowser: async (url: string) => await import('electron').then(electron => electron.shell.openExternal(url)),
+      openInBrowser: (url: string) => electron.shell.openExternal(url),
       models: {
         request: {
           getById: services.request.getById,
--- packages/insomnia/vite.config.ts
+++ packages/insomnia/vite.config.ts
@@ -1,3 +1,4 @@
+import { builtinModules } from 'node:module';
 import path from 'node:path';
 
 import { reactRouter } from '@react-router/dev/vite';
@@ -5,7 +6,8 @@ import tailwindcss from '@tailwindcss/vite';
 import { defaultServerConditions, defineConfig } from 'vite';
 
 import pkg from './package.json';
-
+//These will be excluded from the bundle and remain as runtime dependencies
+export const externalDependencies = ['@apidevtools/swagger-parser', 'mocha', 'tough-cookie'];
 export default defineConfig(({ mode }) => {
   const __DEV__ = mode !== 'production';
 
@@ -37,11 +39,11 @@ export default defineConfig(({ mode }) => {
       target: 'esnext',
       sourcemap: true,
       rollupOptions: {
-        external: ['@getinsomnia/node-libcurl', 'electron', /^electron\//],
+        external: ['@getinsomnia/node-libcurl'],
       },
     },
     optimizeDeps: {
-      exclude: ['@getinsomnia/node-libcurl', 'electron'],
+      exclude: ['@getinsomnia/node-libcurl'],
       force: true, // wipe vite cache
       include: ['codemirror-graphql/utils/SchemaReference', '@stoplight/spectral-core', 'isomorphic-git'],
     },
@@ -59,7 +61,21 @@ export default defineConfig(({ mode }) => {
         'events': path.resolve(__dirname, '../../node_modules/events'),
       },
     },
-    plugins: [reactRouter(), tailwindcss()],
+    plugins: [
+      // Allows us to import modules that will be resolved by Node's require() function.
+      // e.g. import fs from 'fs'; will get transformed to const fs = require('fs'); so that it works in the renderer process.
+      // This is necessary because we use nodeIntegration: true in the renderer process and allow importing modules from node.
+      electronNodeRequire({
+        modules: [
+          'electron',
+          ...externalDependencies,
+          ...builtinModules.filter(m => m !== 'buffer' && m !== 'path'),
+          ...builtinModules.map(m => `node:${m}`),
+        ],
+      }),
+      reactRouter(),
+      tailwindcss(),
+    ],
     worker: {
       format: 'es',
     },
@@ -76,3 +92,114 @@ export default defineConfig(({ mode }) => {
     },
   };
 });
+import { createRequire } from 'node:module';
+
+import type { Plugin } from 'vite';
+
+export interface Options {
+  modules: string[];
+}
+
+/**
+ * Allows Vite to import modules that will be resolved by Node's require() function.
+ */
+export function electronNodeRequire(options: Options): Plugin {
+  const { modules = [] } = options;
+
+  return {
+    name: 'vite-plugin-electron-node-require',
+    config(conf, env) {
+      // If the plugin is used in SSR mode, we don't need to do anything
+      if (env.isSsrBuild) {
+        return conf;
+      }
+      // Exclude the modules from Vite's dependency optimization (pre-bundling)
+      conf.optimizeDeps = {
+        ...conf.optimizeDeps,
+        exclude: [...(conf.optimizeDeps?.exclude ? conf.optimizeDeps.exclude : []), ...modules],
+      };
+
+      // Create aliases for the modules so that we can resolve them with this plugin
+      conf.resolve ??= {};
+      conf.resolve.alias = {
+        ...conf.resolve.alias,
+        ...Object.fromEntries(modules.map(e => [e, `virtual:external:${e}`])),
+      };
+
+      // Ignore the modules from Rollup's commonjs plugin so that we can resolve them with this plugin
+      conf.build ??= {};
+      conf.build.commonjsOptions ??= {};
+      conf.build.commonjsOptions.ignore = [...modules];
+
+      return conf;
+    },
+    resolveId(id, _importer, options) {
+      const externalId = id.split('virtual:external:')[1];
+
+      if (externalId && modules.includes(externalId)) {
+        if (options.ssr) {
+          return null;
+        }
+        // Return a virtual module ID so that Vite knows to use this plugin to resolve the module
+        // The \0 is a special convention by Rollup to indicate that the module is virtual and should not be resolved by other plugins
+        return `\0${id}`;
+      }
+
+      // Return null to indicate that this plugin should not resolve the module
+      return null;
+    },
+    load(id, options) {
+      if (id.includes('virtual:external:')) {
+        const externalId = id.split('virtual:external:')[1];
+
+        // We need to handle electron because it's different when required in the renderer process
+        if (externalId === 'electron') {
+          return `
+            const electron = require('electron');
+            export { electron as default };
+            export const BrowserWindow = electron.BrowserWindow;
+            export const clipboard = electron.clipboard;
+            export const contextBridge = electron.contextBridge;
+            export const crashReporter = electron.crashReporter;
+            export const dialog = electron.dialog;
+            export const ipcRenderer = electron.ipcRenderer;
+            export const nativeImage = electron.nativeImage;
+            export const shell = electron.shell;
+            export const webFrame = electron.webFrame;
+            export const deprecate = electron.deprecate;
+          `;
+        }
+
+        const nodeRequire = createRequire(import.meta.url);
+        const exports = Object.keys(nodeRequire(externalId));
+
+        // Filter out the exports that are valid javascript variable keywords:
+        const validExports = exports.filter(e => {
+          try {
+            new Function(`const ${e} = true`);
+            return true;
+          } catch {
+            return false;
+          }
+        });
+
+        if (options?.ssr) {
+          return [
+            `import requiredModule from '${externalId}';`,
+            `${validExports.map(e => `export const ${e} = requiredModule.${e};`).join('\n')}`,
+            `${exports.includes('default') ? 'export default requiredModule.default;' : 'export default requiredModule'}`,
+          ].join('\n');
+        }
+
+        return [
+          `const requiredModule = require('${externalId}');`,
+          `${validExports.map(e => `export const ${e} = requiredModule.${e};`).join('\n')}`,
+          `${exports.includes('default') ? 'export default requiredModule.default;' : 'export default requiredModule'}`,
+        ].join('\n');
+      }
+
+      // Return null to indicate that this plugin should not resolve the module
+      return null;
+    },
+  };
+}
