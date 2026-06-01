c96a6e659eca737a1c8662ad3191b7f31889e5ac
jackkav <jackkav@gmail.com>
2026-06-01 13:23:17 +0200

    insomnia testing adapter

--- packages/insomnia-testing/src/generate/index.ts
+++ packages/insomnia-testing/src/generate/index.ts
@@ -1,6 +1,6 @@
 import type { Test, TestSuite } from './generate';
 
 export { generate } from './generate';
-export { generateToFile } from './generate-to-file';
+
 
 export type { Test, TestSuite };
--- packages/insomnia-testing/src/index.ts
+++ packages/insomnia-testing/src/index.ts
@@ -1,7 +1,6 @@
 import type { Test, TestSuite } from './generate';
 import type { TestResults } from './run';
 export { generate } from './generate';
-export { generateToFile } from './generate/generate-to-file';
 
 export { runTests, runTestsCli } from './run';
 
