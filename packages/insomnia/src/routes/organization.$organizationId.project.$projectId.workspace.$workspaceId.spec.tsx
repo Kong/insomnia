3bf99086f6aa72013b1ebe984e3c4b54ec53ae6d
Fares Osman <43153226+fiosman@users.noreply.github.com>
2026-06-04 11:37:43 -0400

    chore: use text encoder to satisfy renderer

--- packages/insomnia/src/routes/organization.$organizationId.project.$projectId.workspace.$workspaceId.spec.tsx
+++ packages/insomnia/src/routes/organization.$organizationId.project.$projectId.workspace.$workspaceId.spec.tsx
@@ -435,7 +435,8 @@ const Component = ({ params }: Route.ComponentProps) => {
     }
 
     const RULESET_MAX_BYTES = 1 * 1024 * 1024; // 1 MB
-    if (Buffer.byteLength(content, 'utf8') > RULESET_MAX_BYTES) {
+    const byteLength = new TextEncoder().encode(content).byteLength;
+    if (byteLength > RULESET_MAX_BYTES) {
       showError({
         title: 'Ruleset Too Large',
         message: 'The selected ruleset exceeds the maximum allowed size of 1 MB.',
