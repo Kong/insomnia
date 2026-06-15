2d92b6d1e1f37174ff78b078eb482e29ea6c8723
jackkav <jackkav@gmail.com>
2026-06-15 20:38:33 +0200

    renamed .worker

--- packages/insomnia/src/ui/worker/templating-handler.ts
+++ packages/insomnia/src/ui/worker/templating-handler.ts
@@ -4,7 +4,7 @@ import type { RenderInputType } from '../../templating/types';
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment -- see below
 // @ts-ignore -- inso transpiles to commonjs so doesn't play nice with this
-const worker = new Worker(new URL('templating-worker.ts', import.meta.url), { type: 'module' });
+const worker = new Worker(new URL('templating.worker.ts', import.meta.url), { type: 'module' });
 
 // Triggered by a mistake in the work initialization code above
 worker.addEventListener('error', event => {
