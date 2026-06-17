d9427aa0e83ebc214233e7138715f6490ca096a2
Jay Wu <jay.wu@konghq.com>
2026-06-17 14:24:28 +0800

    remove font-sans

--- packages/insomnia/src/ui/components/project/project-create-form.tsx
+++ packages/insomnia/src/ui/components/project/project-create-form.tsx
@@ -176,7 +176,7 @@ export const ProjectCreateForm: FC<Props> = ({
                 type="submit"
                 form={FORMID}
                 isDisabled={!isGitSyncEnabled || isGitCredentialInvalid}
-                className="flex h-full items-center justify-center gap-2 rounded-md border border-solid border-(--hl-md) bg-(--color-surprise) px-4 py-2 font-sans text-sm font-semibold text-(--color-font-surprise) ring-1 ring-transparent transition-all hover:bg-(--color-surprise)/80 focus:ring-(--hl-md) focus:ring-inset aria-pressed:opacity-80"
+                className="flex h-full items-center justify-center gap-2 rounded-md border border-solid border-(--hl-md) bg-(--color-surprise) px-4 py-2 text-sm font-semibold text-(--color-font-surprise) ring-1 ring-transparent transition-all hover:bg-(--color-surprise)/80 focus:ring-(--hl-md) focus:ring-inset aria-pressed:opacity-80"
               >
                 <span>Scan for files</span>
               </Button>
