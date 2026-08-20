425c601b3ca8036aab192328372d2a5b5dc4d6ac
Kent Wang <kent.wang@konghq.com>
2026-08-20 11:31:30 +0800

    hide tooltip when change one line editor type

--- packages/insomnia/src/ui/components/.client/codemirror/one-line-editor.tsx
+++ packages/insomnia/src/ui/components/.client/codemirror/one-line-editor.tsx
@@ -109,11 +109,12 @@ export const OneLineEditor = forwardRef<OneLineEditorHandle, OneLineEditorProps>
     const { settings } = useRootLoaderData()!;
     const { isOwner, isEnterprisePlan } = usePlanData();
     const { handleRender, handleGetRenderContext } = useNunjucks();
+    const isPasswordEditor = type?.toLowerCase() === 'password';
 
     // Update the tooltip value, including rendering the value of a nunjucks tag if necessary
     const updateTooltipValue = useCallback(
       async (rawValue: string) => {
-        if (type?.toLowerCase() === 'password') {
+        if (isPasswordEditor) {
           return;
         }
         if (!handleRender || !/{{|{%/.test(rawValue)) {
@@ -127,7 +128,7 @@ export const OneLineEditor = forwardRef<OneLineEditorHandle, OneLineEditorProps>
           setTooltipValue(rawValue);
         }
       },
-      [handleRender, type],
+      [handleRender, isPasswordEditor],
     );
 
     const getKeyMap = useCallback(() => {
@@ -563,7 +564,9 @@ export const OneLineEditor = forwardRef<OneLineEditorHandle, OneLineEditorProps>
         delay={1000}
         className="h-full w-full"
         followCursor
-        shouldShow={() => Boolean(tooltipValue) && !isPointerOverNunjucksTag.current && isContentTruncated()}
+        shouldShow={() =>
+          Boolean(tooltipValue) && !isPasswordEditor && !isPointerOverNunjucksTag.current && isContentTruncated()
+        }
       >
         <div
           className={classnames('editor--single-line', {
